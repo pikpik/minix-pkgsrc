78cf7c0c4451163e3bcdc70d97dfbe58cb54bfcd
hubertf <hubertf>
1998-02-09 00:47:25 +0000

    Fix PR 4884 by replacing STRIP with STRIPFLAG. This addes yet another step towards bsd.pkg.mk. :)

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,7 +1,7 @@
 #-*- mode: Fundamental; tab-width: 4; -*-
 # ex:ts=4
 #
-#	$NetBSD: bsd.port.mk,v 1.42 1998/01/30 13:53:53 agc Exp $
+#	$NetBSD: bsd.port.mk,v 1.43 1998/02/02 08:11:49 hubertf Exp $
 #
 #	bsd.port.mk - 940820 Jordan K. Hubbard.
 #	This file is in the public domain.
@@ -551,7 +551,7 @@ SHAREMODE = ${DOCMODE}
 
 # A few aliases for *-install targets
 INSTALL_PROGRAM= \
-	${INSTALL} ${COPY} ${STRIP} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE}
+	${INSTALL} ${COPY} ${STRIPFLAG} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE}
 INSTALL_SCRIPT= \
 	${INSTALL} ${COPY} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE}
 INSTALL_DATA= \
