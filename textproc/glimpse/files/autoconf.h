aa7e961f55400140567067de3058c4dfe5a06c7e
frueauf <frueauf>
1998-12-03 12:39:46 +0000

    Update glimpse to 4.1, provided in pr 6508 by Johnny C. Lam.
    Fixes some bugs, including a serious bug involving Boolean searches
    of length 1 between versions 4.0 and 4.1.

---
+++ textproc/glimpse/files/autoconf.h
@@ -0,0 +1,14 @@
+/*	$NetBSD$	*/
+
+/*
+ * preprocessor variables used during build of glimpse-4.1
+ */
+
+#define HAVE_DIRENT_H 1
+#define HAVE_FLOCK 1
+#define HAVE_GETDTABLESIZE 1
+#define HAVE_SETLINEBUF 1
+#define HAVE_SETRLIMIT 1
+#define HAVE_STRDUP 1
+#define HAVE_STRERROR 1
+#define HAVE_SYSCONF 1
