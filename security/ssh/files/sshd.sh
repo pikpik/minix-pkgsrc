f65a2ffd312aef2ce33ae7fcbf6ae792c036d004
kim <kim>
1999-06-20 19:47:31 +0000

    While sshd doesn't necessarily need a config file, it makes more sense to check for the daemon config than the client config.

--- security/ssh/files/sshd.sh
+++ security/ssh/files/sshd.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# $NetBSD$
+# $NetBSD: sshd.sh,v 1.1 1998/05/24 18:59:43 tron Exp $
 #
 if [ ! -f @SSH_CONF_DIR@/ssh_host_key ]
 then
  @PREFIX@/bin/ssh-keygen -f @SSH_CONF_DIR@/ssh_host_key -N ''
 fi
-if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/ssh_config ]
+if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/sshd_config ]
 then
  @PREFIX@/sbin/sshd
  echo -n ' sshd'
