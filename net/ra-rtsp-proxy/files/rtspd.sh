3e2c70ba8b46458b7cf9bc32799ab559923f2114
tron <tron>
1999-09-03 13:52:17 +0000

    Fix typo.

--- net/ra-rtsp-proxy/files/rtspd.sh
+++ net/ra-rtsp-proxy/files/rtspd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $NetBSD$
+# $NetBSD: rtspd.sh,v 1.1.1.1 1999/09/01 20:25:03 tron Exp $
 #
-if [ -x @PREFIX@/sbin/rtspd
+if [ -x @PREFIX@/sbin/rtspd ]
 then
  @PREFIX@/sbin/rtspd
  echo -n ' rtspd'
