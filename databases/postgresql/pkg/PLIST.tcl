8a39587d42cf32416afd59bd3721c80a5db602fd
jlam <jlam>
1999-09-19 04:24:56 +0000

    Update postgresql to 6.5.1.  Completely libtoolized and ldconfig statements removed, but this should still function correctly on a.out systems.  This package was originally submitted by D'Arcy J.M. Cain, but with major overhauling by me.
    Closes PR#7865 and relevant part of PR#8299.
    
    Major changes from version 6.4.2:
    
            Multi-version concurrency control(MVCC)
              This removes our old table-level locking, and replaces it with a
             locking system that is superior to most commercial database
             systems. In a traditional system, each row that is modified is
             locked until committed, preventing reads by other users. MVCC
             uses the natural multi-version nature of PostgreSQL to allow
             readers to continue reading consistent data during writer
             activity. Writers continue to use the compact pg_log transaction
             system. This is all performed without having to allocate a lock
             for every row like traditional database systems. So, basically,
             we no longer are restricted by simple table-level locking; we
             have something better than row-level locking.
    
            Hot backups from pg_dump
              pg_dump takes advantage of the new MVCC features to give a
             consistant database dump/backup while the database stays online
             and available for queries.
    
            Numeric data type
              We now have a true numeric data type, with user-specified
             precision.
    
            Temporary tables
              Temporary tables are guaranteed to have unique names within a
             database session, and are destroyed on session exit.
    
            New SQL features
              We now have CASE, INTERSECT, and EXCEPT statement support. We
             have new LIMIT/OFFSET, SET TRANSACTION ISOLATION LEVEL, SELECT
             ... FOR UPDATE, and an improved LOCK TABLE command.
    
            Speedups
              We continue to speed up PostgreSQL, thanks to the variety of
             talents within our team. We have sped up memory allocation,
             optimization, table joins, and row transfer routines.
    
            Ports
              We continue to expand our port list, this time including
             WinNT/ix86 and NetBSD/arm32.
    
            Interfaces
              Most interfaces have new versions, and existing functionality
             has been improved.
    
            Documentation
              New and updated material is present throughout the
             documentation. New FAQs have been contributed for SGI and AIX
             platforms. The Tutorial has introductory information on SQL from
             Stefan Simkovics. For the User's Guide, there are reference pages
             covering the postmaster and more utility programs, and a new
             appendix contains details on date/time behavior. The
             Administrator's Guide has a new chapter on troubleshooting from
             Tom Lane. And the Programmer's Guide has a description of query
             processing, also from Stefan, and details on obtaining the
             Postgres source tree via anonymous CVS and CVSup.

---
+++ databases/postgresql/pkg/PLIST.tcl
@@ -0,0 +1,8 @@
+@comment $NetBSD$
+${PGUSER}/bin/pgaccess
+${PGUSER}/bin/pgtclsh
+${PGUSER}/bin/pgtksh
+${PGUSER}/include/libpgtcl.h
+${PGUSER}/lib/libpgtcl.a
+${PGUSER}/lib/libpgtcl.so.2.0
+${PGUSER}/lib/pltcl.so
