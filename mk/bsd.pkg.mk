050290d6f3f87d7876ac396f0f415e090e428fd7
agc <agc>
1999-11-12 11:58:45 +0000

    Use asterisks, which are more visible, when flagging a bad fuzz factor in a package patch.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.365 1999/11/10 10:50:30 agc Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.366 1999/11/12 10:34:47 agc Exp $
 #
 # This file is in the public domain.
 #
@@ -1109,7 +1109,7 @@ do-patch:
 				fuzz="";				\
 				${PATCH} -v > /dev/null 2>&1 && fuzz="${PATCH_FUZZ_FACTOR}"; \
 				${PATCH} $$fuzz ${PATCH_ARGS} < $$i ||	\
-					( ${ECHO} "!!! Fuzzy patch $$i !!!"; ${PATCH} ${PATCH_ARGS} < $$i) || \
+					( ${ECHO} "*** Fuzzy patch $$i ***"; ${PATCH} ${PATCH_ARGS} < $$i) || \
 					( ${ECHO} Patch $$i failed ; exit 1 ) ; \
 			done;						\
 			if [ "X$$fail" != "X" ]; then			\
