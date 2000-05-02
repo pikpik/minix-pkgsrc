b97778f8f2338a099a2e3d775ba2ec2ccd08a817
jlam <jlam>
2000-05-02 15:24:10 +0000

    Updated samba to 2.0.7.  Major changes from version 2.0.6 are below.  A complete list of changes can be found at http://www.samba.org/.
    New Documentation in 2.0.7
    --------------------------
    
    O'Reilly and Associates have donated their book "Using Samba"
    to the Samba community to be updated in a collaberative way
    along with the Samba software. Starting with this release the
    html of "Using Samba" will be distributed with the Samba software
    as the online documentation for Samba. Bug fixes for the book
    are encouraged as is new material. Please help us make this
    documentation the best it can be for Samba !
    
    Windows 2000 Issues
    -------------------
    
    This version of Samba has been tested with Windows 2000 and
    the five known incompatibilities with Windows 2000 have been
    fixed. See the "Changes in 2.0.7" list below for details.
    
    New/Changed parameters in 2.0.7
    -------------------------------
    
    There are 5 new parameters in the smb.conf file.
    
    utmp
    utmp dir
    utmp hostname
    utmp consolidate
    wtmp directory
    
    These parameters are only available if the "--with-utmp"
    option was selected at configure time. The yes/no option "utmp"
    specifies whether utmp records should be recorded on user
    logon/logoff. It defaults to "no". The "utmp dir" and "wtmp dir"
    are string parameters specifying pathnames to the directories containing
    the utmp/wtmp file databases. See the smb.conf man page for more details.
    
    inherit permissions
    
    This boolean parameter causes newly created files and directories
    to inherit their initial permissions from their parent directory.
    This can be very useful in propagating such things as the set-group
    bit in directory heirarchies. See the smb.conf man page for more
    details.
    
    write cache size
    
    This integer parameter specifies (in bytes) the size of a user level
    per-file write cache that smbd will create for an oplocked file. This
    can improve performance significantly for writing files by causing
    writes to be done in large chunk sizes. If this parameter is set (it
    defaults to zero which means no write cache) to the stripe size of
    a raid volume then it will cause writes to be much more efficient.
    Up to 10 write caches can be active simultaneously per smbd (allocated
    for the first 10 oplocked file opens). All normal warnings about the
    dangers of user level caching of data apply. See the smb.conf man page
    for more details.
    
    source environment
    
    This pathname parameter causes Samba to read a list of environment
    variables from a named file on startup. This can be useful in setting
    up Samba in a clustered environment. See the smb.conf man page for more
    details.
    
    Ability to delete users added
    -----------------------------
    
    SWAT and smbpasswd can now delete users from the Samba smbpasswd file.
    See the man page for smbpasswd for details.
    
    Roving profile behavior finalized
    ---------------------------------
    
    The change in behavior with roving profiles (using the "logon home"
    parameter instead of the "logon path" parameter) introduced in 2.0.6
    has been discovered to be consistant with the way Windows NT behaves,
    and has been left as the default action. Please see the additional
    notes in the "logon home" parameter description in the smb.conf man
    page for more details.

--- net/samba/files/samba.sh
+++ net/samba/files/samba.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
-# $NetBSD$
+# $NetBSD: samba.sh,v 1.1 1999/11/16 21:01:05 jlam Exp $
 
-smbd=__PREFIX__/sbin/smbd
-nmbd=__PREFIX__/sbin/nmbd
+smbd=@PREFIX@/sbin/smbd
+nmbd=@PREFIX@/sbin/nmbd
 
 if [ -f ${smbd} ]; then
-	__ECHO__ -n ' samba'
+	@ECHO@ -n ' samba'
 	${smbd} -D
 	${nmbd} -D
 fi
