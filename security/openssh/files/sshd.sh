408bb21c01de0d4c568910893c74d047cd69615f
jlam <jlam>
2000-09-01 20:40:10 +0000

    Don't hardcode /usr/pkg...use @PREFIX@

--- security/openssh/files/sshd.sh
+++ security/openssh/files/sshd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD$
+# $NetBSD: sshd.sh,v 1.3 2000/07/24 16:44:52 jlam Exp $
 #
 
 name="sshd"
@@ -16,7 +16,7 @@ start)
 	fi
 	if [ ! -f @SSH_CONF_DIR@/ssh_host_dsa_key ]
 	then
-		/usr/pkg/bin/ssh-keygen -d -N "" -f /etc/ssh_host_dsa_key
+		@PREFIX@/bin/ssh-keygen -d -N "" -f /etc/ssh_host_dsa_key
 	fi
 	if [ -x @PREFIX@/sbin/sshd -a -f @SSH_CONF_DIR@/sshd_config ]
 	then
