31f1b7651626bd02685636cc0dca822141f243d9
jlam <jlam>
2000-09-12 14:50:51 +0000

    Provide mechanism to allow ap-ssl to force start of apache with "startssl".

--- www/apache/files/apache.sh
+++ www/apache/files/apache.sh
@@ -1,13 +1,36 @@
-#!/bin/sh
+#! /bin/sh
 #
-# $NetBSD: apache.sh,v 1.2 1999/05/11 18:38:31 tron Exp $
+# $NetBSD$
 #
+# PROVIDE: apache
+# REQUIRE: DAEMON
 
-CMD=${1:-start}
+name="apache"
+command="@PREFIX@/sbin/apachectl"
+pidfile="/var/run/httpd.pid"
+conffile="@PREFIX@/etc/httpd/httpd.conf"
 
-if [ -x @PREFIX@/sbin/httpd -a -f @PREFIX@/etc/httpd/httpd.conf ]
+apache_start="start"
+
+if [ -f @PREFIX@/etc/mod_ssl.conf ]
 then
- @PREFIX@/sbin/apachectl $CMD >/dev/null
- echo -n ' apache'
+	# This file can reset apache_start to "startssl"
+	. @PREFIX@/etc/httpd/mod_ssl.conf
 fi
+
+cmd=${1:-start}
+
+case ${cmd} in
+start)
+	if [ -x ${command} -a -f ${conffile} ]; then
+		echo "Starting ${name}."
+		${command} ${apache_start}
+	fi
+	;;
+*)
+	if [ -x ${command} ]; then
+		${command} ${cmd}
+	fi
+	;;
+esac
 exit 0
