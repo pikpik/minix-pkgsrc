f5be4513b58a697c3f2d32dd749c7acb8ac392a5
mjl <mjl>
2000-10-29 16:17:13 +0000

    Initial import of OpenMotif 2.1.30. Based on the FreeBSD and OpenBSD packages, with some adaptations to our pkg framework.

---
+++ x11/openmotif/patches/patch-tests-Performance-Startup-editor.c
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- tests/Performance/Startup/editor.c.orig	Wed May  3 05:12:48 2000
++++ tests/Performance/Startup/editor.c	Wed Aug  9 19:29:43 2000
+@@ -238,7 +238,7 @@
+ 
+     strcpy(tempname, "/tmp/xmeditXXXXXX");
+     
+-    if ((tfp = fopen(mktemp(tempname), "w")) == NULL) {
++    if ((tfp = mkstemp(tempname)) == NULL) {
+        fprintf(stderr, "Warning: unable to open temp file, text not saved.\n");
+        return(False);;
+     }
