e7c8f9a84847893120f2a14ec1f405f7d172ffc1
skrll <skrll>
2001-03-28 14:34:00 +0000

    Minor optimisation

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.701 2001/03/27 03:20:08 hubertf Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.702 2001/03/28 14:01:28 skrll Exp $
 #
 # This file is in the public domain.
 #
@@ -223,9 +223,6 @@ BUILD_DEPENDS+=		libtool>=1.4.20010219nb4:../../devel/libtool
 .if defined(USE_LTDL)
 DEPENDS+=		libtool>=1.4.20010219nb4:../../devel/libtool
 .endif
-.endif
-
-.if defined(USE_LIBTOOL)
 CONFIGURE_ENV+=		LIBTOOL="${LIBTOOL} ${LIBTOOL_FLAGS}"
 MAKE_ENV+=		LIBTOOL="${LIBTOOL} ${LIBTOOL_FLAGS}"
 .endif
