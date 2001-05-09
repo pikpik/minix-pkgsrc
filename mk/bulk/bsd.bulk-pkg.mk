7c61299ff2e34b07f7c67096c2e067b23fbb57ca
dmcmahill <dmcmahill>
2001-05-09 02:39:31 +0000

    don't exit if a depends can't be pkg_add'ed.  In this case it will be rebuilt from sources.

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.24 2001/05/02 13:51:43 dmcmahill Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.25 2001/05/03 21:19:55 dmcmahill Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@netbsd.org>
@@ -246,7 +246,7 @@ bulk-package:
 				if ! ${PKG_INFO} -qe $$pkgname ; then \
 					if [ -f $$pkgfile ]; then \
 						${ECHO_MSG} "BULK>  ${PKG_ADD} $$pkgfile"; \
-						${DO} ${PKG_ADD} $$pkgfile ; \
+						${DO} ${PKG_ADD} $$pkgfile || ${ECHO_MSG} "warning:  could not add $$pkgfile." ; \
 					else \
 						${ECHO_MSG} "BULK> warning:  $$pkgfile does not exist.  It will be rebuilt." ;\
 					fi ;\
