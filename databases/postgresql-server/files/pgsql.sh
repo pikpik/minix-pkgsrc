8a93fa24f46cfefee6ccd459c69ab60b1ec9df29
jlam <jlam>
2001-05-14 21:19:42 +0000

    Surround possible empty string in quotes so [ sees an argument.

--- databases/postgresql-server/files/pgsql.sh
+++ databases/postgresql-server/files/pgsql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: pgsql.sh,v 1.2 2001/05/14 20:38:56 jlam Exp $
+# $NetBSD: pgsql.sh,v 1.3 2001/05/14 21:02:39 jlam Exp $
 #
 # PostgreSQL database rc.d control script
 #
@@ -31,7 +31,7 @@ else
 fi
 
 # $flags from environment overrides $pgsql_flags
-if [ -n ${flags} ]
+if [ -n "${flags}" ]
 then
 	eval ${rcvar}_flags="${flags}"
 fi
