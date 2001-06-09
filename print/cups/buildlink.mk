8f00bebe4173af432e230150af521724b1cb0b21
wiz <wiz>
2001-06-09 15:13:07 +0000

    '        ' -> '\t'

--- print/cups/buildlink.mk
+++ print/cups/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.2 2001/05/26 05:44:10 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.3 2001/05/26 07:23:34 jlam Exp $
 #
 # This Makefile fragment is included by packages that use libcups.
 #
@@ -42,7 +42,7 @@ link-cups-headers:
 			${RM} -f $${dest};				\
 			${LN} -sf $${inc} $${dest};			\
 		fi;							\
-        done
+	done
 
 # This target links the libraries into ${BUILDLINK_LIBDIR}, which should
 # be searched first by the linker.
