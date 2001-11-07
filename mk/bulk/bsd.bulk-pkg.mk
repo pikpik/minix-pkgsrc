cb93915fc974e93743ede8c0d379e0d470b11162
hubertf <hubertf>
2001-11-07 00:29:20 +0000

    The $DEPENDS file has the pkg's directory in the first column, followed by whitespeace, followed by it's required pkgs. Make sure to look for that whitespace, to not catch any wrong positives, e.g.
    	egrep "x11/kdebase.*" $DEPENDSFILE
    
    will catch depends for both KDE1 and KDE2 and do weird things then (among
    other things, think that kdelibs-2.2.1 is ok as a dependency when building
    in x11/kdebase, which is certainly not!)
    
    Eight broken pkgs less in the bulk builds by adding a single space!
    (Now if adding some more would help further... :-)

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.28 2001/07/21 19:36:47 dmcmahill Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.29 2001/10/20 09:07:13 abs Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@netbsd.org>
@@ -218,7 +218,7 @@ bulk-package:
 					fi; \
 					if ${PKG_INFO} -qe $$pkgname ; then \
 						${SHCOMMENT} "Remove only unneeded pkgs" ; \
-						if ! ${EGREP} -q "^${PKGPATH}.* $$pkgdir( |$$)" ${DEPENDSFILE} ; then \
+						if ! ${EGREP} -q "^${PKGPATH} .* $$pkgdir( |$$)" ${DEPENDSFILE} ; then \
 							case "${BULK_PREREQ}" in \
 								*$$pkgdir* ) \
 									${ECHO_MSG} "BULK> Keeping BULK_PREREQ: $$pkgname ($$pkgdir)" ;\
