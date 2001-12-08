e4d85717666d92e1d46b27dc7045083430b2ea69
kristerw <kristerw>
2001-12-08 23:17:18 +0000

    pkgtools/x11-links has been updated.

--- mk/x11.buildlink.mk
+++ mk/x11.buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: x11.buildlink.mk,v 1.11 2001/10/24 22:11:05 jlam Exp $
+# $NetBSD: x11.buildlink.mk,v 1.12 2001/12/02 02:58:35 jlam Exp $
 #
 # This Makefile fragment is included by packages that use X11.
 #
@@ -21,7 +21,7 @@ USE_X11=		# defined
 
 .include "../../mk/bsd.buildlink.mk"
 
-BUILD_DEPENDS+=		x11-links>=0.4:../../pkgtools/x11-links
+BUILD_DEPENDS+=		x11-links>=0.5:../../pkgtools/x11-links
 BUILDLINK_X11_DIR?=	${LOCALBASE}/share/x11-links
 CONFIGURE_ENV+=		BUILDLINK_X11_DIR="${BUILDLINK_X11_DIR}"
 MAKE_ENV+=		BUILDLINK_X11_DIR="${BUILDLINK_X11_DIR}"
