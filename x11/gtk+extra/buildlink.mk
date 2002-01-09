328c0da952e105c066aacb61e01a313002d2fe53
wiz <wiz>
2002-01-09 15:14:12 +0000

    Fix a typo. XXX: doesn't support USE_CONFIG_WRAPPER yet

--- x11/gtk+extra/buildlink.mk
+++ x11/gtk+extra/buildlink.mk
@@ -1,6 +1,6 @@
-# $NetBSD: buildlink.mk,v 1.11 2001/10/03 20:56:55 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.1 2002/01/09 14:46:51 drochner Exp $
 #
-# This Makefile fragment is included by packages that use gtk.
+# This Makefile fragment is included by packages that use gtk+extra.
 #
 # To use this Makefile fragment, simply:
 #
