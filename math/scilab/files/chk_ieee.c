89b2b5ae207d6df1647990c44d2b8febf5dd8361
dmcmahill <dmcmahill>
2002-03-05 21:05:54 +0000

    enable this on NetBSD>1.5U on alpha since alpha FPC code is now in place. While here, add some missing headers and squish a LP64 bug.  Passes all self tests and all but 2 examples on a 1.5ZA alpha.

---
+++ math/scilab/files/chk_ieee.c
@@ -0,0 +1,14 @@
+/* $NetBSD$ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+        double x=0.0;
+        double y=1.0;
+        double z;
+
+        z=y/x;
+
+        return 0;
+}
