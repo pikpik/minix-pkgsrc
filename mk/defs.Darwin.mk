5aba8891afa39efd633a2ce08e528e4d5277306d
yyamano <yyamano>
2002-03-11 12:48:52 +0000

    GNU tar is /usr/bin/gnutar.

--- mk/defs.Darwin.mk
+++ mk/defs.Darwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Darwin.mk,v 1.17 2002/01/19 21:29:41 agc Exp $
+# $NetBSD: defs.Darwin.mk,v 1.18 2002/02/27 04:41:22 yyamano Exp $
 #
 # Variable definitions for the Darwin operating system.
 
@@ -20,7 +20,7 @@ FILE_CMD?=	/usr/bin/file
 FIND?=		/usr/bin/find
 GMAKE?=		/usr/bin/make
 GREP?=		/usr/bin/grep
-GTAR?=		/usr/bin/tar
+GTAR?=		/usr/bin/gnutar	
 GUNZIP_CMD?=	/usr/bin/gunzip -f
 GZCAT?=		/usr/bin/gzcat
 GZIP?=		-9
