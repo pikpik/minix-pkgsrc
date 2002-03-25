b84640297edc746c46ba55503092aa4c151d0355
rh <rh>
2002-03-25 12:54:20 +0000

    Update netsaint-base to 0.0.7.  Package provided in PR pkg/15992 by Murray Armfield <murray@river-styx.org>, thanks!
    Changes include:
    	- several new CGIs
    	- WAP interface
    	- forced service checks
    	- acks w/o notifications
    	- custom intervals for notification escalations
    	- passive check submissions via web interface
    	- auto-save of retention data
    	- extended service information
    	- scheduled downtime
    	- service dependencies
    	- flap detection
    	- aggregated status updates
    	- new macros
    	- user-supplied coordinates in status map
    	- minor bug fixes and tuning

--- net/netsaint-base/files/netsaint-setup.sh
+++ net/netsaint-base/files/netsaint-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#	$NetBSD$
+#       $NetBSD: netsaint-setup.sh,v 1.1.1.1 2001/01/16 15:39:43 rh Exp $
 #
 install -m 0755 -o @@USER@@ -g @@GROUP@@ -d /var/log/netsaint
 install -m 0755 -o @@USER@@ -g @@GROUP@@ -d /var/log/netsaint/archives
