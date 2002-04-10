10850c92755458d327eff670eb66498d017948a3
jlam <jlam>
2002-04-10 15:45:46 +0000

    The freetype2 libraries are needed by some of the bonobo libraries.

--- devel/bonobo/buildlink.mk
+++ devel/bonobo/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.4 2002/01/16 00:12:43 rh Exp $
+# $NetBSD: buildlink.mk,v 1.5 2002/03/13 17:36:47 fredb Exp $
 #
 # This Makefile fragment is included by packages that use bonobo.
 #
@@ -36,6 +36,7 @@ CPPFLAGS+=			-I${BUILDLINK_DIR}/include/gnome-1.0
 BUILDLINK_CONFIG_WRAPPER_SED+=	\
 	-e "s|-I${BUILDLINK_PREFIX.bonobo}/\(include/gnome-1.0\)|-I${BUILDLINK_DIR}/\1|g"
 
+.include "../../graphics/freetype2/buildlink.mk"
 .include "../../graphics/gdk-pixbuf-gnome/buildlink.mk"
 .include "../../print/gnome-print/buildlink.mk"
 .include "../../devel/popt/buildlink.mk"
