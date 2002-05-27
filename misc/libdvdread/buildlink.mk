531925738a911e0fed1c7ef735bf315eb1937e84
veego <veego>
2002-05-27 15:08:16 +0000

    Update to 0.9.3:
      Support for MacOS X/Darwin have been added.
      Ported to use libdvdcss2 (1.2.0 and later).
      Can now use libdvdcss to also decrypt files on a normal filsystem.
      A bit more tolerant of incorrectly mastered DVDs.
      Build robustness fixes from Mark Espie.
      Added some Doxygen markup and comments.
    
    Please note that you need to update libdvdcss to 1.2.0 if you are using it.

--- misc/libdvdread/buildlink.mk
+++ misc/libdvdread/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.1 2001/12/26 00:32:01 zuntum Exp $
+# $NetBSD: buildlink.mk,v 1.1 2002/01/21 15:43:46 zuntum Exp $
 #
 # This Makefile fragment is included by packages that use libdvdread.
 #
@@ -17,7 +17,7 @@ LIBDVDREAD_BUILDLINK_MK=	# defined
 
 .include "../../mk/bsd.buildlink.mk"
 
-BUILDLINK_DEPENDS.libdvdread?=	libdvdread>=0.9.2
+BUILDLINK_DEPENDS.libdvdread?=	libdvdread>=0.9.3
 DEPENDS+=		${BUILDLINK_DEPENDS.libdvdread}:../../misc/libdvdread
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.libdvdread=libdvdread
