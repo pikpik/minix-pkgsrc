1e9bac58b9039880e5a6b913a326762ff7d88854
seb <seb>
2002-05-31 12:59:59 +0000

    Reimport of package skkserv from japanese/skkserv into inputmethod/skkserv. This is part of the japanese category retirement. CATEGORIES adjusted.

---
+++ inputmethod/skkserv/files/skkserv.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+#	$NetBSD$
+#
+# PROVIDE: skkserv
+# REQUIRE: DAEMON
+ 
+name="skkserv"
+command="@PREFIX@/libexec/skkserv"
+pidfile=""
+
+cmd=${1:-start}
+
+case ${cmd} in
+start)
+	if [ -x ${command}  ]; then
+		echo "Starting ${name}."
+		${command} > /dev/null 2>&1
+	fi
+	;;
+*)
+	if [ -x ${command} ]; then
+		${command} ${cmd} > /dev/null 2>&1
+	fi
+	;;
+esac
