33e73233d2f09f920be2d1dc2a36c5804fc68045
seb <seb>
2002-06-28 09:05:20 +0000

    Add PKG_SYSCONFDIR to BUILD_DEFS. Note: it was already as part of CONFIGURE_ENV value, this change only makes it more "readable" IMHO.
    Remove explicit addition of PKG_SYSCONFDIR to BUILD_DEFS in a couple of
    Makefiles.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.995 2002/06/23 02:23:23 kei Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.996 2002/06/23 03:15:28 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -997,6 +997,7 @@ PKG_SYSCONFDIR?=	${PKG_SYSCONFBASE}/${PKG_SYSCONFSUBDIR}
 
 CONFIGURE_ENV+=		PKG_SYSCONFDIR="${PKG_SYSCONFDIR}"
 MAKE_ENV+=		PKG_SYSCONFDIR="${PKG_SYSCONFDIR}"
+BUILD_DEFS+=		PKG_SYSCONFDIR
 
 # Passed to most of script invocations
 SCRIPTS_ENV+= CURDIR=${.CURDIR} DISTDIR=${DISTDIR} \
