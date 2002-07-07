d1fe4967005203c2911d7588b17390e3393c4b95
tv <tv>
2002-07-07 00:45:19 +0000

    Provide the common rc.d directive "reload" as an alias for "apachectl graceful", which reloads the server without killing transfers in progress.

--- www/apache6/files/apache.sh
+++ www/apache6/files/apache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: apache.sh,v 1.16 2002/02/05 06:04:42 jlam Exp $
+# $NetBSD: apache.sh,v 1.19 2002/07/02 11:57:19 wiz Exp $
 #
 # PROVIDE: apache
 # REQUIRE: DAEMON
@@ -19,9 +19,11 @@ rcvar=$name
 command="@PREFIX@/sbin/httpd"
 ctl_command="@PREFIX@/sbin/apachectl"
 required_files="@PKG_SYSCONFDIR@/httpd.conf"
+extra_commands="reload"
 start_cmd="apache_doit start"
 stop_cmd="apache_doit stop"
 restart_cmd="apache_doit restart"
+reload_cmd="apache_doit reload"
 
 # "${apache_start}" is the subcommand sent to apachectl to control how
 # httpd is started.  It's value may be overridden in:
@@ -43,15 +45,16 @@ apache_doit ()
 {
 	case $1 in
 	start)	action=${apache_start} ;;
+	reload)	action=graceful ;;
 	*)	action=$1 ;;
 	esac
 	${ctl_command} ${action}
 }
 
-if [ -f /etc/rc.subr ]
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -d /etc/rc.d -a -f /etc/rc.d/DAEMON ]
 then
 	load_rc_config $name
 	run_rc_command "$1"
 else
-	eval ${start_cmd}
+	apache_doit "$1"
 fi
