f7d9d6f7f02f4165c64c7a9436f78ba6dd16b77f
jlam <jlam>
2002-07-22 22:00:54 +0000

    Adapt to using perl-5.8.0.  Perl 5.8.0 removes the need for libperl as DynaLoader.a is now an archive of PIC objects.

--- lang/perl5/buildlink.mk
+++ lang/perl5/buildlink.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink.mk,v 1.7 2001/11/30 17:21:56 jlam Exp $
+# $NetBSD: buildlink.mk,v 1.8 2001/12/11 06:08:58 jlam Exp $
 #
 # This Makefile fragment is included by packages that use perl.
 #
@@ -10,7 +10,11 @@
 
 .include "../../mk/bsd.buildlink.mk"
 
+.if ${OPSYS} == "Darwin"
+PERL5_REQD?=			5.8.0
+.else
 PERL5_REQD?=			5.0
+.endif
 BUILDLINK_DEPENDS.perl?=	perl>=${PERL5_REQD}
 
 BUILDLINK_PREFIX.perl?=	${LOCALBASE}
