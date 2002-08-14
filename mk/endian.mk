46cd73aae33b45afab4a952df2d91041bebf59ba
grant <grant>
2002-08-14 02:38:25 +0000

    make this work on Solaris by using ${EGREP} instead of ${GREP}.

--- mk/endian.mk
+++ mk/endian.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: endian.mk,v 1.1 2002/04/22 02:30:51 jlam Exp $
 #
 # Determine the endianness of the platform by checking header files.
 #
@@ -40,7 +40,7 @@ MACHINE_ENDIAN!=							\
 			${ECHO} "\#endif";				\
 			${ECHO} "\#endif";				\
 			${ECHO} "BYTE_ORDER";				\
-		) | ${CC} -E - | ${GREP} "^\\(1234\\|4321\\)"		\
+		) | ${CC} -E - | ${EGREP} "^(1234|4321)"		\
 	`;								\
 	case `${ECHO} $${byte_order}` in				\
 	1234)	${ECHO} little ;;					\
