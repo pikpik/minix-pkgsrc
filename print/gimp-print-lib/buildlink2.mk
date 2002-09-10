4af6270e17e03fa85bdaf65f2a7125ad9007faca
jlam <jlam>
2002-09-10 10:42:21 +0000

    Add a buildlink2.mk file for other packages to use.

---
+++ print/gimp-print-lib/buildlink2.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+.if !defined(GIMP_PRINT_BUILDLINK2_MK)
+GIMP_PRINT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=	gimp-print-lib
+BUILDLINK_DEPENDS.gimp-print-lib?=	gimp-print-lib>=4.2.1
+BUILDLINK_PKGSRCDIR.gimp-print-lib?=	../../print/gimp-print-lib
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-print-lib=gimp-print-lib
+BUILDLINK_PREFIX.gimp-print-lib_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gimp-print-lib=		include/gimp-print/*
+BUILDLINK_FILES.gimp-print-lib+=	lib/libgimpprint.*
+
+BUILDLINK_TARGETS+=	gimp-print-lib-buildlink
+
+gimp-print-lib-buildlink: _BUILDLINK_USE
+
+.endif	# GIMP_PRINT_BUILDLINK2_MK
