76946b07acde37d482a6a7ece1a755ac32cb2bc1
wiz <wiz>
2002-09-11 15:25:13 +0000

    Add buildlink2.mk.

---
+++ databases/db4/buildlink2.mk
@@ -0,0 +1,24 @@
+# $NetBSD$
+#
+
+.if !defined(DB4_BUILDLINK2_MK)
+DB4_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=		db4
+BUILDLINK_DEPENDS.db4?=		db4>=4.0.14
+BUILDLINK_PKGSRCDIR.db4?=	../../databases/db4
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.db4=db4
+BUILDLINK_PREFIX.db4_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.db4+=	include/db4/cxx_common.h
+BUILDLINK_FILES.db4+=	include/db4/cxx_except.h
+BUILDLINK_FILES.db4+=	include/db4/db.h
+BUILDLINK_FILES.db4+=	include/db4/db_cxx.h
+BUILDLINK_FILES.db4+=	lib/libdb4.*
+BUILDLINK_FILES.db4+=	lib/libdb4_cxx.*
+
+BUILDLINK_TARGETS+=	db4-buildlink
+
+db4-buildlink: _BUILDLINK_USE
+
+.endif	# DB4_BUILDLINK2_MK
