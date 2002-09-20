b68cb20e9b283938595be2874104db3bd6ad810e
grant <grant>
2002-09-20 00:15:56 +0000

    bump BUILDLINK_DEPENDS after adding dependancy on converters/libiconv.
    as discussed with jlam.

--- databases/freetds/buildlink2.mk
+++ databases/freetds/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:04 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:06 jlam Exp $
 
 .if !defined(FREETDS_BUILDLINK2_MK)
 FREETDS_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		freetds
-BUILDLINK_DEPENDS.freetds?=	freetds>=0.53
+BUILDLINK_DEPENDS.freetds?=	freetds>=0.53nb1
 BUILDLINK_PKGSRCDIR.freetds?=	../../databases/freetds
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.freetds=freetds
