d3d32663bbc574b1b2220e57decca3d1def46167
wiz <wiz>
2002-09-23 16:56:03 +0000

    Convert to buildlink2, add buildlink2.mk.

---
+++ x11/gtkglarea/buildlink2.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+.if !defined(GTKGLAREA_BUILDLINK2_MK)
+GTKGLAREA_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			gtkglarea
+BUILDLINK_DEPENDS.gtkglarea?=		gtkglarea>=1.2.2
+BUILDLINK_PKGSRCDIR.gtkglarea?=		../../x11/gtkglarea
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gtkglarea=gtkglarea
+BUILDLINK_PREFIX.gtkglarea_DEFAULT=	${X11PREFIX}
+BUILDLINK_FILES.gtkglarea+=	include/gtkgl/*
+BUILDLINK_FILES.gtkglarea+=	lib/libgtkgl.*
+
+BUILDLINK_TARGETS+=	gtkglarea-buildlink
+
+gtkglarea-buildlink: _BUILDLINK_USE
+
+.endif	# GTKGLAREA_BUILDLINK2_MK
