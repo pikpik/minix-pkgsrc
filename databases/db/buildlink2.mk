8ba99d5d9c2d2d09957f21755c0ab29af262408a
jlam <jlam>
2002-09-26 22:34:09 +0000

    Add BUILDLINK_CPPFLAGS.<pkg> for these packages to help packages find the db[23] headers.

--- databases/db/buildlink2.mk
+++ databases/db/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:03 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:38:20 jlam Exp $
 
 .if !defined(DB_BUILDLINK2_MK)
 DB_BUILDLINK2_MK=	# defined
@@ -11,6 +11,8 @@ EVAL_PREFIX+=		BUILDLINK_PREFIX.db=db
 BUILDLINK_PREFIX.db_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.db=	include/db2/*
 BUILDLINK_FILES.db+=	lib/libdb2.*
+
+BUILDLINK_CPPFLAGS.db=	-I${BUILDLINK_PREFIX.db}/include/db2
 
 BUILDLINK_TARGETS+=	db-buildlink
 
