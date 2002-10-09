3b500d76ee76ad72dbb7381750fe6144e02ee6f5
wiz <wiz>
2002-10-09 17:41:29 +0000

    Unused.

--- math/py-Numeric/buildlink.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: buildlink.mk,v 1.2 2002/01/15 18:38:46 drochner Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_DEPENDS.pynumeric?= ${PYPKGPREFIX}-Numeric-*
-DEPENDS+= ${BUILDLINK_DEPENDS.pynumeric}:../../math/py-Numeric
