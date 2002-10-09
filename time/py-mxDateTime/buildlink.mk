0905832956160459233e9485c09bff2d64cf187e
wiz <wiz>
2002-10-09 18:28:19 +0000

    Unused.

--- time/py-mxDateTime/buildlink.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD$
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_DEPENDS.pymxdatetime?= ${PYPKGPREFIX}-mxDateTime-*
-DEPENDS+= ${BUILDLINK_DEPENDS.pymxdatetime}:../../time/py-mxDateTime
