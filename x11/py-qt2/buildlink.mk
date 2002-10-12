a9fbb3872b6221fadec938d7a210486219707c77
wiz <wiz>
2002-10-12 09:58:28 +0000

    Unused.

--- x11/py-qt2/buildlink.mk
+++
@@ -1,6 +0,0 @@
-# $NetBSD: buildlink.mk,v 1.1 2002/01/28 17:34:29 drochner Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_DEPENDS.pyqt2?= ${PYPKGPREFIX}-qt2-[0-9]*
-DEPENDS+= ${BUILDLINK_DEPENDS.pyqt2}:../../x11/py-qt2
