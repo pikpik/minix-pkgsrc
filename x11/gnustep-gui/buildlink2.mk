1b30ac3f494fdb53a595d46d0fed4bddc392ebfe
rh <rh>
2002-10-18 09:23:26 +0000

    Update gnustep-gui to 0.8.2
    Noteworthy changes in version `0.8.2'
    =====================================
    
       * Handle fonts that aren't found better.
    
       * Implement pageUp/Down.
    
       * Some window focusing problems fixed.
    
       * Quartz-like interface partially implemented.
    
       * NSSecureTextField partially rewritten. More secure.
    
       * NSBrowser: implement non-separate columns
    
       * Fix firstResponder status in text fields.

--- x11/gnustep-gui/buildlink2.mk
+++ x11/gnustep-gui/buildlink2.mk
@@ -1,11 +1,11 @@
-# $NetBSD$
+# $NetBSD: buildlink2.mk,v 1.1 2002/09/27 21:04:21 rh Exp $
 #
 
 .if !defined(GNUSTEP_GUI_BUILDLINK2_MK)
 GNUSTEP_GUI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnustep-gui
-BUILDLINK_DEPENDS.gnustep-gui?=		gnustep-gui>=0.8.1
+BUILDLINK_DEPENDS.gnustep-gui?=		gnustep-gui>=0.8.2
 BUILDLINK_PKGSRCDIR.gnustep-gui?=	../../x11/gnustep-gui
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnustep-gui=gnustep-gui
