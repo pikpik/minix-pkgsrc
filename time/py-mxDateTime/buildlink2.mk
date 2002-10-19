f8bbd5cc79e21b29300f797217778f025ea89d86
jlam <jlam>
2002-10-19 02:48:20 +0000

    Add inclusion guards and use foo-[0-9]* instead of foo-*.

--- time/py-mxDateTime/buildlink2.mk
+++ time/py-mxDateTime/buildlink2.mk
@@ -1,7 +1,12 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:45 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:24 jlam Exp $
+
+.if !defined(PY_MXDATETIME_BUILDLINK2_MK)
+PY_MXDATETIME_BUILDLINK2_MK=	# defined
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=			pymxdatetime
-BUILDLINK_DEPENDS.pymxdatetime?=	${PYPKGPREFIX}-mxDateTime-*
+BUILDLINK_DEPENDS.pymxdatetime?=	${PYPKGPREFIX}-mxDateTime-[0-9]*
 BUILDLINK_PKGSRCDIR.pymxdatetime?=	../../time/py-mxDateTime
+
+.endif	# PY_MXDATETIME_BUILDLINK2_MK
