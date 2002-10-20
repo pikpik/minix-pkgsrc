37dd43c165dc25f66969785fa23fd1f8bfde78cb
jlam <jlam>
2002-10-20 08:09:46 +0000

    Don't default to cupsd=NO anymore, as rc.d scripts aren't copied into /etc/rc.d by default any longer.  This causes the usual warning to be emitted when the script is executed.

--- print/cups/files/cupsd.sh
+++ print/cups/files/cupsd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: cupsd.sh,v 1.11 2002/09/18 22:58:47 jlam Exp $
+# $NetBSD: cupsd.sh,v 1.12 2002/09/20 02:01:59 grant Exp $
 #
 # Common UNIX Printing System daemon
 #
@@ -21,7 +21,6 @@ extra_commands="reload"
 if [ -f /etc/rc.subr ]
 then
 	load_rc_config $name
-	eval [ -z "\$${rcvar}" ] && eval ${rcvar}=NO
 	run_rc_command "$1"
 else
 	@ECHO@ -n " ${name}"
