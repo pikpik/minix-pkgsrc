e9cad3934e7e5469ed79dc00000b65fd039f9065
jlam <jlam>
2002-10-21 07:12:34 +0000

    Use exec to save forking a new process, and use proper argument quoting.

--- security/uvscan/files/uvscan.sh
+++ security/uvscan/files/uvscan.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# $NetBSD$
+# $NetBSD: uvscan.sh,v 1.2 2001/08/16 05:09:22 jlam Exp $
 #
 # Wrapper script to execute McAfee(R) VirusScan
 
-@PREFIX@/libexec/uvscan/uvscan $*
+exec @PREFIX@/libexec/uvscan/uvscan "$@"
