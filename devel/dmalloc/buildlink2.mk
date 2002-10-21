d9f00ea06d7af37706d611550b0503d395f4748a
wiz <wiz>
2002-10-21 01:38:37 +0000

    Add buildlink2.mk.

---
+++ devel/dmalloc/buildlink2.mk
@@ -0,0 +1,24 @@
+# $NetBSD$
+#
+
+.if !defined(DMALLOC_BUILDLINK2_MK)
+DMALLOC_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			dmalloc
+BUILDLINK_DEPENDS.dmalloc?=		dmalloc>=4.8.2nb2
+BUILDLINK_PKGSRCDIR.dmalloc?=		../../devel/dmalloc
+
+BUILDLINK_DEPMETHOD.dmalloc?=		build
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.dmalloc=dmalloc
+BUILDLINK_PREFIX.dmalloc_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.dmalloc+=	include/dmalloc.h
+BUILDLINK_FILES.dmalloc+=	lib/libdmalloc.*
+BUILDLINK_FILES.dmalloc+=	lib/libdmalloclp.*
+BUILDLINK_FILES.dmalloc+=	lib/libdmallocxx.*
+
+BUILDLINK_TARGETS+=	dmalloc-buildlink
+
+dmalloc-buildlink: _BUILDLINK_USE
+
+.endif	# DMALLOC_BUILDLINK2_MK
