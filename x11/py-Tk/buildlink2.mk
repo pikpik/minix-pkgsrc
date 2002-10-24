9c2d67eb30a5cb556fc73f6162c97d7fd45ade6d
drochner <drochner>
2002-10-24 19:33:39 +0000

    include x11/tk/buildlink2.mk

--- x11/py-Tk/buildlink2.mk
+++ x11/py-Tk/buildlink2.mk
@@ -1,7 +1,9 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:54 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:56 jlam Exp $
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=		pytk
 BUILDLINK_DEPENDS.pytk?=	${PYPKGPREFIX}-Tk-*
 BUILDLINK_PKGSRCDIR.pytk?=	../../x11/py-Tk
+
+.include "../../x11/tk/buildlink2.mk"
