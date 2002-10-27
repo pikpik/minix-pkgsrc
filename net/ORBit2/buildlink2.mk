2cba21c17d9c7f4ea3749d89e61695a4e024009a
rh <rh>
2002-10-27 09:16:09 +0000

    Update ORBit2 to 2.5.0.  Provided in PR pkg/18707 by Julio Merino.
    Changes include:
            * bug fixes
                    + HP/UX / ANSI fixes (Albert Chin)
                    + imodule fixage (James Henstridge)
                            + nested unions, structs, exceptions
                            + fall-through case unions subnames
                            + type mis-inclusion
                    + request handler segv (Mark)
                    + epv typecasts (Gergo)
                    + improve generic marshaller (Michael)
                    + fix TC_void's kind (Michael)
                    + fix linc EOF locking interaction silly (Michael)
                    + kill name server cruft (Mark)
                    + fix linking for -pthread (Mark)
                    + IDL compiler backend fix (Murray Cumming)
                    + internal header / build fixes (Gergo Erdi)
            * features
                    + IDL compiler language backend API (Mark, Gergo)
                      NB. see module 'orbitcpp' for C++ bindings [!]
                    + CORBA_Typecode API (Gergo)
                    + IDL -> IInterface converter (Mark)
                    + improved imodule handling (Mark)
                    + more regression tests (Michael, James)
                    + de-bloated IDL compiler -6k LOBC (Mark)

--- net/ORBit2/buildlink2.mk
+++ net/ORBit2/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.4 2002/09/10 16:06:46 wiz Exp $
+# $NetBSD: buildlink2.mk,v 1.5 2002/09/23 19:57:30 jlam Exp $
 
 .if !defined(ORBIT2_BUILDLINK2_MK)
 ORBIT2_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		orbit2
-BUILDLINK_DEPENDS.orbit2?=	ORBit2>=2.4.0nb2
+BUILDLINK_DEPENDS.orbit2?=	ORBit2>=2.5.0
 BUILDLINK_PKGSRCDIR.orbit2?=	../../net/ORBit2
 
 EVAL_PREFIX+=				BUILDLINK_PREFIX.orbit2=orbit2
