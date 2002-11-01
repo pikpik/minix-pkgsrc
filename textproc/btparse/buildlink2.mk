13a92f5d39cdf42503aef5b3ec0605fa8c74a680
rh <rh>
2002-11-01 04:18:02 +0000

    This doesn't use libtool, so remove USE_LIBTOOL. Add buildlink2.mk

---
+++ textproc/btparse/buildlink2.mk
@@ -0,0 +1,25 @@
+# $NetBSD$
+#
+# This Makefile fragment is included by packages that use btparse.
+#
+# This file was created automatically using createbuildlink 2.2.
+#
+
+.if !defined(BTPARSE_BUILDLINK2_MK)
+BTPARSE_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			btparse
+BUILDLINK_DEPENDS.btparse?=		btparse>=0.33
+BUILDLINK_PKGSRCDIR.btparse?=		../../textproc/btparse
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.btparse=btparse
+BUILDLINK_PREFIX.btparse_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.btparse+=	include/btparse.h
+BUILDLINK_FILES.btparse+=	lib/libbtparse.*
+
+
+BUILDLINK_TARGETS+=	btparse-buildlink
+
+btparse-buildlink: _BUILDLINK_USE
+
+.endif	# BTPARSE_BUILDLINK2_MK
