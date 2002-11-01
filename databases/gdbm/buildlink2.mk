ea996c79e7f072d1f78bef44288fee49587f9962
wiz <wiz>
2002-11-01 00:31:18 +0000

    Update to 1.8.3: CHANGES from 1.8.2 to 1.8.3   1.  Various configure related changes and additional updates. CHANGES from 1.8.1 to 1.8.2   1.  Allow `NEWDB'-opened databases to actually, well, store records. CHANGES from 1.8 to 1.8.1   1.  Lots of bug fixes, including a data corruption bug.   2.  Updated to current autoconf and libtool.   3.  Moved the dbm/ndbm compatibility routines to libgdbm_compat.

--- databases/gdbm/buildlink2.mk
+++ databases/gdbm/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:26 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:38:21 jlam Exp $
 
 .if !defined(GDBM_BUILDLINK2_MK)
 GDBM_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		gdbm
-BUILDLINK_DEPENDS.gdbm?=	gdbm>=1.7.3
+BUILDLINK_DEPENDS.gdbm?=	gdbm>=1.8.3
 BUILDLINK_PKGSRCDIR.gdbm?=	../../databases/gdbm
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gdbm=gdbm
