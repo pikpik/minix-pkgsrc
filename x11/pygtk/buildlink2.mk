11d51fead17fb89516353150f1fa0028790af1d4
jlam <jlam>
2002-11-15 05:29:26 +0000

    Protect against multiple inclusion.

--- x11/pygtk/buildlink2.mk
+++ x11/pygtk/buildlink2.mk
@@ -1,7 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.2 2002/08/22 20:45:57 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:58 jlam Exp $
+
+.if !defined(PY_GTK_BUILDLINK2_MK)
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_PACKAGES+=		pygtk
-BUILDLINK_DEPENDS.pygtk?=	${PYPKGPREFIX}-gtk-*
+BUILDLINK_DEPENDS.pygtk?=	${PYPKGPREFIX}-gtk-[0-9]*
 BUILDLINK_PKGSRCDIR.pygtk?=	../../x11/pygtk
+
+.endif	# PY_GTK_BUILDLINK2_MK
