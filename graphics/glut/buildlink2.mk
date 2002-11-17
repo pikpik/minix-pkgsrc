b958a3f45a4610543f2cd397e60390baac1fe295
jlam <jlam>
2002-11-17 08:37:20 +0000

    glut requires MesaLib headers in order to build.

--- graphics/glut/buildlink2.mk
+++ graphics/glut/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.2 2002/06/21 23:00:31 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:39:08 jlam Exp $
 
 .if !defined(GLUT_BUILDLINK2_MK)
 GLUT_BUILDLINK2_MK=	# defined
@@ -12,6 +12,8 @@ BUILDLINK_PREFIX.glut_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.glut=	include/GL/glut.h
 BUILDLINK_FILES.glut+=	include/GL/glutf90.h
 BUILDLINK_FILES.glut+=	lib/libglut.*
+
+.include "../../graphics/MesaLib/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	glut-buildlink
 
