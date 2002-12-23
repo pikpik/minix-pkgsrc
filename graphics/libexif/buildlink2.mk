9f4fbac5fb52e23525e8907a7d4886a01e8fa989
wiz <wiz>
2002-12-23 22:40:44 +0000

    Update to 0.5.7, mostly bugfixes and more translations. * libexif/exif-data.h: Introduce an array of ExifContents. This   doesn't break binary compatibility, but it breaks compilation.   Do something like "%s/->ifd_0/->ifd[EXIF_IFD_0]" in your source   code to make it compile again.

--- graphics/libexif/buildlink2.mk
+++ graphics/libexif/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:27 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:52 jlam Exp $
 
 .if !defined(LIBEXIF_BUILDLINK2_MK)
 LIBEXIF_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libexif
-BUILDLINK_DEPENDS.libexif?=	libexif>=0.5
+BUILDLINK_DEPENDS.libexif?=	libexif>=0.5.7
 BUILDLINK_PKGSRCDIR.libexif?=	../../graphics/libexif
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.libexif=libexif
