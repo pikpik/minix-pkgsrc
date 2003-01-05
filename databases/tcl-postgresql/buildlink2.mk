e162893e2b0f5511db97f906cfb1269afe1735ed
jlam <jlam>
2003-01-05 20:12:30 +0000

    Updated databases/postgresql to 7.3.1.
          ==================================================================
          | NOTE:  A dump-and-restore is required to update your databases |
          |        if you wish to update postgresql-server. If your        |
          |        application examines the system catalogs, additional    |
          |        changes will be required due to the introduction of     |
          |        schemas in 7.3; for more information, see:              |
          |                                                                |
          |   http://www.ca.postgresql.org/docs/momjian/upgrade_tips_7.3   |
          ==================================================================
    
    Major changes from version 7.2.3 include:
    
       Schemas
              Schemas allow users to create objects in separate namespaces,
              so two people or applications can have tables with the same
              name. There is also a public schema for shared tables.
              Table/index creation can be restricted by removing permissions
              on the public schema.
    
       Drop Column
              PostgreSQL now supports the ALTER TABLE ... DROP COLUMN
              functionality.
    
       Table Functions
              Functions returning multiple rows and/or multiple columns are
              now much easier to use than before. You can call such a "table
              function" in the SELECT FROM clause, treating its output like a
              table. Also, PL/pgSQL functions can now return sets.
    
       Prepared Queries
              PostgreSQL now supports prepared queries, for improved
              performance.
    
       Dependency Tracking
              PostgreSQL now records object dependencies, which allows
              improvements in many areas. "DROP" statements now take either
              CASCADE or RESTRICT to control whether dependent objects are
              also dropped.
    
       Privileges
              Functions and procedural languages now have privileges, and
              functions can be defined to run with the privileges of their
              creator.
    
       Internationalization
              Both multibyte and locale support are now always enabled.
    
       Logging
              A variety of logging options have been enhanced.
    
       Interfaces
              A large number of interfaces have been moved to
              http://gborg.postgresql.org where they can be developed and
              released independently.
    
       Functions/Identifiers
              By default, functions can now take up to 32 parameters, and
              identifiers can be up to 63 bytes long. Also, OPAQUE is now
              deprecated: there are specific "pseudo-datatypes" to represent
              each of the former meanings of OPAQUE in function argument and
              result types.

--- databases/tcl-postgresql/buildlink2.mk
+++ databases/tcl-postgresql/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:05 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:12 jlam Exp $
 
 .if !defined(TCL_POSTGRESQL_BUILDLINK2_MK)
 TCL_POSTGRESQL_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			tcl-postgresql
-BUILDLINK_DEPENDS.tcl-postgresql?=	tcl-postgresql>=7.0
+BUILDLINK_DEPENDS.tcl-postgresql?=	tcl-postgresql>=7.3.1
 BUILDLINK_PKGSRCDIR.tcl-postgresql?=	../../databases/tcl-postgresql
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.tcl-postgresql=tcl-postgresql
