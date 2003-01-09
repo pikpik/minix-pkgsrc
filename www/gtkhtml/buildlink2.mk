6b5573741d9cbd60c6309697bde3f5ddf1a1d4e1
rh <rh>
2003-01-09 11:36:03 +0000

    Sync w/ changed PLIST

--- www/gtkhtml/buildlink2.mk
+++ www/gtkhtml/buildlink2.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/10 16:06:51 wiz Exp $
+# $NetBSD$
 
 .if !defined(GTKHTML_BUILDLINK2_MK)
 GTKHTML_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gtkhtml
-BUILDLINK_DEPENDS.gtkhtml?=		gtkhtml>=1.0.1nb3
+BUILDLINK_DEPENDS.gtkhtml?=		gtkhtml>=1.1.7
 BUILDLINK_PKGSRCDIR.gtkhtml?=		../../www/gtkhtml
 
-EVAL_PREFIX+=			BUILDLINK_PREFIX.gtkhtml=gtkhtml
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gtkhtml=gtkhtml
 BUILDLINK_PREFIX.gtkhtml_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.gtkhtml+=		include/gtkhtml/*
-BUILDLINK_FILES.gtkhtml+=		lib/bonobo/libstorage_http.*
-BUILDLINK_FILES.gtkhtml+=		lib/libgtkhtml.*
+BUILDLINK_FILES.gtkhtml+=	include/gtkhtml-1.1/gtkhtml/*.h
+BUILDLINK_FILES.gtkhtml+=	lib/bonobo/plugin/libstorage_http.*
+BUILDLINK_FILES.gtkhtml+=	lib/libgtkhtml-1.1.*
+BUILDLINK_FILES.gtkhtml+=	lib/pkgconfig/gtkhtml-1.1.pc
 
 .include "../../devel/bonobo/buildlink2.mk"
 .include "../../devel/gal/buildlink2.mk"
@@ -20,7 +21,7 @@ BUILDLINK_FILES.gtkhtml+=		lib/libgtkhtml.*
 .include "../../www/libghttp/buildlink2.mk"
 .include "../../x11/controlcenter/buildlink2.mk"
 
-BUILDLINK_TARGETS+=		gtkhtml-buildlink
+BUILDLINK_TARGETS+=	gtkhtml-buildlink
 
 gtkhtml-buildlink: _BUILDLINK_USE
 
