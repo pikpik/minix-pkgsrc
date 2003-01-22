b147b6d3199ae21e7b82b5ad6d45aaf04d2f96d4
jmmv <jmmv>
2003-01-22 16:23:40 +0000

    Update neon to 0.23.6, with patch provided in PR pkg/19998 by Marc Recht.
    Changes since 0.23.5:
         * Fixes for error handling in socket layer on Win32 from Johan Lindh and
           Sergey N Ushakov:
              * meaningful error messages rather than "No error"
              * handle persistent connection timeouts properly
         * Fix to use RFC2617-style digest auth when possible (had reverted to
           only using RFC2068-style in 0.16.1).
         * Fix NULL pointer dereference on certain ill-formed PROPFIND responses.
         * Allow ne_sock_init to re-initialize after ne_sock_finish has been
           called (Sergey N Ushakov).
    
    Changes in the package:
         * Switch to libxml2.

--- www/neon/buildlink2.mk
+++ www/neon/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:40:24 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2002/11/25 20:01:43 wiz Exp $
 
 .if !defined(NEON_BUILDLINK2_MK)
 NEON_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		neon
-BUILDLINK_DEPENDS.neon?=	neon>=0.23.5
+BUILDLINK_DEPENDS.neon?=	neon>=0.23.6
 BUILDLINK_PKGSRCDIR.neon?=	../../www/neon
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.neon=neon
@@ -14,7 +14,7 @@ BUILDLINK_FILES.neon+=		lib/libneon.*
 
 .include "../../devel/zlib/buildlink2.mk"
 .include "../../security/openssl/buildlink2.mk"
-.include "../../textproc/libxml/buildlink2.mk"
+.include "../../textproc/libxml2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=		neon-buildlink
 
