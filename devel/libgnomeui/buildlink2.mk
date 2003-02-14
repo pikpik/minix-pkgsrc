f53287d1f0aa60ff738ea974d077a8bcd6a2e5e0
jmmv <jmmv>
2003-02-14 20:16:22 +0000

    Updated to 2.2.0.1.  Changes from 2.0.6:
    * configure.in (LIBGNOMEUI_AGE): Add EXTRAVERSION and set to .1
    * libgnomeui/Makefile.am (libgnomeui_2_la_LDFLAGS): Correct version number bug
    * configure.in: Added "mn" to ALL_LINGUAS.
    * configure.in: Fixup libtool versioning and release 2.2.0
    * configure.in: Added Amharic (am), Arabic (ar), Macedonian (mk) and Dutch
      (nl) to ALL_LINGUAS.
    * libgnomeui/gnome-icon-theme.c (theme_lookup_icon): Don't scale up unless we
      have to. Scaling down gives much better results.
    * libgnomeui/gnome-uidefs.h (GNOME_KEY_MOD_PAUSE_GAME): Use GDK_Pause as the
      pause key. #92358.
    * libgnomeui/gnome-app-helper.c: (gnome_app_install_statusbar_menu_hints):
      Apply fix for #102812, by Tom Ball.
    * libgnomeui/gnome-popup-menu.c: (gnome_popup_menu_do_popup_modal): Apply fix
      for #96899, by Erwann Chenede.

--- devel/libgnomeui/buildlink2.mk
+++ devel/libgnomeui/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/22 09:52:36 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/12/24 06:09:57 wiz Exp $
 
 .if !defined(LIBGNOMEUI_BUILDLINK2_MK)
 LIBGNOMEUI_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libgnomeui
-BUILDLINK_DEPENDS.libgnomeui?=	libgnomeui>=2.0.5nb2
+BUILDLINK_DEPENDS.libgnomeui?=	libgnomeui>=2.2.0.1
 BUILDLINK_PKGSRCDIR.libgnomeui?=	../../devel/libgnomeui
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libgnomeui=libgnomeui
@@ -13,6 +13,7 @@ BUILDLINK_FILES.libgnomeui=	include/libgnomeui-2.0/libgnomeui/*
 BUILDLINK_FILES.libgnomeui+=	include/libgnomeui-2.0/*
 BUILDLINK_FILES.libgnomeui+=	lib/libglade/2.0/libgnome.*
 BUILDLINK_FILES.libgnomeui+=	lib/libgnomeui-2.*
+BUILDLINK_FILES.libgnomeui+=	lib/pkgconfig/libgnomeui-2.0.pc
 
 .include "../../audio/esound/buildlink2.mk"
 .include "../../devel/gettext-lib/buildlink2.mk"
