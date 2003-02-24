aaa7b8741ba48a9f4e363ce4d1edf9f7b78030fc
jschauma <jschauma>
2003-02-24 20:50:19 +0000

    Update to 1.3.4 using patches provided by roskens at elfin dot net in PR pkg/20270 with minor nitpicks by myself. Some of the changes since 1.2.1:
    	* configure.in: fix minor problem where we used the guile-gtk
    	config program even when it wasn't found.
    	* acconfig.h: deleted since deprecated -- replaced by three arg
    	AC_DEFINEs in configure.in.
    	* doc/.cvsignore: add texinfo.tex.
    	* configure.in (AM_INIT_AUTOMAKE): bump version for release.
    	* bunch of subversion cleanups.
    	* change license to LGPL.
    	* g-wrap/gw-glib-spec.scm: coerce to (gpointer) in gw:glist-of and
    	gw:gslist-of code where needed.
    	* g-wrap/enumeration.scm (gw:wrap-enumeration): make val->int and
    	int->val functions static so they don't cause conflicts.
    	* configure.in (GW_GUILE_GTK_CFLAGS): delete any occurences of -I
    	/usr/include or similar.  Newer gcc's don't like that at all.
    	* bin/g-wrap-config.in (c-compile-args): don't output -I if it's
    	/usr/include.  The new gcc doesn't like -I /usr/include at all.
    	* test/Makefile.am (gw-test-enumeration.scm gw-test-enumeration.h
    	gw-test-enumeration.c gw-test-enumeration.html
    	gw-test-enumeration-autogen.h): use LD_LIBRARY_PATH.
    	(gw-test-parent.scm gw-test-parent.h gw-test-parent.c
    	gw-test-parent.html gw-test-parent-autogen.h): use LD_LIBRARY_PATH.
    	* doc/g-wrap.texi: minor re-editing -- still needs much more.
    	* configure.in (GW_GUILE_GTK_CFLAGS): add and AC_SUBST.
    	(GW_GUILE_GTK_LIBFLAGS): add and AC_SUBST.
    	* g-wrap/simple-type.scm: new file -- support for simple types.
    	* g-wrap/runtime.scm: new file -- scm support for runtime wrapset
    	database.
    	* g-wrap/gw-wct-spec.scm: new file -- wrapped C pointer types.
    	* g-wrap/gw-standard-spec.scm: new file -- standard wrapped types.
    	* g-wrap/gw-gtk-spec.scm: new file -- wrappers for gtk.
    	* g-wrap/gw-glib-spec.scm: new file -- wrappers for glib.
    	* g-wrap/gtkobj.scm: support for wrapping new C ptr types as
    	gtkobjects.
    	* g-wrap/Makefile.am: major changes -- support gw-glib, gw-wct,
    	gw-gtk, etc.
    	* g-wrap/g-wrap-wct.h: support header for gw-wct.  Part of
    	libgwrap-wct.
    	* g-wrap/g-wrap-wct.c: support code for gw-wct.  Part of
    	libgwrap-wct.
    	* g-wrap/g-wrap-glib.h: support header for gw-glib.  Part of
    	libgwrap-glib.
    	* g-wrap/g-wrap-glib.c: support code for gw-glib.  Part of
    	libgwrap-glib.
    	* g-wrap/output-file.scm (flatten-display): make public.
    	* g-wrap/g-wrap-compatibility.h: start guile version compatibility
    	library.
    	* g-wrap/enumeration.scm: break out enumeration support.
    	* g-wrap/dummy-main.c: new file - for link testing.
    	* bin/g-wrap-config.in (c-link-args): add support for glib, wct,
    	etc.
    	* g-wrap.scm: major overhaul -- modules -> wrapsets, ccodegens ->
    	init/decl/lists, etc.
    	* configure.in: remove rscheme (and other dead bits).  Add support
    	for glib and gnome-guile.  Handle libruntime reorg/removal.
    	* acconfig.h: accomodate reorg/removal of libruntime.
    	* g-wrapped/gw-runtime-spec.scm: switch to use of SCM_FALSEP and
    	SCM_NFALSEP rather than direct SCM_BOOL_*
    	* libruntime-guile/g-wrap-runtime-guile.c: switch to use of
    	SCM_FALSEP and SCM_NFALSEP rather than direct SCM_BOOL_*
    	comparisons.
    	* g-wrap.scm: switch to use of SCM_FALSEP and SCM_NFALSEP rather
    	than direct SCM_BOOL_* comparisons.

--- devel/g-wrap/buildlink2.mk
+++ devel/g-wrap/buildlink2.mk
@@ -4,13 +4,14 @@
 G_WRAP_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		g-wrap
-BUILDLINK_DEPENDS.g-wrap?=	g-wrap>=1.2.1
+BUILDLINK_DEPENDS.g-wrap?=	g-wrap>=1.3.4
 BUILDLINK_PKGSRCDIR.g-wrap?=	../../devel/g-wrap
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.g-wrap=g-wrap
 BUILDLINK_PREFIX.g-wrap_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.g-wrap=		include/g-wrap-runtime-guile.h
-BUILDLINK_FILES.g-wrap+=	lib/libg-wrap-runtime-guile.*
+BUILDLINK_FILES.g-wrap=		include/g-wrap/*
+BUILDLINK_FILES.g-wrap+=        lib/libgw-*
+BUILDLINK_FILES.g-wrap+=        lib/libgwrap-*
 
 .include "../../lang/guile14/buildlink2.mk"
 
