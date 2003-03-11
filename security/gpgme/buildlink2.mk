0cd2921423a37844d04cced1fc863c27c56f3893
salo <salo>
2003-03-11 00:11:31 +0000

    Updated to latest stable version 0.3.15.
    Changes:
    
    - The progress status is sent via the progress callbacks in
      gpgme_op_edit.
    - Bug fix for signing operations with explicit signer settings
      for the CMS protocol.

--- security/gpgme/buildlink2.mk
+++ security/gpgme/buildlink2.mk
@@ -1,10 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:40 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:15 jlam Exp $
+#
 
 .if !defined(GPGME_BUILDLINK2_MK)
 GPGME_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		gpgme
-BUILDLINK_DEPENDS.gpgme?=	gpgme>=0.3.0
+BUILDLINK_DEPENDS.gpgme?=	gpgme>=0.3.15
 BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 
 EVAL_PREFIX+=		BUILDLINK_PREFIX.gpgme=gpgme
