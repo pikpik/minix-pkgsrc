a493d129dbd71a7965bbc07f671c06b17708f670
markd <markd>
2003-03-11 13:33:56 +0000

    Update KDE to 3.1.
    Many bugfixes and functionality enhancements including:
    
    kdemultimedia
    
     Started a video framework
     More consistent internationalisation in many applications.
     NEW IN KDE: KAudioCreator, a CD ripper and audio encoder frontend.
     NEW IN KDE: KRec, an audio recorder using aRts.
     KMediaPlayer interface added to kdelibs to reuse Kaboodle.
     Noatun: Added option to clear the playlist when running with a file as
     command line option.
     Added video thumbnail creator using xine-arts.
     KMix: Added mute control support.

--- audio/arts/buildlink2.mk
+++ audio/arts/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/21 22:32:25 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2002/10/07 05:09:58 jlam Exp $
 
 .if !defined(ARTS_BUILDLINK2_MK)
 ARTS_BUILDLINK2_MK=	# defined
@@ -6,7 +6,7 @@ ARTS_BUILDLINK2_MK=	# defined
 .include "../../mk/bsd.prefs.mk"
 
 BUILDLINK_PACKAGES+=		arts
-BUILDLINK_DEPENDS.arts?=	arts>=1.0.0
+BUILDLINK_DEPENDS.arts?=	arts>=1.1
 BUILDLINK_PKGSRCDIR.arts?=	../../audio/arts
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.arts=arts
