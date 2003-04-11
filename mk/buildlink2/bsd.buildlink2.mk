ed821ff162d37eb2984c9a10a7c40cdf8b601ba5
jmc <jmc>
2003-04-11 18:13:09 +0000

    Make sure wrappee is actually a file/link before considering it found. On solaris for instance if one put /usr/lib in their path before /usr/ccs/bin it would match /usr/lib/ld rather than /usr/ccs/bin/ld except that /usr/lib/ld is a directory...

--- mk/buildlink2/bsd.buildlink2.mk
+++ mk/buildlink2/bsd.buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.buildlink2.mk,v 1.78 2003/01/14 09:32:12 jlam Exp $
+# $NetBSD: bsd.buildlink2.mk,v 1.79 2003/01/25 07:57:21 jlam Exp $
 #
 # An example package buildlink2.mk file:
 #
@@ -729,7 +729,9 @@ ${BUILDLINK_${_wrappee_}}:						\
 			case $${dir} in					\
 			*${BUILDLINK_DIR}*)				\
 				;;					\
-			*)	if [ -x $${dir}/$${wrappee} ]; then	\
+			*)	if [ -f $${dir}/$${wrappee} ] ||	\
+				   [ -h $${dir}/$${wrappee} ] &&	\
+				   [ -x $${dir}/$${wrappee} ]; then	\
 					absdir=$${dir}/;		\
 					wrappee=$${absdir}$${wrappee};	\
 					break;				\
