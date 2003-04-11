eaf73a259f9efd39c68d4e9dd641597c40f96457
uebayasi <uebayasi>
2003-04-11 01:07:51 +0000

    Bye bye old places.

--- editors/emacs21/files/site-init.el
+++
@@ -1,9 +0,0 @@
-;; $NetBSD$
-
-;; NetBSD puts info files in /usr/share.
-(setq Info-default-directory-list
-      (cons "/usr/share/info/"
-	    Info-default-directory-list))
-
-(autoload 'send-pr "/usr/share/gnats/send-pr.el" 
-  "Command to create and send a problem report." t)
