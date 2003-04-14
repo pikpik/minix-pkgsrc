3f9c9a987058ab526c2f8cc2bed69cf2895e5e95
grant <grant>
2003-04-14 22:50:19 +0000

    tidy up NetBSD/Linux support, ala phoenix-bin-nightly.

--- www/mozilla-bin/files/mozilla.sh
+++ www/mozilla-bin/files/mozilla.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-# $NetBSD: mozilla.sh,v 1.1.1.1 2002/11/10 12:41:55 grant Exp $
-exec @PREFIX@/lib/mozilla-linux/mozilla "$@"
+#!@SH@
+# $NetBSD: mozilla.sh,v 1.1 2003/04/14 20:28:22 grant Exp $
+exec @PREFIX@/lib/mozilla-@PLATFORM@/mozilla "$@"
