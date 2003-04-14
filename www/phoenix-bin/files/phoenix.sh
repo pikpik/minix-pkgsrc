523ecea66f59e4a32b395e05b4330b4c48545f42
grant <grant>
2003-04-14 22:59:32 +0000

    tidy up NetBSD/Linux support, ala phoenix-bin-nightly.

--- www/phoenix-bin/files/phoenix.sh
+++ www/phoenix-bin/files/phoenix.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-# $NetBSD: phoenix.sh,v 1.3 2002/10/31 03:57:40 grant Exp $
-exec @PREFIX@/lib/phoenix-linux/phoenix "$@"
+#!@SH@
+# $NetBSD: phoenix.sh,v 1.1 2003/04/14 19:56:29 grant Exp $
+exec @PREFIX@/lib/phoenix-@PLATFORM@/phoenix "$@"
