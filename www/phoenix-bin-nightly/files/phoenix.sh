157a0d2bca4f74cf07cd8b39c2b5803fe06ea9a6
grant <grant>
2003-04-14 21:28:11 +0000

    add support for the Solaris/sparc binary package, tested on Solaris 9. tidy up NetBSD/Linux support while I'm here - no functional changes, so no PKGREVISION bump.

--- www/phoenix-bin-nightly/files/phoenix.sh
+++ www/phoenix-bin-nightly/files/phoenix.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!@SH@
 # $NetBSD: phoenix.sh,v 1.1.1.1 2003/03/26 12:55:04 grant Exp $
-exec @PREFIX@/lib/phoenix-linux/phoenix "$@"
+
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/phoenix-@PLATFORM@"
+export LD_LIBRARY_PATH
+
+exec @PREFIX@/lib/phoenix-@PLATFORM@/phoenix "$@"
