db4eddb4c50c77c6ee1e20a941860ed3a25c938b
jtb <jtb>
2003-04-29 23:05:22 +0000

    Update to version 1.4.5.
    Changes:
    
    * Allow scalar dataspaces to be used for parallel I/O.  QAK - 2002/11/05
    * Added environment variable "HDF5_DISABLE_VERSION_CHECK", which disables
      the version checking between the header files and the library linked
      into an application if set to '1'.  This should be used with caution,
      mis-matched headers and library binaries can cause _serious_ problems.
      QAK - 2002/10/15
    * Partially fixed space allocation inefficiencies in the file by
      improving our algorithms for re-using freed space.  QAK - 2002/08/27
    * API tracing has been improved. Nested API calls don't screw up the
      output format; function call and return event times can be logged;
      total time spent in each function can be logged.  The following
      HDF5_DEBUG environment variable words affect tracing:
    	trace   -- turn on/off basic tracing
    	ttimes  -- turn on tracing and report event times and
                           time spent in each API function.
            ttop    -- turn on tracing but display only top-level
                           API calls.
    * Several missing fortran APIs have been added to the library:
    
       h5get_libversion_f        h5tget_member_index_f  h5dget_storage_size_f
       h5check_version_f         h5tvlen_create_f       h5dvlen_get_max_len_f
       h5garbage_collect_f                              h5dwrite_vl_f
       h5dont_atexit_f                                  h5dread_vl_f
    
    Functions h5dvlen_get_max_len_f, h5dwrite_vl_f, and h5dread_vl_f support
    VL Length C APIs functionality for integer, real and string datatypes.
    See HDF5 Reference Manual and HDF5 FORTRAN90 User's Notes for more
    information and for the functions description.
    
    * H5Fopen without the H5F_ACC_CREAT flag should not succeed in creating
      a new file with the 'core' VFL driver.  QAK - 2003/01/24
    * Corrected metadata caching bug in parallel I/O which could cause hangs
      when chunked datasets were accessed with independent transfer mode.
      QAK - 2003/01/23
    * Allow opening objects with unknown object header messages.
      QAK - 2003/01/21
    * Added improved error assertion for nil VL strings.  It return error
      stack instead of a simple assertion.  SLU - 2002/12/16
    * Fixed h5dump bug(cannot dump data and datatype) for VL string.
      SLU - 2002/11/18
    * Fixed error condition where "none" selections were not being handled
      correctly in serial & parallel.  QAK - 2002/10/29
    * Fixed problem where optimized hyperslab routines were incorrectly
      invoked for parallel I/O operations in collective mode.  QAK - 2002/07/22
    * Fixed metadata corruption problem which could occur when many objects
      are created in a file during parallel I/O.  QAK - 2002/07/19
    * Fixed minor problem with configuration when users specified /usr/include
      and /usr/lib for the --with-* options that some compilers can't
      handle. BW - 2003/01/23

--- devel/hdf5/buildlink2.mk
+++ devel/hdf5/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:12 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:22:30 jlam Exp $
 
 .if !defined(HDF5_BUILDLINK2_MK)
 HDF5_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		hdf5
-BUILDLINK_DEPENDS.hdf5?=	hdf5>=1.4.3
+BUILDLINK_DEPENDS.hdf5?=	hdf5>=1.4.5
 BUILDLINK_PKGSRCDIR.hdf5?=	../../devel/hdf5
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.hdf5=hdf5
