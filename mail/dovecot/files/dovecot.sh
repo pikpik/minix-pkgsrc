7ad871469a542aab6a8c5a2a3363ddf6bf1e1d43
salo <salo>
2003-05-14 04:17:31 +0000

    Import of dovecot-0.99.9.1: Secure IMAP and POP3 server.
    Dovecot is a secure and compact IMAP/POP3 server which is in the early stages
    of developement.  It supports Maildirs and mbox formats and much of the IMAP
    v4 protocol including SSL/TLS.  IPv6 support is also included.
    
    Package provided by Juan RP via pkgsrc-wip with modifications by me.

---
+++ mail/dovecot/files/dovecot.sh
@@ -0,0 +1,17 @@
+#!@RCD_SCRIPTS_SHELL@
+#
+# $NetBSD$
+#
+
+# PROVIDE: dovecot
+# REQUIRE: DAEMON LOGIN
+
+. /etc/rc.subr
+
+name="dovecot"
+rcvar=$name
+command="@PREFIX@/sbin/${name}"
+required_files="@PKG_SYSCONFDIR@/$name.conf"
+
+load_rc_config $name
+run_rc_command "$1"
