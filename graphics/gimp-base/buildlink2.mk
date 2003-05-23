43fce2720c09e0dcfd273100fdb32a06069b7d6f
salo <salo>
2003-05-23 18:04:33 +0000

    Updated to 1.2.4.
    Changes:
    
    - DESCR has 80 columns
    - better COMMENT
    - sync buildlink2.mk with reality
    - style nits
    
    1.2.4:
    ======
    - numerous bugfixes, too many to mention.  see ChangeLog for details
    - print plug-in now depends on libgimpprint

--- graphics/gimp-base/buildlink2.mk
+++ graphics/gimp-base/buildlink2.mk
@@ -1,20 +1,30 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/09/22 21:49:48 wiz Exp $
+# $NetBSD$
+#
+# This Makefile fragment is included by packages that use gimp-base.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
 
 .if !defined(GIMP_BASE_BUILDLINK2_MK)
 GIMP_BASE_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=		gimp-base
-BUILDLINK_DEPENDS.gimp-base?=	gimp-base>=1.2.3nb5
-BUILDLINK_PKGSRCDIR.gimp-base?=	../../graphics/gimp-base
-DEPENDS+=		${BUILDLINK_DEPENDS.gimp-base}:../../graphics/gimp-base
+BUILDLINK_PACKAGES+=			gimp-base
+BUILDLINK_DEPENDS.gimp-base?=		gimp-base>=1.2.4
+BUILDLINK_PKGSRCDIR.gimp-base?=		../../graphics/gimp-base
 
-EVAL_PREFIX+=		BUILDLINK_PREFIX.gimp-base=gimp-base
-BUILDLINK_PREFIX.gimp-base_DEFAULT=	${X11PREFIX}
-BUILDLINK_FILES.gimp-base=	include/libgimp/*
-BUILDLINK_FILES.gimp-base+=	lib/libgimp.*
-BUILDLINK_FILES.gimp-base+=	lib/libgimpui.*
+EVAL_PREFIX+=	BUILDLINK_PREFIX.gimp-base=gimp-base
+BUILDLINK_PREFIX.gimp-base_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.gimp-base+=	include/gck/*
+BUILDLINK_FILES.gimp-base+=	include/libgimp/*
+BUILDLINK_FILES.gimp-base+=	lib/libgck*
+BUILDLINK_FILES.gimp-base+=	lib/libgimp*
 
-.include "../../devel/glib/buildlink2.mk"
+.include "../../devel/gettext-lib/buildlink2.mk"
+.include "../../graphics/jpeg/buildlink2.mk"
+.include "../../graphics/mpeg-lib/buildlink2.mk"
+.include "../../graphics/png/buildlink2.mk"
+.include "../../graphics/tiff/buildlink2.mk"
+.include "../../graphics/xpm/buildlink2.mk"
 .include "../../x11/gtk/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gimp-base-buildlink
