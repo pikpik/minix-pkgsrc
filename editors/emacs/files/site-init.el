edc720b038490c5105adc41816ab940708e9b3fc
epg <epg>
2003-06-02 08:12:25 +0000

    Don't add /usr/share/info to Info-default-directory-list; emacs has had that by default for quite some time.

--- editors/emacs/files/site-init.el
+++ editors/emacs/files/site-init.el
@@ -1,9 +1,4 @@
-;; $NetBSD: site-init.el,v 1.1 2002/05/02 20:21:31 drochner Exp $
-
-;; NetBSD puts info files in /usr/share.
-(setq Info-default-directory-list
-      (cons "/usr/share/info/"
-	    Info-default-directory-list))
+;; $NetBSD: site-init.el,v 1.2 2003/04/12 10:16:39 uebayasi Exp $
 
 (autoload 'send-pr "/usr/share/gnats/send-pr.el" 
   "Command to create and send a problem report." t)
