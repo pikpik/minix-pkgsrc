f7b05387ded5f51fafa2ee368dcb33c3ab2950f3
wiz <wiz>
2003-06-05 20:53:23 +0000

    Add test target; depends on fftw2. Remove bogus fftw[3] dependency. fftw dependency also not needed in buildlink2.mk, so remove it there too and bump PKGREVISION.

--- audio/libsamplerate/buildlink2.mk
+++ audio/libsamplerate/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/12/14 18:23:16 jmmv Exp $
 #
 # This Makefile fragment is included by packages that use libsamplerate.
 #
@@ -9,7 +9,7 @@
 LIBSAMPLERATE_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			libsamplerate
-BUILDLINK_DEPENDS.libsamplerate?=	libsamplerate>=0.0.13
+BUILDLINK_DEPENDS.libsamplerate?=	libsamplerate>=0.0.13nb1
 BUILDLINK_PKGSRCDIR.libsamplerate?=	../../audio/libsamplerate
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libsamplerate=libsamplerate
@@ -19,7 +19,6 @@ BUILDLINK_FILES.libsamplerate+=	lib/libsamplerate.*
 BUILDLINK_FILES.libsamplerate+=	lib/pkgconfig/samplerate.pc
 
 .include "../../audio/libsndfile/buildlink2.mk"
-.include "../../math/fftw/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libsamplerate-buildlink
 
