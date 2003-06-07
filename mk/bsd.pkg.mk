d57e0bf30925d9989b3e51d3af4f62864d45e902
abs <abs>
2003-06-07 09:22:37 +0000

    if CC is defined, add it to MAKE_ENV

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1190 2003/06/05 17:20:04 jmmv Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1191 2003/06/06 19:30:47 drochner Exp $
 #
 # This file is in the public domain.
 #
@@ -301,6 +301,9 @@ MAKE_ENV+=		PREFIX=${PREFIX} LOCALBASE=${LOCALBASE}
 MAKE_ENV+=		X11BASE=${X11BASE} CFLAGS="${CFLAGS}"
 MAKE_ENV+=		CPPFLAGS="${CPPFLAGS}" FFLAGS="${FFLAGS}"
 MAKE_ENV+=		X11PREFIX=${X11PREFIX}
+.if defined(CC)
+MAKE_ENV+=		CC="${CC}"
+.endif
 
 .if exists(${ZOULARISBASE}/bin/ftp)			# Zoularis
 FETCH_CMD?=		${ZOULARISBASE}/bin/ftp
