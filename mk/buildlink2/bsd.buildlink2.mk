ab2234bf0a9d3f82ef83f67c2d040bdf2eae5f41
jschauma <jschauma>
2003-06-19 17:20:42 +0000

    Apply patch by Joachim Koenig-Baltes (joachim at handshake dot de) in followup to PR pkg/20529:
    Some libraries (in this example, a given Linux's native libpthread) don't
    come with a version number at all and/or are in fact symlinks.  Test for
    this and act accordingly in fake-la.
    
    This allows packages that use pthread to buildlink under various Linuxen.
    
    Should close PR pkg/20529 and PR pkg/21854.  Thanks, Joachim.

--- mk/buildlink2/bsd.buildlink2.mk
+++ mk/buildlink2/bsd.buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.buildlink2.mk,v 1.83 2003/05/02 08:32:26 agc Exp $
+# $NetBSD: bsd.buildlink2.mk,v 1.84 2003/05/06 17:42:02 jmmv Exp $
 #
 # An example package buildlink2.mk file:
 #
@@ -934,6 +934,7 @@ ${_BLNK_FAKE_LA}: ${.CURDIR}/../../mk/buildlink2/fake-la
 		-e "s|@BUILDLINK_DIR@|${BUILDLINK_DIR}|g"		\
 		-e "s|@BUILDLINK_X11_DIR@|${BUILDLINK_X11_DIR}|g"	\
 		-e "s|@BUILDLINK_SHELL@|${BUILDLINK_SHELL}|g"		\
+		-e "s|@AWK@|${AWK:Q}|g"					\
 		-e "s|@BASENAME@|${BASENAME:Q}|g"			\
 		-e "s|@CC@|${BUILDLINK_CC:Q}|g"				\
 		-e "s|@CP@|${CP:Q}|g"					\
