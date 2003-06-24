d57572f9b10c1dcbed26d25f8f98f5154fbdd03f
jschauma <jschauma>
2003-06-24 02:34:55 +0000

    If included, set F77 and PKG_FC to this g77 as well.

--- lang/gcc3/files/gcc3.mk
+++ lang/gcc3/files/gcc3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: gcc3.mk,v 1.2 2003/04/14 04:30:22 salo Exp $
+# $NetBSD: gcc3.mk,v 1.3 2003/05/27 07:12:34 grant Exp $
 #
 # make configuration file for @PKGNAME_NOREV@
 
@@ -6,3 +6,5 @@ USE_GCC3=	# defined
 CC=		@GCC_PREFIX@/bin/cc
 CPP=		@GCC_PREFIX@/bin/cpp
 CXX=		@GCC_PREFIX@/bin/c++
+F77=		@GCC_PREFIX@/bin/g77
+PKG_FC=		@GCC_PREFIX@/bin/g77
