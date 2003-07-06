7ee6069a46dd793b5c383b5142a183ee3cf0da31
drochner <drochner>
2003-07-06 12:15:13 +0000

    Split the gcc3 pkg into per-language pieces. Needs some cleanup and integration; eventually lang/gcc3 can turn into a meta-pkg.

---
+++ lang/gcc3-c++/files/gcc3.mk
@@ -0,0 +1,8 @@
+# $NetBSD: gcc3.mk,v 1.5 2003/07/03 11:58:05 drochner Exp $
+#
+# make configuration file for @PKGNAME_NOREV@
+
+USE_GCC3=	# defined
+CC=		@GCC_PREFIX@/bin/cc
+CPP=		@GCC_PREFIX@/bin/cpp
+CXX=		@GCC_PREFIX@/bin/c++
---
+++ lang/gcc3-c/files/gcc3.mk
@@ -0,0 +1,10 @@
+# $NetBSD: gcc3.mk,v 1.6 2003/07/03 15:56:38 drochner Exp $
+#
+# make configuration file for @PKGNAME_NOREV@
+
+USE_GCC3=	# defined
+CC=		@GCC_PREFIX@/bin/cc
+CPP=		@GCC_PREFIX@/bin/cpp
+CXX=		@GCC_PREFIX@/bin/c++
+F77=		@GCC_PREFIX@/bin/g77
+PKG_FC=		@GCC_PREFIX@/bin/g77
