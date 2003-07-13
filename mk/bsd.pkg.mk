87db732ea09b3dec604ad09c33eece05b707a9b3
grant <grant>
2003-07-13 13:10:43 +0000

    don't set USE_GCC2 by default as it implies the use pkgsrc gcc2, and move handling of gcc shared libraries to bsd.prefs.mk.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1213 2003/07/12 15:34:37 wiz Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1214 2003/07/13 06:44:02 grant Exp $
 #
 # This file is in the public domain.
 #
@@ -217,12 +217,6 @@ CONFIGURE_ENV+=	F77="${F77}"
 CONFIGURE_ENV+=	FFLAGS="${FFLAGS}"
 MAKE_ENV+=	F77="${F77}"
 MAKE_ENV+=	FC="${FC}"
-.endif
-
-# Ensure the correct rpath is passed to the linker to enable packages
-# to find shared libraries from gcc.
-.if defined(USE_GCC_SHLIB)
-.  include "../../mk/gcc.buildlink2.mk"
 .endif
 
 # Automatically increase process limit where necessary for building.
