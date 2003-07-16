c35cf7a9409effec31eb16df0d7d4098d15cded3
drochner <drochner>
2003-07-16 20:24:00 +0000

    elaborate the INSTALLTO_SUBPREFIX thing a bit more: modify the PKGNAME if we are installing to an alternative location so that the pkg system doesn't complain about conflicts. Now it is possible to install a gcc3 tree to some location where it can be used to bootstrap another instance of itself...

--- lang/gcc3-c/buildaddon.mk
+++ lang/gcc3-c/buildaddon.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildaddon.mk,v 1.1 2003/07/08 15:04:56 drochner Exp $
+# $NetBSD: buildaddon.mk,v 1.2 2003/07/09 15:11:21 drochner Exp $
 
 .include "../../lang/gcc3-c/Makefile.common"
 
@@ -7,7 +7,7 @@ pre-build:
 	${LN} -sf ${GCC_PREFIX}/lib/libiberty.a ${WRKSRC}/libiberty
 	${LN} -sf ${GCC_PREFIX}/bin/gcc ${WRKSRC}/gcc/xgcc
 
-BUILDLINK_DEPENDS.gcc3c?=	gcc3-c-3.3
+BUILDLINK_DEPENDS.gcc3c?=	gcc3${GCC3_PKGMODIF}-c-3.3
 BUILDLINK_PKGSRCDIR.gcc3c?=	../../lang/gcc3-c
 
 BUILDLINK_PREFIX.gcc3c=	${LOCALBASE}
