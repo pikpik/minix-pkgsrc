4cbd738068463e48663c843e921c45395a080cf7
jmmv <jmmv>
2003-08-03 15:47:46 +0000

    Initial import of beecrypt, version 2.1.0:
    BeeCrypt is an open source cryptography library that contains highly
    optimized C and assembler implementations of many well-known algorithms
    including Blowfish, MD5, SHA-1, Diffie-Hellman, and ElGamal.
    
    Unlike some other crypto libraries, BeeCrypt is not designed to solve
    one specific problem, like file encryption, but to be a general purpose
    toolkit which can be used in a variety of applications.
    
    There are also no patent or royalty issues associated with BeeCrypt, and
    it is released under the GNU LGPL license, which means it can used for
    free in both open source and closed source commercial projects.

---
+++ security/beecrypt/buildlink2.mk
@@ -0,0 +1,24 @@
+# $NetBSD$
+#
+# This Makefile fragment is included by packages that use beecrypt.
+#
+# This file was created automatically using createbuildlink 2.6.
+#
+
+.if !defined(BEECRYPT_BUILDLINK2_MK)
+BEECRYPT_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			beecrypt
+BUILDLINK_DEPENDS.beecrypt?=		beecrypt>=2.1.0
+BUILDLINK_PKGSRCDIR.beecrypt?=		../../devel/beecrypt
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.beecrypt=beecrypt
+BUILDLINK_PREFIX.beecrypt_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.beecrypt+=	include/beecrypt/*
+BUILDLINK_FILES.beecrypt+=	lib/libbeecrypt.*
+
+BUILDLINK_TARGETS+=	beecrypt-buildlink
+
+beecrypt-buildlink: _BUILDLINK_USE
+
+.endif	# BEECRYPT_BUILDLINK2_MK
