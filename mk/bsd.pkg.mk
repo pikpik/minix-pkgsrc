8e3f20e48eba06f4496ebff2fa3c3dd616043fc4
jlam <jlam>
2003-08-23 03:57:49 +0000

    Fix a long-standing past-o that was harmless.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1240 2003/08/22 13:32:40 grant Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1241 2003/08/22 22:04:03 seb Exp $
 #
 # This file is in the public domain.
 #
@@ -1373,7 +1373,7 @@ tools: patch
 
 # Disable buildlink
 .PHONY: buildlink
-.if defined(NO_BUILDLINK) && !target(configure)
+.if defined(NO_BUILDLINK) && !target(buildlink)
 buildlink: tools
 	${_PKG_SILENT}${_PKG_DEBUG}${TOUCH} ${TOUCH_FLAGS} ${BUILDLINK_COOKIE}
 .endif
