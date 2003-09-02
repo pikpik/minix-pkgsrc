866878addf3a89dbedd8cfe0a82dc9e708872aec
jlam <jlam>
2003-09-02 07:14:49 +0000

    Update pkgtools/x11-links to 0.13.  This package now uses buildlink3 to build.  I'm bumping the version number just in case the installed package is different from version 0.12 (I can't test on different systems).

---
+++ pkgtools/x11-links/xfree.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+FILES_LIST=	${FILESDIR}/xfree
+
+X11_MODULES=	MesaLib Xrender freetype2 xpm
+
+.for _module_ in ${X11_MODULES}
+BUILDLINK_CHECK_BUILTIN.${_module_}=	YES
+.endfor
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/xpm/buildlink3.mk"
+.include "../../x11/Xrender/buildlink3.mk"
+
+.for _module_ in ${X11_MODULES}
+.  if !empty(BUILDLINK_IS_BUILTIN.${_module_}:M[yY][eE][sS])
+FILES_LIST+=	${FILESDIR}/xfree.${_module_}
+.  endif
+.endfor
