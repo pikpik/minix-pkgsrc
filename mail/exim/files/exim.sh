bede4b28fd6d5246e748ee0c3bcc015a84721562
abs <abs>
2003-09-02 10:34:30 +0000

    Update exim to 4.22. Too many changes to list since 4.10 Package changes include addition of Makefile.common (to be used by Upcoming exim-exiscan-acl package), and exim.8

--- mail/exim/files/exim.sh
+++ mail/exim/files/exim.sh
@@ -1,18 +1,17 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-#	$NetBSD: exim,v 1.1 2001/06/26 00:21:47 wiz Exp $
+#	$NetBSD: exim.sh,v 1.4 2003/08/02 17:00:09 jmmv Exp $
 #
 # PROVIDE: mail
 # REQUIRE: LOGIN
-#
 
 . /etc/rc.subr
 
-name="exim"                      
-rcvar=$name         
+name="exim"
+rcvar=$name
 command="@PREFIX@/sbin/${name}"
 pidfile="/var/run/${name}.pid"
 required_files="@PKG_SYSCONFDIR@/configure"
-              
+
 load_rc_config $name
 run_rc_command "$1"
