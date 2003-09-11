8ac82ff979511c28dcc8b2f861faee3b42070ec7
jlam <jlam>
2003-09-11 04:13:05 +0000

    Garbage-collect USE_OPENSSL_VERSION now that openssl/buildlink2.mk supports just setting BUILDLINK_DEPENDS.openssl.  USE_OPENSSL_VERSION wasn't actually needed here anyway since the minimum version allowed by openssl/buildlink2.mk exceeded the version requested here.

--- x11/kdelibs3/buildlink2.mk
+++ x11/kdelibs3/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.19 2003/08/04 21:48:02 drochner Exp $
+# $NetBSD: buildlink2.mk,v 1.20 2003/08/05 13:46:58 drochner Exp $
 
 .if !defined(KDELIBS3_BUILDLINK2_MK)
 KDELIBS3_BUILDLINK2_MK=	# defined
@@ -18,7 +18,6 @@ BUILDLINK_FILES_CMD.kdelibs3= \
 KDEDIR=				${BUILDLINK_PREFIX.kdelibs3}
 
 BUILDLINK_DEPENDS.audiofile=	libaudiofile>=0.2.3nb1
-USE_OPENSSL_VERSION=		${OPENSSL_VERSION_096}
 
 .include "../../audio/libaudiofile/buildlink2.mk"
 .include "../../audio/arts/buildlink2.mk"
