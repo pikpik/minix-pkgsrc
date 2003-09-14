4d1f1ddf39cbc50c3e1eca352a0c7c6276771e52
jlam <jlam>
2003-09-14 05:02:50 +0000

    Update PKGTOOLS_REQD to 20030914.  This version fixes a problem in pkg_delete(1) where recursive pkg_delete'ing didn't work if ${PKG_DBDIR} != /var/db/pkg.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1280 2003/09/13 14:40:40 jlam Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1281 2003/09/13 18:26:15 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -706,7 +706,7 @@ uptodate-digest:
 .if defined(_OPSYS_PKGTOOLS_REQD)
 PKGTOOLS_REQD=		${_OPSYS_PKGTOOLS_REQD}
 .else
-PKGTOOLS_REQD=		20030912
+PKGTOOLS_REQD=		20030914
 .endif
 
 # Check that we are using up-to-date pkg_* tools with this file.
