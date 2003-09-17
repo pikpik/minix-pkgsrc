8a5c26782c1524bbe7147533b843239a98d077e9
markd <markd>
2003-09-17 23:01:47 +0000

    Update arts to 1.1.4 (for KDE3.1.4)
    Changes:
    * Fix artsbuilder crash with changed construction order in gcc 3.3.2 release.
    * Fix stereo volume control.
    * Fix compilation problems.
    * Crash fixes.

--- audio/arts/buildlink2.mk
+++ audio/arts/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.11 2003/07/30 13:01:28 markd Exp $
+# $NetBSD: buildlink2.mk,v 1.12 2003/08/05 12:22:30 markd Exp $
 #
 # This Makefile fragment is included by packages that use arts.
 
@@ -60,7 +60,6 @@ BUILDLINK_FILES.arts+=	lib/mcop/x11globalcomm.mcoptype
 .include "../../audio/libaudiofile/buildlink2.mk"
 .include "../../audio/libogg/buildlink2.mk"
 .include "../../audio/libvorbis/buildlink2.mk"
-#.include "../../x11/kde3/buildlink2.mk"
 .include "../../mk/ossaudio.buildlink2.mk"
 
 BUILDLINK_TARGETS+=	arts-buildlink
