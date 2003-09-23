b9d05f3b7cffde08ca6bbcd71e59c88ecadc889d
yyamano <yyamano>
2003-09-23 09:58:12 +0000

    Mark curses on Darwin as incompatible. Close PR pkg/21649 by Michael Wolfson.

--- devel/ncurses/buildlink2.mk
+++ devel/ncurses/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.7 2003/03/10 03:27:59 jschauma Exp $
+# $NetBSD: buildlink2.mk,v 1.8 2003/07/13 13:51:22 wiz Exp $
 #
 # Optionally define:
 #
@@ -31,10 +31,10 @@ _INCOMPAT_CURSES+=      NetBSD-1.4.*-* NetBSD-1.4[A-X]-*
 #
 _INCOMPAT_CURSES+=	SunOS-*-*
 #
-# Similarly for IRIX
+# Similarly for IRIX and Darwin
 #
 _INCOMPAT_CURSES+=      IRIX-*-*
-
+_INCOMPAT_CURSES+=     	Darwin-*-* 
 INCOMPAT_CURSES?=	# empty
 .  for _pattern_ in ${_INCOMPAT_CURSES} ${INCOMPAT_CURSES}
 .    if !empty(MACHINE_PLATFORM:M${_pattern_})
