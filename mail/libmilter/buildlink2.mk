f7489d0264fe69a667640f44ce3a9ee7cfe4edd1
markd <markd>
2003-09-24 12:43:57 +0000

    Allow use of base system libmilter if it exists and is complete enough.
    Closes PR 22708 from wiz.

--- mail/libmilter/buildlink2.mk
+++ mail/libmilter/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2002/09/23 13:03:31 markd Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2003/05/02 11:55:39 wiz Exp $
 #
 # This Makefile fragment is included by packages that use libmilter.
 #
@@ -6,12 +6,30 @@
 .if !defined(LIBMILTER_BUILDLINK2_MK)
 LIBMILTER_BUILDLINK2_MK=	# defined
 
-BUILDLINK_PACKAGES+=			libmilter
+.include "../../mk/bsd.prefs.mk"
+
 BUILDLINK_DEPENDS.libmilter?=		libmilter>=8.12.9nb1
 BUILDLINK_PKGSRCDIR.libmilter?=		../../mail/libmilter
 
+.if exists(/usr/lib/libmilter.a)
+_MILTER_FILELIST!= ar t /usr/lib/libmilter.a
+.  if empty(_MILTER_FILELIST:Merrstring.o)
+_NEED_LIBMILTER=	YES
+.  else
+_NEED_LIBMILTER=	NO
+.  endif
+.else
+_NEED_LIBMILTER=	YES
+.endif
+
+.if ${_NEED_LIBMILTER} == "YES"
+BUILDLINK_PACKAGES+=		libmilter
 EVAL_PREFIX+=	BUILDLINK_PREFIX.libmilter=libmilter
 BUILDLINK_PREFIX.libmilter_DEFAULT=	${LOCALBASE}
+.else
+BUILDLINK_PREFIX.libmilter=	/usr
+.endif
+
 BUILDLINK_FILES.libmilter+=	include/libmilter/mfapi.h
 BUILDLINK_FILES.libmilter+=	include/libmilter/mfdef.h
 BUILDLINK_FILES.libmilter+=	lib/libmilter.*
