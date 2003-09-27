eb83339f49edf2d0137da5525f7e2e81632c8c4c
grant <grant>
2003-09-27 14:13:08 +0000

    rename phoenix-bin-* to MozillaFirebird-bin-*.

--- www/phoenix-bin/files/phoenix.sh
+++
@@ -1,7 +0,0 @@
-#!@SH@
-# $NetBSD: phoenix.sh,v 1.3 2003/05/18 03:43:02 grant Exp $
-
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@PREFIX@/lib/MozillaFirebird-@PLATFORM@:@PREFIX@/lib"
-export LD_LIBRARY_PATH
-
-exec @PREFIX@/lib/MozillaFirebird-@PLATFORM@/MozillaFirebird "$@"
