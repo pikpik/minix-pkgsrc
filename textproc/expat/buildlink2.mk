df94b9092a935d277ae35c26e774ab87da9729e1
grant <grant>
2003-10-11 07:10:52 +0000

    expat >= 1.95.4 provide libexpat.4 which other packages need, so bump BUILDLINK_DEPENDS accordingly.
    fixes build problems observed by a couple of users on tech-pkg,
    well as fixing PR pkg/21325.

--- textproc/expat/buildlink2.mk
+++ textproc/expat/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.3 2002/06/21 23:00:39 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:40:01 jlam Exp $
 
 .if !defined(EXPAT_BUILDLINK2_MK)
 EXPAT_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		expat
-BUILDLINK_DEPENDS.expat?=	expat>=1.95.2
+BUILDLINK_DEPENDS.expat?=	expat>=1.95.4
 BUILDLINK_PKGSRCDIR.expat?=	../../textproc/expat
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.expat=expat
