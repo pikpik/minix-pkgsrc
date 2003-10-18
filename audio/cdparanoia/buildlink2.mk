cd04a6685cb84a7a0c5fb75ffb1242672513a965
grant <grant>
2003-10-18 11:53:48 +0000

    as this is ONLY_FOR_PLATFORM Linux and NetBSD, don't try to buildlink it if we're on a different platform.

--- audio/cdparanoia/buildlink2.mk
+++ audio/cdparanoia/buildlink2.mk
@@ -1,8 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:03:53 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:21:46 jlam Exp $
 
 .if !defined(CDPARANOIA_BUILDLINK2_MK)
 CDPARANOIA_BUILDLINK2_MK=	# defined
 
+.include "../../mk/bsd.prefs.mk"
+
+.if ${OPSYS} == "Linux" || ${OPSYS} == "NetBSD"
 BUILDLINK_PACKAGES+=			cdparanoia
 BUILDLINK_DEPENDS.cdparanoia?=		cdparanoia>=3.0
 BUILDLINK_PKGSRCDIR.cdparanoia?=	../../audio/cdparanoia
@@ -16,5 +19,6 @@ BUILDLINK_FILES.cdparanoia+=	lib/libcdda_interface.*
 BUILDLINK_TARGETS+=	cdparanoia-buildlink
 
 cdparanoia-buildlink: _BUILDLINK_USE
+.endif	# OPSYS == Linux || NetBSD
 
 .endif	# CDPARANOIA_BUILDLINK2_MK
