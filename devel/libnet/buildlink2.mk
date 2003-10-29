5ec403a51ffcb8e76023fe022b97a1251b0d3ea8
cube <cube>
2003-10-29 18:39:45 +0000

    Bump PKGREVISION and requirement in buildlink2.mk, since last gcc3-related fix in an installed header file.

--- devel/libnet/buildlink2.mk
+++ devel/libnet/buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD: buildlink2.mk,v 1.1.2.1 2002/06/23 23:04:13 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 18:38:32 jlam Exp $
 
 .if !defined(LIBNET_BUILDLINK2_MK)
 LIBNET_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		libnet
-BUILDLINK_DEPENDS.libnet?=	libnet-[0-9]*
+BUILDLINK_DEPENDS.libnet?=	libnet>=1.0.1bnb3
 BUILDLINK_PKGSRCDIR.libnet?=	../../devel/libnet
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.libnet=libnet
