6a0f3e3ecc48dd5e60942954f317633bfaf34ce9
jlam <jlam>
2003-10-29 23:02:34 +0000

    Sync pkgtools/pkg_install with src/usr.sbin/pkg_install from 20031003. New features include fetching binary packages using HTTP (thanks wiz!) and some minor speed optimizations for pkg_delete.

--- pkgtools/pkg_install/files/lib/fexec.c
+++ pkgtools/pkg_install/files/lib/fexec.c
@@ -65,7 +65,7 @@
 #include "lib.h"
 
 #ifndef lint
-__RCSID("$NetBSD: fexec.c,v 1.3 2003/09/23 07:13:52 grant Exp $");
+__RCSID("$NetBSD: fexec.c,v 1.7 2003/09/23 15:07:43 christos Exp $");
 #endif
 
 static int	vfcexec(const char *, int, const char *, va_list);
@@ -82,7 +82,7 @@ vfcexec(const char *path, int skipempty, const char *arg, va_list ap)
 	if (argv == NULL) {
 		argv = malloc(max * sizeof(const char *));
 		if (argv == NULL) {
-			warnx("vfcexec can't alloc arg space");
+			warn("vfcexec: Can't alloc arg space");
 			return -1;
 		}
 	}
@@ -98,8 +98,10 @@ vfcexec(const char *path, int skipempty, const char *arg, va_list ap)
 			new = max * 2;
 			ptr = realloc(argv, new * sizeof(const char *));
 			if (ptr == NULL) {
-				warnx("vfcexec can't alloc arg space");
+				warn("vfcexec: Can't alloc arg space");
 				free(argv);
+				argv = NULL;
+				max = 4;
 				return -1;
 			}
 			argv = ptr;
