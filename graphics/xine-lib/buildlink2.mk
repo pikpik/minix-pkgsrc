426ddfa576dade4e219c2f4b1ea98c9771719b23
reed <reed>
2003-11-09 00:52:46 +0000

    The xine-lib plugins (libraries) are built with libfreetype.so. So add freetype2 buildlink2.mk so it will be used in build. Noticed problem when building xine-ui. (Okay'd by wiz.)

--- graphics/xine-lib/buildlink2.mk
+++ graphics/xine-lib/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.6 2003/07/26 21:41:27 jmmv Exp $
+# $NetBSD: buildlink2.mk,v 1.7 2003/09/05 09:58:36 drochner Exp $
 #
 # This Makefile fragment is included by packages that use xine-lib.
 #
@@ -107,6 +107,7 @@ BUILDLINK_FILES.xine-lib+=	lib/xine/plugins/1.0.0/xineplug_vo_out_xv.*
 .include "../../audio/libvorbis/buildlink2.mk"
 .include "../../graphics/MesaLib/buildlink2.mk"
 .include "../../graphics/aalib/buildlink2.mk"
+.include "../../graphics/freetype2/buildlink2.mk"
 .include "../../graphics/mng/buildlink2.mk"
 .include "../../graphics/png/buildlink2.mk"
 
