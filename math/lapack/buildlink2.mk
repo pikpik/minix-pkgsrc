ef083865565c0e7f016d629b3188ae4d8042c517
adam <adam>
2003-12-04 11:27:16 +0000

    buildlink fix

--- math/lapack/buildlink2.mk
+++ math/lapack/buildlink2.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:07 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.3 2003/06/12 19:08:34 wiz Exp $
 
 .if !defined(LAPACK_BUILDLINK2_MK)
 LAPACK_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		lapack
-BUILDLINK_DEPENDS.lapack?=	lapack>=20010201
+BUILDLINK_DEPENDS.lapack?=	lapack>=20010201nb1
 BUILDLINK_PKGSRCDIR.lapack?=	../../math/lapack
-BUILDLINK_DEPMETHOD.lapack?=	build
 
 EVAL_PREFIX+=			BUILDLINK_PREFIX.lapack=lapack
 BUILDLINK_PREFIX.lapack_DEFAULT=	${LOCALBASE}
