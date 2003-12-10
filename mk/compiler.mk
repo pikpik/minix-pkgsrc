4627526fe782d37574983f130339a2c28a0cdbdb
sketch <sketch>
2003-12-10 09:08:24 +0000

    Ensure CPP is defined to use the Sun compiler when building packages under USE_SUNPRO, to avoid the possibility of picking up "cpp" from a local GCC installation.

--- mk/compiler.mk
+++ mk/compiler.mk
@@ -1,4 +1,4 @@
-# $NetBSD: compiler.mk,v 1.21 2003/10/18 09:14:47 grant Exp $
+# $NetBSD: compiler.mk,v 1.23 2003/11/12 13:13:43 grant Exp $
 
 # This Makefile fragment implements handling for supported
 # C/C++/fortran compilers.
@@ -65,6 +65,7 @@ COMPILER_MK=	# defined
 # command line.
 .if defined(USE_SUNPRO) && defined(CC) && !empty(CC:Mgcc)
 CC=	/opt/SUNWspro/bin/cc
+CPP=	/opt/SUNWspro/bin/cc -E
 CXX=	/opt/SUNWspro/bin/CC
 .endif
 
