20406754b70e5766cc76019625b98f66f2cec96e
recht <recht>
2003-12-16 08:51:42 +0000

    initial import of xcursor 1.1.1 based upon the pkgsrc-wip pkg by xtraeme@
    This package, libXcursor, provides the client-side cursor loading
    library for X.  This cursor management library is a simple library
    designed to help locate and load cursors (from files or memory).
    
    This is part of the X Libraries and Protocol Headers Project hosted
    at freedesktop.org.

---
+++ x11/xcursor/buildlink2.mk
@@ -0,0 +1,24 @@
+# $NetBSD$
+
+.if !defined(XCURSOR_BUILDLINK2_MK)
+XCURSOR_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			xcursor
+BUILDLINK_DEPENDS.xcursor?=		xcursor>=1.1.1
+BUILDLINK_PKGSRCDIR.xcursor?=		../../x11/xcursor
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.xcursor=xcursor
+BUILDLINK_PREFIX.xcursor_DEFAULT=	${LOCALBASE}
+BUILDLINK_FILES.xcursor+=	include/X11/Xcursor/Xcursor.h
+BUILDLINK_FILES.xcursor+=	lib/libXcursor.*
+
+USE_X11=	yes
+
+.include "../../x11/Xfixes/buildlink2.mk"
+.include "../../x11/Xrender/buildlink2.mk"
+
+BUILDLINK_TARGETS+=	xcursor-buildlink
+
+xcursor-buildlink: _BUILDLINK_USE
+
+.endif	# XCURSOR_BUILDLINK2_MK
