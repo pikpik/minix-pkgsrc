0728701a93c68a5786c939762c6434f80bf83a3d
jmmv <jmmv>
2003-12-24 12:51:32 +0000

    Update teTeX packages to 2.0.2 (really merge teTeX2 into teTeX).  The 2.x branch becomes the default teTeX package.

--- print/teTeX-bin/kpathsea.buildlink2.mk
+++ print/teTeX-bin/kpathsea.buildlink2.mk
@@ -1,10 +1,10 @@
-# $NetBSD$
+# $NetBSD: kpathsea.buildlink2.mk,v 1.1 2002/09/21 01:50:37 jlam Exp $
 
 .if !defined(KPATHSEA_BUILDLINK2_MK)
 KPATHSEA_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=		kpathsea
-BUILDLINK_DEPENDS.kpathsea?=	teTeX-bin>=1.0.7nb2
+BUILDLINK_DEPENDS.kpathsea?=	teTeX-bin>=2.0.2
 BUILDLINK_PKGSRCDIR.kpathsea?=	../../print/teTeX-bin
 BUILDLINK_DEPMETHOD.kpathsea?=	build
 
