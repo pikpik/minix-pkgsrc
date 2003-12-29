e5062ca7a4cff99b17e297301bac666bdb7b38ab
xtraeme <xtraeme>
2003-12-29 00:02:47 +0000

    Update to 2.4.1, thanks to Min Sik Kim and Soren Jacobsen for doing this.
    Changes have been several translation updates and some bugs fixed, please
    review Changelog file to see a full list of changes.

--- games/gnome2-games/buildlink2.mk
+++ games/gnome2-games/buildlink2.mk
@@ -1,28 +1,30 @@
-# $NetBSD: buildlink2.mk,v 1.5 2003/09/28 09:13:57 jlam Exp $
+# $NetBSD$
 #
 # This Makefile fragment is included by packages that use gnome-games.
 #
-# This file was created automatically using createbuildlink 2.4.
+# This file was created automatically using createbuildlink 2.8.
 #
 
 .if !defined(GNOME_GAMES_BUILDLINK2_MK)
 GNOME_GAMES_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gnome-games
-BUILDLINK_DEPENDS.gnome-games?=		gnome-games>=2.2.1nb6
-BUILDLINK_PKGSRCDIR.gnome-games?=		../../games/gnome-games
+BUILDLINK_DEPENDS.gnome-games?=		gnome-games>=2.4.1
+BUILDLINK_PKGSRCDIR.gnome-games?=		../../gnome-2.4/gnome2-games
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gnome-games=gnome-games
-BUILDLINK_PREFIX.gnome-games_DEFAULT=	${X11PREFIX}
+BUILDLINK_PREFIX.gnome-games_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.gnome-games+=	include/gdkcardimage/gdk-card-image.h
 BUILDLINK_FILES.gnome-games+=	lib/gnome-stones/objects/libgnome-stones.*
 BUILDLINK_FILES.gnome-games+=	lib/gnome-stones/objects/libgnomekoban.*
 BUILDLINK_FILES.gnome-games+=	lib/libgdkcardimage.*
 
+.include "../../devel/GConf2-ui/buildlink2.mk"
 .include "../../devel/gettext-lib/buildlink2.mk"
-.include "../../devel/ncurses/buildlink2.mk"
-.include "../../lang/guile14/buildlink2.mk"
-.include "../../x11/gnome-core/buildlink2.mk"
+.include "../../devel/libgnome/buildlink2.mk"
+.include "../../devel/libgnomeui/buildlink2.mk"
+.include "../../sysutils/gnome-vfs2/buildlink2.mk"
+.include "../../lang/guile/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gnome-games-buildlink
 
