f0938e9d07ce92a962a85f716cf83ef47eb1eac1
jlam <jlam>
2004-01-06 04:29:33 +0000

    Fix bug where "!empty" was missing from a .if test.

--- mk/buildlink3/bsd.buildlink3.mk
+++ mk/buildlink3/bsd.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.buildlink3.mk,v 1.36 2004/01/05 11:05:47 jlam Exp $
+# $NetBSD: bsd.buildlink3.mk,v 1.37 2004/01/05 21:50:13 jlam Exp $
 #
 # An example package buildlink3.mk file:
 #
@@ -171,7 +171,7 @@ BUILDLINK_IS_BUILTIN.${_pkg_}?=		no
 # If the "built-in" package (the software supplied by the base operating
 # system) is available, then use it by default.
 #
-.  if (BUILDLINK_IS_BUILTIN.${_pkg_}:M[yY][eE][sS])
+.  if !empty(BUILDLINK_IS_BUILTIN.${_pkg_}:M[yY][eE][sS])
 BUILDLINK_USE_BUILTIN.${_pkg_}?=	yes
 .  else
 BUILDLINK_USE_BUILTIN.${_pkg_}?=	no
