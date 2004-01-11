63d582a7a4f9409bca1d83957a792bc54e111019
jlam <jlam>
2004-01-11 18:47:34 +0000

    Add a buildlink3.mk file for libao.

---
+++ audio/libao/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBAO_BUILDLINK3_MK:=	${LIBAO_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libao
+.endif
+
+.if !empty(LIBAO_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		libao
+BUILDLINK_DEPENDS.libao?=	libao>=0.8.4
+BUILDLINK_PKGSRCDIR.libao?=	../../audio/libao
+
+.  include "../../devel/pkgconfig/buildlink3.mk"
+.endif	# LIBAO_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
