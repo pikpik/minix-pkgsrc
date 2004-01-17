67d8e5367a162a5e2c41c1ab7081eb0b35278285
recht <recht>
2004-01-17 15:25:39 +0000

    initial import of libcddb-0.9.4
    Libcddb is a library that implements the different protocols (CDDBP,
    HTTP, SMTP) to access data on a CDDB server (http://freedb.org).  It
    tries to be as cross-platform as possible.  The initial libary will
    have a C API.

---
+++ audio/libcddb/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBCDDB_BUILDLINK3_MK:=	${LIBCDDB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libcddb
+.endif
+
+.if !empty(LIBCDDB_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libcddb
+BUILDLINK_DEPENDS.libcddb?=		libcddb>=0.9.4
+BUILDLINK_PKGSRCDIR.libcddb?=		../../audio/libcddb.work
+
+.include "../../misc/libcdio/buildlink3.mk"
+
+.endif # LIBCDDB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
