e51a4835b58813daa29f27cbc5433f175c61d3c8
xtraeme <xtraeme>
2004-01-24 09:23:23 +0000

    Added a new variable for XFree86 packages:
    USE_PKGSRC_XFREE86
    
    By default it is disabled, if you want to use XFree86 packages from
    pkgsrc, please add "USE_PKGSRC_XFREE86=YES" in /etc/mk.conf.

--- mk/bsd.pkg.defaults.mk
+++ mk/bsd.pkg.defaults.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.defaults.mk,v 1.207 2004/01/18 18:55:24 epg Exp $
+# $NetBSD: bsd.pkg.defaults.mk,v 1.208 2004/01/22 10:19:58 xtraeme Exp $
 #
 
 # A file providing defaults for pkgsrc and the packages collection.
@@ -2009,6 +2009,11 @@ USE_WNN4?= YES
 # (pkgsrc/mail/face)
 # Possible: not defined, YES
 # Default: not defined
+
+USE_PKGSRC_XFREE86?=	NO
+# Used in the XFree86 packages to install them automatically.
+# Possible: YES, or NO.
+# Default: NO.
 
 USERPPP_GROUP?=	network
 # Used in the userppp package to specify the default group.
