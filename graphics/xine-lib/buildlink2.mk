cf0c6499be801cc59a7a30f33aa7229913269265
tron <tron>
2004-01-24 00:13:50 +0000

    Include buildlink 2 glue code for "libiconv" package to fix build problems in the "xine-arts" package.

--- graphics/xine-lib/buildlink2.mk
+++ graphics/xine-lib/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.8 2003/11/09 00:52:46 reed Exp $
+# $NetBSD: buildlink2.mk,v 1.9 2003/12/08 14:41:40 wiz Exp $
 #
 # This Makefile fragment is included by packages that use xine-lib.
 #
@@ -99,6 +99,7 @@ BUILDLINK_FILES.xine-lib+=	lib/xine/plugins/1.0.0/xineplug_vo_out_syncfb.*
 BUILDLINK_FILES.xine-lib+=	lib/xine/plugins/1.0.0/xineplug_vo_out_xshm.*
 BUILDLINK_FILES.xine-lib+=	lib/xine/plugins/1.0.0/xineplug_vo_out_xv.*
 
+.include "../../converters/libiconv/buildlink2.mk"
 .include "../../devel/gettext-lib/buildlink2.mk"
 .include "../../devel/pkgconfig/buildlink2.mk"
 .include "../../devel/SDL/buildlink2.mk"
