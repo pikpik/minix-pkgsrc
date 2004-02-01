6386b672555d30bb1bfe4cc16a6b5ac55aedeed6
jlam <jlam>
2004-02-01 01:03:22 +0000

    Prepend ${MIPSPROBASE}/bin to the PATH so that the correct compilers are found by ccache and distcc.

--- mk/compiler/mipspro.mk
+++ mk/compiler/mipspro.mk
@@ -1,9 +1,10 @@
-# $NetBSD: mipspro.mk,v 1.2 2004/02/01 00:59:51 jlam Exp $
+# $NetBSD: mipspro.mk,v 1.3 2004/02/01 01:02:37 jlam Exp $
 
 .if !defined(COMPILER_MIPSPRO_MK)
 COMPILER_MIPSPRO_MK=	defined
 
 MIPSPROBASE?=	/usr
+PATH:=		${MIPSPROBASE}/bin:${PATH}
 
 CC=	${MIPSPROBASE}/bin/cc
 CPP=	${MIPSPROBASE}/bin/cc -E
