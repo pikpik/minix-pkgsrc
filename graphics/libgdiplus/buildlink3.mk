9453d979667a249fc03996207de3e30655275a10
recht <recht>
2004-02-03 12:31:35 +0000

    initial import of libgdiplus-0.1
    An Open Source implementation of the GDI+ API. Spun off from Mono's C#
    library.
    libgdiplus is needed for using the System.Drawing APIs.

---
+++ graphics/libgdiplus/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBGDIPLUS_BUILDLINK3_MK:=	${LIBGDIPLUS_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgdiplus
+.endif
+
+.if !empty(LIBGDIPLUS_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libgdiplus
+BUILDLINK_DEPENDS.libgdiplus?=		libgdiplus>=0.1
+BUILDLINK_PKGSRCDIR.libgdiplus?=		../../graphics/libgdiplus
+
+.include "../../graphics/cairo/buildlink3.mk"
+.include "../../lang/mono/buildlink3.mk"
+.include "../../x11/Xrender/buildlink3.mk"
+
+.endif # LIBGDIPLUS_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
