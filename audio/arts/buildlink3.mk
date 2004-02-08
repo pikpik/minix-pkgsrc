10d7d765017e9a5051482cd9f5b20d88d6d4b9d8
tron <tron>
2004-02-08 16:22:37 +0000

    Use buildink *3* and not 2 glue code for "glib2".

--- audio/arts/buildlink3.mk
+++ audio/arts/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/02/04 13:35:51 markd Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/02/06 13:48:05 tron Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ARTS_BUILDLINK3_MK:=	${ARTS_BUILDLINK3_MK}+
@@ -17,7 +17,7 @@ BUILDLINK_PKGSRCDIR.arts?=	../../audio/arts
 .  include "../../audio/libmad/buildlink3.mk"
 .  include "../../audio/libogg/buildlink3.mk"
 .  include "../../audio/libvorbis/buildlink3.mk"
-.  include "../../devel/glib2/buildlink2.mk"
+.  include "../../devel/glib2/buildlink3.mk"
 .  include "../../mk/ossaudio.buildlink3.mk"
 .endif	# ARTS_BUILDLINK3_MK
 
