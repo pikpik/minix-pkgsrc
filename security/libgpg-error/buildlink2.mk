526103088cfe68a9c8bf198581404381c6d4ec27
minskim <minskim>
2004-02-10 05:31:48 +0000

    Add missing gettext-lib/buildlink2.mk.

--- security/libgpg-error/buildlink2.mk
+++ security/libgpg-error/buildlink2.mk
@@ -1,8 +1,6 @@
-# $NetBSD$
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/12/05 23:24:31 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use libgpg-error.
-#
-# This file was created automatically using createbuildlink 2.7.
 #
 
 .if !defined(LIBGPG_ERROR_BUILDLINK2_MK)
@@ -16,6 +14,8 @@ EVAL_PREFIX+=	BUILDLINK_PREFIX.libgpg-error=libgpg-error
 BUILDLINK_PREFIX.libgpg-error_DEFAULT=	${LOCALBASE}
 BUILDLINK_FILES.libgpg-error+=	include/gpg-error.h
 BUILDLINK_FILES.libgpg-error+=	lib/libgpg-error.*
+
+.include "../../devel/gettext-lib/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	libgpg-error-buildlink
 
