76ccff942e305389bdacb7f346529548fcc6770e
jlam <jlam>
2004-02-10 00:13:24 +0000

    bl3ify and remove libiconv and gettext-lib buildlink files.  This package really only needs gettext-lib through libgpg-error, and doesn't need libiconv at all unless it uses the pkgsrc gettext-lib.  The gettext-lib buildlink3.mk file combined with the buildlink3 framework is considerably better at detecting this than the buildlink2.mk, which broke in a lot of instances.

---
+++ security/libgcrypt/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBGCRYPT_BUILDLINK3_MK:=	${LIBGCRYPT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgcrypt
+.endif
+
+.if !empty(LIBGCRYPT_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=		libgcrypt
+BUILDLINK_DEPENDS.libgcrypt+=	libgcrypt>=1.1.90
+BUILDLINK_PKGSRCDIR.libgcrypt?=	../../security/libgcrypt
+
+.  include "../../security/libgpg-error/buildlink3.mk"
+.endif # LIBGCRYPT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
