9f9589d1804f5854f0294dfee25e0bd5b666d501
xtraeme <xtraeme>
2004-02-11 22:57:16 +0000

    Increase BUILDLINK_DEPENDS.

--- graphics/imlib2/buildlink3.mk
+++ graphics/imlib2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1 2004/02/11 05:51:53 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use imlib2.
 #
@@ -14,7 +14,7 @@ BUILDLINK_DEPENDS+=	imlib2
 
 .if !empty(IMLIB2_BUILDLINK3_MK:M+)
 BUILDLINK_PACKAGES+=			imlib2
-BUILDLINK_DEPENDS.imlib2+=		imlib2>=1.1.0nb1
+BUILDLINK_DEPENDS.imlib2+=		imlib2>=1.1.0nb2
 BUILDLINK_PKGSRCDIR.imlib2?=		../../graphics/imlib2
 
 .include "../../graphics/freetype2/buildlink3.mk"
