819bcb0738225aab65e299fe148b8e279d791322
minskim <minskim>
2004-02-14 20:43:27 +0000

    Sync required Xrender version with that in Makefile.

--- fonts/Xft2/buildlink3.mk
+++ fonts/Xft2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/02/09 23:56:32 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/02/10 20:45:01 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XFT2_BUILDLINK3_MK:=	${XFT2_BUILDLINK3_MK}+
@@ -13,6 +13,7 @@ BUILDLINK_DEPENDS.Xft2+=	Xft2>=2.1nb1
 BUILDLINK_PKGSRCDIR.Xft2?=	../../fonts/Xft2
 
 BUILDLINK_DEPENDS.freetype2+=  freetype2>=2.0.9
+BUILDLINK_DEPENDS.Xrender+=	Xrender>=0.8.2
 
 .  include "../../devel/zlib/buildlink3.mk"
 .  include "../../fonts/fontconfig/buildlink3.mk"
