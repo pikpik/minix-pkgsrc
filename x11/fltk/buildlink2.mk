7c565788c9e4c05aaab88fae3ad2213bf48ada57
ben <ben>
2004-02-18 05:07:34 +0000

    Update from FLTK 1.0.11 to FLTK 1.1.4.

--- x11/fltk/buildlink2.mk
+++ x11/fltk/buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink2.mk,v 1.3 2002/11/25 18:21:02 jlam Exp $
+# $NetBSD: buildlink2.mk,v 1.4 2003/03/12 22:07:29 jschauma Exp $
 
 .if !defined(FLTK_BUILDLINK2_MK)
 FLTK_BUILDLINK2_MK=	# defined
@@ -11,7 +11,7 @@ EVAL_PREFIX+=		BUILDLINK_PREFIX.fltk=fltk
 BUILDLINK_PREFIX.fltk_DEFAULT=	${X11PREFIX}
 BUILDLINK_FILES.fltk=	include/FL/*
 BUILDLINK_FILES.fltk+=	include/Fl/*
-BUILDLINK_FILES.fltk+=	lib/libfltk.*
+BUILDLINK_FILES.fltk+=	lib/libfltk*
 
 .include "../../graphics/MesaLib/buildlink2.mk"
 .include "../../graphics/glu/buildlink2.mk"
