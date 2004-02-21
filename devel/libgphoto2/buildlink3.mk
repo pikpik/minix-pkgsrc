66ecbab0714e4cacd1358cfe95989a3e90cf46c1
cube <cube>
2004-02-21 22:58:08 +0000

    Make this compile with gcc-2.95 (hi drochner!).  Closes PR 24515.
    bl3ify, and provide a buildlink3.mk file for future bl3ification of
    dependencies.

---
+++ devel/libgphoto2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBGPHOTO2_BUILDLINK3_MK:=	${LIBGPHOTO2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libgphoto2
+.endif
+
+.if !empty(LIBGPHOTO2_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			libgphoto2
+BUILDLINK_DEPENDS.libgphoto2+=		libgphoto2>=2.1.2
+BUILDLINK_PKGSRCDIR.libgphoto2?=		../../devel/libgphoto2
+.include "../../devel/gettext-lib/buildlink3.mk"
+.include "../../graphics/libexif/buildlink3.mk"
+
+.endif # LIBGPHOTO2_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
