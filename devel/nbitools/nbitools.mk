41c5f766eb86ef6d9002b1f4aaa18f3192eaa922
minskim <minskim>
2004-02-24 22:10:39 +0000

    Include buildlink2.mk to make .tmpl files available to itools/xmkmf. This will fix build problems of inputmethods/sj3-*.  Noted by Ishihara on tech-pkg-ja@jp.NetBSD.org.

--- devel/nbitools/nbitools.mk
+++ devel/nbitools/nbitools.mk
@@ -1,9 +1,9 @@
-# $NetBSD$
+# $NetBSD: nbitools.mk,v 1.1 2002/06/03 09:53:46 sakamoto Exp $
 .if !defined(NBITOOLS_MK)
 NBITOOLS_MK=	# defined
 
-BUILD_DEPENDS+=	nbitools>=6.3:../../devel/nbitools
-EVAL_PREFIX+=	NBITOOLSDIR=nbitools
 XMKMF_CMD=	${NBITOOLSDIR}/libexec/itools/xmkmf
+
+.include "../../devel/nbitools/buildlink2.mk"
 
 .endif	# NBITOOLS_MK
