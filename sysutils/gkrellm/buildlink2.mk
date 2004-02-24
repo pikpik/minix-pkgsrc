ea43bd82a579b9320c94275c2856db063ee267b0
cube <cube>
2004-02-24 22:21:15 +0000

    Merge import of gkrellm2.

--- sysutils/gkrellm/buildlink2.mk
+++ sysutils/gkrellm/buildlink2.mk
@@ -1,22 +1,23 @@
-# $NetBSD: buildlink2.mk,v 1.3 2003/07/13 13:53:15 wiz Exp $
-#
+# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/02/24 22:11:28 cube Exp $
 
 .if !defined(GKRELLM_BUILDLINK2_MK)
 GKRELLM_BUILDLINK2_MK=	# defined
 
 BUILDLINK_PACKAGES+=			gkrellm
-BUILDLINK_DEPENDS.gkrellm?=		gkrellm>=1.2.13nb3
+BUILDLINK_DEPENDS.gkrellm?=		gkrellm>=2.1.22
 BUILDLINK_PKGSRCDIR.gkrellm?=		../../sysutils/gkrellm
 
 EVAL_PREFIX+=	BUILDLINK_PREFIX.gkrellm=gkrellm
 BUILDLINK_PREFIX.gkrellm_DEFAULT=	${LOCALBASE}
-BUILDLINK_FILES.gkrellm+=	include/gkrellm/gkrellm.h
-BUILDLINK_FILES.gkrellm+=	include/gkrellm/gkrellm_public_proto.h
+BUILDLINK_FILES.gkrellm+=	include/gkrellm2/gkrellm.h
+BUILDLINK_FILES.gkrellm+=	include/gkrellm2/gkrellm-public-proto.h
 
-.include "../../graphics/imlib/buildlink2.mk"
+PTHREAD_OPTS+=		require
+
+.include "../../x11/gtk2/buildlink2.mk"
 
 BUILDLINK_TARGETS+=	gkrellm-buildlink
 
 gkrellm-buildlink: _BUILDLINK_USE
 
-.endif	# GKRELLM_BUILDLINK2_MK
+.endif	# GKRELLM2_BUILDLINK2_MK
