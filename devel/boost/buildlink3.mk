535f1497a67501634202572544d5a73b4c76fd13
wiz <wiz>
2004-02-29 18:29:02 +0000

    Add buildlink3.mk. Closes PR 24600.

---
+++ devel/boost/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+#
+# This Makefile fragment is included by packages that use boost.
+#
+# This file was created automatically using createbuildlink-3.1.
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BOOST_BUILDLINK3_MK:=	${BOOST_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	boost
+.endif
+
+.if !empty(BOOST_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			boost
+BUILDLINK_DEPENDS.boost+=		boost>=1.30.2
+BUILDLINK_PKGSRCDIR.boost?=		../../devel/boost
+
+.endif # BOOST_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
