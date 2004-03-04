580f63f29d7ee0677089fbf314200a63ec98eb1f
recht <recht>
2004-03-04 11:11:30 +0000

    bl3ify Patch provided by Michal Pasternak in PR 24664 (with fixes by me).

---
+++ time/py-mxDateTime/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+PY_MXDATETIME_BUILDLINK3_MK:=	${PY_MXDATETIME_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	py-mxDateTime
+.endif
+
+.if !empty(PY_MXDATETIME_BUILDLINK3_MK:M+)
+BUILDLINK_PACKAGES+=			py-mxDateTime
+BUILDLINK_DEPENDS.py-mxDateTime+=		${PYPKGPREFIX}-mxDateTime>=2.0.5
+BUILDLINK_PKGSRCDIR.py-mxDateTime?=		../../time/py-mxDateTime
+
+.endif # PY_MXDATETIME_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
