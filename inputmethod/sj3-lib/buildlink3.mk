30233db946cf3835ff4c438df324c1d2826b7f75
minskim <minskim>
2004-03-05 21:23:00 +0000

    bl3ify and enable pkgviews installation.

---
+++ inputmethod/sj3-lib/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SJ3_LIB_BUILDLINK3_MK:=	${SJ3_LIB_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	sj3-lib
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsj3-lib}
+BUILDLINK_PACKAGES+=	sj3-lib
+
+.if !empty(SJ3_LIB_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.sj3-lib+=	sj3-lib>=2.0.1.20nb1
+BUILDLINK_PKGSRCDIR.sj3-lib?=	../../inputmethod/sj3-lib
+
+.include "../../devel/nbitools/buildlink3.mk"
+
+.endif	# SJ3_LIB_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
