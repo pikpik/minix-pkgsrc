c8b9e6bc9a4880d6cd71c64f1882508630124595
minskim <minskim>
2004-03-06 06:26:21 +0000

    Update blt to 2.4z, update MASTER_SITES and HOMEPAGE, bl3ify, and enable pkgviews installation.
    Many new features and bug fixes since 2.4.o.  See NEWS in the distfile
    for the complete list.

---
+++ x11/blt/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+BLT_BUILDLINK3_MK:=	${BLT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	blt
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nblt}
+BUILDLINK_PACKAGES+=	blt
+
+.if !empty(BLT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.blt+=	blt>=2.4o
+BUILDLINK_PKGSRCDIR.blt?=	../../x11/blt
+
+.include "../../lang/tcl/buildlink3.mk"
+.include "../../x11/tk/buildlink3.mk"
+
+.endif	# BLT_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
