61391aaee59a5d4ae59847f118c5e551552e8e08
xtraeme <xtraeme>
2004-03-10 11:53:38 +0000

    bl3ify, s,USE_X11BASE,USE_X11,g. Welcome to 4.0.4nb1, ok'ed by martti@

---
+++ x11/libxfce4util/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBXFCE4UTIL_BUILDLINK3_MK:=	${LIBXFCE4UTIL_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	libxfce4util
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxfce4util}
+BUILDLINK_PACKAGES+=	libxfce4util
+
+.if !empty(LIBXFCE4UTIL_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.libxfce4util+=	libxfce4util>=4.0.4nb1
+BUILDLINK_PKGSRCDIR.libxfce4util?=	../../x11/libxfce4util
+
+.include "../../devel/glib2/buildlink3.mk"
+
+.endif	# LIBXFCE4UTIL_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
