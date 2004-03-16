155e0cbb8d463f900ac2c8b3ea0279dbbc2be08c
jlam <jlam>
2004-03-16 17:55:35 +0000

    BUILDLINK_DEPENDS.<pkg> is appended to.

--- x11/libxklavier/buildlink3.mk
+++ x11/libxklavier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/02/14 17:34:25 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 19:25:42 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXKLAVIER_BUILDLINK3_MK:=	${LIBXKLAVIER_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibxklavier}
 BUILDLINK_PACKAGES+=	libxklavier
 
 .if !empty(LIBXKLAVIER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libxklavier?=		libxklavier>=0.97
+BUILDLINK_DEPENDS.libxklavier+=		libxklavier>=0.97
 BUILDLINK_PKGSRCDIR.libxklavier?=	../../x11/libxklavier
 
 .include "../../textproc/libxml2/buildlink3.mk"
