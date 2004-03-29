620ae6acc3af267e2fe3743db1d8719829bfaa08
tv <tv>
2004-03-29 02:27:44 +0000

    umask should be 002.

--- mk/defs.Interix.mk
+++ mk/defs.Interix.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.Interix.mk,v 1.5 2004/03/11 21:18:32 tv Exp $
+# $NetBSD: defs.Interix.mk,v 1.6 2004/03/11 22:57:22 agc Exp $
 #
 # Variable definitions for the Interix operating system.
 
@@ -81,7 +81,7 @@ USERADD?=	${FALSE}
 GROUPADD?=	${FALSE}
 
 CPP_PRECOMP_FLAGS?=	# unset
-DEF_UMASK?=		0022
+DEF_UMASK?=		002
 EXPORT_SYMBOLS_LDFLAGS?=-Wl,-E	# add symbols to the dynamic symbol table
 
 MOTIF_TYPE_DEFAULT?=	openmotif	# default 2.0 compatible libs type
