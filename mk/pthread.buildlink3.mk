499bbcf93aae7e7cd59d0b1f1d331a8437d42c2b
seb <seb>
2004-03-29 15:16:58 +0000

    When PTHREAD_TYPE is 'native' set USE_BUILTIN.pthread to YES and add 'pthread' to BUILDLINK_PACKAGES so that BUILDLINK_{{LD,C}FLAGS,LDADD}.pthread actually have an effect.
    This should fix the build of at least tcl/tk related packages now
    that tcl and tk packages are thread-aware.
    
    Remove comments about linking native pthread libraries and headers into
    ${BUILDLINK_DIR} as this does not happen.

--- mk/pthread.buildlink3.mk
+++ mk/pthread.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: pthread.buildlink3.mk,v 1.2 2004/01/25 16:17:37 jlam Exp $
+# $NetBSD: pthread.buildlink3.mk,v 1.3 2004/03/18 09:12:13 jlam Exp $
 #
 # The pthreads strategy for pkgsrc is to "bless" a particular pthread
 # package as the Official Pthread Replacement (OPR).  A package that uses
@@ -141,9 +141,9 @@ PKG_SKIP_REASON= "${PKGNAME} requires a working pthreads implementation."
 .endif
 
 .if ${PTHREAD_TYPE} == "native"
-#
-# Link the native pthread libraries and headers into ${BUILDLINK_DIR}.
-#
+BUILDLINK_PACKAGES:=		${BUILDLINK_PACKAGES:Npthread}
+BUILDLINK_PACKAGES+=		pthread
+USE_BUILTIN.pthread=		YES
 BUILDLINK_PREFIX.pthread=	/usr
 BUILDLINK_CFLAGS.pthread=	-pthread
 BUILDLINK_LDFLAGS.pthread=	# empty
