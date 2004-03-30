9d7e0b8b160474b9c7677417f99ad697023dc9ee
xtraeme <xtraeme>
2004-03-30 21:58:45 +0000

    Check for !empty(X11_TYPE:Mnative) instead of checking for empty(X11_TYPE:MXFree86), because X11_TYPE=xlibs could be used too...

--- mk/buildlink2/bsd.buildlink2.mk
+++ mk/buildlink2/bsd.buildlink2.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.buildlink2.mk,v 1.121 2004/03/29 05:42:58 jlam Exp $
+# $NetBSD: bsd.buildlink2.mk,v 1.122 2004/03/30 20:57:19 xtraeme Exp $
 #
 # An example package buildlink2.mk file:
 #
@@ -91,7 +91,7 @@ MAKE_ENV+=		BUILDLINK_CACHE_ALL=yes
 USE_X11_LINKS?=		YES
 X11_TYPE?=		native
 .  if empty(USE_X11_LINKS:M[nN][oO]) && \
-      empty(X11_TYPE:MXFree86)
+      !empty(X11_TYPE:Mnative)
 BUILD_DEPENDS+=		x11-links>=0.23:../../pkgtools/x11-links
 _BLNK_X11_DIR=		${LOCALBASE}/share/x11-links
 .  elif empty(USE_X11_LINKS:M[nN][oO]) && \
