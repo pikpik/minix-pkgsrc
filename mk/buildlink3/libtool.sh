5e7a7be25859a2dc858929bdf41df5fd316fcb6e
jlam <jlam>
2004-03-30 17:25:19 +0000

    Extend the capabilities of buildlink3's libtool wrapper a bit compared to the stock libtool: you can now copy or symlink an uninstalled libtool archive file somewhere else than its build directory, and you can still link against it.  This allows us to more easily bolt libtool build machinery onto packages that have unusual(ly crappy) build systems that rely on installing libraries to some common build directory after they're built.
    We do this by adding a "buildlibdir" variable to the uninstalled
    libtool archive that points to the build directory of the archive.
    Whenever we link against this archive, we rewrite the path to archive
    on the libtool command line so that it points to the true archive.
    This allows the real libtool to find the files under $buildlibdir/.libs.

--- mk/buildlink3/libtool.sh
+++ mk/buildlink3/libtool.sh
@@ -1,6 +1,6 @@
 #!@BUILDLINK_SHELL@
 #
-# $NetBSD: libtool.sh,v 1.15 2004/02/12 20:29:34 jlam Exp $
+# $NetBSD: libtool.sh,v 1.16 2004/03/13 03:41:13 uebayasi Exp $
 
 Xsed='@SED@ -e 1s/^X//'
 sed_quote_subst='s/\([\\`\\"$\\\\]\)/\\\1/g'
@@ -30,9 +30,12 @@ updatecache="${BUILDLINK_UPDATE_CACHE-yes}"
 cacheall="${BUILDLINK_CACHE_ALL-no}"
 reorder="${BUILDLINK_REORDER-no}"
 
+basename="@BASENAME@"
 cat="@CAT@"
+dirname="@DIRNAME@"
 echo="@ECHO@"
 expr="@EXPR@"
+pwd="@PWD_CMD@"
 test="@TEST@"
 
 BUILDLINK_DIR="@BUILDLINK_DIR@"
