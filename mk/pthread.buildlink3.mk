6f306f9c6ec6c444382674f23529b58b4283351b
seb <seb>
2004-03-31 20:18:46 +0000

    revert previous.

--- mk/pthread.buildlink3.mk
+++ mk/pthread.buildlink3.mk
@@ -141,9 +141,9 @@ PKG_SKIP_REASON= "${PKGNAME} requires a working pthreads implementation."
 .endif
 
 .if ${PTHREAD_TYPE} == "native"
-BUILDLINK_PACKAGES:=		${BUILDLINK_PACKAGES:Npthread}
-BUILDLINK_PACKAGES+=		pthread
-USE_BUILTIN.pthread=		YES
+#
+# Link the native pthread libraries and headers into ${BUILDLINK_DIR}.
+#
 BUILDLINK_PREFIX.pthread=	/usr
 BUILDLINK_CFLAGS.pthread=	-pthread
 BUILDLINK_LDFLAGS.pthread=	# empty
