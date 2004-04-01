5711c227fa5117fe420ddb400e290d17888b8468
jmmv <jmmv>
2004-04-01 18:34:27 +0000

    Update to 2.5.2:
    - plenty of code cleanups
    - removed the dependency on libgnome, only glib required now
    - Linux 2.6 support, NetBSD support and better FreeBSD support
    - Make the block size available for filesystem statistics

--- devel/libgtop2/buildlink3.mk
+++ devel/libgtop2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/03/05 19:25:11 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/03/18 09:12:10 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGTOP2_BUILDLINK3_MK:=	${LIBGTOP2_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgtop2}
 BUILDLINK_PACKAGES+=	libgtop2
 
 .if !empty(LIBGTOP2_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.0.0nb4
+BUILDLINK_DEPENDS.libgtop2+=	libgtop2>=2.5.2
 BUILDLINK_PKGSRCDIR.libgtop2?=	../../devel/libgtop2
 .endif	# LIBGTOP2_BUILDLINK3_MK
 
