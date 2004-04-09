919ef5a552b92788c81e98b4ccbdeee70da05ada
grant <grant>
2004-04-09 11:31:55 +0000

    Initial import of p5-Net-Google 0.61 into the NetBSD packages collection.
    Net::Google provides a simple OOP-ish interface to the Google SOAP
    API.

---
+++ net/p5-Net-Google/buildlink2.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+#
+
+.if !defined(P5_NET_GOOGLE_BUILDLINK2_MK)
+P5_NET_GOOGLE_BUILDLINK2_MK=	# defined
+
+BUILDLINK_PACKAGES+=			p5-Net-Google
+BUILDLINK_DEPENDS.p5-Net-Google?=	p5-Net-Google>=0.61
+BUILDLINK_PKGSRCDIR.p5-Net-Google?=	../../net/p5-Net-Google
+
+EVAL_PREFIX+=	BUILDLINK_PREFIX.p5-Net-Google=p5-Net-Google
+BUILDLINK_PREFIX.p5-Net-Google_DEFAULT=	${LOCALBASE}
+
+BUILDLINK_TARGETS+=	p5-Net-Google-buildlink
+
+p5-Net-Google-buildlink: _BUILDLINK_USE
+
+.endif	# P5_NET_GOOGLE_BUILDLINK2_MK
