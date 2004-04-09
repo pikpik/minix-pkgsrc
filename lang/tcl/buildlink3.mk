6a4880bc2805854d6c86381ed964544f6bc241d3
danw <danw>
2004-04-09 23:37:28 +0000

    Fix up some Makefile logic that got broken by the libtool patches (and prevented it from building on some platforms). Ensure that the built library is named libtcl84 (as opposed to libtcl8.4) on all platforms.

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2004/03/24 15:24:36 jwise Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2004/03/25 22:45:27 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TCL_BUILDLINK3_MK:=	${TCL_BUILDLINK3_MK}+
@@ -16,10 +16,11 @@ BUILDLINK_PKGSRCDIR.tcl?=	../../lang/tcl
 
 BUILDLINK_FILES.tcl=	bin/tclsh*
 #
-# Make "-ltcl" resolve into "-ltcl84", so that we don't need to patch so
-# many Makefiles.
+# Make "-ltcl" and "-ltcl8.4" resolve into "-ltcl84", so that we don't
+# need to patch so many Makefiles.
 #
 BUILDLINK_TRANSFORM+=	l:tcl:tcl84
+BUILDLINK_TRANSFORM+=	l:tcl8.4:tcl84
 
 TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 
