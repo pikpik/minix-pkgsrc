db6418bae3e1961b6241be626907e1284007dfe4
rh <rh>
2004-04-11 01:09:14 +0000

    Initial import of Renaissance-0.7.0, a structured GNUstep user interface development framework.

---
+++ devel/Renaissance/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+RENAISSANCE_BUILDLINK3_MK:=	${RENAISSANCE_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	Renaissance
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NRenaissance}
+BUILDLINK_PACKAGES+=	Renaissance
+
+.if !empty(RENAISSANCE_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.Renaissance+=		Renaissance>=0.7.0
+BUILDLINK_PKGSRCDIR.Renaissance?=	../../devel/Renaissance
+.endif	# RENAISSANCE_BUILDLINK3_MK
+
+.include "../../x11/gnustep-back/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
