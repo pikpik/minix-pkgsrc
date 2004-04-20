2f921626051404f8730d9fc82cf94860489cf47d
markd <markd>
2004-04-20 13:59:34 +0000

    bl3ify and update to kde3.2.2.
    Changes:
    * kpat: Fix Highscores
    * kpat: Fix [PATCH] Remove deprecated KLineEditDlg
    * kpat: Disable the Hint/Demo/Redeal actions in case a game cannot be won
      anymore
    * artwork: Fix KFoulEggs icon
    * docs: Fix Error in kpatience documentation
    * docs: Fix Errors in kreversi doc
    * docs: Fix typo in ktron documentation
    * configure: Fix Extraneous message in admin/Makefile.common using --prefix

---
+++ games/kdegames3/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+KDEGAMES_BUILDLINK3_MK:=	${KDEGAMES_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	kdegames
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nkdegames}
+BUILDLINK_PACKAGES+=	kdegames
+
+.if !empty(KDEGAMES_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.kdegames+=	kdegames>=3.2.0
+BUILDLINK_RECOMMENDED.kdegames?=	kdegames>=3.2.2
+BUILDLINK_PKGSRCDIR.kdegames?=	../../games/kdegames3
+.endif	# KDEGAMES_BUILDLINK3_MK
+
+.include "../../x11/kdebase3/buildlink3.mk"
+.include "../../x11/kdelibs3/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
