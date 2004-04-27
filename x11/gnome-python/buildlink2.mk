965a0a511bb428079f6a32afd20e5457e8f8cb45
snj <snj>
2004-04-27 02:04:07 +0000

    Convert to buildlink3.

--- x11/gnome-python/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.7 2004/01/03 18:49:56 reed Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pygnome
-BUILDLINK_DEPENDS.pygnome?=	${PYPKGPREFIX}-gnome>=1.4.4nb4
-BUILDLINK_RECOMMENDED.pygnome?=	${PYPKGPREFIX}-gnome>=1.4.4nb5
-BUILDLINK_PKGSRCDIR.pygnome?=	../../x11/gnome-python
