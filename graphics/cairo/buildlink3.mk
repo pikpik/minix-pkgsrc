285e7a914340d4515a22106c02f11e1b0cd89ac0
adam <adam>
2004-04-27 14:16:05 +0000

    buildlink3 fix

--- graphics/cairo/buildlink3.mk
+++ graphics/cairo/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.6 2004/04/04 16:54:22 recht Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CAIRO_BUILDLINK3_MK:=	${CAIRO_BUILDLINK3_MK}+
@@ -21,7 +21,9 @@ BUILDLINK_DEPENDS.Xrender+=	Xrender>=0.8
 
 .include "../../fonts/Xft2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/glitz/buildlink3.mk"
 .include "../../graphics/libpixman/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
