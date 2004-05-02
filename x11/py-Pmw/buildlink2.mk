7ba6c53417b737131ca76c48bf75c6541b4e425c
snj <snj>
2004-05-02 05:27:20 +0000

    No longer used.

--- x11/py-Pmw/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.2 2002/08/25 19:23:56 jlam Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pypmw
-BUILDLINK_PKGBASE.pypmw?=	${PYPKGPREFIX}-Pmw
-BUILDLINK_DEPENDS.pypmw?=	${PYPKGPREFIX}-Pmw-[0-9]*
-BUILDLINK_PKGSRCDIR.pypmw?=	../../x11/py-Pmw
