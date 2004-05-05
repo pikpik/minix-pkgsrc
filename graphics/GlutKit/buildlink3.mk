387af98b006d9f23d7754816bc2bd0258c6394ec
snj <snj>
2004-05-05 03:36:26 +0000

    Convert to buildlink3.

---
+++ graphics/GlutKit/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GLUTKIT_BUILDLINK3_MK:=	${GLUTKIT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	GlutKit
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NGlutKit}
+BUILDLINK_PACKAGES+=	GlutKit
+
+.if !empty(GLUTKIT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.GlutKit+=	GlutKit>=0.3.1r2nb4
+BUILDLINK_RECOMMENDED.GlutKit?=	GlutKit>=0.3.1r2nb6
+BUILDLINK_PKGSRCDIR.GlutKit?=	../../graphics/GlutKit
+.endif	# GLUTKIT_BUILDLINK3_MK
+
+.include "../../graphics/RenderKit/buildlink3.mk"
+.include "../../graphics/glut/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
---
+++ graphics/RenderKit/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+RENDERKIT_BUILDLINK3_MK:=	${RENDERKIT_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	RenderKit
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NRenderKit}
+BUILDLINK_PACKAGES+=	RenderKit
+
+.if !empty(RENDERKIT_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.RenderKit+=	RenderKit>=0.3.1r2nb3
+BUILDLINK_RECOMMENDED.RenderKit?=	RenderKit>=0.3.1r2nb5
+BUILDLINK_PKGSRCDIR.RenderKit?=	../../graphics/RenderKit
+.endif	# RENDERKIT_BUILDLINK3_MK
+
+.include "../../graphics/GeometryKit/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
