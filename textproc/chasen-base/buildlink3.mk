98647b6ca3c4664f40abd6c9b4389398385cc9e1
kristerw <kristerw>
2004-05-06 17:36:47 +0000

    Correct path to devel/darts.

--- textproc/chasen-base/buildlink3.mk
+++ textproc/chasen-base/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1 2004/05/06 13:32:38 taca Exp $
 #
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
@@ -16,6 +16,6 @@ BUILDLINK_DEPENDS.chasen-base+=	chasen-base>=2.3.3
 BUILDLINK_PKGSRCDIR.chasen-base?=	../../textproc/chasen-base
 .endif	# CHASEN_BASE_BUILDLINK3_MK
 
-.include "../../textproc/darts/buildlink3.mk"
+.include "../../devel/darts/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
