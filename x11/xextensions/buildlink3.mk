d9fec4fc4333ac69d4ce617faab762143deeb156
minskim <minskim>
2004-05-08 21:57:04 +0000

    Set BUILDLINK_DEPMETHOD to "build" because this package installs header files only.

--- x11/xextensions/buildlink3.mk
+++ x11/xextensions/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/04/13 09:58:07 xtraeme Exp $
 #
 # This Makefile fragment is included by packages that use xextensions.
 #
@@ -16,6 +16,7 @@ BUILDLINK_PACKAGES+=	xextensions
 .if !empty(XEXTENSIONS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xextensions?=		xextensions>=1.0
 BUILDLINK_PKGSRCDIR.xextensions?=	../../x11/xextensions
+BUILDLINK_DEPMETHOD.xextensions?=	build
 .endif # XEXTENSIONS_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
