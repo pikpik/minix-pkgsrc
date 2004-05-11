c12be272408e9fc69e5fa10d04e7eb55d0be9407
recht <recht>
2004-05-11 17:37:48 +0000

    update to libgdiplus-0.5 (part of mono beta1)
    Sadly, there's no real news file avaiable. So, I guess it's the usual
    bug-fixes and optimizations.

--- graphics/libgdiplus/buildlink3.mk
+++ graphics/libgdiplus/buildlink3.mk
@@ -11,11 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgdiplus}
 BUILDLINK_PACKAGES+=	libgdiplus
 
 .if !empty(LIBGDIPLUS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgdiplus+=	libgdiplus>=0.2
+BUILDLINK_DEPENDS.libgdiplus+=	libgdiplus>=0.5
 BUILDLINK_PKGSRCDIR.libgdiplus?=	../../graphics/libgdiplus
 .endif	# LIBGDIPLUS_BUILDLINK3_MK
 
 .include "../../graphics/cairo/buildlink3.mk"
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
 .include "../../lang/mono/buildlink3.mk"
 .include "../../x11/Xrender/buildlink3.mk"
 
