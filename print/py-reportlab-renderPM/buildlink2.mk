4a3dde238450b03130e6100f539d03437471cdea
recht <recht>
2004-05-23 16:59:05 +0000

    unused

--- print/py-reportlab-renderPM/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/07/28 15:08:50 drochner Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyrlrenderpm
-BUILDLINK_PKGBASE.pyrlrenderpm?= ${PYPKGPREFIX}-reportlab-renderPM
-BUILDLINK_DEPENDS.pyrlrenderpm?= ${PYPKGPREFIX}-reportlab-renderPM-[0-9]*
-BUILDLINK_PKGSRCDIR.pyrlrenderpm=../../print/py-reportlab-renderPM
--- textproc/py-xml/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2004/01/23 15:25:39 recht Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pyxml
-BUILDLINK_PKGBASE.pyxml?=	${PYPKGPREFIX}-xml
-BUILDLINK_DEPENDS.pyxml?=	${PYPKGPREFIX}-xml-[0-9]*
-BUILDLINK_PKGSRCDIR.pyxml?=	../../textproc/py-xml
