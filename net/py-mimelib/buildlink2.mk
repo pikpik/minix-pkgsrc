5d0fa5b26e119d92d4c6447c82c44734e0509b8b
recht <recht>
2004-05-23 11:58:21 +0000

    unused

--- net/py-mimelib/buildlink2.mk
+++
@@ -1,8 +0,0 @@
-# $NetBSD: buildlink2.mk,v 1.1.1.1 2003/08/11 14:56:54 drochner Exp $
-
-.include "../../lang/python/pyversion.mk"
-
-BUILDLINK_PACKAGES+=		pymimelib
-BUILDLINK_PKGBASE.pymimelib?=	${PYPKGPREFIX}-mimelib
-BUILDLINK_DEPENDS.pymimelib?=	${PYPKGPREFIX}-mimelib-[0-9]*
-BUILDLINK_PKGSRCDIR.pymimelib?=	../../net/py-mimelib
