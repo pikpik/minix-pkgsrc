7108d3f6eed21e35a5faf5aaca3db226f8df90f9
recht <recht>
2004-06-01 16:59:49 +0000

    update to evolution-data-server-0.0.93
    No real NEWS file available, so I'm guessing from the ChangeLog:
    - added groupwise support
    - bugfixes
    
    From the pkgsrc pov: use the bundled libdb4.

--- mail/evolution-data-server/buildlink3.mk
+++ mail/evolution-data-server/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/04/01 20:20:23 xtraeme Exp $
+# $NetBSD$
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 EVOLUTION_DATA_SERVER_BUILDLINK3_MK:=	${EVOLUTION_DATA_SERVER_BUILDLINK3_MK}+
@@ -11,11 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nevolution-data-server}
 BUILDLINK_PACKAGES+=	evolution-data-server
 
 .if !empty(EVOLUTION_DATA_SERVER_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.evolution-data-server+=	evolution-data-server>=0.0.90
+BUILDLINK_DEPENDS.evolution-data-server+=	evolution-data-server>=0.0.93
 BUILDLINK_PKGSRCDIR.evolution-data-server?=	../../mail/evolution-data-server
-.endif  # EVOLUTION_DATA_SERVER_BUILDLINK3_MK
+.endif	# EVOLUTION_DATA_SERVER_BUILDLINK3_MK
 
-.include "../../databases/db4/buildlink3.mk"
 .include "../../databases/openldap/buildlink3.mk"
 .include "../../devel/GConf2/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
