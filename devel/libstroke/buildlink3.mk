ffc176315677af60e3d90c295890a7689b24e12a
minskim <minskim>
2004-06-03 16:21:14 +0000

    Update libstroke to 0.5.1 and switch to Tcl 8.4.  This closes PR pkg/25574.
    Changes since 0.3:
      The big NEWS is that LibStroke now can draw the strokes! Yeeepeeee!
      But it does not do so by default, you need to pass --with-mouse-footprints
      to ./configure. After compiling everything try tests/stroke_test to
      see how this works.
    
      There's even more: libgstroke, a stroke implementation for GNOME has
      been contributed by Dan Nicolaescu. It is fully functional (it
      also supports mouse footprints by default!) and has a very easy to
      use API. Try tests/gnome_stroke_test to see how it works (source
      code in tests/gnome_stroke_test.c).
    
      We are proposing a set of strokes to be standard strokes. A pretty
      picture with them their string representation can be found in
      doc/standard_strokes.jpg
      A text listing is in doc/standard_strokes.txt (we will find some
      better names than the current ones).
      We'll also add some "sloppy strokes" for the standard strokes.
    
      We created autoconf macros for libstroke and libgstroke, they are in
      the macros subdir.
    
      Java port included and some misc bug fixes with the install process.

--- devel/libstroke/buildlink3.mk
+++ devel/libstroke/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/08 19:52:51 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/03/18 09:12:10 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSTROKE_BUILDLINK3_MK:=	${LIBSTROKE_BUILDLINK3_MK}+
@@ -11,10 +11,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibstroke}
 BUILDLINK_PACKAGES+=	libstroke
 
 .if !empty(LIBSTROKE_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libstroke+=	libstroke>=0.3nb1
+BUILDLINK_DEPENDS.libstroke+=	libstroke>=0.5.1
 BUILDLINK_PKGSRCDIR.libstroke?=	../../devel/libstroke
 .endif	# LIBSTROKE_BUILDLINK3_MK
 
-.include "../../lang/tcl83/buildlink3.mk"
+.include "../../lang/tcl/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
