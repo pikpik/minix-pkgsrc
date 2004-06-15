bce157dbe3f9aa1e8129c2c3ea6d3721a46e74cf
jwise <jwise>
2004-06-15 16:39:12 +0000

    Reverse previous commit (disabling thread-safety), which was poorly though through, unacceptably breaks threaded apps, and was made without consulting the package's maintainer.
    To quote my mail to packages@:
    
    I am reversing this change.
    
    With this change, tcl is _not_ usable in threaded apps, while without
    this change, tcl is perfectly usable in non-threaded apps.
    
    The fix for a package which is incorrectly written is not to break the
    package it depends on, but do your job correctly when writing the
    package.
    
    With buildlink3, this is mind-numbingly easy to do.
    
    That you neither contacted the maintainer of this package, nor did the
    buildlink and dependent package changes which your change requires shows
    that you have not really thought this through.
    
    So, as I said, I am reversing this change.  Fix your own package, don't
    break others.

--- lang/tcl/buildlink3.mk
+++ lang/tcl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2004/04/09 23:37:28 danw Exp $
+# $NetBSD$
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 TCL_BUILDLINK3_MK:=	${TCL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntcl}
 BUILDLINK_PACKAGES+=	tcl
 
 .if !empty(TCL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.tcl+=		tcl>=8.4.6nb5
+BUILDLINK_DEPENDS.tcl+=		tcl>=8.4.6nb1
 BUILDLINK_PKGSRCDIR.tcl?=	../../lang/tcl
 
 BUILDLINK_FILES.tcl=	bin/tclsh*
@@ -26,7 +26,6 @@ TCLCONFIG_SH?=		${BUILDLINK_PREFIX.tcl}/lib/tclConfig.sh
 
 .endif	# TCL_BUILDLINK3_MK
 
-# see comment in Makefile
-#.include "../../mk/pthread.buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
