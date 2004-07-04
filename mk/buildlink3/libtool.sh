12120a283b5cb63bd800aeb1175af8cbf5da5805
wiz <wiz>
2004-07-04 09:52:04 +0000

    Handle --execute the same way as --install (not modifying the arguments). Needed to convert inputmethods/xcin to bl3. Idea from jlam.

--- mk/buildlink3/libtool.sh
+++ mk/buildlink3/libtool.sh
@@ -1,6 +1,6 @@
 #!@BUILDLINK_SHELL@
 #
-# $NetBSD: libtool.sh,v 1.16 2004/03/13 03:41:13 uebayasi Exp $
+# $NetBSD: libtool.sh,v 1.17 2004/03/30 17:25:19 jlam Exp $
 
 Xsed='@SED@ -e 1s/^X//'
 sed_quote_subst='s/\([\\`\\"$\\\\]\)/\\\1/g'
@@ -102,7 +102,7 @@ cmd="@WRAPPEE@"
 ldflags=
 libs=
 case $mode in
-install)
+execute|install)
 	#
 	# We're doing libtool installation, so just quote all of the
 	# command-line arguments and append them to $cmd.  We don't worry
