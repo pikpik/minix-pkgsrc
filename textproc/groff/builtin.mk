b23e6b4aa4e052a3fa0cb1ce13aeb8a27a16df05
drochner <drochner>
2004-07-09 12:30:17 +0000

    set the GROFF and TBL variables to the full path

--- textproc/groff/builtin.mk
+++ textproc/groff/builtin.mk
@@ -1,10 +1,15 @@
-# $NetBSD$
+# $NetBSD: builtin.mk,v 1.1 2004/07/07 19:06:23 drochner Exp $
 
 .if !defined(IS_BUILTIN.groff)
-IS_BUILTIN.groff=    no
 .  if exists(/usr/bin/groff)
-IS_BUILTIN.groff=    yes
+IS_BUILTIN.groff=	yes
+_GROFFPATH=		/usr/bin
+.  else
+IS_BUILTIN.groff=	no
+_GROFFPATH=		${LOCALBASE}/bin
 .  endif
 .endif  # IS_BUILTIN.groff
 
-USE_BUILTIN.groff?=  ${IS_BUILTIN.groff}
+USE_BUILTIN.groff?=	${IS_BUILTIN.groff}
+GROFF?=			${_GROFFPATH}/groff
+TBL?=			${_GROFFPATH}/tbl
