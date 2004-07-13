d0ea3c648bcfe7741e0adc5a82b9157d7c80039c
recht <recht>
2004-07-13 14:22:27 +0000

    update boehm-gc to 6.3b1
    changes:
    - Add a patch provided by UCHIYAMA Yasushi in PR 26180 to support NetBSD/sh3.
    - Correctly define ELFSIZE for NetBSD. Fixes PR 26252 by Kouichirou Hiratsuka.
    
    Bump BUILDLINK_DEPNDS to 6.3nb1 for the ELFSIZE fix.

--- devel/boehm-gc/buildlink3.mk
+++ devel/boehm-gc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/01/24 03:26:46 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/03/05 19:25:10 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BOEHM_GC_BUILDLINK3_MK:=	${BOEHM_GC_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nboehm-gc}
 BUILDLINK_PACKAGES+=	boehm-gc
 
 .if !empty(BOEHM_GC_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.boehm-gc+=	boehm-gc>=6.2nb1
+BUILDLINK_DEPENDS.boehm-gc+=	boehm-gc>=6.3nb1
 BUILDLINK_PKGSRCDIR.boehm-gc?=	../../devel/boehm-gc
 .endif	# BOEHM_GC_BUILDLINK3_MK
 
