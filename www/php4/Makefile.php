f8e54c4eae07dfc416befecc1e555c4a99b52ef8
jdolecek <jdolecek>
2004-07-14 08:04:37 +0000

    Update php4 to 4.3.8.
    This is security fix release, fixing several important security
    issues. From the ChangeLog:
    
    * Fixed strip_tags() to correctly handle '\0' characters.
    * Improved stability during startup when memory_limit is used.
    * Replace alloca() with emalloc() for better stack protection.
    * Added missing safe_mode checks inside ftok and itpc.
    * Fixed bug #28963 Fixed address allocation routine in IMAP extension.
    * Fixed bug #28632 Prevent open_basedir bypass via MySQL's LOAD DATA LOCAL.
    
    Note: package update also includes extra patches from PHP CVS not
    present in stock PHP 4.3.8 release - compilation fix for
    mssql extension and Zend engine memory-use-after-free fix.

--- www/php4/Makefile.php
+++ www/php4/Makefile.php
@@ -1,9 +1,9 @@
-# $NetBSD: Makefile.php,v 1.13 2004/04/26 08:35:22 jlam Exp $
+# $NetBSD: Makefile.php,v 1.14 2004/07/13 12:34:33 jdolecek Exp $
 
 .include "../../www/php4/Makefile.common"
 
-# revision of the core PHP interepreter
-PKGREVISION=		1
+# revision of the core PHP interepreter package
+PKGREVISION=		# empty
 
 DISTINFO_FILE=		${.CURDIR}/../../www/php4/distinfo
 PATCHDIR=			${.CURDIR}/../../www/php4/patches
