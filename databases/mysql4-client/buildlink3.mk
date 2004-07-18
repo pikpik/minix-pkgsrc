3a54bc81ebe1a75850d58139962d8d2ab27d2270
jlam <jlam>
2004-07-18 22:30:27 +0000

    The libraries don't link against libreadline, only the client binaries, so remove the readline buildlink3.mk file.

--- databases/mysql4-client/buildlink3.mk
+++ databases/mysql4-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/22 14:31:35 seb Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/06/20 18:48:44 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -17,8 +17,7 @@ BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql4-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
 
-.include "../../devel/readline/buildlink3.mk"
 .include "../../security/openssl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
