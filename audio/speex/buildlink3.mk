e8d439db22f530d2e4886e05cf3ce15092ba0a00
wiz <wiz>
2004-07-23 15:48:47 +0000

    Update to 1.0.4: * Headers are now in ${PREFIX}/include/speex/ (but a copy is still   in ${PREFIX}/include for compat reasons). * Pseudo-gapless playback (i.e. playback has the same number of   samples) * Fixed a potential bug (unconfirmed) that might cause a segfault   in special circumstances.
    Also includes a shlib major bump, so update DEPENDS in buildlink3.mk.

--- audio/speex/buildlink3.mk
+++ audio/speex/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:08 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/12 23:15:01 salo Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SPEEX_BUILDLINK3_MK:=	${SPEEX_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nspeex}
 BUILDLINK_PACKAGES+=	speex
 
 .if !empty(SPEEX_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.speex+=	speex>=1.0.2nb1
+BUILDLINK_DEPENDS.speex+=	speex>=1.0.4
 BUILDLINK_PKGSRCDIR.speex?=	../../audio/speex
 .endif	# SPEEX_BUILDLINK3_MK
 
