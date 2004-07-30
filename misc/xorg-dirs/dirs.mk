3e0fc253cee4b94b63f101c5210678e243cebf16
xtraeme <xtraeme>
2004-07-30 11:24:38 +0000

    <x11>/Makefile.common should be included before dirs.mk to have X11ROOT defined! (hi jmmv).
    Bump version to 1.1, both XFree86-dirs and xorg-dirs.

--- misc/xorg-dirs/dirs.mk
+++ misc/xorg-dirs/dirs.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: dirs.mk,v 1.1.1.1 2004/07/29 07:26:03 xtraeme Exp $
 #
 
 .if !defined(DIRS_XORG_MK)
@@ -6,7 +6,6 @@ DIRS_XORG_MK=		# defined
 
 oldval:=		${XORG_ONLY_DIRS}
 XORG_ONLY_DIRS:=	YES
-.include "../../meta-pkgs/xorg/Makefile.common"
 XORG_ONLY_DIRS:=	${oldval}
 .undef oldval
 
