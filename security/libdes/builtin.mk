ca20e27177187642dbe0e553aed7005dd63ff6ec
tv <tv>
2004-07-31 05:31:57 +0000

    Detect builtin libdes.  (NetBSD 2.0, for instance, has one.)

---
+++ security/libdes/builtin.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+.if !defined(IS_BUILTIN.libdes)
+IS_BUILTIN.libdes=		no
+.  if exists(/usr/include/libdes.h)
+IS_BUILTIN.libdes=		yes
+.  elif exists(/usr/include/des.h)
+
+IS_BUILTIN.libdes=		yes
+BUILDLINK_TARGETS+=		buildlink-libdes-libdes-h
+
+buildlink-libdes-libdes-h:
+	${LN} -s -f /usr/include/des.h ${BUILDLINK_DIR}/include/libdes.h
+
+.  endif
+.endif	# IS_BUILTIN.libdes
+
+USE_BUILTIN.libdes?=		${IS_BUILTIN.libdes}
