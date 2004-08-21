378ad09481a5b1d17a5ff69c07a8d631f1decf63
markd <markd>
2004-08-21 14:56:18 +0000

    Update to KDE 3.3
    Changes:
    * Kopete
      o Implement Jabber file transfers. Till Gerken
      o Add Jabber group chat support. Till Gerken
      o Complete Kopete's handling of external changes to IM data stored
        in KABC - add new contacts if added in KABC and rearrange
        metacontacts following the data in KABC, similarly, remove. Will
        Stephenson
      o New connection API that supporting logging in as a different status
        than "online" Matt Rogers
      o New disconnect API so we can tell when we've been disconnected by
        the server and can then reconnect. Matt Rogers
      o Latex render plugin Duncan Mac-Vicar
      o Add support for bold, italic and underlined messages to Yahoo Matt
        Rogers
      o Add new mail notifications to the Yahoo! plugin Matt Rogers
      o Add SSL Support in IRC Jason Keirstead
      o Add the ability to associate custom KNotify event notifications with
        a metacontact (Buddy Pounce) Will Stephenson
      o Add support for irc:// protocols in Konqueror Jason Keirstead
      o Change the KopeteAwayAction to be more like Konqueror's Recent
        Documents Jason Keirstead
      o Add an alias plugin Jason Keirstead
      o Seperate the password handling from KopeteAccount Richard Smith
      o Support amaroK in Kopete's Now Listening plugin Will Stephenson
      o Action to toggle encryption on/off in a chat. Olivier Goffart
      o Implement KIMIface in Kopete to enable presence and messaging
        integration across the desktop. Will Stephenson
      o Merge data acquired from Kopete's protocols to the KDE address book,
        e.g. names, email addresses and phone numbers. Will Stephenson
      o Plugin to invite MSN contacts to uses gnomemeeting. Olivier Goffart
      o "Send Email..." context menu entry. Reuben Sutton
      o ICQ, support mimetype application/x-icq to add contacts Stefan Gehn
      o AIM, support aim: protocol to add contacts Stefan Gehn
      o ICQ, support for ignore-, invisible- and visible-list Stefan Gehn
      o MSN incoming File transfers trought the chat session as MSN
        Messenger 6 does.Olivier Goffart
    * Remote Desktop Connection (krdc)
      o Rewrote the RDP client to use an external rdesktop process, which
        includes support for RDP 5. Currently this requires a patched
        rdesktop version to be installed. Future rdesktop versions will have
        this support built-in. Arend van Beelen jr.
      o Switch to enforce the local cursor. Tim Jansen
    * KWiFiManager
      o when multiple cards are in use, each instance shows information
        for one card Stefan Winter
      o major code cleanup Stefan Winter
      o support for wireless scanning Stefan Winter
    * File Sharing
      o Create an advanced fileshare Control Center module, based on
        KSambaPlugin and KNFSPlugin Jan Schaefer
      o Create an advanced Konqueror properties dialog plugin, based on
        KSambaPlugin and KNFSPlugin Jan Schaefer

--- net/kdenetwork3/buildlink3.mk
+++ net/kdenetwork3/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1 2004/04/20 13:02:56 markd Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 KDENETWORK_BUILDLINK3_MK:=	${KDENETWORK_BUILDLINK3_MK}+
@@ -18,6 +18,7 @@ BUILDLINK_PKGSRCDIR.kdenetwork?=	../../net/kdenetwork3
 
 .include "../../converters/uulib/buildlink3.mk"
 .include "../../databases/gdbm/buildlink3.mk"
+.include "../../devel/libidn/buildlink3.mk"
 .include "../../net/openslp/buildlink3.mk"
 .include "../../x11/kdebase3/buildlink3.mk"
 .include "../../x11/kdelibs3/buildlink3.mk"
