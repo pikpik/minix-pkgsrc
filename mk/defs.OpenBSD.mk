b1139e7c9f7447bd0d990a343965d2bc6e2b2ef1
jlam <jlam>
2004-08-27 06:29:11 +0000

    Replace RPATH_FLAG with LINKER_RPATH_FLAG and COMPILER_RPATH_FLAG, which are the full option names used to set rpath directives for the linker and the compiler, respectively.  In places were we are invoking the linker, use "${LINKER_RPATH_FLAG} <path>", where the space is inserted in case the flag is a word, e.g. -rpath.  The default values of *_RPATH_FLAG are set by the compiler/*.mk files, depending on the compiler that you use.  They may be overridden on a ${OPSYS}-specific basis by setting _OPSYS_LINKER_RPATH_FLAG and _OPSYS_COMPILER_RPATH_FLAG, respectively.  Garbage-collect _OPSYS_RPATH_NAME and _COMPILER_LD_FLAG.

--- mk/defs.OpenBSD.mk
+++ mk/defs.OpenBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defs.OpenBSD.mk,v 1.35 2004/06/18 11:22:41 kristerw Exp $
+# $NetBSD: defs.OpenBSD.mk,v 1.36 2004/07/21 21:50:41 xtraeme Exp $
 #
 # Variable definitions for the OpenBSD operating system.
 
@@ -129,7 +129,6 @@ _OPSYS_HAS_MANZ=	yes	# MANZ controls gzipping of man pages
 _OPSYS_HAS_OSSAUDIO=	yes	# libossaudio is available
 _OPSYS_PERL_REQD=		# no base version of perl required
 _OPSYS_PTHREAD_AUTO=	no	# -lpthread needed for pthreads
-_OPSYS_RPATH_NAME=	-R	# name of symbol in rpath directive to linker 
 _OPSYS_SHLIB_TYPE=	ELF/a.out	# shared lib type
 _PATCH_CAN_BACKUP=	yes	# native patch(1) can make backups
 .if ${OS_VERSION} >= 3.4
