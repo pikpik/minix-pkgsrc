e4373bd5379f0f6de3e38538e69cae7eee5ca8f5
jlam <jlam>
2004-08-30 05:03:32 +0000

    Check for the right output on a platform that has _USE_RPATH == no.

--- regress/buildlink-transform/tests/rpath-pkgsubdir.mk
+++ regress/buildlink-transform/tests/rpath-pkgsubdir.mk
@@ -1,8 +1,15 @@
-# $NetBSD$
+# $NetBSD: rpath-pkgsubdir.mk,v 1.1.1.1 2004/08/27 21:34:58 jlam Exp $
 
 .PHONY: rpath-pkgsubdir
 do-regress: rpath-pkgsubdir
 rpath-pkgsubdir:
 	@test="${COMPILER_RPATH_FLAG}${LOCALBASE}/lib/mysql";		\
-	expected="${COMPILER_RPATH_FLAG}${LOCALBASE}/lib/mysql";	\
+	case "${_USE_RPATH}" in						\
+	[yY][eE][sS])							\
+		expected="${COMPILER_RPATH_FLAG}${LOCALBASE}/lib/mysql"; \
+		;;							\
+	*)								\
+		expected="";						\
+		;;							\
+	esac;								\
 	${TEST_RESULT}
