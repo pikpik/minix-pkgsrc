28145ffe028ad3fc4b99c7a2499ec472678400aa
reed <reed>
2004-08-31 23:16:23 +0000

    Added two patches for fixing possible security issue. The CVS security ID is CAN-2004-0797.
    The fix is same as used by OpenBSD, Debian and Gentoo.
    (Didn't see any reference to issue on zlib webpages.)
    
    The OpenBSD announcement "zlib reliabilty fix" says:
    "could allow an attacker to crash programs linked
    with it."
    
    And the Gentoo announcement says "zlib contains a bug in the handling
    of errors in the inflate() and inflateBack() functions. ... An
    attacker could exploit this vulnerability to launch a Denial of
    Service attack on any application using the zlib library."
    
    PKGREVISION is bumped and BUILDLINK_RECOMMENDED.zlib added to
    buildlink3.mk file.

--- devel/zlib/buildlink3.mk
+++ devel/zlib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2004/02/18 16:35:27 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2004/03/10 17:57:14 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 ZLIB_BUILDLINK3_MK:=	${ZLIB_BUILDLINK3_MK}+
@@ -12,6 +12,7 @@ BUILDLINK_PACKAGES+=	zlib
 
 .if !empty(ZLIB_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.zlib+=	zlib>=1.1.4nb1
+BUILDLINK_RECOMMENDED.zlib+=	zlib>=1.2.1nb2
 BUILDLINK_PKGSRCDIR.zlib?=	../../devel/zlib
 .endif	# ZLIB_BUILDLINK3_MK
 
