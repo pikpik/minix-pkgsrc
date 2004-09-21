4fcf54bd94ba252d808f1b8d19b059313627e55a
jmmv <jmmv>
2004-09-21 16:49:04 +0000

    Update to 2.8.0.  This version corresponds to GNOME 2.8.0. While here, convert to the bsd.options.mk framework to handle the cups functionality.
    libgnomeprint 2.8.0
    
    Jody
    	* Bump automake req to 1.7
    	* clean up some improperly namespaced private symbols
    	* Recognize .ttc files as truetype
    
    J.H.M. Dassen
    	* Explicitly require gmodule
    
    ------------------------------------------------------------------------------
    libgnomeprint 2.7.90
    
    Jody
    	http://bugzilla.gnome.org/show_bug.cgi?id=147349
    	* Improve test for zlib
    	http://bugzilla.gnome.org/show_bug.cgi?id=133924
    	* Fix transformations of text in pdf backend
    
    Kmaraas
    	* Patch some warnings from sparse
    
    Owen
    	http://bugzilla.gnome.org/show_bug.cgi?id=143878
    	* Fix gnome_font_find_closest to call gnome_font_face_find_closest
    
    Morten
    	* Keep more private symbols from polluting the global namespace.
    
    Suresh Chandrasekharan
    	http://bugzilla.gnome.org/show_bug.cgi?id=149858
    	* Don't modify the global font table
    
    ------------------------------------------------------------------------------
    libgnomeprint 2.7.2
    
    Andreas
    	* Fix 1#49160
    	* Fix #149161
    
    Jody
    	* #Fix 149452
    	* more portability
    
    Suresh Chandrasekharan
    	* Fix #148674
    	* Fix #149858
    
    ------------------------------------------------------------------------------
    libgnomeprint 2.7.1
    
    Andreas
    	* Add module to scan /etc/printcap
    
    Colin Walters :
    	* Support for new gnomeprintui printer list (state polling)
    
    Jody
    	* Some win32 build fixes
    	* Build fixes for sft code from glib
    
    J.H.M. Dassen (Ray) :
    	* link fixes
    
    Matthias Clasen:
    	* fix handling of no cups printers
    
    ------------------------------------------------------------------------------
    libgnomeprint 2.7.0
    
    Andreas
    	* Improve handling of multiple instances of a cups printer.
    	* Add all paper sizes to PDF writer
    
    Grzegorz Golawski
    	* Use G_MODULE_SUFFIX
    
    Owen Taylor :
    	* Improved pango integration (Requires Pango 1.5.0)
    
    Suresh Chandrasekharan :
    	* Improved font subsetting for PS and PDF backends
    
    Dom Lachowicz :
    	* Fix win32 compilation
    
    Jody Goldberg :
    	* Fix pdf generation
    
    J.H.M. Dassen (Ray):
    	* Fixed unresolved symbols in the CUPS module.

--- print/libgnomeprintui/buildlink3.mk
+++ print/libgnomeprintui/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:14 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:29:40 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOMEPRINTUI_BUILDLINK3_MK:=	${LIBGNOMEPRINTUI_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgnomeprintui}
 BUILDLINK_PACKAGES+=	libgnomeprintui
 
 .if !empty(LIBGNOMEPRINTUI_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libgnomeprintui+=	libgnomeprintui>=2.6.0
+BUILDLINK_DEPENDS.libgnomeprintui+=	libgnomeprintui>=2.8.0
 BUILDLINK_PKGSRCDIR.libgnomeprintui?=	../../print/libgnomeprintui
 .endif	# LIBGNOMEPRINTUI_BUILDLINK3_MK
 
