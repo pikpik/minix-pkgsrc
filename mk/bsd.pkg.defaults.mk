e050bd4d8674a68de4c269284e9efeffbfadbdd6
xtraeme <xtraeme>
2004-09-21 17:51:30 +0000

    X11_TYPE=xorg users:
    * Use builtin versions for Xfixes, fixesext, randrext and render.
      There's no need to install these packages from pkgsrc when using
      X.org >= 6.8.0, they were already installed.

--- mk/bsd.pkg.defaults.mk
+++ mk/bsd.pkg.defaults.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.defaults.mk,v 1.280 2004/09/10 19:51:50 jlam Exp $
+# $NetBSD: bsd.pkg.defaults.mk,v 1.281 2004/09/14 08:34:52 wiz Exp $
 #
 
 # A file providing defaults for pkgsrc and the packages collection.
@@ -174,6 +174,10 @@ VARBASE?=	/var
 X11ROOT_PREFIX?=	XFree86
 .elif defined(X11_TYPE) && !empty(X11_TYPE:Mxorg)
 X11ROOT_PREFIX?=	xorg
+USE_BUILTIN.fixesext=	yes
+USE_BUILTIN.Xfixes=	yes
+USE_BUILTIN.render=	yes
+USE_BUILTIN.randrext=	yes
 .else
 X11ROOT_PREFIX?=	# empty
 .endif
