5ced44618bbd464f38e01f3c30dd5174e55b4f55
tv <tv>
2004-10-15 11:54:11 +0000

    Update linkage to libltdl, now in its own package.

--- databases/unixodbc/buildlink3.mk
+++ databases/unixodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/25 01:11:11 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:24 tv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 UNIXODBC_BUILDLINK3_MK:=	${UNIXODBC_BUILDLINK3_MK}+
@@ -15,8 +15,5 @@ BUILDLINK_DEPENDS.unixodbc+=	unixodbc>=2.0.11nb1
 BUILDLINK_RECOMMENDED.unixodbc+=	unixodbc>=2.0.11nb3
 BUILDLINK_PKGSRCDIR.unixodbc?=	../../databases/unixodbc
 .endif	# UNIXODBC_BUILDLINK3_MK
-
-.include "../../devel/readline/buildlink3.mk"
-.include "../../devel/libtool/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
