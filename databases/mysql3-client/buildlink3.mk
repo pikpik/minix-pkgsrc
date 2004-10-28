145afb9dfa9d57e1a677514907376ae274d10e9d
xtraeme <xtraeme>
2004-10-28 01:17:33 +0000

    s/mysql/mysql3/ for Makefile.common and buildlink3.mk files.

--- databases/mysql3-client/buildlink3.mk
+++ databases/mysql3-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2004/10/03 00:13:21 tv Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/10/28 01:01:32 xtraeme Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 MYSQL_CLIENT_BUILDLINK3_MK:=	${MYSQL_CLIENT_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_PACKAGES+=	mysql-client
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=3.23.58
 BUILDLINK_RECOMMENDED.mysql-client+=	mysql-client>=3.23.58nb1
-BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
+BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql3-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 .endif	# MYSQL_CLIENT_BUILDLINK3_MK
 
