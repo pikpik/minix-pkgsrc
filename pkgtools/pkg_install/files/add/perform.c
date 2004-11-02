44b351c8e700f7198a8dc9ad46f4a1715055e850
erh <erh>
2004-11-02 00:10:15 +0000

    Add a -N option to pkg_delete to delete registration information for a package but not its files. Override PKG_DELETE when building this package so -N can be used to upgrade an existing install. Bump version to 20041101.

--- pkgtools/pkg_install/files/add/perform.c
+++ pkgtools/pkg_install/files/add/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.102 2004/07/30 11:35:46 agc Exp $	*/
+/*	$NetBSD: perform.c,v 1.24 2004/08/06 16:57:03 jlam Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -11,7 +11,7 @@
 #if 0
 static const char *rcsid = "from FreeBSD Id: perform.c,v 1.44 1997/10/13 15:03:46 jkh Exp";
 #else
-__RCSID("$NetBSD: perform.c,v 1.102 2004/07/30 11:35:46 agc Exp $");
+__RCSID("$NetBSD: perform.c,v 1.24 2004/08/06 16:57:03 jlam Exp $");
 #endif
 #endif
 
@@ -966,7 +966,7 @@ bomb:
 fail:
 	/* Nuke the whole (installed) show, XXX but don't clean directories */
 	if (!Fake)
-		delete_package(FALSE, FALSE, &Plist);
+		delete_package(FALSE, FALSE, &Plist, FALSE);
 
 success:
 	/* delete the packing list contents */
