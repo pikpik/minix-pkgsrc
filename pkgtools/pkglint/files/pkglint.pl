12705eb521656225a0a73e41a041230c10350556
he <he>
2004-11-04 21:17:40 +0000

    Ignore use of words which when used in make shell commands should be used in variable-expansion version, e.g. `test' vs `${TEST}' in settings of TEST_TARGET and ALL_TARGET.  Gets rid of warning about "test" in TEST_TARGET setting from lang/perl5/module.mk. Bump package version. Reviewed by wiz.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -11,7 +11,7 @@
 # Freely redistributable.  Absolutely no warranty.
 #
 # From Id: portlint.pl,v 1.64 1998/02/28 02:34:05 itojun Exp
-# $NetBSD: pkglint.pl,v 1.121 2004/10/16 15:04:26 wiz Exp $
+# $NetBSD: pkglint.pl,v 1.122 2004/10/28 13:03:10 wiz Exp $
 #
 # This version contains lots of changes necessary for NetBSD packages
 # done by Hubert Feyrer <hubertf@netbsd.org>,
@@ -1334,6 +1334,9 @@ EOF
 	$j =~ s/\n#[\n]*/\n#/;
 	# ...nor COMMENTs
 	$j =~ s/\nCOMMENT[\t ]*=[\t ]*[^\n]*\n/\nCOMMENT=#replaced\n/;
+	# ...nor settings of TEST_TARGET & ALL_TARGET
+	$j =~ s/\nTEST_TARGET[\t ]*.*=[\t ]*[^\n]*\n/\nTEST_TARGET=#replaced\n/;
+	$j =~ s/\nALL_TARGET[\t ]*.*=[\t ]*[^\n]*\n/\nALL_TARGET=#replaced\n/;
 	if ($opt_warn_directcmd) {
 		foreach my $i (keys %cmdnames) {
 			if ($j =~ /[ \t\/@]$i[ \t\n;]/) {
