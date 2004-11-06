65e836a10706764654a578623bfc68e407a47a18
grant <grant>
2004-11-06 15:09:22 +0000

    pull up revision 1.38 from src:
    Remove some code which makes file lookup rely on the fact that
    the first two directory entries are "." and "..".
    This behaviour is not required by applicable standards, and
    actually not provided by "coda".
    Now we get the "." and ".." into the per-directiry hash tables,
    but this should not hurt.
    
    fixes bmake build on Fedora Core 2, PR pkg/26140 from Shoichi Miyake.

--- bootstrap/bmake/dir.c
+++ bootstrap/bmake/dir.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: dir.c,v 1.1.1.1 2002/09/19 10:40:07 agc Exp $	*/
+/*	$NetBSD: dir.c,v 1.38 2003/09/09 14:44:35 drochner Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990 The Regents of the University of California.
@@ -39,20 +39,20 @@
  */
 
 #ifdef MAKE_BOOTSTRAP
-static char rcsid[] = "$NetBSD: dir.c,v 1.1.1.1 2002/09/19 10:40:07 agc Exp $";
+static char rcsid[] = "$NetBSD: dir.c,v 1.1.1.1 2004/03/11 13:04:07 grant Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)dir.c	8.2 (Berkeley) 1/2/94";
 #else
-__RCSID("$NetBSD: dir.c,v 1.1.1.1 2002/09/19 10:40:07 agc Exp $");
+__RCSID("$NetBSD: dir.c,v 1.1.1.1 2004/03/11 13:04:07 grant Exp $");
 #endif
 #endif /* not lint */
 #endif
 
 #if !defined(MAKE_BOOTSTRAP) && !defined(lint)
-__IDSTRING(rcs_id,"$Id: dir.c,v 1.1.1.1 2004/03/11 13:04:07 grant Exp $");
+__IDSTRING(rcs_id,"$Id: dir.c,v 1.2 2004/11/06 15:09:22 grant Exp $");
 #endif
 
 /*-
@@ -1251,12 +1251,6 @@ Dir_AddDir (path, name)
 	    p->hits = 0;
 	    p->refCount = 1;
 	    Hash_InitTable (&p->files, -1);
-
-	    /*
-	     * Skip the first two entries -- these will *always* be . and ..
-	     */
-	    (void)readdir(d);
-	    (void)readdir(d);
 
 	    while ((dp = readdir (d)) != (struct dirent *) NULL) {
 #if defined(sun) && defined(d_ino) /* d_ino is a sunos4 #define for d_fileno */
