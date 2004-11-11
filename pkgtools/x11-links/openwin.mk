1dd0a47bd945a68fc9c77f6e3fc9c5fc65b0fc36
sketch <sketch>
2004-11-11 17:44:39 +0000

    Fix checks for __STDC__ in older versions of OpenWindows headers so that the SunPro compiler gets the correct syntax for macro concatenation.
    Bump PKGREVISION.

--- pkgtools/x11-links/openwin.mk
+++ pkgtools/x11-links/openwin.mk
@@ -1,3 +1,21 @@
-# $NetBSD$
+# $NetBSD: openwin.mk,v 1.1 2003/09/02 07:14:42 jlam Exp $
 
 FILES_LIST=	${FILESDIR}/openwin
+
+# Fix bugs in older versions of openwin headers
+STDC_REPLACE=	-e 's/^\#if (__STDC__/\#if (defined(__STDC__)/'
+STDC_REPLACE+=	-e 's/^\#if ((__STDC__/\#if ((defined(__STDC__)/'
+
+CREATE_X11LINK=	case $$file in						\
+		include/X11/Xlibint.h)					\
+			${SED} ${STDC_REPLACE} < $$src > $$dest;	\
+			;;						\
+		include/X11/Xmd.h)					\
+			${SED} ${STDC_REPLACE} < $$src > $$dest;	\
+			;;						\
+		include/X11/extensions/multibufst.h)			\
+			${SED} ${STDC_REPLACE} < $$src > $$dest;	\
+			;;						\
+		*)							\
+			${LN} -s $$src $$dest;				\
+		esac
