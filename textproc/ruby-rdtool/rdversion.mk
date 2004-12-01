f5a2602433dba12cdd85229f56ebcc413c254fc2
taca <taca>
2004-12-01 15:32:39 +0000

    Add RUBY_RD_LIST; list of possible ruby-rdtool's package names which made from RUBY_VERSION_LIST.

--- textproc/ruby-rdtool/rdversion.mk
+++ textproc/ruby-rdtool/rdversion.mk
@@ -1,6 +1,19 @@
-# $NetBSD: Makefile.common,v 1.7 2003/09/10 15:57:31 taca Exp $
+# $NetBSD: rdversion.mk,v 1.1 2004/11/27 15:28:30 taca Exp $
 #
+
+.ifndef _RD_VERSION_MK
+_RD_VERSION_MK=		# defined
+
 .include "../../lang/ruby/rubyversion.mk"
 
 RUBY_RD=		${LOCALBASE}/bin/rd2
 RUBY_RD_VERSION =	0.6.14
+RUBY_RD_REQD=		0.6.14
+
+# create depends list
+.for f in ${RUBY_VERSION_LIST}
+_RD_NAME+=	ruby${f}-rdtool
+.endfor
+RUBY_RD_LIST=	${_RD_NAME:ts,}
+
+.endif
