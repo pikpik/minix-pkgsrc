71cbd8b82120f768e7aa8b8affce2ccd24ec6f48
tron <tron>
2004-12-30 16:42:22 +0000

    Backout last change because the check in "buildlink3.mk" doesn't work and therefore causes build failures in e.g. the "kdewebdev3" package.

--- textproc/libxslt/buildlink3.mk
+++ textproc/libxslt/buildlink3.mk
@@ -16,13 +16,8 @@ BUILDLINK_RECOMMENDED.libxslt+=	libxslt>=1.1.10nb1
 BUILDLINK_PKGSRCDIR.libxslt?=	../../textproc/libxslt
 .endif	# LIBXSLT_BUILDLINK3_MK
 
-PKG_OPTIONS.libxslt?=       ${PKG_DEFAULT_OPTIONS}
-
-.if !empty(PKG_OPTIONS.libxslt:Mcrypto)
-.  include "../../security/libgcrypt/buildlink3.mk"
-.endif
-
 .include "../../lang/perl5/buildlink3.mk"
+.include "../../security/libgcrypt/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
