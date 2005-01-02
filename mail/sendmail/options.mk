27adbcbc6071fa7e2f47d09a05971a6433be28e1
adrianp <adrianp>
2005-01-02 11:59:36 +0000

    Remove inet6 from PKG_DEFAULT_OPTIONS as this can cause the package to build in IPv6 support on systems where USE_INET6 has been set to NO.
    This addresses PR #28708 opened by georg.schwarz (at) freenet.de

--- mail/sendmail/options.mk
+++ mail/sendmail/options.mk
@@ -1,11 +1,11 @@
-# $NetBSD: options.mk,v 1.2 2004/09/02 21:09:17 adrianp Exp $
+# $NetBSD: options.mk,v 1.3 2004/11/17 19:56:49 xtraeme Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sendmail
 PKG_SUPPORTED_OPTIONS=	inet6 db2 db4 ldap sasl sasl2 starttls tcpwrappers \
 			socketmap
 
 .if !defined(PKG_OPTIONS.sendmail)
-PKG_DEFAULT_OPTIONS+=	inet6 tcpwrappers
+PKG_DEFAULT_OPTIONS+=	tcpwrappers
 .endif
 
 .include "../../mk/bsd.options.mk"
