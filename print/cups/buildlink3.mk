c95aa18975534bda5f91a36e684cbb3a68a5f961
salo <salo>
2005-01-11 00:09:21 +0000

    Bump BUILDLINK_RECOMMENDED after latest security update.

--- print/cups/buildlink3.mk
+++ print/cups/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2004/12/28 23:18:21 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2005/01/04 14:48:22 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 CUPS_BUILDLINK3_MK:=	${CUPS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	cups
 
 .if !empty(CUPS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.cups+=	cups>=1.1.19nb3
-BUILDLINK_RECOMMENDED.cups+=	cups>=1.1.20nb4
+BUILDLINK_RECOMMENDED.cups+=	cups>=1.1.23
 BUILDLINK_PKGSRCDIR.cups?=	../../print/cups
 .endif	# CUPS_BUILDLINK3_MK
 
