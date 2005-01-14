725f6cfaa374a53dda81b5bfa8bb8d751c4741b5
cube <cube>
2005-01-14 17:15:21 +0000

    Add support for versions such as 2.0_STABLE.
    XXX:  actually, it would be nice if pkg_add didn't complain when adding a
          2.0 package to a 2.0_STABLE system.

--- pkgtools/libkver/files/lib/kver.c
+++ pkgtools/libkver/files/lib/kver.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: kver.c,v 1.4 2003/12/13 17:45:59 seb Exp $	*/
+/*	$NetBSD: kver.c,v 1.5 2004/07/06 09:45:38 cube Exp $	*/
 
 #include <sys/types.h>
 #include <sys/param.h>
@@ -93,6 +93,10 @@ str2osrevision(char *s)
 			return KVER_INVALID_OSRELEASE;
 		return r;
 	}
+	/* or optional underscore followed by release status */
+	if (*s == '_')
+		/* ignore the trailing label */
+		return r;
 	/* or optional letters: release */
 	n = 0;
 	c = 'Z';
