e7f43e5e17b54962e649e554c786fed8d3bbec52
jlam <jlam>
2005-01-20 15:22:39 +0000

    Also match NetBSD security releases, e.g. 2.0.1, when deciding whether using dlopen() implies linking against libpthread.  Idea for fix from PR pkg/29022 and probably also fixes PR pkg/28800.

--- mk/dlopen.builtin.mk
+++ mk/dlopen.builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dlopen.builtin.mk,v 1.8 2004/12/23 14:27:04 jlam Exp $
+# $NetBSD: dlopen.builtin.mk,v 1.9 2005/01/03 04:57:51 minskim Exp $
 
 .for _lib_ in dl
 .  if !defined(_BLNK_LIB_FOUND.${_lib_})
@@ -36,7 +36,8 @@ USE_BUILTIN.dl=		no	# Darwin uses devel/dlcompat
 # support.
 #
 _DLOPEN_REQUIRE_PTHREAD_PLATFORMS=					\
-	NetBSD-2.[0-9]-* NetBSD-2.[0-9]_*-*				\
+	NetBSD-2.[0-9]_*-*						\
+	NetBSD-2.[0-9]-* NetBSD-2.[0-9].[0-9]*-*			\
 	NetBSD-2.[0-8][0-9]*-* NetBSD-2.9[0-8]*-*			\
 	NetBSD-2.99.[0-9]-* NetBSD-2.99.10-*
 
