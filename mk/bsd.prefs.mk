f7893c6624e90ea50160ed4a38279645a6edee17
tv <tv>
2005-01-27 04:05:08 +0000

    Move the PREPEND_PATH for LOCALBASE/bin (and optionally X11BASE/bin) to a spot that will come before compiler.mk (in bsd.prefs.mk).  Previously, LOCALBASE/bin was appearing earlier in the path than work/.<compiler>/bin, which could cause the Wrong Thing to happen.

--- mk/bsd.prefs.mk
+++ mk/bsd.prefs.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.prefs.mk,v 1.176 2004/10/31 22:05:02 wiz Exp $
+# $NetBSD: bsd.prefs.mk,v 1.177 2004/11/20 04:37:08 grant Exp $
 #
 # Make file, included to get the site preferences, if any.  Should
 # only be included by package Makefiles before any .if defined()
@@ -535,6 +535,9 @@ WRKLOG?=		${WRKDIR}/.work.log
 
 PKG_DEFAULT_OPTIONS?=	# empty
 PKG_OPTIONS?=		# empty
+
+# we want this *before* compiler.mk, so that compiler.mk paths override them
+PREPEND_PATH+=		${LOCALBASE}/bin ${USE_IMAKE:D${X11BASE}/bin}
 
 # Wrapper framework definitions
 .if exists(${PKGSRCDIR}/mk/wrapper/wrapper-defs.mk)
