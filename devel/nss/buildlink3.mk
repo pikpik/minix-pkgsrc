e68070e0cd617b682fc2fbd5a04a2951bc5e24ec
jschauma <jschauma>
2005-02-03 18:05:40 +0000

    We can't install these libraries into ${PREFIX}/lib, since mozilla browsers might then falsely load these instead of their own. So: Install the libraries into their own directory. Bump PKGREVISION.

--- devel/nss/buildlink3.mk
+++ devel/nss/buildlink3.mk
@@ -11,9 +11,17 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nnss}
 BUILDLINK_PACKAGES+=	nss
 
 .if !empty(NSS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.nss+=		nss>=3.9.2
-BUILDLINK_RECOMMENDED.nss+=	nss>=3.9.2
+BUILDLINK_DEPENDS.nss+=		nss>=3.9.2nb1
+BUILDLINK_RECOMMENDED.nss+=	nss>=3.9.2nb1
 BUILDLINK_PKGSRCDIR.nss?=	../../devel/nss
+
+BUILDLINK_FILES.nss+=   	lib/nss/*
+BUILDLINK_FILES.nss+=   	include/nss/*
+
+BUILDLINK_INCDIRS.nss+=        include/nss
+BUILDLINK_LIBDIRS.nss+=        lib/nss
+BUILDLINK_RPATHDIRS.nss+=      lib/nss
+
 .endif  # NSS_BUILDLINK3_MK
 
 .include "../../devel/nspr/buildlink3.mk"
