f573afaec2edaa0aaa1cf8058556ed2c582a181f
tv <tv>
2005-02-28 01:44:17 +0000

    Use boost-<foo>-1.32.* as the dependency version pattern, to ensure that ABI is consistent with dependents.  (This works around the fact that the sonames of Boost libraries do not change between ABI-incompatible versions, or in other words:  they don't have major version numbers.)

--- devel/boost-python/buildlink3.mk
+++ devel/boost-python/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2005/01/24 20:12:09 recht Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2005/02/26 22:48:35 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BOOST_PYTHON_BUILDLINK3_MK:=	${BOOST_PYTHON_BUILDLINK3_MK}+
@@ -11,6 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nboost-python}
 BUILDLINK_PACKAGES+=	boost-python
 
 .if !empty(BOOST_PYTHON_BUILDLINK3_MK:M+)
+# Use a dependency pattern that guarantees the proper ABI.
 BUILDLINK_DEPENDS.boost-python+=	boost-python-1.32.*
 BUILDLINK_PKGSRCDIR.boost-python?=	../../devel/boost-python
 
