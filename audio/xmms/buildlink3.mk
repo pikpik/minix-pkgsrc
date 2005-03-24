a7940bae762e252d6bd03c69173a887a64126b24
wiz <wiz>
2005-03-24 10:21:17 +0000

    Does not really need GLUT or GLU, so replace Mesa/bl3 with MesaLib/bl3. Addresses PR 29778 by Richard Rauch. Bump PKGREVISION.

--- audio/xmms/buildlink3.mk
+++ audio/xmms/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 19:25:08 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/03/18 09:12:09 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XMMS_BUILDLINK3_MK:=	${XMMS_BUILDLINK3_MK}+
@@ -20,7 +20,7 @@ PTHREAD_OPTS+=	require
 .include "../../audio/libmikmod/buildlink3.mk"
 .include "../../audio/libvorbis/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
-.include "../../graphics/Mesa/buildlink3.mk"
+.include "../../graphics/MesaLib/buildlink3.mk"
 .include "../../textproc/libxml/buildlink3.mk"
 .include "../../x11/gtk/buildlink3.mk"
 
