2e687ea4638099a1f17301d6461f00a113a754a3
abs <abs>
2005-04-06 18:33:57 +0000

    update jakarta-tomcat5-5.0.30 to nb1 Install into ${PREFIX}/tomcat5 in the same way as tomcat4 does, to avoid conflicts with tomcat55.

--- www/jakarta-tomcat5/files/tomcat5.sh
+++ www/jakarta-tomcat5/files/tomcat5.sh
@@ -1,6 +1,6 @@
 #! @RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: tomcat.sh,v 1.10 2004/04/29 18:24:55 erh Exp $
+# $NetBSD: tomcat.sh,v 1.1.1.1 2005/04/01 13:44:58 abs Exp $
 #
 # PROVIDE: tomcat
 # REQUIRE: DAEMON
