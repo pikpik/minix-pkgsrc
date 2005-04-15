51fd58b3b5268aab10a4f1951efce526974cc5ca
jlam <jlam>
2005-04-15 02:04:57 +0000

    Add a new yes/no variable USE_NEW_TOOLS to ease testing the new tools framework.  USE_NEW_TOOLS defaults to "no".

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1609 2005/04/13 14:07:30 jlam Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1610 2005/04/13 16:15:59 rillig Exp $
 #
 # This file is in the public domain.
 #
@@ -926,7 +926,12 @@ USE_LANGUAGES?=		# empty
 #
 .include "../../mk/compiler.mk"
 
+USE_NEW_TOOLS?=	no
+.if !empty(USE_NEW_TOOLS:M[yY][eE][sS])
+.include "../../mk/tools/bsd.tools.mk"
+.else
 .include "../../mk/tools.mk"
+.endif
 
 .include "../../mk/wrapper/bsd.wrapper.mk"
 
