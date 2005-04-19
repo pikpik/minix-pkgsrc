62a23e1ec4236ad567589aa96465602371b4bc6b
epg <epg>
2005-04-19 14:55:30 +0000

    Include ../../converters/libiconv/buildlink3.mk in buildlink3.mk, and let it worry about whether libiconv is built-in or not.  Remove all references to libiconv from builtin.mk.
    The logic in builtin.mk was broken and unnecessary, leading to a
    build failure on at least some Linux systems (such as Debian woody
    without any gettext packages installed).

--- devel/gettext-lib/buildlink3.mk
+++ devel/gettext-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2004/05/17 21:32:34 seb Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2004/10/03 00:13:27 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GETTEXT_BUILDLINK3_MK:=	${GETTEXT_BUILDLINK3_MK}+
@@ -16,7 +16,6 @@ BUILDLINK_RECOMMENDED.gettext+=	gettext-lib>=0.11.5nb4
 BUILDLINK_PKGSRCDIR.gettext?=	../../devel/gettext-lib
 .endif	# GETTEXT_BUILDLINK3_MK
 
-# libiconv/buildlink3.mk is included by gettext-lib/builtin.mk
-#.include "../../converters/libiconv/buildlink3.mk"
+.include "../../converters/libiconv/buildlink3.mk"
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
