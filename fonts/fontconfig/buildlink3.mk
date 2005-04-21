28330925b160df558cdb9d66af49ed6cea2043e5
kristerw <kristerw>
2005-04-21 17:40:19 +0000

    Include converters/libiconv/buildlink3.mk.

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2004/03/18 09:12:11 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2004/10/03 00:14:42 tv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FONTCONFIG_BUILDLINK3_MK:=	${FONTCONFIG_BUILDLINK3_MK}+
@@ -16,6 +16,7 @@ BUILDLINK_RECOMMENDED.fontconfig+=	fontconfig>=2.2.96nb1
 BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 .endif	# FONTCONFIG_BUILDLINK3_MK
 
+.include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../textproc/expat/buildlink3.mk"
