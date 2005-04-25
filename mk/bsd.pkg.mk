5cdc74a6b51937791546400d7ca9237e4454052f
drochner <drochner>
2005-04-25 16:47:18 +0000

    whitespace fix -- does nobody use emacs here?

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1615 2005/04/21 15:53:53 tv Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1616 2005/04/22 02:20:22 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -2904,7 +2904,7 @@ check-files:
 	         ${WRKDIR}/.files.diff ${WRKDIR}/.files.expected;	\
 	${TEST} $$errors -eq 0
 .endif
-	
+
 .PHONY: acquire-extract-lock acquire-patch-lock acquire-tools-lock
 .PHONY: acquire-wrapper-lock acquire-configure-lock acquire-build-lock
 .PHONY: acquire-install-lock acquire-package-lock
