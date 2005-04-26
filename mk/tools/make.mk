b3481453e8284729b5f4504f0d6ce20f603d35eb
jlam <jlam>
2005-04-26 15:32:05 +0000

    As of bsd.tool.mk:1.6, TOOLS_CMD.<tool> defaults to ${TOOLS_DIR}/bin/<tool>, so remove redundant settings.

--- mk/tools/make.mk
+++ mk/tools/make.mk
@@ -1,9 +1,8 @@
-# $NetBSD: make.mk,v 1.1 2005/04/15 00:00:21 jlam Exp $
+# $NetBSD: make.mk,v 1.2 2005/04/22 04:19:26 jlam Exp $
 
 # Always provide a symlink from ${TOOLS_DIR}/bin/make to the "make"
 # used to build the package.  This lets a bare "make" invoke the
 # correct program if called from within a makefile or script.
 #
 TOOLS_SYMLINK+=		make
-TOOLS_CMD.make=		${TOOLS_DIR}/bin/make
 TOOLS_REAL_CMD.make=	${MAKE_PROGRAM}
