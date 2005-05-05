21c2d1a38d84fa749d76cc0e9739b2c206379dd2
jlam <jlam>
2005-05-05 18:59:25 +0000

    IRIX and Interix use the install-sh script from bootstrap as their install tool.  This is handled directly within bootstrap.mk, so remove these unnecessary definitions.

--- mk/tools/tools.Interix.mk
+++ mk/tools/tools.Interix.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.Interix.mk,v 1.7 2005/05/04 06:42:43 jlam Exp $
+# $NetBSD: tools.Interix.mk,v 1.8 2005/05/05 18:47:00 jlam Exp $
 #
 # System-supplied tools for the Interix operating system.
 
@@ -29,7 +29,6 @@ TOOLS_PLATFORM.gzip?=		/usr/contrib/bin/gzip -nf ${GZIP}
 TOOLS_PLATFORM.head?=		/bin/head
 TOOLS_PLATFORM.hostname?=	/bin/hostname
 TOOLS_PLATFORM.id?=		/bin/id
-TOOLS_PLATFORM.install?=	/bin/install
 TOOLS_PLATFORM.ldconfig?=	/bin/true
 TOOLS_PLATFORM.ln?=		/bin/ln
 TOOLS_PLATFORM.ls?=		/bin/ls
