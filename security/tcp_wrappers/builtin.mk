e20ab036d9a64073583c357dee603e7b8ca24570
jschauma <jschauma>
2005-05-10 02:41:15 +0000

    In addition to checking for the header, also check for the library, specifically, check in /usr/lib${ABI}, since it's possible that on multi-ABI platforms only one version is installed, in which case we'd need to build and install the other from pkgsrc.

--- security/tcp_wrappers/builtin.mk
+++ security/tcp_wrappers/builtin.mk
@@ -1,8 +1,8 @@
-# $NetBSD: builtin.mk,v 1.1 2004/03/10 17:57:15 jlam Exp $
+# $NetBSD: builtin.mk,v 1.2 2004/03/29 05:43:35 jlam Exp $
 
 .if !defined(IS_BUILTIN.tcp_wrappers)
 IS_BUILTIN.tcp_wrappers=	no
-.  if exists(/usr/include/tcpd.h)
+.  if exists(/usr/include/tcpd.h) && exists(/usr/lib${ABI}/libwrap.so)
 IS_BUILTIN.tcp_wrappers=	yes
 .  endif
 .endif	# IS_BUILTIN.tcp_wrappers
