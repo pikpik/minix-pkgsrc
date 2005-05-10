5880c84c982a7e5dca75b34a84b5d947f1d4325d
jlam <jlam>
2005-05-10 19:52:30 +0000

    Split out "strip" from the USE_TOOLS processing in replace.mk.  Instead, we create a "strip" tool unconditionally, and make it either a no-op or the real thing depending on the whether we want unstripped files or not.

---
+++ mk/tools/strip.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+#
+# Depending on whether _INSTALL_UNSTRIPPED is defined, we cause
+# ${TOOLS_DIR}/bin/strip to be either the correct strip tool or a no-op.
+#
+.if defined(_INSTALL_UNSTRIPPED) || !defined(TOOLS_PLATFORM.strip)
+TOOLS_NOOP+=		strip
+.else
+TOOLS_CREATE+=		strip
+TOOLS_REAL_CMD.strip=	${TOOLS_PLATFORM.strip}
+.endif
+
+# Make ${STRIP} call "strip" through the PATH, which should find the one
+# under ${TOOLS_DIR}.
+#
+STRIP?=		strip
