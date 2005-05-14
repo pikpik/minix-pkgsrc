22b2004975821bbc7d868da5eb0f9b54bd8a047f
rillig <rillig>
2005-05-14 10:14:29 +0000

    Fixed a typo when looking for /usr/bin/cmp.

--- mk/tools/tools.SunOS.mk
+++ mk/tools/tools.SunOS.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.SunOS.mk,v 1.11 2005/05/10 20:14:27 jlam Exp $
+# $NetBSD: tools.SunOS.mk,v 1.12 2005/05/14 01:04:37 rillig Exp $
 #
 # System-supplied tools for the Solaris operating system.
 
@@ -14,7 +14,7 @@ TOOLS_PLATFORM.chmod?=		/usr/bin/chmod
 TOOLS_PLATFORM.chown?=		/usr/bin/chown
 .if exists(/bin/cmp)
 TOOLS_PLATFORM.cmp?=		/bin/cmp
-.elif exists(/bin/cmp)
+.elif exists(/usr/bin/cmp)
 TOOLS_PLATFORM.cmp?=		/usr/bin/cmp
 .endif
 TOOLS_PLATFORM.cp?=		/bin/cp
