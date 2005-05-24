b370ecdb81c465445eea5dee986a330a888ac28a
frueauf <frueauf>
2005-05-24 19:01:34 +0000

    transform PKG_DEFAULT_OPTIONS+= to PKG_SUGGESTED_OPTIONS. Suggested by Jukka Salmi.

--- mail/fetchmail/options.mk
+++ mail/fetchmail/options.mk
@@ -1,11 +1,9 @@
-# $NetBSD: options.mk,v 1.7 2005/01/21 15:46:39 frueauf Exp $
+# $NetBSD: options.mk,v 1.8 2005/01/21 20:45:09 frueauf Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.fetchmail
 PKG_SUPPORTED_OPTIONS=	inet6 kerberos4 kerberos gssapi ssl
 
-.if !defined(PKG_OPTIONS.fetchmail)
-PKG_DEFAULT_OPTIONS+=	ssl
-.endif
+PKG_SUGGESTED_OPTIONS= ssl
 
 .include "../../mk/bsd.options.mk"
 
