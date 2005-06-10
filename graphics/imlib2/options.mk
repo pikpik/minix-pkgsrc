af7eac88b5942f79084b2a95436d76b6c861eece
wiz <wiz>
2005-06-10 18:21:45 +0000

    Slightly different solution to previous: just ignore mmx pkg_option if set on non-i386.

--- graphics/imlib2/options.mk
+++ graphics/imlib2/options.mk
@@ -1,14 +1,13 @@
-# $NetBSD: options.mk,v 1.1 2005/05/31 21:36:36 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2005/06/10 18:14:23 wiz Exp $
 
-.include "../../mk/bsd.prefs.mk"
-
-.if ${MACHINE_ARCH} == "i386"
 PKG_OPTIONS_VAR=	PKG_OPTIONS.imlib2
 PKG_SUPPORTED_OPTIONS=	mmx
 
 .include "../../mk/bsd.options.mk"
+.include "../../mk/bsd.prefs.mk"
 
 .if !empty(PKG_OPTIONS:Mmmx)
+.if ${MACHINE_ARCH} == "i386"
 CONFIGURE_ARGS+=	--enable-mmx
 .endif
 .endif
