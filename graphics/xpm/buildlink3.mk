1e6117aafc2043c014ffacd9d858e2f4131ea07e
jlam <jlam>
2005-06-14 18:10:37 +0000

    Apply fixes derived from the HEAD branch of X.Org (6.8.99) to address problems noted in CAN-2004-0914:
        Multiple vulnerabilities in libXpm for 6.8.1 and earlier, as
        used in XFree86 and other packages, include (1) multiple integer
        overflows, (2) out-of-bounds memory accesses, (3) directory
        traversal, (4) shell metacharacter, (5) endless loops, and (6)
        memory leaks, which could allow remote attackers to obtain
        sensitive information, cause a denial of service (application
        crash), or execute arbitary code via a certain XPM image file.
    
    Bump PKGREVISION to 4.  Since this is a security-related fix, also
    bump the BUILDLINK_RECOMMENDED version for this package.

--- graphics/xpm/buildlink3.mk
+++ graphics/xpm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.16 2004/09/16 15:09:01 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2005/06/01 18:02:58 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 XPM_BUILDLINK3_MK:=	${XPM_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	xpm
 
 .if !empty(XPM_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.xpm+=		xpm>=3.4k
-BUILDLINK_RECOMMENDED.xpm?=	xpm>=3.4knb2
+BUILDLINK_RECOMMENDED.xpm?=	xpm>=3.4knb4
 BUILDLINK_PKGSRCDIR.xpm?=	../../graphics/xpm
 .endif	# XPM_BUILDLINK3_MK
 
