cbaffe732f72c7fbecb09a756da8f9adc411c922
adam <adam>
2005-07-22 21:21:47 +0000

    Changes 2.6.3: * Gtk::Action::get_proxies()   Use the correct ownership semantics. * Gdk::Display::list_devices()   Use the correct ownership semantics. * examples/book/menus_and_toolbars/   Distribute the .png file.
    Changes 2.6.2:
    * TreeView: When using the append_column() convenience API,
      prevent a crash when the window closes while the cell is
      editing.
    * Allow derived CellRenderers to be edited, by removing
      some debug code that sneaked into 2.6.1.
    * Documented many signals, based on the GTK+ documentation.
    
    Changes 2.6.1:
    * CellRenderer: Prevent an infinite loop.
    * MSVC++ .Net 2003 build fixes.
    * Documentation:
      - Hide deprecated widgets from the list of widgets.
      - Correct the description of the IO input example.
    
    Chamges 2.6.0:
    gtkmm 2.6 wraps new API in GTK+ 2.6, and is API/ABI-compatibile with gtkmm 2.4.

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/03/09 20:56:35 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/03/18 09:12:17 jlam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GTKMM_BUILDLINK3_MK:=	${GTKMM_BUILDLINK3_MK}+
@@ -11,11 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngtkmm}
 BUILDLINK_PACKAGES+=	gtkmm
 
 .if !empty(GTKMM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gtkmm+=	gtkmm>=2.2.8nb2
+BUILDLINK_DEPENDS.gtkmm+=	gtkmm>=2.6.3
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 .endif	# GTKMM_BUILDLINK3_MK
 
-.include "../../devel/libsigc++/buildlink3.mk"
+.include "../../devel/glibmm/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
