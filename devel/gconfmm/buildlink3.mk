bd2709b8150453b5288824e7182dc1f91baaaa5d
adam <adam>
2005-07-22 21:25:59 +0000

    Changes 2.10.0: gconfmm 2.10 is API/ABI-compatible with gconfmm 2.8.x and gconfmm 2.6.x. Changes compared to gconfmm 2.8: * Client: Added set_*_list() methods.

--- devel/gconfmm/buildlink3.mk
+++ devel/gconfmm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/12 05:11:44 snj Exp $
+# $NetBSD$
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GCONFMM_BUILDLINK3_MK:=	${GCONFMM_BUILDLINK3_MK}+
@@ -11,13 +11,11 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngconfmm}
 BUILDLINK_PACKAGES+=	gconfmm
 
 .if !empty(GCONFMM_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.gconfmm+=	gconfmm>=2.0.1nb5
-BUILDLINK_RECOMMENDED.gconfmm+=	gconfmm>=2.0.2nb1
+BUILDLINK_DEPENDS.gconfmm+=	gconfmm>=2.10.0
 BUILDLINK_PKGSRCDIR.gconfmm?=	../../devel/gconfmm
 .endif	# GCONFMM_BUILDLINK3_MK
 
 .include "../../devel/GConf2/buildlink3.mk"
-.include "../../devel/libsigc++/buildlink3.mk"
 .include "../../x11/gtkmm/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
