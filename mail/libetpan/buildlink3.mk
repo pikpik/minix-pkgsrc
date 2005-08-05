0d417c0e69c8c440b6b60007544811ec17ee5c21
xtraeme <xtraeme>
2005-08-05 19:29:56 +0000

    Update to 0.38.
        * release 0.37 - Deadwing
    
            * fixed SSL
            * IMAP improvements
            * SASL
            * windows port
            * fixed IMAP implementation (for sylpheed)
    
        * release 0.36 - Silver Drop
    
            * memory leak
            * fixed maildir support
    
        * release 0.35 - Dimebag Darrell
    
            * minor bugfixes.
    
        * release 0.34 - edge
    
            * general
               - support for debian systems mbox
               - ability to disable thread-safe support

--- mail/libetpan/buildlink3.mk
+++ mail/libetpan/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/07/08 14:41:34 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:12:53 tv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBETPAN_BUILDLINK3_MK:=	${LIBETPAN_BUILDLINK3_MK}+
@@ -11,8 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibetpan}
 BUILDLINK_PACKAGES+=	libetpan
 
 .if !empty(LIBETPAN_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.libetpan+=	libetpan>=0.32
-BUILDLINK_RECOMMENDED.libetpan+=	libetpan>=0.32nb1
+BUILDLINK_DEPENDS.libetpan+=	libetpan>=0.38
 BUILDLINK_PKGSRCDIR.libetpan?=	../../mail/libetpan
 .endif	# LIBETPAN_BUILDLINK3_MK
 
