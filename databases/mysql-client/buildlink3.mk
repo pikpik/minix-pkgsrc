f185b13cd4f1b616537fd0a8e577e77b2ab67bac
salo <salo>
2005-08-12 16:15:03 +0000

    Back the last change out after discussion with <xtraeme>. The ABI didn't change and the vulnerability is server side-only.

--- databases/mysql-client/buildlink3.mk
+++ databases/mysql-client/buildlink3.mk
@@ -11,7 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nmysql-client}
 BUILDLINK_PACKAGES+=	mysql-client
 
 .if !empty(MYSQL_CLIENT_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.0.25
+BUILDLINK_DEPENDS.mysql-client+=	mysql-client>=4.0.18nb1
+BUILDLINK_RECOMMENDED.mysql-client?=	mysql-client>=4.0.24
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql-client
 BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
