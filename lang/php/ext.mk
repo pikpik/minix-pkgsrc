07925390caff850e95bc6310074eca1ed704eab7
abs <abs>
2005-09-19 23:07:10 +0000

    Set PKGNAME= rather than PKGNAME?= Previously php-gettext would end up with a PKGNAME of php-{ver}, with obviously breakage.

--- lang/php/ext.mk
+++ lang/php/ext.mk
@@ -1,4 +1,4 @@
-# $NetBSD: ext.mk,v 1.6 2005/06/01 20:08:00 jlam Exp $
+# $NetBSD: ext.mk,v 1.7 2005/06/12 15:29:48 jdolecek Exp $
 #
 # PHP extension package framework, for both PECL and bundled PHP extensions.
 #
@@ -19,13 +19,13 @@ PLIST_SUBST+=		MODNAME=${PKGMODNAME}
 
 .if !defined(PECL_VERSION)
 # bundled extension
-PKGNAME?=		php-${MODNAME}-${PHP_BASE_VERS}
+PKGNAME=		php-${MODNAME}-${PHP_BASE_VERS}
 EXTRACT_ELEMENTS?=	${DISTNAME}/ext/${PKGMODNAME}
 WRKSRC?=		${WRKDIR}/${EXTRACT_ELEMENTS}
 DISTINFO_FILE=		${.CURDIR}/${PHPPKGSRCDIR}/distinfo
 .else
 # PECL extension
-PKGNAME?=		php-${MODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
+PKGNAME=		php-${MODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
 MASTER_SITES?=		http://pecl.php.net/get/
 DISTNAME=		${MODNAME}-${PECL_VERSION}
 DIST_SUBDIR=		php-${MODNAME}
