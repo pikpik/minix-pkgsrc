f8c318675e6b93e102245359b43727215fafb988
minskim <minskim>
2005-10-02 15:53:35 +0000

    Update py-wxWidgets to 2.6.1.  Based on patches provided by wulf@.
    Many changes since 2.4.2.4.  Now this package depends on x11/wxGTK
    instead of x11/wxGTK24.  See docs/changes.txt for complete list of
    changes.

--- x11/py-wxWidgets/buildlink3.mk
+++ x11/py-wxWidgets/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2004/12/25 19:56:28 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/08/13 09:28:49 veego Exp $
 
 BUILDLINK_DEPTH:=			${BUILDLINK_DEPTH}+
 PY_WXWIDGETS_BUILDLINK3_MK:=	${PY_WXWIDGETS_BUILDLINK3_MK}+
@@ -13,10 +13,10 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:N${PYPKGPREFIX}-wxWidgets}
 BUILDLINK_PACKAGES+=	${PYPKGPREFIX}-wxWidgets
 
 .if !empty(PY_WXWIDGETS_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.${PYPKGPREFIX}-wxWidgets+=	${PYPKGPREFIX}-wxWidgets>=2.4.2.4nb4
+BUILDLINK_DEPENDS.${PYPKGPREFIX}-wxWidgets+=	${PYPKGPREFIX}-wxWidgets>=2.6.1.0
 BUILDLINK_PKGSRCDIR.${PYPKGPREFIX}-wxWidgets?=	../../x11/py-wxWidgets
 
-.include "../../x11/wxGTK24/buildlink3.mk"
+.include "../../x11/wxGTK/buildlink3.mk"
 
 .endif	# PY_WXWIDGETS_BUILDLINK3_MK
 
