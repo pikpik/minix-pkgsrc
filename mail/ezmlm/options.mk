b2cacae936de06618f048cd2f4cbc67683d5178b
schmonz <schmonz>
2005-10-05 05:46:54 +0000

    Remove legacy DJB_ERRNO_HACK (replaced with djbware-errno-hack option).

--- mail/ezmlm/options.mk
+++ mail/ezmlm/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2005/08/19 04:44:09 schmonz Exp $
+# $NetBSD: options.mk,v 1.5 2005/10/05 04:28:00 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ezmlm
 PKG_SUPPORTED_OPTIONS+=	ezmlm-althash
 
 .if ${OPSYS} == "Darwin"
-PKG_SUGGESTED_OPTIONS=	ezmlm-althash
+PKG_SUGGESTED_OPTIONS+=	ezmlm-althash
 .endif
 
 .include "../../mk/bsd.options.mk"
--- mail/qmail/options.mk
+++ mail/qmail/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.15 2005/08/19 04:44:10 schmonz Exp $
+# $NetBSD: options.mk,v 1.16 2005/10/05 04:28:00 schmonz Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.qmail
 PKG_SUPPORTED_OPTIONS+=	darwin sasl syncdir tls qmail-badrcptto qmail-bigdns
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS+=	qmail-netqmail qmail-outgoingip qmail-qregex
 PKG_SUPPORTED_OPTIONS+=	qmail-realrcptto qmail-viruscan
 
 .if ${OPSYS} == "Darwin"
-PKG_SUGGESTED_OPTIONS=	darwin
+PKG_SUGGESTED_OPTIONS+=	darwin
 .endif
 
 .include "../../mk/bsd.options.mk"
