076555795127aefa4631d4a11abfcbc767bf7f22
jmmv <jmmv>
2005-10-08 17:18:47 +0000

    Update to 0.34.1:
    Version 0.34.1
    
    * automake/autoconf fixes and improvements
      (Rodney Dawes, Stepan Kasal)
    * extraction of XML attributes fixed
      (Danilo Segan)
    
    Version 0.34
    
    * Fix the regex for stripping the intltool type tags in POTFILES.in
      -- bug #171330 (Stepan Kasal)
    * Add notice about using DISTCLEANFILES for toplevel Makefile.am
      -- bug #309455 (Tim-Philipp Muller)
    * Discard not-well-formed translations when merging XML
      -- bug #170471 (Danilo Segan)
    * Correct handling of xml:space inheritance
      -- bug #160262 (Danilo Segan)
    * Merge multiple comments for the same string in XML files
      -- bug #171579 (Danilo Segan)
    * Update sed script to be more practictal for translatable XML
      -- bug #304365 (Nickolay V. Shmyrev)
    * Support disting files that may not exist for all packages via EXTRA_DISTFILES
      -- bugs #168788 and #171327 (Rodney Dawes)
    * Do not create the pot file during dist, but do build it for check
      (Stepan Kasal)
    * Add support for use on win32 native perl (Tor Lillqvist)
    * Do not dist .gmo files, and remove them with distclean
      Remove Makefile.in.in with maintainer-clean
      -- bug #166724 (Rodney Dawes)
    * Minor fixes to Makefile.in.in (Stepan Kasal)
    * Use --verbose instead of --statistics for msgfmt
      -- bug #165881
    * Remove the need for patching Makefile.in.in and just copy in our own
      -- bug #165066 (Rodney Dawes)

--- textproc/intltool/buildlink3.mk
+++ textproc/intltool/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2005/05/31 19:48:18 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2005/07/16 19:10:40 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 INTLTOOL_BUILDLINK3_MK:=	${INTLTOOL_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nintltool}
 BUILDLINK_PACKAGES+=	intltool
 
 .if !empty(INTLTOOL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.intltool+=	intltool>=0.32.1
+BUILDLINK_DEPENDS.intltool+=	intltool>=0.34.1
 BUILDLINK_PKGSRCDIR.intltool?=	../../textproc/intltool
 BUILDLINK_DEPMETHOD.intltool?=	build
 .endif	# INTLTOOL_BUILDLINK3_MK
