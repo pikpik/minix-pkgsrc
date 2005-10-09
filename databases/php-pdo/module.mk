15bb2a9ef0a4f7dbf6773f1a1f600da4942d9e61
jdolecek <jdolecek>
2005-10-09 13:13:55 +0000

    do not set PKGREVISION here

--- databases/php-pdo/module.mk
+++ databases/php-pdo/module.mk
@@ -1,11 +1,10 @@
-# $NetBSD$
+# $NetBSD: module.mk,v 1.1.1.1 2005/10/09 13:11:18 jdolecek Exp $
 
 # Items common for all PDO drivers
 
 MODNAME=		PDO_${PDO_DRIVER:tu}
 PKGMODNAME=		pdo_${PDO_DRIVER}
 PKGNAME=		php-${PKGMODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
-PKGREVISION=		# empty
 PDO_VERSION?=		1.0RC1
 PECL_VERSION?=		${PDO_VERSION}
 CATEGORIES+=		databases
