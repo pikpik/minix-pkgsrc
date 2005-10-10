fd486699ba7c50708e4a2f83723905ed7aa053c3
tv <tv>
2005-10-10 14:29:05 +0000

    ${ECHO} and ${TR}.

--- databases/php-pdo/module.mk
+++ databases/php-pdo/module.mk
@@ -1,8 +1,8 @@
-# $NetBSD: module.mk,v 1.2 2005/10/09 13:13:55 jdolecek Exp $
+# $NetBSD: module.mk,v 1.3 2005/10/10 14:12:05 tv Exp $
 
 # Items common for all PDO drivers
 
-PDO_DRIVER_TR=		echo ${PDO_DRIVER:Q} | tr a-z A-Z
+PDO_DRIVER_TR=		${ECHO} ${PDO_DRIVER:Q} | ${TR} a-z A-Z
 MODNAME=		PDO_${PDO_DRIVER_TR:sh}
 PKGMODNAME=		pdo_${PDO_DRIVER}
 PKGNAME=		php-${PKGMODNAME}-${PHP_BASE_VERS}.${PECL_VERSION}
