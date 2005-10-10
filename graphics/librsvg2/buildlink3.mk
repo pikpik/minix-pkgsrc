95cafc61920ca8bd9e67c57668de78d6bf5adfd5
jmmv <jmmv>
2005-10-10 13:59:00 +0000

    Update librsvg2 to 2.12.6.
    While doing this, merge the librsvg2-gtk package into librsvg2.  The reason
    is that from now on the core library (librsvg) uses gdk, so there is no
    gain in separating both things (the GTK+ dependency is needed anyway).
    
    Also enable gnome-vfs support, as one of our dependents (libgsf) is already
    pulling it in (so the new dependency is not "bloat").  For completeness,
    enable libgnomeprint* support too.
    
    2005-10-06  Dom Lachowicz <cinamod@hotmail.com>
    
    	* autogen.sh: Let librsvg build out of srcdir. Bug #158331
    	* test-display.c: Populate save dialogs with the SVG's name. Bug #318010.
    	* configure.in: Ditto
    	* rsvg-structure.c: Related to bug #318025, don't set has_width or has_height unless they are > 0.
    	* rsvg.c: Bug #168567, mega hack to make multiImage sort-of work.
    	* configure.in: 2.12.6
    
    2005-10-06  Dom Lachowicz <cinamod@hotmail.com>
    
    	* rsvg-paint-server.c: Backport Caleb's patch to 317871, fixing a crash
    	inside of libart for some gradients.
    	* rsvg-art-paint-server.c: Ditto
    
    2005-10-04  Dom Lachowicz <cinamod@hotmail.com>
    
    	* rsvg-shapes.c: Fix problems in non-C locales. Bug #317615
    	* configure.in: 2.12.5
    
    2005-10-01  Dom Lachowicz <cinamod@hotmail.com>
    
    	* configure.in: 2.12.4
    	* gdk-pixbuf-loader/io-svg.c: Because bug 308021 was fixed, and bug 317225 still
    	exists (although possibly not within librsvg itself), I've re-enabled a match for
    	"<?xml", with a priority of "1".
    
    2005-10-01  Dom Lachowicz <cinamod@hotmail.com>
    
    	* configure.in: bug 314593, support firefox-config
    
    2005-09-29  Dom Lachowicz <cinamod@hotmail.com>
    
    	* rsvg-filter.c: Fix bug #317549
    	* configure.in: 2.12.3
    
    2005-09-25  Dom Lachowicz <cinamod@hotmail.com>
    
    	* rsvg.c: Bug 309660 - signedness issues
    	* configure.in: 2.12.2
    
    2005-09-25  Dom Lachowicz <cinamod@hotmail.com>
    
    	* 2.12.1
    
    	* rsvg-structure.c:
    	* rsvg-shapes.c: Bug 371148 - C89 fixes
    
    	* gdk-pixbuf-loader/Makefile.am
    	* gtk-engine/Makefile.am: Bug 316803 - Win32 mingw build fixes
    
    	* moz-plugin/Makefile.am: Bug 316588 - add glib cflags to INCLUDES
    
    	* librsvg.spec.in: Backport from HEAD branch
    
    2005-09-22  Rodney Dawes  <dobey@novell.com>
    
    	* configure.in: GNOME_REQUIRE_PKGCONFIG doesn't exist, so remove it
    	so that we don't get a weird warning when running configure
    
    2005-09-16  Dom Lachowicz <cinamod@hotmail.com>
    
    	* gdk-pixbuf-loader/io-svg.c: Scan the first 1024 chars for <svg. 100
    	Wasn't sufficient for a lot of icon themes.
    
    2005-09-15  Dom Lachowicz <cinamod@hotmail.com>
    
    	* 2.12.0
    
    2005-08-29  Christian Schaller <uraeus@gnome.org>
    
    	* Do 2.11.1 release
    
    2005-08-26  Dom Lachowicz <cinamod@hotmail.com>
    
    	* rsvg.[ch]: Unbreak dpi API. See bug 314628
    	* test-rsvg.c: Ditto
    	* test-display.c: Ditto
    	* gdk-pixbuf-loader/io-svg.c: Backport for the GdkPixbufLoader module
    	API changes, sot hat it does better pattern recognition.
    
    2005-08-25  Caleb Moore <c.moore@student.unsw.edu.au>
    
    	* rsvg.c: disabled char nodes. Chopped off namespaces.
    	* configure.in: bumped version number to 2.11.0

--- graphics/librsvg2/buildlink3.mk
+++ graphics/librsvg2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2004/05/27 19:33:29 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2004/09/21 16:51:01 jmmv Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBRSVG2_BUILDLINK3_MK:=	${LIBRSVG2_BUILDLINK3_MK}+
@@ -16,13 +16,6 @@ BUILDLINK_PKGSRCDIR.librsvg2?=	../../graphics/librsvg2
 .endif	# LIBRSVG2_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libgsf/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
-.include "../../devel/popt/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../graphics/libart2/buildlink3.mk"
-.include "../../textproc/libcroco/buildlink3.mk"
-.include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
