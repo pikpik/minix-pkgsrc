e2ae0717e7a7c7085f2db4460dbe55578409903a
minskim <minskim>
2005-10-13 21:02:14 +0000

    Add gettext-lib/buildlink3.mk to match with Makefile.

--- devel/libidn/buildlink3.mk
+++ devel/libidn/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/05/26 14:55:21 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/10/03 00:13:30 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBIDN_BUILDLINK3_MK:=	${LIBIDN_BUILDLINK3_MK}+
@@ -17,6 +17,7 @@ BUILDLINK_PKGSRCDIR.libidn?=	../../devel/libidn
 .endif	# LIBIDN_BUILDLINK3_MK
 
 .include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/libgetopt/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
