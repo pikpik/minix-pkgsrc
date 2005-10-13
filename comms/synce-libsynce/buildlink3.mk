9449188ff34ba780c40a924960e6f675a0526123
wiz <wiz>
2005-10-13 15:00:13 +0000

    Initial import of synce-libsynce-0.9.1:
    Libsynce is part of the SynCE project.
    
    The SynCE library is required to compile (at least) the following
    parts of the SynCE project:
    
    	o librapi2
    	o dccmd

---
+++ comms/synce-libsynce/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SYNCE_LIBSYNCE_BUILDLINK3_MK:=	${SYNCE_LIBSYNCE_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	synce-libsynce
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsynce-libsynce}
+BUILDLINK_PACKAGES+=	synce-libsynce
+
+.if !empty(SYNCE_LIBSYNCE_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.synce-libsynce+=	synce-libsynce>=0.9.1
+BUILDLINK_PKGSRCDIR.synce-libsynce?=	../../comms/synce-libsynce
+.endif	# SYNCE_LIBSYNCE_BUILDLINK3_MK
+
+.include "../../converters/libiconv/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
