70d947169def86bff00a4a6855891dd868d1300d
wiz <wiz>
2005-10-13 15:03:42 +0000

    Initial import of synce-librapi2-0.9.1:
    Librapi is part of the SynCE project.
    
    The RAPI library is an open source implementation that works like
    RAPI.DLL, available on Microsoft operating systems. The library
    makes it possible to make remote calls to a computer running Pocket
    PC.
    
    In order to use librapi, a daemon that the Pocket PC client connects
    to must be running on the computer using librapi.

---
+++ comms/synce-librapi2/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+SYNCE_LIBRAPI2_BUILDLINK3_MK:=	${SYNCE_LIBRAPI2_BUILDLINK3_MK}+
+
+.if !empty(BUILDLINK_DEPTH:M+)
+BUILDLINK_DEPENDS+=	synce-librapi2
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsynce-librapi2}
+BUILDLINK_PACKAGES+=	synce-librapi2
+
+.if !empty(SYNCE_LIBRAPI2_BUILDLINK3_MK:M+)
+BUILDLINK_DEPENDS.synce-librapi2+=	synce-librapi2>=0.9.1
+BUILDLINK_PKGSRCDIR.synce-librapi2?=	../../comms/synce-librapi2
+.endif	# SYNCE_LIBRAPI2_BUILDLINK3_MK
+
+.include "../../comms/synce-libsynce/buildlink3.mk"
+
+BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
