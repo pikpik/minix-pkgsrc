fb3252f09d806147d3864ca2c4d8179c8cc2763b
wiz <wiz>
2005-10-31 09:45:47 +0000

    Split off more stuff into Makefile.common, for bacula-client. Add group argument to bacula-fd script. From Geert Hendrickx.

--- sysutils/bacula/options.mk
+++ sysutils/bacula/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2005/10/07 11:33:28 wiz Exp $
+# $NetBSD: options.mk,v 1.3 2005/10/10 15:27:45 wiz Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.bacula
 PKG_OPTIONS_REQUIRED_GROUPS=	database
@@ -6,11 +6,3 @@ PKG_OPTIONS_GROUP.database=	catalog-sqlite catalog-pgsql catalog-mysql
 PKG_SUGGESTED_OPTIONS=		catalog-sqlite
 
 .include "../../mk/bsd.options.mk"
-
-# Other options
-
-BACULA_GROUP?=		bacula
-BACULA_DIR_USER?=	bacula-dir
-BACULA_SD_USER?=	bacula-sd
-
-BUILD_DEFS=	BACULA_DIR_USER BACULA_SD_USER BACULA_GROUP
