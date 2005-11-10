5f59bd07c587f6140adee8a165ec1ca49e09ed4a
rillig <rillig>
2005-11-10 07:11:37 +0000

    Replaced another instance of grep|awk with a simple awk. A warning is printed if no package name could be found for a package directory.

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.91 2005/11/07 18:22:22 tv Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.92 2005/11/10 07:04:00 rillig Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@NetBSD.org>
@@ -365,8 +365,8 @@ bulk-package:
 			${SHCOMMENT} "Install required depends via binarypkgs XXX" ; \
 			${ECHO_MSG} "BULK> Installing packages which are required to build ${PKGNAME}." ;\
 			for pkgdir in `${SED} -n -e "/^${_ESCPKGPATH} / s;^[^:]*:;;p" ${DEPENDSFILE}` ${BULK_PREREQ} ; do \
-				pkgname=`${GREP} "^$$pkgdir " ${INDEXFILE} | ${AWK} '{print $$2}'` ; \
-				if [ -z "$$pkgname" ]; then continue ; fi ;\
+				pkgname=`${AWK} '$$1 == "'"$$pkgdir"'" { print $$2; }' ${INDEXFILE}`; \
+				if [ -z "$$pkgname" ]; then ${ECHO} "BULK> WARNING: could not find package name for directory $$pkgdir"; continue ; fi ;\
 				pkgfile=${PACKAGES}/All/$${pkgname}${PKG_SUFX} ;\
 				if ${PKG_INFO} -qe $$pkgname ; then \
 					${ECHO_MSG} "BULK> Required package $$pkgname ($$pkgdir) is already installed" ; \
