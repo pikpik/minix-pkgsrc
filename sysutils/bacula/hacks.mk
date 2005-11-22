e2be7c5506389dbc07278b6992735fe6cb0b0506
wiz <wiz>
2005-11-22 18:35:25 +0000

    Update to 1.38.1, provided by Geert Hendrickx in private mail.
    - Corrected ACL for Solaris (David Duchscher and Attila Fulop).
    - Add bacula_mail_summary.sh to examples directory. It makes a single email
      summary of any number of jobs. Submitted by Adrew J. Millar.
    - Unmount command now unloads autochanger.
    - Fix hang in FD (Martin Simmons)
    - Fix Win98 stat() problem in FD (Thorsten Engel)
    - Fix update slots which did not clear missing tapes.
    - Fix autostart install for FreeBSD (user reported)
    - Fix several problems with PostgreSQL scripts (Eric Bollinger)
    - Critical: allow restore of the first file of non-portable Win32 backup.
    - Important: with muliple concurrent jobs the autochanger could get
      confused because of a missing mutex.
    - Fix accessing last slot in label and update slots.
    - Modify configure.in to add execute option to sqlite3 catalog  scripts.
    - Create update_xxx_table_8_to_9 scripts for updatedb
    - Move the -lcrypt for PostgreSQL after the PostgreSQL libs in
      autoconf/bacula-macros/db.m4 as suggested by user.  Fixes bug #457.
    - Remove @STATIC_CONS@ from tray-monitor Makefile as suggested by user.
      Fixes bug #456.

---
+++ sysutils/bacula/hacks.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+
+.ifndef BACULA_HACKS_MK
+BACULA_HACKS_MK=	#defined
+
+### gcc 4.0* has been reported to produce bad code on amd64
+### with optimizations (-O2) turned on (causes segfaults).
+###
+.if ${MACHINE_ARCH} == "amd64"
+
+.include "../../mk/compiler.mk"
+
+.if !empty(CC_VERSION:Mgcc-4.0*)
+PKG_HACKS+=		dontoptimze
+BUILDLINK_TRANSFORM+=	rm:-O[0-9]*
+.endif
+
+.endif
+
+.endif
