ef182fd62746e66bd38cd934f8bd6a7cd451c20e
wiz <wiz>
2005-12-02 17:04:35 +0000

    Convert to options framework.

---
+++ net/tnftpd/options.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.tnftpd
+PKG_SUPPORTED_OPTIONS=		inet6
+PKG_OPTIONS_OPTIONAL_GROUPS+=	socks
+PKG_OPTIONS_GROUP.socks=	socks4 socks5
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Msocks4)
+CONFIGURE_ARGS+=	--with-socks4
+.include "../../net/socks4/buildlink3.mk"
+.elif !empty(PKG_OPTIONS:Msocks5)
+CONFIGURE_ARGS+=	--with-socks5
+.include "../../net/socks5/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.else
+CONFIGURE_ARGS+=	--disable-ipv6
+.endif
