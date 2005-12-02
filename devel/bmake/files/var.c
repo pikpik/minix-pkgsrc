effbcaa752ee0299234fbe80b5a2f83538b7808a
sjg <sjg>
2005-12-02 00:14:56 +0000

    Actually import the bmake src into files, and apply pkgsrc patches. Also provide an import script to help keep imports consistent.

--- devel/bmake/files/var.c
+++ devel/bmake/files/var.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: var.c,v 1.100 2005/08/27 08:04:26 christos Exp $	*/
+/*	$NetBSD: var.c,v 1.1 2005/10/31 21:34:25 reed Exp $	*/
 
 /*
  * Copyright (c) 1988, 1989, 1990, 1993
@@ -69,14 +69,14 @@
  */
 
 #ifndef MAKE_NATIVE
-static char rcsid[] = "$NetBSD: var.c,v 1.100 2005/08/27 08:04:26 christos Exp $";
+static char rcsid[] = "$NetBSD: var.c,v 1.1 2005/10/31 21:34:25 reed Exp $";
 #else
 #include <sys/cdefs.h>
 #ifndef lint
 #if 0
 static char sccsid[] = "@(#)var.c	8.3 (Berkeley) 3/19/94";
 #else
-__RCSID("$NetBSD: var.c,v 1.100 2005/08/27 08:04:26 christos Exp $");
+__RCSID("$NetBSD: var.c,v 1.1 2005/10/31 21:34:25 reed Exp $");
 #endif
 #endif /* not lint */
 #endif
@@ -125,7 +125,16 @@ __RCSID("$NetBSD: var.c,v 1.100 2005/08/27 08:04:26 christos Exp $");
 
 #ifndef NO_REGEX
 #include    <sys/types.h>
+#include    <nbcompat/nbconfig.h>
+/* bmake supplies its own <sys/cdefs.h> so this header is always present. */
+#ifndef HAVE_SYS_CDEFS_H
+#define HAVE_SYS_CDEFS_H 1
+#endif
+#if HAVE_REGEX_H
 #include    <regex.h>
+#else
+#include    <nbcompat/regex.h>
+#endif
 #endif
 #include    <ctype.h>
 #include    <stdlib.h>
