ce73caaec8cb9aaedfd71c918cdab57560056cf7
wiz <wiz>
2005-12-09 20:40:56 +0000

    Add x11 option to the package, on by default.
    Addresses PR 23130 by reed@

---
+++ textproc/groff/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.groff
+PKG_SUPPORTED_OPTIONS=	x11
+PKG_SUGGESTED_OPTIONS=	x11
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mx11)
+CONFIGURE_ARGS+=	--with-x
+CONFIGURE_ARGS+=	--with-appresdir=${PREFIX}/lib/X11/app-defaults
+PLIST_SRC=		PLIST.x11 PLIST
+.include "../../mk/xaw.buildlink3.mk"
+.include "../../mk/x11.buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-x
+.endif
