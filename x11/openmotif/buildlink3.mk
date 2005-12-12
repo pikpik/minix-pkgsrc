342e04006821cfcd4ec80fc1aa334d5ef10e75ab
joerg <joerg>
2005-12-12 19:57:52 +0000

    2.2.3 and 2.1.x are not compatible, force use of 2.2.3.

--- x11/openmotif/buildlink3.mk
+++ x11/openmotif/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/04/11 18:21:33 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/07/24 00:55:43 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 OPENMOTIF_BUILDLINK3_MK:=	${OPENMOTIF_BUILDLINK3_MK}+
@@ -11,7 +11,7 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nopenmotif}
 BUILDLINK_PACKAGES+=	openmotif
 
 .if !empty(OPENMOTIF_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.openmotif+=	openmotif>=2.1.30
+BUILDLINK_DEPENDS.openmotif+=	openmotif>=2.2.3
 BUILDLINK_PKGSRCDIR.openmotif?=	../../x11/openmotif
 .endif	# OPENMOTIF_BUILDLINK3_MK
 
