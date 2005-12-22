1e2d60dc93749ebbcf29e00f81c95f190e0ccdfc
ghen <ghen>
2005-12-22 19:27:36 +0000

    - update pearpc to 0.4 - move options to options.mk file and reorganize a bit - sort PLIST - grab MAINTAINERship
    ok by xtraeme.
    
    ChangeLog for pearpc-0.4:
    	- CPU: stfiwx implemented
    	- config: "ppc_start_fullscreen" for starting in fullscreen mode
    	- POSIX: allow bridging of tun device
    	- use FISTTP instruction on SSE3 aware processors
    	- merged native_cd branch from Alexander Stockinger
    	- endianess fixes
    	- warn that the generic CPU is slow
    	- better detection of HFS+ volumes
    	- support for DVD-Drives
    	- correctly check for NASM
    	- Work-around for graphic errors in Mac OS X 10.1
    	- POSIX/Linux: fall back to 1 GHz if /proc/cpuinfo doesn't provide cpu
    	  frequency
    	- 3c90x emulation fixed
    	- some PROM fixed for yaboot
    	- alteration of x86asm interface to be more programmer friendly
    	- use transparent cursors instead of altering cursor visibility (hack
    	  for using PearPC with remote desktop)
    	- DVD emulation implemented.  Fixes problems with Tiger installation
    	- Updated the read_effective_* commands to be faster !!! REGRESSED !!!
    	- fixed lvalue casts (compiles now with gcc 4.x)
    	- booting from disks with bootfile about 4 GiB limit fixed

---
+++ emulators/pearpc/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.pearpc
+PKG_OPTIONS_REQUIRED_GROUPS=	ui
+PKG_OPTIONS_GROUP.ui=		x11 sdl
+PKG_SUGGESTED_OPTIONS=		x11
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mx11)
+CONFIGURE_ARGS+=		--enable-ui=x11
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl)
+CONFIGURE_ARGS+=		--enable-ui=sdl
+.include "../../devel/SDL/buildlink3.mk"
+.endif
