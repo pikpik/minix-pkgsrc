48f04260b9e487dc2ed6db2f91a85a516f0d78fd
ghen <ghen>
2005-12-28 15:47:30 +0000

    Reimport skipstone from pkgsrc-wip.
    SkipStone is a Gtk+ Web Browser that embeds Mozilla's rendering component
    Gecko. It aims to be light and fast with few dependencies.
    
    SkipStone is currently under heavy development, but it's progressing quickly
    and coming along nicely. It provides a nice and simple configurable interface,
    bookmarks and all of the other mandantory browsing features.
    
    Downloading is handled by SkipDownload by default, a Gtk+ frontend to the
    famous web downloading utility wget. However, if you prefer to use other
    download utilities / managers, this option is configurable.
    
    Mail and external url handlers are also handled by external applications which
    can be configured to the users choice, leaving SkipStone to do what it does
    best - browsing.

---
+++ www/skipstone/options.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.skipstone
+PKG_SUPPORTED_OPTIONS=	wget
+PKG_SUGGESTED_OPTIONS=	wget
+
+.include "../../mk/bsd.options.mk"
+
+# SkipDownload uses wget to download files from the Internet, but since this
+# is not strictly needed for SkipStone to run, it's an optional dependency.
+.if !empty(PKG_OPTIONS:Mwget)
+DEPENDS+=		wget>=1:../../net/wget
+.endif
