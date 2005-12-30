5fa81654f46972f9fa67c7444bc6a4dc198cefb1
jlam <jlam>
2005-12-30 21:04:03 +0000

    Update databases/iodbc to 3.52.3.  Changes from version 2.50.3 include adding some support for the ODBC 3.x specifications and fixing quite a lot of bugs.

--- databases/iodbc/buildlink3.mk
+++ databases/iodbc/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1 2004/05/05 03:16:02 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:18 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 IODBC_BUILDLINK3_MK:=	${IODBC_BUILDLINK3_MK}+
@@ -11,9 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Niodbc}
 BUILDLINK_PACKAGES+=	iodbc
 
 .if !empty(IODBC_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.iodbc+=	iodbc>=2.50.3
-BUILDLINK_RECOMMENDED.iodbc+=	iodbc>=2.50.3nb1
+BUILDLINK_DEPENDS.iodbc+=	iodbc>=3.52.3
 BUILDLINK_PKGSRCDIR.iodbc?=	../../databases/iodbc
 .endif	# IODBC_BUILDLINK3_MK
+
+# ODBC drivers don't need these bits, but ODBC clients do.
+.if !defined(ODBC_DRIVER)
+.  include "../../mk/dlopen.buildlink3.mk"
+.  include "../../mk/pthread.buildlink3.mk"
+.endif
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
