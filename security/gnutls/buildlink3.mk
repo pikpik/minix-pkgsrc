4b1e70c9c7cc21348bbe1c1b3511e9d76e5474bd
wiz <wiz>
2005-12-31 11:20:11 +0000

    Update to 1.3.2 bumped library major version -- bump BUILDLINK_RECOMMENDED.

--- security/gnutls/buildlink3.mk
+++ security/gnutls/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2005/05/02 19:48:37 salo Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2005/09/05 07:34:05 adam Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GNUTLS_BUILDLINK3_MK:=	${GNUTLS_BUILDLINK3_MK}+
@@ -12,7 +12,7 @@ BUILDLINK_PACKAGES+=	gnutls
 
 .if !empty(GNUTLS_BUILDLINK3_MK:M+)
 BUILDLINK_DEPENDS.gnutls+=	gnutls>=1.0.13
-BUILDLINK_RECOMMENDED.gnutls+=	gnutls>=1.2.3
+BUILDLINK_RECOMMENDED.gnutls+=	gnutls>=1.3.2
 BUILDLINK_PKGSRCDIR.gnutls?=	../../security/gnutls
 .endif	# GNUTLS_BUILDLINK3_MK
 
