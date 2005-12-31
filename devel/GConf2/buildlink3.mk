e26ea0a9e2228855f86a11d193242d05ea4f695d
wiz <wiz>
2005-12-31 00:05:11 +0000

    Update to 2.12.1:
    2.12.1
    ======
    
     Fixes
      - Code cleanups (Kjartan Maraas)
      - Open GModules with G_MODULE_BIND_LAZY (Kjartan Maraas)
      - Fix change notification with multiple sources (Alexander Larsson)
      - Allow space in config source names on windows (Tor Lillqvist)
      - Read xml files in larger chunks (Mark McLoughlin)
      - Simplify the handling of --enable-schemas-install (Stepan Kasal)
      - Win32 Fixes (Tor Lillqvist)
    
     Translators
    
      - Alexander Shopov (bg)
      - Samia Niamatullah (bn)
      - Miloslav Trmac (cs)
      - Martin Willemoes Hansen (da)
      - Adam Weinberger (en_CA)
      - Francisco Javier F. Serrador (es)
      - Ivar Smolin (et)
      - Hizkuntza Politikarako Sailburuordetza (eu)
      - Ilkka Tuohela (fi)
      - Ignacio Casal Quinteiro (gl)
      - Ankit Patel (gu)
      - Yair Hershkovitz (he)
      - Erdal Ronahi (ku)
      - Tino Meinen (nl)
      - Mişu Moldovan (ro)
      - Marcel Telka (sk)
      - Laurent Dhima (sq)
      - Christian Rose (sv)
      - Maxim Dziumanenko (uk)
      - Clytie Siddall (vi)
      - Funda Wang (zh_CN)

--- devel/GConf2/buildlink3.mk
+++ devel/GConf2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2004/04/01 18:17:29 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2004/09/21 16:44:50 jmmv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 GCONF2_BUILDLINK3_MK:=	${GCONF2_BUILDLINK3_MK}+
@@ -26,6 +26,7 @@ BUILDLINK_PKGSRCDIR.GConf2?=	../../devel/GConf2
 BUILDLINK_TARGETS+=	GConf2-buildlink-fake
 _GCONF2_FAKE=		${BUILDLINK_DIR}/bin/gconftool-2
 
+.PHONY: GConf2-buildlink-fake
 GConf2-buildlink-fake:
 	${_PKG_SILENT}${_PKG_DEBUG}					\
 	if [ ! -f ${_GCONF2_FAKE} ]; then				\
