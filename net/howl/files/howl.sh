b77d11d74ab325d8063efef44c3228959c072572
tron <tron>
2006-01-01 18:27:53 +0000

    Update "howl" package to version 1.0.0. Changes since version 0.9.10: - update to mDNSResponder core 58-8-1 - compile out heap debugging when NDEBUG macro is set during compilation - errors encountered when parsing conf file are logged - fix various unsafe sprintf calls (contributed by   David Young dyoung@pobox.com) - compiles cleanly using cygwin - support for compiling under OpenBSD - error in docs regarding swapped parameters to sw_discovery_publish_reply

--- net/howl/files/howl.sh
+++ net/howl/files/howl.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD$
+# $NetBSD: howl.sh,v 1.1 2005/01/08 06:24:41 schmonz Exp $
 #
 
 # PROVIDE: howl
@@ -11,8 +11,8 @@
 name="howl"
 rcvar=$name
 command="@PREFIX@/bin/mDNSResponder"
-stop_cmd=":"
-howl_user="howl"
+howl_user="@HOWL_USER@"
+sig_stop=KILL
 
 load_rc_config $name
 run_rc_command "$1"
