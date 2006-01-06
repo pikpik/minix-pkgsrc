7863f075874befda1a6bb80d3e50c117c2f08adf
joerg <joerg>
2006-01-06 00:15:21 +0000

    Use SUBST framework. Be pedantic and also replace /bin/sh with ${SH}.

--- net/samba/files/deluser.sh
+++ net/samba/files/deluser.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!@SH@
 #
-# $NetBSD$
+# $NetBSD: deluser.sh,v 1.2 2004/01/11 02:26:31 jlam Exp $
 #
 # This is a deluser script for NetBSD systems whose userdel(8) doesn't
 # accept "$" in the username.
