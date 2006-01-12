4b7331c64376525f29d0210259b4352e0ea63899
wiz <wiz>
2006-01-12 17:35:15 +0000

    Update to 1.3.24 with contents of devel/swig13.

--- devel/swig/buildlink3.mk
+++ devel/swig/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/05/23 22:25:03 epg Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 SWIG_BUILDLINK3_MK:=	${SWIG_BUILDLINK3_MK}+
@@ -11,10 +11,12 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nswig}
 BUILDLINK_PACKAGES+=	swig
 
 .if !empty(SWIG_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.swig+=	swig>=1.1p5nb1
+BUILDLINK_DEPENDS.swig+=	swig>=1.3.24
 BUILDLINK_PKGSRCDIR.swig?=	../../devel/swig
 # only contains a static library
 BUILDLINK_DEPMETHOD.swig?=	build
+
+BUILDLINK_FILES.swig+=		share/swig/1.3/* share/swig/1.3/*/*
 .endif	# SWIG_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
