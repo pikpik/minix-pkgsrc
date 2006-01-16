b0a733f10962f80c16e9299fb82915395b1a1499
adam <adam>
2006-01-16 22:41:46 +0000

    Changes 2.12.1: * Gnome::Main: Fix a crash on 64-bit systems.

--- devel/libgnomemm/buildlink3.mk
+++ devel/libgnomemm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2005/07/22 22:00:24 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2005/12/31 12:32:33 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOMEMM_BUILDLINK3_MK:=	${LIBGNOMEMM_BUILDLINK3_MK}+
@@ -19,4 +19,4 @@ BUILDLINK_PKGSRCDIR.libgnomemm?=	../../devel/libgnomemm
 .include "../../devel/libgnome/buildlink3.mk"
 .include "../../x11/gtkmm/buildlink3.mk"
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
