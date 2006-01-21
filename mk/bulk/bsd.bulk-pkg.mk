0c9ce0abebb44dcf881a09f93c310942d3e52658
rillig <rillig>
2006-01-21 11:29:33 +0000

    - Added a new top level file that saves the REPORT_BASEDIR from the first   call to the mk/bulk/build script. This is to allow restarting the bulk   build without getting confused by the various timestamps that occur   during a bulk build. It also fixes some problems where the text/plain   report had ended up in a directory different from the other files.
    - Changed the default filename for the text/plain report from
      "pkgsrc-results-${BUILDDATE}.txt" to simply "report.txt", as the
      ${BUILDDATE} variable didn't give a hint to when the bulk build had been
      started, which is often more important than when the bulk build stopped.
    
    - Changed the way to specify the directory and the URL where the bulk build
      reports are made available. See
    
          http://mail-index.netbsd.org/tech-pkg/2005/12/05/0002.html
    
      for details. Specifying the directories using the FTPHOST and FTPURL
      variables will be possible for some months, after which it support for
      it will be dropped.

--- mk/bulk/bsd.bulk-pkg.mk
+++ mk/bulk/bsd.bulk-pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.bulk-pkg.mk,v 1.108 2005/11/28 20:08:37 rillig Exp $
+#	$NetBSD: bsd.bulk-pkg.mk,v 1.111 2006/01/17 22:35:13 jdc Exp $
 
 #
 # Copyright (c) 1999, 2000 Hubert Feyrer <hubertf@NetBSD.org>
@@ -144,6 +144,10 @@ ORDERFILE?=	${BULKFILESDIR}/.order${BULK_ID}
 # File which is used as a timestamp for when the build started.  This is used
 # eventually for looking for leftover files (files not properly deinstalled)
 STARTFILE?=	${BULKFILESDIR}/.start${BULK_ID}
+
+# This top level file saves the bulk build ID. As it most often has a time
+# stamp, it is only generated once and later retrieved from this file.
+BULK_BUILD_ID_FILE?=	${BULKFILESDIR}/.bulk_build_id
 
 # File created and used by lintpkgsrc(8) to cache package metadata for
 # pruning and bulk-upload exclusions.
