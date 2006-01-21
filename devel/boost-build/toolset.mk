2f2a0315ceb6c5eddeec05eba9d641187d522e4e
jmmv <jmmv>
2006-01-21 09:02:16 +0000

    Fix build and install of Boost under Mac OS X: - Correctly use threads. - Use the correct tool set. - Make libraries (Boost.Test) with undefined symbols build correctly. - Change the installed library names so that they match other systems   (thus avoiding manual PLIST substitutions).  There is a hack here,   though, to let the dylib stuff kick in...

--- devel/boost-build/toolset.mk
+++ devel/boost-build/toolset.mk
@@ -1,8 +1,10 @@
-# $NetBSD$
+# $NetBSD: toolset.mk,v 1.1 2005/02/26 22:48:35 jmmv Exp $
 
 .include "../../mk/compiler.mk"
 
-.if !empty(PKGSRC_COMPILER:Mgcc)
+.if ${OPSYS} == "Darwin"
+BOOST_TOOLSET=		darwin
+.elif !empty(PKGSRC_COMPILER:Mgcc)
 BOOST_TOOLSET=		gcc
 .elif !empty(PKGSRC_COMPILER:Mmipspro*)
 BOOST_TOOLSET=		mipspro
