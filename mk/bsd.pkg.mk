de8210f6f9cf77ffc2f011716527b84cdf11b7ee
rillig <rillig>
2006-02-02 21:15:46 +0000

    If the replace-interpreter program cannot find a file, a warning is printed.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1796 2006/01/21 10:10:42 rillig Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1797 2006/01/21 21:32:51 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -1522,6 +1522,8 @@ replace-interpreter:
 			    ${CHMOD} a+x $${f}.new;			\
 		    fi;							\
 		    ${MV} -f $${f}.new $${f};				\
+	    else							\
+		${ECHO_MSG} "[bsd.pkg.mk:replace-interpreter] WARNING: Skipping non-existent file \"$$f\"." 1>&2; \
 	    fi;								\
 	done
 .    endfor
