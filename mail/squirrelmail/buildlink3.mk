91f91f0a462f498d0cc47275b5db6e170625e166
martti <martti>
2006-02-17 07:04:25 +0000

    Fixed warnings found by pkglint -Wall.

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2005/07/20 15:42:34 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/02/05 23:09:58 joerg Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
@@ -16,4 +16,4 @@ BUILDLINK_RECOMMENDED.squirrelmail?=	squirrelmail>=1.4.5nb5
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
