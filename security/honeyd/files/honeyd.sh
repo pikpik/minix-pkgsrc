c570a9a5bad45c0eab3661918f08865ea6eeda28
adrianp <adrianp>
2006-02-19 17:23:18 +0000

    Update to honeyd 1.5
    New features include:
    * Statistics Collector: A daemon that can process netflow-like information
    exported by several Honeyd instances and do computations on the data - see
    live data.
    * Improved Subsystems: Improved support for subsystems permits running more
    complicated UNIX applications like mwcollect as a subsystem for Honeyd.
    * Proxy and SMTP subsystems: Example subsystems to simulate open proxies and
    mail relays. These subsystems are written with performance in mind and have
    no problem in keeping up with a busy network.
    
    Bugfixes include:
    A bug in Honeyd's IP reassembly code allows adversaries to remotely fingerprint
    honeypots. Thanks to Jon Oberheide for finding the bug; see adv.2006-01 for
    more information

--- security/honeyd/files/honeyd.sh
+++ security/honeyd/files/honeyd.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD$
+# $NetBSD: honeyd.sh,v 1.1.1.1 2004/04/20 09:14:43 xtraeme Exp $
 #
 
 # PROVIDE: honeyd
@@ -12,7 +12,7 @@
 name="honeyd"
 rcvar=$name
 command="@PREFIX@/bin/${name}"
-pidfile="/var/run/${name}.pid"
+pidfile="@VARBASE@/run/${name}.pid"
 
 load_rc_config $name
 run_rc_command "$1"
