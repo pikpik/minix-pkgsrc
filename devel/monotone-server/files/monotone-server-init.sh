ab58b21fd52d9567601d6a715adb6603426b2169
jmmv <jmmv>
2006-02-20 16:54:42 +0000

    Minor typo corrected.

--- devel/monotone-server/files/monotone-server-init.sh
+++ devel/monotone-server/files/monotone-server-init.sh
@@ -1,6 +1,6 @@
 #!@SH@
 #
-# $NetBSD$
+# $NetBSD: monotone-server-init.sh,v 1.1.1.1 2005/01/12 12:52:50 jmmv Exp $
 #
 
 progname=$(basename $0)
@@ -64,7 +64,7 @@ fi
 
 cd ${home}
 
-echo "Initializing database: \`${home}'/monotone.db"
+echo "Initializing database: \`${home}/monotone.db'"
 ${MONOTONE} --db=monotone.db db init
 
 cat <<EOF
