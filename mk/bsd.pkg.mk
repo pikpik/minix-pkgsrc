edc90781be6559c65fb69d1b6021c57715bbd377
jlam <jlam>
2006-03-09 23:31:51 +0000

    Make it safe to include bsd.pkginstall.mk directly in bsd.pkg.mk, and drop pkginstall.mk, which did the same thing.  Also, rework some of the targets so that we avoid needing to inspect *_MEMBERS variables within make -- we defer the check to the shell code invoked by the targets.
    All changes are internal and don't affect existing packages in a
    visible way.

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.1802 2006/03/09 00:20:27 jlam Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.1803 2006/03/09 15:58:10 rillig Exp $
 #
 # This file is in the public domain.
 #
@@ -420,7 +420,7 @@ SCRIPTS_ENV+=	${INSTALL_MACROS}
 #
 .include "../../mk/alternatives.mk"
 
-.include "../../mk/install/pkginstall.mk"
+.include "../../mk/install/bsd.pkginstall.mk"
 
 # Set INSTALL_FILE to be the name of any INSTALL file
 .if !defined(INSTALL_FILE) && exists(${PKGDIR}/INSTALL)
