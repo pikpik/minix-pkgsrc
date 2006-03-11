fc7caa6fe8ec75b6f62f136e9eadd486f6fb0362
adam <adam>
2006-03-11 08:34:13 +0000

    Changes 0.1.12: Fix structure padding issues on some architectures Fix compile warnings with newer versions of gcc Fix memory leak when continuously rescanning for devices Check for device nodes in /dev/usb under Linux Fix compile problem with Darwin/Mac OS X port Add support for Darwin/Mac OS X 10.4 Fix error when transactions are aborted Requery endpoints when switching alternate settings Support more BSD based distributions Fix problem with short control messages in BSD port Various other minor fixes

--- devel/libusb/buildlink3.mk
+++ devel/libusb/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/03/05 19:25:11 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2004/10/03 00:13:31 tv Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBUSB_BUILDLINK3_MK:=	${LIBUSB_BUILDLINK3_MK}+
@@ -16,4 +16,4 @@ BUILDLINK_RECOMMENDED.libusb+=	libusb>=0.1.8nb2
 BUILDLINK_PKGSRCDIR.libusb?=	../../devel/libusb
 .endif	# LIBUSB_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
