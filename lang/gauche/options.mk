f2f7cf2c31a2f4af5c85d54dfe3e4a4b947bf4a4
jlam <jlam>
2006-03-31 18:35:22 +0000

    * Clean up the way this package was trying to pass in rpath flags for   iconv and gdbm.  We now patch Makefile.in instead of the configure   script and just pass in thr rpath flags via environment variables.
    * Honor PKGINFODIR.

--- lang/gauche/options.mk
+++ lang/gauche/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2005/05/31 10:01:37 dillo Exp $
+# $NetBSD: options.mk,v 1.3 2005/12/05 23:55:09 rillig Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gauche
 PKG_SUPPORTED_OPTIONS=	gdbm multibyte
@@ -15,6 +15,7 @@ PKG_SUGGESTED_OPTIONS=	gdbm multibyte
 .if !empty(PKG_OPTIONS:Mgdbm)
 .  include "../../databases/gdbm/buildlink3.mk"
 PLIST_SUBST+=	USE_GDBM=''
+MAKE_ENV+=	GDBM_LDFLAGS=${COMPILER_RPATH_FLAG}${BUILDLINK_PREFIX.gdbm}/lib
 .else
 PLIST_SUBST+=	USE_GDBM='@comment '
 .endif
