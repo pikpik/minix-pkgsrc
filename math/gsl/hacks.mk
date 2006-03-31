88bc09a7191348e4f802bc2d2623e8ed4a1a30a8
jlam <jlam>
2006-03-31 18:57:28 +0000

    Move ICE workaround to a hacks.mk file.

---
+++ math/gsl/hacks.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+.include "../../mk/compiler.mk"
+
+### [Mon Feb 28 12:11:29 2005 UTC : dmcmahill]
+### At least gcc-3.3.5 failed to compile gsl-1.6/statistics/lag1.c
+### with -mcpu=v9 on a solaris/sparc system.
+###
+.if (${OPSYS} == "SunOS") && !empty(MACHINE_ARCH:Msparc) && \
+    !empty(CC_VERSION:M*gcc*)
+PKG_HACKS+=		solaris-sparc-ice
+BUILDLINK_TRANSFORM+=	rm:-mcpu=v9
+.endif
