21dee5872eb26c8b02d917400a260529bb5ec4d1
wiz <wiz>
2006-04-04 06:27:13 +0000

    Pull over 1.36 from src/usr.sbin: revision 1.36 date: 2005/12/03 01:08:38;  author: hubertf;  state: Exp;  lines: +3 -3 Only warn about running as non-root when -v is given (or when running in non-unprivilegedmode, but how to detect?)

--- pkgtools/pkg_install/files/delete/main.c
+++ pkgtools/pkg_install/files/delete/main.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: main.c,v 1.34 2004/12/29 11:35:00 agc Exp $	*/
+/*	$NetBSD: main.c,v 1.36 2005/12/03 01:08:38 hubertf Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -11,7 +11,7 @@
 #if 0
 static char *rcsid = "from FreeBSD Id: main.c,v 1.11 1997/10/08 07:46:48 charnier Exp";
 #else
-__RCSID("$NetBSD: main.c,v 1.15 2004/11/03 14:03:53 wiz Exp $");
+__RCSID("$NetBSD: main.c,v 1.36 2005/12/03 01:08:38 hubertf Exp $");
 #endif
 #endif
 
@@ -191,7 +191,7 @@ main(int argc, char **argv)
 		usage();
 	}
 #ifndef __INTERIX
-	if (!Fake && getuid() != 0) {
+	if (!Fake && Verbose && getuid() != 0) {
 		warnx("not running as root - trying to delete anyways");
 	}
 #endif
