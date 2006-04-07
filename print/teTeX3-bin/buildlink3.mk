d3887f4325ff1efe76f1f41fb7321200ecacf834
minskim <minskim>
2006-04-07 14:59:06 +0000

    Remove obsolete PRINT_PLIST_AWK patterns.  They are handled by texmf-dirs.

--- print/teTeX3-bin/buildlink3.mk
+++ print/teTeX3-bin/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/04/06 06:22:37 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2006/04/07 05:21:01 minskim Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 TETEX_BIN_BUILDLINK3_MK:=	${TETEX_BIN_BUILDLINK3_MK}+
@@ -23,11 +23,5 @@ PKG_TEXMFPREFIX=	${PREFIX}/share/texmf
 PKG_LOCALTEXMFPREFIX=	${PREFIX}/share/texmf-local
 PLIST_SUBST+=	PKG_TEXMFPREFIX=${PKG_TEXMFPREFIX:C|^${PREFIX}/||}
 PLIST_SUBST+=	PKG_LOCALTEXMFPREFIX=${PKG_LOCALTEXMFPREFIX:C|^${PREFIX}/||}
-
-PRINT_PLIST_AWK+=	/^@dirrm ${PKG_LOCALTEXMFPREFIX:S|${PREFIX}/||:S|/|\\/|g}(\/bibtex(\/bib|\/bst)?|\/tex(\/latex)?)?$$/ \
-			{ next; }
-PRINT_PLIST_AWK+=	/^(@dirrm )?${PKG_LOCALTEXMFPREFIX:S|${PREFIX}/||:S|/|\\/|g}/ \
-			{ gsub(/${PKG_LOCALTEXMFPREFIX:S|${PREFIX}/||:S|/|\\/|g}/, "$${PKG_LOCALTEXMFPREFIX}"); \
-			print; next; }
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
