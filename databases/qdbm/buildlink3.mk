3444a8107f7f7a5bfa747b2251cfe639ea37c598
wiz <wiz>
2006-05-07 10:38:40 +0000

    Update to 1.8.50, provided by OBATA Akio in PR 33404: Changes: 2006-04-28  Mikio Hirabayashi
            - The utility API was enhanced.
            - Release: 1.8.50

--- databases/qdbm/buildlink3.mk
+++ databases/qdbm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/04/06 06:21:43 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2006/04/12 10:27:05 rillig Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 QDBM_BUILDLINK3_MK:=	${QDBM_BUILDLINK3_MK}+
@@ -13,17 +13,19 @@ BUILDLINK_PACKAGES+=	qdbm
 .if !empty(QDBM_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.qdbm+=	qdbm>=1.8.40
 BUILDLINK_PKGSRCDIR.qdbm?=	../../databases/qdbm
+
+.include "../../mk/pthread.buildlink3.mk"
+
+.  if defined(PTHREAD_TYPE) && ${PTHREAD_TYPE} != "none"
+CONFIGURE_ARGS+=	--enable-pthread
+BUILDLINK_CFLAGS.qdbm+=	${PTHREAD_CFLAGS}
+BUILDLINK_LDFLAGS.qdbm+=${PTHREAD_LDFLAGS}
+.  endif
 .endif	# QDBM_BUILDLINK3_MK
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../archivers/lzo/buildlink3.mk"
 .include "../../converters/libiconv/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
-.include "../../mk/pthread.buildlink3.mk"
-.if defined(PTHREAD_TYPE) && ${PTHREAD_TYPE} != "none"
-CONFIGURE_ARGS+=	--enable-pthread
-BUILDLINK_CFLAGS.qdbm+=	${PTHREAD_CFLAGS}
-BUILDLINK_LDFLAGS.qdbm+=${PTHREAD_LDFLAGS}
-.endif
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
