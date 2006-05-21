c42ec65d3c4462fd39f86bd7f2f51ac5e26e5dbc
ben <ben>
2006-05-21 22:21:10 +0000

    Initial import for portaudio-devel-20060521 (at this moment also known as portaudio version 19), a portable cross-platform Audio API.
    PortAudio is a free, cross platform, open-source, audio I/O library.  It
    lets you write simple audio programs in 'C' that will compile and run on
    many platforms including Windows, Macintosh (8,9,X), Unix (OSS), SGI, and
    BeOS. PortAudio is intended to promote the exchange of audio synthesis
    software between developers on different platforms, and was recently
    selected as the audio component of a larger PortMusic project that includes
    MIDI and sound file support.
    
    PortAudio provides a very simple API for recording and/or playing sound
    using a simple callback function.  Example programs are included that
    synthesize sine waves and pink noise, perform fuzz distortion on a guitar,
    list available audio devices, etc.

---
+++ audio/portaudio-devel/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PORTAUDIO_DEVEL_BUILDLINK3_MK:=	${PORTAUDIO_DEVEL_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	portaudio-devel
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nportaudio-devel}
+BUILDLINK_PACKAGES+=	portaudio-devel
+
+.if ${PORTAUDIO_DEVEL_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.portaudio-devel+=	portaudio-devel>=20060521
+BUILDLINK_PKGSRCDIR.portaudio-devel?=	../../audio/portaudio-devel
+BUILDLINK_INCDIRS.portaudio-devel?=	include/portaudio2
+BUILDLINK_LIBDIRS.portaudio-devel?=	lib/portaudio2
+BUILDLINK_CPPFLAGS.portaudio-devel?=	-I${BUILDLINK_PREFIX.portaudio-devel}/include/portaudio2
+.endif	# PORTAUDIO_DEVEL_BUILDLINK3_MK
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
