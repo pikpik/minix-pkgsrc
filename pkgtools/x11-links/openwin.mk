7e71c53b6b1869eeb91e8a608955315373afeddf
jlam <jlam>
2006-05-22 22:22:05 +0000

    Move the check-shlibs target from bsd.pkg.mk to bsd.pkg.check.mk where it will live with other "check" targets run after package installation.
    Get rid of SHLIB_HANDLING, whose meaning had mutated over the years
    from one thing to another.  Currently, it is used to basically note
    whether the system's "ldd" command can be usefully run on the package's
    binaries and libraries.  Rename this variable to CHECK_SHLIBS_SUPPORTED
    for more clarity.
    
    CHECK_SHLIBS is now a variable set exclusively by the user in /etc/mk.conf
    to note whether the check for missing run-time search paths is performed
    after a package is installed.  It defaults to "no" unless PKG_DEVELOPER
    is set.

--- pkgtools/x11-links/openwin.mk
+++ pkgtools/x11-links/openwin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: openwin.mk,v 1.2 2004/11/11 17:44:39 sketch Exp $
+# $NetBSD: openwin.mk,v 1.3 2005/02/20 07:48:48 grant Exp $
 
 FILES_LIST=	${FILESDIR}/openwin
 
@@ -23,4 +23,5 @@ CREATE_X11LINK=	case $$file in						\
 # disable checking of shared library dependencies, as openwin/dt can
 # have some libraries missing in a "normal" installation and this
 # shouldn't break the package.
-CHECK_SHLIBS=		no
+#
+CHECK_SHLIBS_SUPPORTED=	no
