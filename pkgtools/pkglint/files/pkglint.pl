5d6a5085fd58909370628a340fa6c786ec447382
rillig <rillig>
2006-05-23 09:12:35 +0000

    Prevented warnings when defining the new FETCH_MESSAGE.

--- pkgtools/pkglint/files/pkglint.pl
+++ pkgtools/pkglint/files/pkglint.pl
@@ -1,5 +1,5 @@
 #! @PERL@
-# $NetBSD: pkglint.pl,v 1.588 2006/05/22 10:22:36 rillig Exp $
+# $NetBSD: pkglint.pl,v 1.589 2006/05/23 08:59:47 rillig Exp $
 #
 
 # pkglint - static analyzer and checker for pkgsrc packages
@@ -1581,6 +1581,7 @@ sub get_regex_plurals() {
 		BUILDLINK_LDADD
 		COMMENT
 		EXTRACT_ONLY
+		FETCH_MESSAGE
 		GENERATE_PLIST
 		PLIST_CAT
 		PLIST_PRE
