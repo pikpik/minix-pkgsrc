ee08662d6e3008e0126687288e8e9258f3039f83
hira <hira>
2006-06-08 09:29:50 +0000

    Fix indentation.

--- audio/faac/options.mk
+++ audio/faac/options.mk
@@ -1,9 +1,9 @@
-# $NetBSD: options.mk,v 1.1 2005/12/11 11:09:06 hira Exp $
+# $NetBSD: options.mk,v 1.2 2006/06/07 21:33:21 hira Exp $
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.faac
-PKG_SUPPORTED_OPTIONS=	mpeg4ip
+PKG_OPTIONS_VAR=		PKG_OPTIONS.faac
+PKG_SUPPORTED_OPTIONS=		mpeg4ip
 PKG_SUGGESTED_OPTIONS=
-PKG_OPTIONS_LEGACY_OPTS+=faad:mpeg4ip
+PKG_OPTIONS_LEGACY_OPTS+=	faad:mpeg4ip
 
 .include "../../mk/bsd.options.mk"
 
