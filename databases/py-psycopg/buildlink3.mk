1e8a85913e472c001ed92eb1051dccb18fb17672
rillig <rillig>
2006-06-16 10:58:23 +0000

    Fixed pkglint warnings.

--- databases/py-psycopg/buildlink3.mk
+++ databases/py-psycopg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2006/04/06 06:21:42 reed Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2006/04/12 10:27:05 rillig Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 PY_PSYCOPG_BUILDLINK3_MK:=	${PY_PSYCOPG_BUILDLINK3_MK}+
@@ -11,8 +11,9 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npsycopg}
 BUILDLINK_PACKAGES+=	psycopg
 
 .if !empty(PY_PSYCOPG_BUILDLINK3_MK:M+)
+.  include "../../lang/python/pyversion.mk"
 BUILDLINK_API_DEPENDS.psycopg+=	${PYPKGPREFIX}-psycopg>=1.1.21
-BUILDLINK_ABI_DEPENDS.psycopg?=	${PYPKGPREFIX}-psycopg>=1.1.21nb1
+BUILDLINK_ABI_DEPENDS.psycopg+=	${PYPKGPREFIX}-psycopg>=1.1.21nb1
 BUILDLINK_PKGSRCDIR.psycopg?=	../../databases/py-psycopg
 .endif	# PY_PSYCOPG_BUILDLINK3_MK
 
