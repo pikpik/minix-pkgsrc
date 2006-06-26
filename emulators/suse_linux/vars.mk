4f6a5de23e0381b59c16be27c5358dc24fb2020a
salo <salo>
2006-06-26 17:54:21 +0000

    Finally remove SuSE 7.3 packages, long overdue..

--- emulators/suse_linux/vars.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD: vars.mk,v 1.1 2005/04/03 21:23:15 jdolecek Exp $
-
-.if !defined(SUSE_VARS_MK)
-SUSE_VARS_MK=	# defined
-
-SUSE_VERSION=		7.3
-SUSE_DIR_PREFIX=	suse
-
-.endif # SUSE_VARS_MK
