52f190c7fe08532527e810c37da1f6e57f811fa9
jlam <jlam>
2006-07-05 07:09:33 +0000

    Make this into a proper rc.subr-using rc.d script.

--- inputmethod/skkserv/files/skkserv.sh
+++ inputmethod/skkserv/files/skkserv.sh
@@ -1,26 +1,15 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-#	$NetBSD: skkserv.sh,v 1.1 2002/05/31 12:59:58 seb Exp $
+# $NetBSD: skkserv.sh,v 1.2 2003/07/29 22:08:29 jmmv Exp $
 #
 # PROVIDE: skkserv
 # REQUIRE: DAEMON
+
+. /etc/rc.subr
  
 name="skkserv"
-command="@PREFIX@/libexec/skkserv"
-pidfile=""
+rcvar=$name
+command="@PREFIX@/libexec/${name}"
 
-cmd=${1:-start}
-
-case ${cmd} in
-start)
-	if [ -x ${command}  ]; then
-		echo "Starting ${name}."
-		${command} > /dev/null 2>&1
-	fi
-	;;
-*)
-	if [ -x ${command} ]; then
-		${command} ${cmd} > /dev/null 2>&1
-	fi
-	;;
-esac
+load_rc_config $name
+run_rc_command "$1"
