57832f497b5589667bb58963dcb2df4911efff93
jlam <jlam>
2006-07-05 09:08:35 +0000

    Make the check-vulnerable target more self-sufficient, by moving some of the logic from fetch/fetch.mk into flavor/pkg/check.mk, so that check-vulnerable can be used as a source target.
    Make check-vulnerable a source target for every phase of the build
    workflow, which ensures that it is always run if the user starts a
    new phase from the command line.
    
    Fix the cookie-generation targets so that they don't append, only
    overwrite to the cookie file.  This works around potential problems
    due to recursive makes.
    
    Move the cookie checks so that they surround the corresponding phase
    target.  The presence of the cookie should now inform the make process
    to avoid doing any processing of phases that occur before the phase
    corresponding to the cookie.

--- mk/check/check-vulnerable.mk
+++ mk/check/check-vulnerable.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check-vulnerable.mk,v 1.1 2006/06/03 23:11:42 jlam Exp $
+# $NetBSD: check-vulnerable.mk,v 1.2 2006/06/05 22:49:44 jlam Exp $
 
 ###########################################################################
 ### check-vulnerable (PRIVATE, override)
@@ -9,5 +9,9 @@
 .PHONY: check-vulnerable
 .if !target(check-vulnerable)
 check-vulnerable:
+.  if defined(ALLOW_VULNERABLE_PACKAGES)
+	@${DO_NADA}
+.  else
 	@${PHASE_MSG} "Skipping vulnerability checks."
+.  endif
 .endif
