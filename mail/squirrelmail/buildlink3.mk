7215944dc8799863182f2f620b1081cefa6627c3
martti <martti>
2006-07-13 07:59:34 +0000

    Updated squirrelmail to 1.4.7
      - Fixed URL for Read Receipts being incorrect in some cases (#1177518).
      - Fixed endless loop when trying to parse "From: )(" (#1517867).
      - Using is_file() instead of file_exists() in fortune plugin (#1499134).
      - Add manual page for conf.pl under contrib.
      - Don't allow selecting INBOX as Sent, Draft or Trash folder (#1242346).

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.10 2006/07/08 22:39:25 jlam Exp $
+# $NetBSD$
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 SQUIRRELMAIL_BUILDLINK3_MK:=	${SQUIRRELMAIL_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	squirrelmail
 .endif
 
@@ -11,9 +11,8 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nsquirrelmail}
 BUILDLINK_PACKAGES+=	squirrelmail
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}squirrelmail
 
-.if !empty(SQUIRRELMAIL_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.squirrelmail+=	{ja-,}squirrelmail>=1.4.6
-BUILDLINK_ABI_DEPENDS.squirrelmail?=	squirrelmail>=1.4.6
+.if ${SQUIRRELMAIL_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.7
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif	# SQUIRRELMAIL_BUILDLINK3_MK
 
