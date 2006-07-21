3f6bf7bc05e3fe12ece639b1fc985b700d9cfaf5
wiz <wiz>
2006-07-21 16:17:55 +0000

    regen, with correct library dependencies. Broke at least hobbitmon.

--- databases/rrdtool/buildlink3.mk
+++ databases/rrdtool/buildlink3.mk
@@ -1,9 +1,9 @@
-# $NetBSD: buildlink3.mk,v 1.13 2006/07/08 22:39:05 jlam Exp $
+# $NetBSD$
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 RRDTOOL_BUILDLINK3_MK:=	${RRDTOOL_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	rrdtool
 .endif
 
@@ -11,15 +11,14 @@ BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nrrdtool}
 BUILDLINK_PACKAGES+=	rrdtool
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}rrdtool
 
-.if !empty(RRDTOOL_BUILDLINK3_MK:M+)
+.if ${RRDTOOL_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.rrdtool+=	rrdtool>=1.2.9
 BUILDLINK_ABI_DEPENDS.rrdtool+=	rrdtool>=1.2.12nb3
 BUILDLINK_PKGSRCDIR.rrdtool?=	../../databases/rrdtool
 .endif	# RRDTOOL_BUILDLINK3_MK
 
-.include "../../graphics/freetype-lib/buildlink3.mk"
-.include "../../graphics/gd/buildlink3.mk"
-.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/libart2/buildlink3.mk"
 .include "../../www/cgilib/buildlink3.mk"
 
