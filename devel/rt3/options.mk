264b0e70d09651497c0f0dab8e7c5a1a0b8b0b1d
rillig <rillig>
2006-10-01 18:44:57 +0000

    Made the dependency to SpeedyCGI optional, disabled by default. Bumped PKGREVISION.

--- devel/rt3/options.mk
+++ devel/rt3/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.1 2005/09/08 13:43:05 cube Exp $
+# $NetBSD: options.mk,v 1.2 2006/05/01 09:38:08 cube Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rt
 PKG_OPTIONS_REQUIRED_GROUPS=	backend frontend
 PKG_OPTIONS_GROUP.backend=	mysql oracle pgsql sqlite
 PKG_OPTIONS_GROUP.frontend=	modperl1 modperl2 \
-				fastcgi
+				fastcgi speedycgi
 PKG_SUGGESTED_OPTIONS=	mysql fastcgi
 
 PKG_OPTIONS_LEGACY_OPTS=	backend-mysql:mysql backend-pgsql:pgsql	\
