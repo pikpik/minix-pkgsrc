b501883e0bc3a0526d27652da89020aca9d3b587
dmcmahill <dmcmahill>
2006-10-23 09:43:13 +0000

    update to 20061020
    Release notes for the gEDA/gaf 20061020 snapshot
    
       Changes
         _________________________________________________________________
    
       libgeda/gschem
    
       libgeda  and gschem no longer use libgdgeda (which was a gEDA specific
           hack to the libgd library). gEDA/gaf now wants to use the original
           GD  library.  (Wojciech  Kazubski  and  Carlos  Nieves  Onega with
           cleanup by others)
         *
         * gEDA/gaf now compiles out of the box on cygwin. (Cesar Strauss)
         * Fixed  bug  #1553544: "New pages inadvertently created when adding
           nets in gschem" (Peter Clifton)
         * New component selection dialog box for gschem. This one is so much
           better  than  the  original  one.  Please  test  it out and submit
           comments to the mailing lists. (Patrick Bernaud)
         * The preview window is inside the file selectors again. The preview
           window has been refactored and improved. (Patrick Bernaud)
         * gschem  now places a title block (or any component the user wants)
           when a new page/window is created. This solves the common complain
           that the initial zoom is way to far out. (Carlos Nieves Onega)
         * Improvements  to  the  new  print  dialog box: Make print settings
           sticky for session. (Peter Brett)
         * Fixed  bug #1527465: Do a zoom extents for all pages when the main
           window is maximized. (Carlos Nieves Onega and others)
         * Fixed  bug  #1565433:  Added  the border in the gdk-pixbuf's image
           output. (Carlos Nieves Onega)
         * New  dialog  for  user  confirmation  before  closing  a page or a
           window.  This  is  also a great improvement over the previous exit
           confirm dialog box. (Patrick Bernaud)
         * Various language translations updated.
         * libgeda's shared library version is now 27:0:0.
         * Many more bug fixes and code cleanups. (various people)
    
       gnetlist
         * Fixed   PCB  cursor  related  issues  (Peter  Clifton  and  Stuart
           Brorson).
         * Minor cleanup of gnet-PCB backend. (Thien-Thi Nguyen)
    
       gattrib
         * Made  gattrib throw up GUI window warning user of no components or
           attributes. (Stuart Brorson)
         * File  browsers  in gattrib are now the same ones that gschem uses.
           (Patrick Bernaud)
         * The usual bug fixes and code cleanup. (various)
    
       docs
         * Updated the wiki snapshot to the latest web wiki version.
    
       symbols
         * linear/lm311-1.sym:  Removed  the  GND  false  connection  of  the
           symbol.  (Ramakrishnan  Muthukrishnan,  John  Luciani,  and Carlos
           Nieves Onega).
         * Lots of off grid symbols cleaned up. (Werner Hoch)
    
       gsymcheck
         * Adds  newline that is missing from the "Found Pintype=..." message
           in s_check_pintype() in s_check.c. (Jeff Mallatt and Carlos Nieves
           Onega)
         * gsymcheck  now  counts the number of distinct pinnumbers specified
           in  all  slotdef= attributes. Uses that number, plus the number of
           net=  pins, to compare with the "footprint size" when checking for
           that warning. (Jeff Mallatt and Carlos Nieves Onega).
    
       utils
         * Applied  patches for the cygwin port. Lots of various improvements
           to  make  gschemdoc more Windows friendly (at least under cygwin).
           (Cesar Strauss, Peter Brett, and Carlos Nieves Onega)
    
       examples
         * No significant changes
    
       For more a detailed changes, please look in the appropriate ChangeLogs
       in the source tarballs.

--- cad/libgeda/buildlink3.mk
+++ cad/libgeda/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2006/09/01 12:00:43 dmcmahill Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2006/09/10 19:47:54 dmcmahill Exp $
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
 LIBGEDA_BUILDLINK3_MK:=	${LIBGEDA_BUILDLINK3_MK}+
@@ -12,12 +12,12 @@ BUILDLINK_PACKAGES+=	libgeda
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libgeda
 
 .if !empty(LIBGEDA_BUILDLINK3_MK:M+)
-BUILDLINK_API_DEPENDS.libgeda+=	libgeda-20060906{,nb[0-9]*}
-BUILDLINK_ABI_DEPENDS.libgeda?=	libgeda>=20060906
+BUILDLINK_API_DEPENDS.libgeda+=	libgeda-20061020{,nb[0-9]*}
+BUILDLINK_ABI_DEPENDS.libgeda?=	libgeda>=20061020
 BUILDLINK_PKGSRCDIR.libgeda?=	../../cad/libgeda
 .endif	# LIBGEDA_BUILDLINK3_MK
 
-.include "../../graphics/libgdgeda/buildlink3.mk"
+.include "../../graphics/gd/buildlink3.mk"
 .include "../../lang/guile/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 
