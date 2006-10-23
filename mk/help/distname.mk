d08cc0f4d331a8d2b86f9cd4d2270874b2cd82f7
rillig <rillig>
2006-10-23 13:52:32 +0000

    Added a file describing the DISTNAME variable, so "make help topic=DISTNAME" produces useful results. And no, I didn't start the PKGNAME vs. DISTNAME discussion here.

---
+++ mk/help/distname.mk
@@ -0,0 +1,11 @@
+# $NetBSD$
+#
+# DISTNAME
+#	Every package definition should start with the definition of this
+#	variable. It is used in three places:
+#
+#	1. It is the first part of the default value for DISTFILES.
+#	2. It is the default value for PKGNAME, except for the trailing
+#	   "nb" part.
+#	3. The default value of WRKSRC is ${WRKDIR}/${DISTNAME}.
+#
