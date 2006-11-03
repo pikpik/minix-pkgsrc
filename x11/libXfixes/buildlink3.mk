afc6a5b30451f882130b8c66bc1bc98dab0a3909
joerg <joerg>
2006-11-03 21:31:40 +0000

    Import libXfixes-4.0.1 from pkgsrc-wip: Xfixes extension and library of X RandR from the modular X.org X11.

---
+++ x11/libXfixes/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBXFIXES_BUILDLINK3_MK:=	${LIBXFIXES_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libXfixes
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:NlibXfixes}
+BUILDLINK_PACKAGES+=	libXfixes
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXfixes
+
+.if ${LIBXFIXES_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libXfixes+=	libXfixes>=4.0.1
+BUILDLINK_PKGSRCDIR.libXfixes?=	../../x11/libXfixes
+.endif	# LIBXFIXES_BUILDLINK3_MK
+
+.include "../../x11/fixesproto/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
