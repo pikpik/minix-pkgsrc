56465b589de1bcfe6885235a1e3ff745190f0ee0
joerg <joerg>
2006-11-03 20:02:34 +0000

    Import trapproto-3.4.3 from pkgsrc-wip: This package provides the Trap extension headers from modular X.org.

---
+++ x11/trapproto/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD$
+
+BUILDLINK_DEPMETHOD.trapproto?=	build
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+TRAPPROTO_BUILDLINK3_MK:=	${TRAPPROTO_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	trapproto
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntrapproto}
+BUILDLINK_PACKAGES+=	trapproto
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}trapproto
+
+.if ${TRAPPROTO_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.trapproto+=	trapproto>=3.4.3
+BUILDLINK_PKGSRCDIR.trapproto?=	../../x11/trapproto
+.endif	# TRAPPROTO_BUILDLINK3_MK
+
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXi/buildlink3.mk"
+.include "../../x11/xproto/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
