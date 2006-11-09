c0dade5d1a17c84d42beb73fbf1f42f5c998f8cc
rillig <rillig>
2006-11-09 14:36:18 +0000

    Rewrote check-portability in awk instead of shell, since the shell has a huge performance problem: When reading files, it calls read(2) for every single byte. awk instead reads a whole line at a time. For the lang/php5 package, the execution time changed from (7.8 real 4.5 user 3.1 sys) to (1.6 real 1.5 user 0.4 sys).

--- mk/check/check-portability.mk
+++ mk/check/check-portability.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check-portability.mk,v 1.8 2006/10/26 14:42:53 tv Exp $
+# $NetBSD: check-portability.mk,v 1.1 2006/11/09 02:53:15 rillig Exp $
 #
 # This file contains some checks that are applied to the configure
 # scripts to check for certain constructs that are known to cause
@@ -46,5 +46,5 @@ _check-portability:
 	[ -d ${WRKSRC}/. ] || exit 0;					\
 	cd ${WRKSRC};							\
 	env	PKGSRCDIR=${PKGSRCDIR:Q}				\
-		SKIP_FILTER=${CHECK_PORTABILITY_SKIP:@p@${p}) continue;;@:Q} \
+		SKIP_FILTER=${CHECK_PORTABILITY_SKIP:@p@${p}) skip=yes;;@:Q} \
 		sh ${PKGSRCDIR}/mk/check/check-portability.sh
