223b5c638726c7ad5310fa8f1b502b6fb0d64f61
rillig <rillig>
2006-11-17 09:19:37 +0000

    Added a check for whether X11BASE is set correctly. Currently the error message is
        Syntax error: word unexpected (expecting ")")
    
    which is not helpful at all.

--- mk/buildlink3/bsd.buildlink3.mk
+++ mk/buildlink3/bsd.buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.buildlink3.mk,v 1.187 2006/09/10 19:49:53 schwarz Exp $
+# $NetBSD: bsd.buildlink3.mk,v 1.188 2006/11/05 14:39:11 joerg Exp $
 #
 # Copyright (c) 2004 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -570,6 +570,10 @@ ${_BLNK_COOKIE.${_pkg_}}:
 		exit 1;							\
 		;;							\
 	esac
+	${RUN} [ ${X11BASE:Q}"" ] || {					\
+		${ERROR_MSG} "[bsd.buildlink3.mk] X11BASE is not set correctly."; \
+		exit 1;							\
+	}
 	${_PKG_SILENT}${_PKG_DEBUG}					\
 	case ${BUILDLINK_PREFIX.${_pkg_}} in				\
 	${LOCALBASE})   buildlink_dir="${BUILDLINK_DIR}" ;;		\
