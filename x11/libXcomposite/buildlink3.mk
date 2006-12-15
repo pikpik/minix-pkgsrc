d40a22abecc18d8af8a4b0f2d7cd508f086443ff
joerg <joerg>
2006-12-15 20:16:18 +0000

    Add compat forward to x11/Xcomposite.

--- x11/libXcomposite/buildlink3.mk
+++ x11/libXcomposite/buildlink3.mk
@@ -1,5 +1,11 @@
 # $NetBSD$
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular"
+.include "../../x11/Xcomposite/buildlink3.mk"
+.else
+
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBXCOMPOSITE_BUILDLINK3_MK:=	${LIBXCOMPOSITE_BUILDLINK3_MK}+
 
@@ -13,7 +19,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libXcomposite
 
 .if ${LIBXCOMPOSITE_BUILDLINK3_MK} == "+"
 BUILDLINK_API_DEPENDS.libXcomposite+=	libXcomposite>=0.3.1
-BUILDLINK_PKGSRCDIR.libXcomposite?=	../../wip/libXcomposite
+BUILDLINK_PKGSRCDIR.libXcomposite?=	../../x11/libXcomposite
 .endif	# LIBXCOMPOSITE_BUILDLINK3_MK
 
 .include "../../x11/compositeproto/buildlink3.mk"
@@ -23,3 +29,5 @@ BUILDLINK_PKGSRCDIR.libXcomposite?=	../../wip/libXcomposite
 .include "../../x11/libXfixes/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
+
+.endif
