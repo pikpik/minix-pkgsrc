dce471838a8d8e21da833b033378cba6985893d0
schwarz <schwarz>
2006-12-24 14:07:55 +0000

    added hack to work around gcc issue with -O3 optimization

---
+++ pkgtools/pkg_install-info/hacks.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+.if !defined(PKG_INSTALL-INFO_HACKS_MK)
+PKG_INSTALL-INFO_HACKS_MK=	#defined
+
+### [Sat Dec 23 16:09:15 CET 2006: schwarz]
+### work around gcc optimization issue (gcc bug report c/14675)
+### see http://gcc.gnu.org/ml/gcc-bugs/2004-03/msg02604.html
+###
+.  if !empty(PKGSRC_COMPILER:Mgcc*) && !empty(CFLAGS:M-O3)
+PKG_HACKS+=	suppress-Werror
+BUILDLINK_TRANSFORM+=	rm:-Werror
+.  endif
+
+.endif
