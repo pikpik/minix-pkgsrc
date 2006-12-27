dfe240fcfbda6398bb627349007116e66e12db10
joerg <joerg>
2006-12-27 18:18:22 +0000

    Update to gpl ghostscript-8.54 aka print/ghostscript-afpl relaunched under GPL with options.mk. This was discussed on tech-pkg@.

--- print/ghostscript/options.mk
+++ print/ghostscript/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.2 2006/11/06 10:59:52 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ghostscript
 PKG_SUPPORTED_OPTIONS=	x11
@@ -7,11 +7,10 @@ PKG_SUGGESTED_OPTIONS=	x11
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mx11)
-XLIBS=			Xt SM ICE Xext X11
-GS_DISPLAY_DEVICE=	x11.dev
-MAKEFRAGS+=		${FILESDIR}/devs.x11
-.include "../../mk/x11.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-x
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
 .else
-XLIBS?=			# empty
-GS_DISPLAY_DEVICE?=	# empty
+CONFIGURE_ARGS+=        --without-x
 .endif
