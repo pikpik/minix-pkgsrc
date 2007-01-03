c0ce6cf1bc1c187f69f4531389aa00baec12d253
tv <tv>
2007-01-03 15:25:12 +0000

    Require "inet6" option to be set, forcing IPv6 capability before building.

--- net/aiccu/options.mk
+++ net/aiccu/options.mk
@@ -1,7 +1,9 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2006/09/01 06:48:27 ghen Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.aiccu
 PKG_SUPPORTED_OPTIONS=	gnutls
+PKG_OPTIONS_REQUIRED_GROUPS=	inet6 # require inet6 capability
+PKG_OPTIONS_GROUP.inet6=	inet6
 
 .include "../../mk/bsd.options.mk"
 
