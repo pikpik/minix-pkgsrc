8a9db6611c4ece57cd821a81195a80a47ce03922
rillig <rillig>
2007-01-04 00:38:23 +0000

    As all GNOME libraries, this package needs a buildlink3 file.

---
+++ devel/gdl/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+GDL_BUILDLINK3_MK:=	${GDL_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	gdl
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ngdl}
+BUILDLINK_PACKAGES+=	gdl
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gdl
+
+.if ${GDL_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.gdl+=	gdl>=0.6.1
+BUILDLINK_PKGSRCDIR.gdl?=	../../devel/gdl
+.endif	# GDL_BUILDLINK3_MK
+
+.include "../../devel/libbonoboui/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
