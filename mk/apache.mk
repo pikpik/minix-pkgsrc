8d5af58863b5d674d5375b17acf7080ab1265087
epg <epg>
2007-01-24 19:48:44 +0000

    Oops, this was meant to go in with the rest of the apr changes. Use devel/apr0 for apache2 and devel/apr and devel/apr-util for apache22.

--- mk/apache.mk
+++ mk/apache.mk
@@ -1,4 +1,4 @@
-# $NetBSD: apache.mk,v 1.17 2006/12/08 23:58:46 xtraeme Exp $
+# $NetBSD: apache.mk,v 1.18 2006/12/09 08:24:10 wiz Exp $
 #
 # This file is meant to be included by packages that require an apache
 # web server.
@@ -124,9 +124,10 @@ PKG_APACHE=		none
 APACHE_PKG_PREFIX=	${_APACHE_PKG_PREFIX.${PKG_APACHE}}
 
 .if (${PKG_APACHE} == "apache2") && !empty(USE_APR:M[yY][eE][sS])
-.  include "../../devel/apr/buildlink3.mk"
+.  include "../../devel/apr0/buildlink3.mk"
 .elif (${PKG_APACHE} == "apache22") && !empty(USE_APR:M[Yy][Ee][Ss])
-.  include "../../devel/apr1/buildlink3.mk"
+.  include "../../devel/apr/buildlink3.mk"
+.  include "../../devel/apr-util/buildlink3.mk"
 .endif
 
 .endif	# APACHE_MK
