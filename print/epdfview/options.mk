6ed3257880df81691c9b4593f560e5ebad06e091
dillo <dillo>
2007-01-28 00:13:49 +0000

    Add cups option. Explicitly disable cups if option is not set -- fixes build againts cups-enabled gtk2.
    Okayed by maintainer.

---
+++ print/epdfview/options.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.epdfview
+PKG_SUPPORTED_OPTIONS=	cups
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mcups)
+.include "../../print/cups/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-cups
+.else
+CONFIGURE_ARGS+=	--without-cups
+.endif
