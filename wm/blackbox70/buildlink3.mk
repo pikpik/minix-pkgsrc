7d84842c04f9390443979d45cb8dbcedd4dc4957
joerg <joerg>
2007-02-01 21:59:34 +0000

    Modular Xorg support.

--- wm/blackbox70/buildlink3.mk
+++ wm/blackbox70/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1 2006/08/20 23:07:17 jschauma Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 BLACKBOX70_BUILDLINK3_MK:=	${BLACKBOX70_BUILDLINK3_MK}+
@@ -16,5 +16,7 @@ BUILDLINK_API_DEPENDS.blackbox70+=	blackbox70>=0.70.0
 BUILDLINK_ABI_DEPENDS.blackbox70+=	blackbox70>=0.70.0
 BUILDLINK_PKGSRCDIR.blackbox70?=	../../wm/blackbox70
 .endif	# BLACKBOX70_BUILDLINK3_MK
+
+.include "../../x11/libX11/buildlink3.mk"
 
 BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
