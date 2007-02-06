116005ef0eb87caff4b4bef32662b378d8ac9974
joerg <joerg>
2007-02-06 18:16:02 +0000

    Modular Xorg support.

--- emulators/BasiliskII/options.mk
+++ emulators/BasiliskII/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2005/08/31 16:11:34 hira Exp $
+# $NetBSD: options.mk,v 1.3 2005/09/01 11:31:57 hira Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.BasiliskII
 PKG_SUPPORTED_OPTIONS=	esound gtk sdl
@@ -25,5 +25,11 @@ CONFIGURE_ARGS+=	--enable-sdl-audio
 CONFIGURE_ARGS+=	--enable-sdl-video
 .include "../../devel/SDL/buildlink3.mk"
 .else
-.include "../../mk/x11.buildlink3.mk"
+BUILDLINK_DEPMETHOD.libXt?=	build
+
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
 .endif
