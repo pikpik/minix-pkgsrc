3f52b60f8aa392721e1eb1123ea37d84523c6f58
drochner <drochner>
2007-02-09 20:27:16 +0000

    -build against libmp4v2 instead of the full mpeg4ip -update to 1.25 which contains small bug fixes according to the notes -the most serious change seems to be that faac comes with an own copy  of libmp4v2 now which it tries to use exclusively -- add some patches  to still use an external lib (that's all optional, needed for tagging  support)

--- audio/faac/options.mk
+++ audio/faac/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.2 2006/06/07 21:33:21 hira Exp $
+# $NetBSD: options.mk,v 1.3 2006/06/08 09:29:50 hira Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.faac
 PKG_SUPPORTED_OPTIONS=		mpeg4ip
@@ -8,5 +8,8 @@ PKG_OPTIONS_LEGACY_OPTS+=	faad:mpeg4ip
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mmpeg4ip)
-.  include "../../multimedia/mpeg4ip/buildlink3.mk"
+.  include "../../multimedia/libmp4v2/buildlink3.mk"
+.else
+# ignore bundled mp4v2 lib
+CONFIGURE_ARGS+= --without-mp4v2
 .endif
