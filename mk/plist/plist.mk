48f6ad708b1608b63985d0df5e9c2df2354211a0
rillig <rillig>
2007-03-07 16:32:45 +0000

    Fixed a GNU awk warning about quoting.

--- mk/plist/plist.mk
+++ mk/plist/plist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: plist.mk,v 1.18 2006/11/05 15:10:08 joerg Exp $
+# $NetBSD: plist.mk,v 1.19 2007/02/20 09:22:14 uebayasi Exp $
 #
 # This Makefile fragment handles the creation of PLISTs for use by
 # pkg_create(8).
@@ -245,5 +245,5 @@ ${PLIST}:
 INFO_FILES_cmd=								\
 	${CAT} ${PLIST} |						\
 	${SETENV} ${_PLIST_AWK_ENV} ${AWK} ${_PLIST_INFO_AWK} |		\
-	${AWK} '($$0 !~ "-[0-9]*(\.gz)?$$") { print }'
+	${AWK} '($$0 !~ "-[0-9]*(\\.gz)?$$") { print }'
 .endif
