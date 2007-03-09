8bc643d993dc63c42a57d54b9dd4be2f560c02cb
rillig <rillig>
2007-03-09 04:07:50 +0000

    Fixed a quoting bug that occurred when PKG_DEVELOPER=#yes, CHECK_SHLIBS=yes and some shared library doesn't have the execute bit set.

--- mk/flavor/pkg/metadata.mk
+++ mk/flavor/pkg/metadata.mk
@@ -1,4 +1,4 @@
-# $NetBSD: metadata.mk,v 1.14 2007/03/02 09:08:33 wiz Exp $
+# $NetBSD: metadata.mk,v 1.15 2007/03/09 00:39:55 rillig Exp $
 
 ######################################################################
 ### The targets below are all PRIVATE.
@@ -74,7 +74,7 @@ ${_BUILD_INFO_FILE}: plist
 		linklibs=`${AWK} '/[^@].*\.so\.[0-9\.]+$$/ { print "${DESTDIR}${PREFIX}/" $$0 }' ${PLIST}`; \
 		for i in $$linklibs; do					\
 			if ${TEST} -r $$i -a ! -x $$i -a ! -h $$i; then	\
-				${TEST} ${PKG_DEVELOPER:Uno:Q} = "no" || \
+				${TEST} ${PKG_DEVELOPER:Uno:Q}"" = "no" || \
 					${ECHO} "$$i: installed without execute permission; fixing (should use [BSD_]INSTALL_LIB)"; \
 				${CHMOD} +x $$i;			\
 			fi;						\
