3fdc6ba21e14c1df72c6bef058cd737bc319ebcf
adam <adam>
2007-03-24 15:51:41 +0000

    Fixed sound.c compilation, added options.mk with SDL interface as an option

---
+++ emulators/fuse/options.mk
@@ -0,0 +1,17 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.fuse
+PKG_SUPPORTED_OPTIONS=	sdl gtk
+PKG_SUGGESTED_OPTIONS=	gtk
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgtk)
+CONFIGURE_ARGS+=	--with-gtk2
+.include "../../x11/gtk2/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Msdl)
+CONFIGURE_ARGS+=	--with-sdl
+.include "../../devel/SDL/buildlink3.mk"
+.endif
