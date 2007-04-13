e8976a6d2bdb56e90c2947aa8c342c269be041b4
adam <adam>
2007-04-13 21:25:48 +0000

    Changes 0.9.35: - Broken aRts sound driver now removed for good. - Many fixes to the Quartz DLL sound support. - File I/O performance improvements. - The usual assortment of Direct3D fixes. - Lots of bug fixes.

--- emulators/wine/options.mk
+++ emulators/wine/options.mk
@@ -1,14 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2006/03/31 10:48:00 adam Exp $
+# $NetBSD: options.mk,v 1.6 2006/05/31 19:06:08 ghen Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.wine
-PKG_SUPPORTED_OPTIONS=	arts cups opengl sane
+PKG_SUPPORTED_OPTIONS=	cups opengl sane
 PKG_SUGGESTED_OPTIONS=	opengl
 
 .include "../../mk/bsd.options.mk"
-
-.if !empty(PKG_OPTIONS:Marts)
-.include "../../audio/arts/buildlink3.mk"
-.endif
 
 .if !empty(PKG_OPTIONS:Mcups)
 .include "../../print/cups/buildlink3.mk"
