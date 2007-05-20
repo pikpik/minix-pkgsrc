bb134049900e17bf47954653d3c3fb230489c62b
taca <taca>
2007-05-20 17:02:17 +0000

    Add squirrelmail-japanese and squirrelmail-lite option. These options supersedes ja-squirrelmail package.
    Bump PKGREVISION.

---
+++ mail/squirrelmail/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.squirrelmail
+PKG_SUPPORTED_OPTIONS=	squirrelmail-japanese squirrelmail-lite
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Msquirrelmail-japanese)
+PATCH_SITES=		http://www.yamaai-tech.com/~masato/Download/
+PATCHFILES=		squirrelmail-1.4.10a-ja-20070511-patch.gz
+PATCH_DIST_STRIP=	-p1
+
+.endif
+
+.if !empty(PKG_OPTIONS:Msquirrelmail-lite)
+PATCH_SITES+=		ftp://ftp.back-street.net/pub/squirrelmail/
+PATCHFILES+=		squirrelmail-1.4.10a-lite-20070511-patch
+.endif
