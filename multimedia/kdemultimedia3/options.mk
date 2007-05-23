f580f28501a8fb7f85c21cfc210b6dfea889c8cf
markd <markd>
2007-05-23 13:16:11 +0000

    Update to KDE 3.5.7
    JuK
    * Tell Google not to send JavaScript when we request images to reduce
      errors in the cover art search, and make sure that images we request
      have the appropriate http:// protocol.

--- multimedia/kdemultimedia3/options.mk
+++ multimedia/kdemultimedia3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2007/01/26 02:58:41 markd Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kdemultimedia
 PKG_SUPPORTED_OPTIONS=	gstreamer oss
@@ -6,8 +6,8 @@ PKG_SUPPORTED_OPTIONS=	gstreamer oss
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgstreamer)
-.include "../../multimedia/gstreamer0.8/buildlink3.mk"
-.include "../../multimedia/gst-plugins0.8/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
 .endif
 
 .if !empty(PKG_OPTIONS:Moss)
