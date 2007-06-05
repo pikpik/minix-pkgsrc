60020d9b91e0936e3b1ed3fef370b4cb52d00b03
wiz <wiz>
2007-06-05 05:37:41 +0000

    opencdk shlib major changed; bump ABI depends and PKGREVISIONs of affected packages.

--- devel/libgnome/buildlink3.mk
+++ devel/libgnome/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2006/09/16 12:20:32 jmmv Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2006/10/25 01:40:30 dmcmahill Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBGNOME_BUILDLINK3_MK:=	${LIBGNOME_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libgnome
 
 .if !empty(LIBGNOME_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libgnome+=	libgnome>=2.8.0
-BUILDLINK_ABI_DEPENDS.libgnome+=	libgnome>=2.16.0nb1
+BUILDLINK_ABI_DEPENDS.libgnome+=	libgnome>=2.18.0nb1
 BUILDLINK_PKGSRCDIR.libgnome?=	../../devel/libgnome
 .endif	# LIBGNOME_BUILDLINK3_MK
 
--- net/libsoup-devel/buildlink3.mk
+++ net/libsoup-devel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/07/08 22:39:33 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2006/07/08 23:11:03 jlam Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBSOUP_DEVEL_BUILDLINK3_MK:=	${LIBSOUP_DEVEL_BUILDLINK3_MK}+
@@ -13,7 +13,7 @@ BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libsoup-devel
 
 .if !empty(LIBSOUP_DEVEL_BUILDLINK3_MK:M+)
 BUILDLINK_API_DEPENDS.libsoup-devel+=	libsoup-devel>=2.2.0
-BUILDLINK_ABI_DEPENDS.libsoup-devel?=	libsoup-devel>=2.2.7nb1
+BUILDLINK_ABI_DEPENDS.libsoup-devel?=	libsoup-devel>=2.2.100nb1
 BUILDLINK_PKGSRCDIR.libsoup-devel?=	../../net/libsoup-devel
 .endif	# LIBSOUP_DEVEL_BUILDLINK3_MK
 
