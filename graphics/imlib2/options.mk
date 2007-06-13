bd8865a882c4a1dbdbb9f7367773e7ba63bda7c9
obache <obache>
2007-06-13 14:01:11 +0000

    PKG_SUPPORTED_OPTIONS must be defined before include bsd.options.mk, even if empty.  Problem reported by Magnus Henoch in PR 36483.

--- graphics/imlib2/options.mk
+++ graphics/imlib2/options.mk
@@ -1,8 +1,9 @@
-# $NetBSD: options.mk,v 1.4 2005/06/11 16:54:31 wiz Exp $
+# $NetBSD: options.mk,v 1.5 2006/12/16 11:20:23 wiz Exp $
 
 .include "../../mk/bsd.prefs.mk"
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.imlib2
+PKG_SUPPORTED_OPTIONS=	#empty
 .if ${MACHINE_ARCH} == "i386"
 PKG_SUPPORTED_OPTIONS+=	mmx
 .elif ${MACHINE_ARCH} == "x86_64"
