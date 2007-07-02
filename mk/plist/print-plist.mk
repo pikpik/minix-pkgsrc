963c7b6fdba2b1609e68c836aa65e0a18a3e7cee
joerg <joerg>
2007-07-02 14:54:21 +0000

    Remove USE_MTREE support. Keep one copy (the NetBSD version) for the purpose of print-PLIST in plist/common-dirs.mtree.
    Discussed with wiz@, no objections on tech-pkg@.

--- mk/plist/print-plist.mk
+++ mk/plist/print-plist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: print-plist.mk,v 1.11 2006/11/15 10:40:34 joerg Exp $
+# $NetBSD: print-plist.mk,v 1.12 2007/03/02 09:08:33 wiz Exp $
 
 ###
 ### Automatic PLIST generation
@@ -57,6 +57,8 @@ _PRINT_PLIST_AWK_IGNORE+=	|| ($$0 ~ /^.*\/fonts\.cache-1/)
 # Common (system) directories not to generate @dirrm statements for
 # Reads _MTREE_FILE and generate awk statements that will
 # sort out which directories NOT to include into the PLIST @dirrm list
+_COMMON_MTREE_FILE=	${PKGSRCDIR}/mk/plist/common-dirs.mtree
+
 .if make(print-PLIST)
 _PRINT_PLIST_COMMON_DIRS!=	${AWK} 'BEGIN {				\
 			i=0;						\
@@ -80,7 +82,7 @@ _PRINT_PLIST_COMMON_DIRS!=	${AWK} 'BEGIN {				\
 			}						\
 		}							\
 		END { print "{ print $$$$0; }"; }			\
-	' <${_MTREE_FILE}
+	' <${_COMMON_MTREE_FILE}
 .endif
 
 # scan $PREFIX for any files/dirs modified since the package was extracted
