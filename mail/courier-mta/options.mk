37e322283b61af907e18056188fa9f444983b843
jlam <jlam>
2007-07-04 20:55:07 +0000

    Make it easier to build and install packages "unprivileged", where the owner of all installed files is a non-root user.  This change affects most packages that require special users or groups by making them use the specified unprivileged user and group instead.
    (1) Add two new variables PKG_GROUPS_VARS and PKG_USERS_VARS to
        unprivileged.mk.  These two variables are lists of other bmake
        variables that define package-specific users and groups.  Packages
        that have user-settable variables for users and groups, e.g. apache
        and APACHE_{USER,GROUP}, courier-mta and COURIER_{USER,GROUP},
        etc., should list these variables in PKG_USERS_VARS and PKG_GROUPS_VARS
        so that unprivileged.mk can know to set them to ${UNPRIVILEGED_USER}
        and ${UNPRIVILEGED_GROUP}.
    
    (2) Modify packages to use PKG_GROUPS_VARS and PKG_USERS_VARS.

--- mail/courier-mta/options.mk
+++ mail/courier-mta/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2007/06/15 21:59:23 jlam Exp $
+# $NetBSD: options.mk,v 1.4 2007/06/19 21:29:10 jlam Exp $
 
 # Global and legacy options
 
@@ -99,6 +99,8 @@ CONF_FILES_PERMS+=	${EGDIR}/module.local				\
 COURIER_TRANSPORTS+=	uucp
 PKG_GROUPS+=		${UUCP_GROUP}
 PKG_USERS+=		${UUCP_USER}:${UUCP_GROUP}
+PKG_GROUPS_VARS+=	UUCP_GROUP
+PKG_USERS_VARS+=	UUCP_USER
 PLIST_SRC+=		${PKGDIR}/PLIST.uucp
 
 CONF_FILES_PERMS+=	${EGDIR}/module.uucp				\
