3076f99bcf06fd2f56ff89bc17894b3afa037a23
rillig <rillig>
2007-07-25 18:07:34 +0000

    Documented PKGCONFIG_OVERRIDE and PKGCONFIG_OVERRIDE_STAGE.

--- mk/configure/pkg-config-override.mk
+++ mk/configure/pkg-config-override.mk
@@ -1,4 +1,15 @@
-# $NetBSD: pkg-config-override.mk,v 1.1 2006/07/05 06:09:15 jlam Exp $
+# $NetBSD: pkg-config-override.mk,v 1.2 2006/07/10 16:12:20 jlam Exp $
+
+# Package-settable variables:
+#
+# PKGCONFIG_OVERRIDE
+#	The list of pkg-config files relative to WRKSRC in which the
+#	compiler flags need to be adjusted.
+#
+# PKGCONFIG_OVERRIDE_STAGE
+#	The stage in which the pkg-config files will be adjusted. By
+#	default, this is pre-configure.
+#
 
 ######################################################################
 ### pkg-config-override (PRIVATE)
