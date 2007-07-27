aea30be47d86cb7cb8c77d2607d3c443f52f1d67
joerg <joerg>
2007-07-27 17:08:27 +0000

    As reported by tnn@, the old approach to find the byte order doesn't work on HP-UX/Itanium. Use the standard approach using AC_C_BIGENDIAN. Tested by myself on i386, he@ on SPARC and tnn@ on HP-UX.

--- pkgtools/digest/files/sha1.c
+++ pkgtools/digest/files/sha1.c
@@ -51,7 +51,7 @@
  * blk0() and blk() perform the initial expand.
  * I got the idea of expanding during the round function from SSLeay
  */
-#if BYTE_ORDER == LITTLE_ENDIAN
+#ifndef WORDS_BIGENDIAN
 # define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
     |(rol(block->l[i],8)&0x00FF00FF))
 #else
