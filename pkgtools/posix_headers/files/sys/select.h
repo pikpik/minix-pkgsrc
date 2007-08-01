fdde9074fa818fc822f8c5c7d0661265b1bd44f5
tnn <tnn>
2007-08-01 21:33:48 +0000

    Near full rewrite of posix_headers: - Implement a simple framework for automated header testing and generation   using the c preprocessor. - Make it installable on all platforms. On most platforms it will not install   any files however. - Bump revision to 0.5.
    The changes should make it easier to add Interix support to this package.
    Note that the package is still far from complete; for example the replacement
    stdint.h does not yet provide fallback definitions for int_*t and friends
    but relies on inttypes.h to provide them, which turns out to work for HPUX.

--- pkgtools/posix_headers/files/sys/select.h
+++
@@ -1,9 +0,0 @@
-/* $NetBSD$ */
-#ifndef _PKGSRC_SYS_SELECT_H_
-#define _PKGSRC_SYS_SELECT_H_
-#ifdef __hpux
-#include <sys/time.h>
-#else
-#error This header file only applies to HP-UX.
-#endif /* __hpux */
-#endif /* _PKGSRC_SYS_SELECT_H_ */
