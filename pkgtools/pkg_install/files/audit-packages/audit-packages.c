067f27c673536d2c6a9aa9a405b3b4752f3bcfe9
taca <taca>
2007-08-01 23:58:15 +0000

    Include <stdint.h> if HAVE_STDINT_H is defined, not HAVE_INTTYPES_H.
    Fix build problem on SunOS 5.8.

--- pkgtools/pkg_install/files/audit-packages/audit-packages.c
+++ pkgtools/pkg_install/files/audit-packages/audit-packages.c
@@ -1,4 +1,4 @@
-/* $NetBSD: audit-packages.c,v 1.5 2007/07/23 12:29:32 adrianp Exp $ */
+/* $NetBSD: audit-packages.c,v 1.6 2007/07/26 11:27:58 joerg Exp $ */
 
 /*
  * Copyright (c) 2007 Adrian Portelli <adrianp@NetBSD.org>.
@@ -42,7 +42,7 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-#ifdef HAVE_INTTYPES_H
+#ifdef HAVE_STDINT_H
 #include <stdint.h>
 #endif
 #if defined(HAVE_ERR_H) || !defined(PKGSRC)
