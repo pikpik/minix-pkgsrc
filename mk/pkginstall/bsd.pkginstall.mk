aaafc6238a7c1b0bd0f6a12d772c71e0c9e42f9e
jlam <jlam>
2007-08-02 16:00:33 +0000

    Don't bother using environment variables to pass info the shlib-type shell script.  Just specify everything on the command line.  Also, since shlib-type is a plain-old shell script with no bells and whistles, we can safely run it with "sh" and not "${SH}", which is necessary because "${SH}" isn't defined at this point.

--- mk/pkginstall/bsd.pkginstall.mk
+++ mk/pkginstall/bsd.pkginstall.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkginstall.mk,v 1.28 2007/07/23 15:23:47 jlam Exp $
+# $NetBSD: bsd.pkginstall.mk,v 1.29 2007/07/27 18:37:35 jlam Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and implements the
 # common INSTALL/DEINSTALL scripts framework.  To use the pkginstall
@@ -700,8 +700,8 @@ ${_INSTALL_SHELL_FILE}: ../../mk/pkginstall/shell
 #
 SHLIB_TYPE=		${_SHLIB_TYPE_cmd:sh}
 _SHLIB_TYPE_cmd=							\
-	${SETENV} PKG_INFO_CMD=${PKG_INFO_CMD:Q}			\
-	${SH} ${.CURDIR}/../../mk/scripts/shlib-type ${_OPSYS_SHLIB_TYPE:Q}
+	sh ${.CURDIR}/../../mk/scripts/shlib-type			\
+		${_OPSYS_SHLIB_TYPE:Q} ${PKG_INFO_CMD:Q}
 
 LDCONFIG_ADD_CMD?=		${_LDCONFIG_ADD_CMD.${OPSYS}}
 LDCONFIG_REMOVE_CMD?=		${_LDCONFIG_REMOVE_CMD.${OPSYS}}
