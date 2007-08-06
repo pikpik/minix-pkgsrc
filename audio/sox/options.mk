1fc07f71b53ed44eaad7f3daa2b933db724951f7
wiz <wiz>
2007-08-06 05:28:37 +0000

    Add an option to turn on OSS support (instead of turning it off unconditionally), for DragonFly. Based on a patch provided by Steve O'Hara-Smith on pkgsrc-users.
    While here, split off options handling into an options.mk file.

---
+++ audio/sox/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.sox
+PKG_SUPPORTED_OPTIONS=	lame oss
+# lame has LICENSE= issues and thus should not be SUGGESTED.
+PKG_SUGGESTED_OPTIONS=
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlame)
+# This is an option due to LICENSE= issues.
+.include "../../audio/lame/buildlink3.mk"
+.endif
+
+.if empty(PKG_OPTIONS:Moss)
+CONFIGURE_ARGS+=	--without-oss
+.endif
