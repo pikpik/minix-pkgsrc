d689e336f743816ae13276e17d2d0e079817cdd5
jlam <jlam>
2007-08-10 17:56:57 +0000

    Update mail/courier-imap to 4.1.3.  Changes from version 4.1.1 include:
    + Remove BDB_LIB workaround which should already be handled by the wrapper
      scripts and db1.builtin.mk.
    
    * Plug some resource leaks in the maildir library code.

--- mail/courier-imap/files/courierpops.sh
+++ mail/courier-imap/files/courierpops.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: courierpops.sh,v 1.14 2006/06/09 16:35:04 reed Exp $
+# $NetBSD: courierpops.sh,v 1.15 2006/06/17 19:26:48 jlam Exp $
 #
 # Courier POP3/SSL services daemon
 #
@@ -17,8 +17,8 @@ ctl_command="@PREFIX@/sbin/pop3d-ssl"
 pidfile="@VARBASE@/run/pop3d-ssl.pid"
 required_files="@PKG_SYSCONFDIR@/pop3d @PKG_SYSCONFDIR@/pop3d-ssl"
 
-start_cmd="courierpops_doit start"
-stop_cmd="courierpops_doit stop"
+start_cmd="${name}_doit start"
+stop_cmd="${name}_doit stop"
 
 courierpops_doit()
 {
