b13f089e4589a4dab3a306c01ec20bcceabca3a9
taca <taca>
2007-08-18 15:09:13 +0000

    Update sudo package to 1.6.9p4.
    pkgsrc change:
    
    Make these options mutual exclusive: kerberos pam skey.
    (Really, combinations of kerberos and pam, pam and skey are conflicts.)
    
    CHANGES:
    
    609) Worked around a bug ins some PAM implementations that caused a crash
         when no tty was present.
    
    610) Fixed a crash on some platforms in the error logging function.
    
    611) Documentation improvements.
    
    Sudo 1.6.9p1 released.
    
    612) Fixed updating of the saved environment when the environ pointer
         gets changed out from underneath us.
    
    Sudo 1.6.9p2 released.
    
    613) Fixed a bug related to supplemental group matching introduced
         in 1.6.9.
    
    Sudo 1.6.9p3 released.
    
    614) Added IPv6 support from YOSHIFUJI Hideaki.
    
    615) Fixed sudo_noexec installation path.
    
    616) Fixed a K&R compilation error.
    
    Sudo 1.6.9p4 released.

--- security/sudo/options.mk
+++ security/sudo/options.mk
@@ -1,8 +1,10 @@
-# $NetBSD: options.mk,v 1.11 2006/05/31 18:22:26 ghen Exp $
+# $NetBSD: options.mk,v 1.12 2007/07/23 16:38:36 tls Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sudo
-PKG_SUPPORTED_OPTIONS=	pam kerberos ldap skey
+PKG_SUPPORTED_OPTIONS=	kerberos ldap pam skey
+PKG_OPTIONS_OPTIONAL_GROUPS= auth
+PKG_OPTIONS_GROUP.auth=	kerberos pam skey
 
 .if ${OPSYS} == "NetBSD" && exists(/usr/include/skey.h)
 PKG_SUGGESTED_OPTIONS=	skey
