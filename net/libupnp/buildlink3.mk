5a60c1342c8a4dc9439f857a27dba3cafb57f539
obache <obache>
2007-08-19 01:36:23 +0000

    Update libupnp to 1.6.0.  Merge from wip/libupnp. No functionally changes since RC1.

--- net/libupnp/buildlink3.mk
+++ net/libupnp/buildlink3.mk
@@ -12,12 +12,8 @@ BUILDLINK_PACKAGES+=	libupnp
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libupnp
 
 .if ${LIBUPNP_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.libupnp+=	libupnp>1.4.6
+BUILDLINK_API_DEPENDS.libupnp+=	libupnp>=1.4.6
 BUILDLINK_PKGSRCDIR.libupnp?=	../../net/libupnp
-
-PRINT_PLIST_AWK+=	/^@dirrm include\/upnp$$/ { print "@comment in net/libupnp: " $$0; next; }
-PRINT_PLIST_AWK+=	/^@exec \$${MKDIR} %D\/include\/upnp$$/ { next; }
-
 .endif	# LIBUPNP_BUILDLINK3_MK
 
 BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
