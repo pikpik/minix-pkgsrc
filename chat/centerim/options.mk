0260a27932a480e937476940ea4c0b4a84317331
jnemeth <jnemeth>
2007-08-30 00:47:02 +0000

    Text mode menu- and window-driven IM interface that supports the ICQ2000, Yahoo!, AIM, IRC, MSN, Gadu-Gadu and Jabber protocols. It allows you to send, receive, and forward messages, URLs, SMSes (both through the ICQ server and e-mail gateways supported by Mirabilis), contacts, and email express messages. It also lets you set your own and fetch others' away messages, and define external handlers for incoming events. Apart from IM it supports LiveJournal management and receiving RSS feeds.  This is a fork of CenterICQ.

---
+++ chat/centerim/options.mk
@@ -0,0 +1,16 @@
+# $NetBSD: options.mk,v 1.2 2007/04/15 20:16:42 wiz Exp $
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.centerim
+PKG_SUPPORTED_OPTIONS=	gpgme
+PKG_SUGGESTED_OPTIONS=	gpgme
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mgpgme)
+#CONFIGURE_ARGS+=	--with-gpgme=${BUILDLINK_PREFIX.gpgme}
+#CONFIGURE_ARGS+=	--with-gpgme=${BUILDLINK_PREFIX.gpgme:Q}/bin/gpgme-config
+.  include "../../security/gpgme/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-gpgme
+.endif
