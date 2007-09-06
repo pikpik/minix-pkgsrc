25cc0811b6132319a42cab8ae3b57ce1d6b1f09e
rillig <rillig>
2007-09-06 08:28:01 +0000

    One line of code can say as much as ten.

--- mk/bsd.pkg.update.mk
+++ mk/bsd.pkg.update.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bsd.pkg.update.mk,v 1.7 2006/10/05 12:56:27 rillig Exp $
+# $NetBSD: bsd.pkg.update.mk,v 1.8 2007/09/06 08:24:12 rillig Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and contains the targets
 # and variables for "make update".
@@ -125,10 +125,7 @@ update-dirlist:
 
 
 ${_DDIR}: ${_DLIST}
-	${RUN}								\
-	for pkg in `${CAT} ${_DLIST}`; do				\
-		${PKG_INFO} -Q PKGPATH "$${pkg}";			\
-	done >> ${_DDIR}
+	${RUN} ${PKG_INFO} -Q PKGPATH `${CAT} ${_DLIST}` > ${_DDIR}
 
 ${_DLIST}: ${WRKDIR}
 	${_PKG_SILENT}${_PKG_DEBUG}					\
