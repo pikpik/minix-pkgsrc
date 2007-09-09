5e924a8e710a633b69b3d8481efbe747133bea1b
taca <taca>
2007-09-09 14:17:13 +0000

    Update ruby18-base to 1.8.6.36 (Ruby 1.8.6 patchlevel 36).
    Ruby 1.8.6 patchlevel 36 is maintainous release of Ruby.
    
    Changes are too many, please see ChangeLog:
    
        http://svn.ruby-lang.org/repos/ruby/tags/v1_8_6_36/ChangeLog

--- lang/ruby18-base/options.mk
+++ lang/ruby18-base/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.1 2006/12/06 16:24:27 taca Exp $
+# $NetBSD: options.mk,v 1.2 2007/01/28 15:15:58 taca Exp $
 
-PKG_OPTIONS_VAR=		PKG_OPTIONS.ruby
-PKG_SUPPORTED_OPTIONS=		ruby-build-ri-db
+PKG_OPTIONS_VAR=	PKG_OPTIONS.ruby
+PKG_SUPPORTED_OPTIONS=	ruby-build-ri-db
+PKG_SUGGESTED_OPTIONS=	ruby-build-ri-db
 
 .include "../../mk/bsd.options.mk"
 
