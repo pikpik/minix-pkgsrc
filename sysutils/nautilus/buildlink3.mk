7c629d9e79af4df1eddfebfa1ca740d2049ff608
wiz <wiz>
2007-09-20 21:21:39 +0000

    Adapt eel dependencies; bump PKGREVISION.

--- sysutils/nautilus/buildlink3.mk
+++ sysutils/nautilus/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2007/06/05 05:37:01 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2007/09/19 23:13:31 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 NAUTILUS_BUILDLINK3_MK:=	${NAUTILUS_BUILDLINK3_MK}+
@@ -24,7 +24,7 @@ PRINT_PLIST_AWK+=	/^@dirrm lib\/nautilus\/extensions-1.0$$/ \
 
 .include "../../audio/esound/buildlink3.mk"
 .include "../../devel/GConf2/buildlink3.mk"
-.include "../../devel/eel2/buildlink3.mk"
+.include "../../devel/eel/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../devel/libbonobo/buildlink3.mk"
 .include "../../devel/libbonoboui/buildlink3.mk"
