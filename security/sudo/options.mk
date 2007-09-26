561cbb2bdd787e583c6967a05387f68bdce36507
bjs <bjs>
2007-09-26 05:47:46 +0000

    PKG_OPTIONS_OPTIONAL_GROUPS/PKG_OPTIONS_NONEMPTY_SETS have their respective options added to PKG_SUPPORTED_OPTIONS automagically.  Duplicate options removed.

--- security/sudo/options.mk
+++ security/sudo/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.12 2007/07/23 16:38:36 tls Exp $
+# $NetBSD: options.mk,v 1.13 2007/08/18 15:09:12 taca Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.sudo
-PKG_SUPPORTED_OPTIONS=	kerberos ldap pam skey
+PKG_SUPPORTED_OPTIONS=	ldap
 PKG_OPTIONS_OPTIONAL_GROUPS= auth
 PKG_OPTIONS_GROUP.auth=	kerberos pam skey
 
