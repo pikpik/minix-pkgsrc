ceda923a24d20c894d18a7b0297eac943340d2e5
rillig <rillig>
2007-09-29 15:58:16 +0000

    obsolete.

--- mk/apachever.mk
+++ mk/apachever.mk
@@ -1,3 +1,5 @@
-# $NetBSD: apachever.mk,v 1.1 2006/06/02 18:27:57 joerg Exp $
+# $NetBSD: apachever.mk,v 1.2 2006/10/14 01:09:45 rillig Exp $
+#
+# This file is obsolete. It will be removed after branching 2007Q4.
 #
 .include "${.PARSEDIR}/apache.mk"
