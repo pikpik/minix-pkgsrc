6d3456e1764edf4c5c9a11bbb132a74cd6258585
aymeric <aymeric>
2007-10-02 15:04:53 +0000

    remove option xterm-toolbar from the default options. Toolbar support seems to be broken in xterm for now. OK bjs@

--- x11/xterm/options.mk
+++ x11/xterm/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.4 2006/12/15 20:33:06 joerg Exp $
+# $NetBSD: options.mk,v 1.5 2007/09/17 06:36:21 bjs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.xterm
 PKG_SUPPORTED_OPTIONS=	pcre xterm-luit freetype xterm-toolbar
 PKG_OPTIONS_OPTIONAL_GROUPS=	color
 PKG_OPTIONS_GROUP.color=	xterm-88color xterm-256color
-PKG_SUGGESTED_OPTIONS=	xterm-256color xterm-luit xterm-toolbar
+PKG_SUGGESTED_OPTIONS=	xterm-256color xterm-luit
 
 .include "../../mk/bsd.options.mk"
 
