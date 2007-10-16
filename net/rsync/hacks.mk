24e1eabd8143d01c9c1fce1773fa3f30ec453a0a
tnn <tnn>
2007-10-16 23:49:02 +0000

    Fix abusers of LOWER_OPSYS to check OPSYS or MACHINE_PLATFORM instead.

--- net/rsync/hacks.mk
+++ net/rsync/hacks.mk
@@ -1,9 +1,9 @@
-# $NetBSD: hacks.mk,v 1.1 2006/04/10 13:26:15 schwarz Exp $
+# $NetBSD: hacks.mk,v 1.1 2006/11/21 19:36:16 tv Exp $
 
 ### [Tue Nov 21 19:32:00 UTC 2006 : tvierling]
 ### iconv(..., NULL, 0, NULL, 0) crashes in libc version of iconv(3)
 ###
-.if !empty(LOWER_OPSYS:Minterix*)
+.if ${OPSYS} == "Interix"
 PKG_HACKS+=	pkgsrc-iconv
 .include "../../converters/libiconv/buildlink3.mk"
 .endif
