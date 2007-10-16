f9a185e2dfa3d96d8af45d053b7a73c362aad7fc
bjs <bjs>
2007-10-16 17:12:23 +0000

    Add bzip2/zlib support to links.  Add destdir support.  Bump rev.

---
+++ www/links/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.links
+PKG_SUPPORTED_OPTIONS=	bzip2 links-zlib
+PKG_SUGGESTED_OPTIONS=	bzip2 links-zlib
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mbzip2)
+.  include "../../archivers/bzip2/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-bzip2
+.endif
+
+.if !empty(PKG_OPTIONS:Mlinks-zlib)
+.  include "../../devel/zlib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-zlib
+.endif
