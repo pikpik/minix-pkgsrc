0494f1dbcdd99a467877d6d2c9327017f61e4309
taca <taca>
2007-10-28 02:07:41 +0000

    Add cp932-patch which was ja-patch as PKG_OPTION and enabled default.
    Bump PKGREVISION.

---
+++ converters/libiconv/options.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libiconv
+PKG_SUPPORTED_OPTIONS=	libiconv-cp932
+PKG_SUGGESTED_OPTIONS=	libiconv-cp932
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mlibiconv-cp932)
+PATCH_SITES=	http://www2d.biglobe.ne.jp/~msyk/software/libiconv/
+PATCHFILES=	libiconv-1.11-cp932.patch.gz
+PATCH_DIST_STRIP=	-p1
+.endif
