8c8c1b81b0337f0f0da6fa0cdf0df84f1e7bc40e
drochner <drochner>
2007-10-31 19:52:36 +0000

    update to 1.20.1 This switches to the new gnome-2.20 branch. Also add the Python bindings as a seperate pkg. I don't see where this is used, but is is a simple addition.

---
+++ devel/py-at-spi/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+PY_AT_SPI_BUILDLINK3_MK:=	${PY_AT_SPI_BUILDLINK3_MK}+
+
+.include "../../lang/python/pyversion.mk"
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	py-at-spi
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Npy-at-spi}
+BUILDLINK_PACKAGES+=	py-at-spi
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}py-at-spi
+
+.if ${PY_AT_SPI_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.py-at-spi+=	${PYPKGPREFIX}-at-spi>=1.20.1
+BUILDLINK_PKGSRCDIR.py-at-spi?=		../../devel/py-at-spi
+.endif	# PY_AT_SPI_BUILDLINK3_MK
+
+.include "../../devel/at-spi/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
