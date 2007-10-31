9a81f25ae7e4209955b2a5e94064e7f790b2ec12
rillig <rillig>
2007-10-31 12:07:13 +0000

    Simpler .include lines.

--- mk/flavor/bsd.flavor.mk
+++ mk/flavor/bsd.flavor.mk
@@ -1,7 +1,7 @@
-# $NetBSD$
+# $NetBSD: bsd.flavor.mk,v 1.1 2006/06/03 23:11:42 jlam Exp $
 #
 # This Makefile fragment is included by bsd.pkg.mk and pulls in the correct
 # target and variable overrides for the selected package system flavor.
 #
 
-.include "${PKGSRCDIR}/mk/flavor/${PKG_FLAVOR}/flavor.mk"
+.include "${PKG_FLAVOR}/flavor.mk"
