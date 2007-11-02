ef1f9f85442ef8be60f3850a1ea72b845e5790ee
uebayasi <uebayasi>
2007-11-02 00:41:04 +0000

    Optionalize X11 support.  Requested & tested by Fredrik Ljung.
    Reviewed By:	abs

---
+++ fonts/t1lib/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.t1lib
+PKG_SUPPORTED_OPTIONS+= x11
+PKG_SUGGESTED_OPTIONS+= x11
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXaw/buildlink3.mk"
+.include "../../x11/libXp/buildlink3.mk"
+.include "../../x11/libXpm/buildlink3.mk"
+.include "../../x11/libXt/buildlink3.mk"
+.endif
