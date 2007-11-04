bed741cfe1f1abe7f930bfef6328adfe8c8de9e6
rillig <rillig>
2007-11-04 10:06:06 +0000

    Imported obby.
    libobby is a library which provides synced document buffers. It supports
    multiple documents in one session and is portable to both Windows and
    Unix-like platforms.

---
+++ editors/obby/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+#
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+OBBY_BUILDLINK3_MK:=	${OBBY_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	obby
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nobby}
+BUILDLINK_PACKAGES+=	obby
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}obby
+
+.if ${OBBY_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.obby+=	obby>=0.4.4
+BUILDLINK_PKGSRCDIR.obby?=	../../editors/obby
+.endif	# OBBY_BUILDLINK3_MK
+
+.include "../../devel/libsigc++/buildlink3.mk"
+.include "../../net/net6/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
