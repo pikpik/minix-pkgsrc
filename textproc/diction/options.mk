5a3dfb70a4780b127b180a9c75716271b9b643db
bjs <bjs>
2007-12-04 00:57:41 +0000

    Update to version 1.11.  While here:  libtoolize, switch to gmake (generating po files properly seems to need this), add pcre support.
    Changes before version 1.10 are unavailable.
    
    The following things have been changed compared to version 1.10:
    
    o  Bugfix in ellipsis processing
    o  Support for Dutch
    o  GPL 3.0
    o  Use autoconf 2.61

---
+++ textproc/diction/options.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.diction
+PKG_SUPPORTED_OPTIONS=	pcre
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpcre)
+LIBPCREPOSIX=	-lpcreposix
+MAKE_ENV+=	LIBPCREPOSIX=${LIBPCREPOSIX:Q}
+.  include "../../devel/pcre/buildlink3.mk"
+.endif
