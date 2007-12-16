20f8cb1162a30baf57111496937be3bcdaf35901
tron <tron>
2007-12-16 12:27:28 +0000

    Mac OS X's Leopard "/bin/sh" doesn't support "echo -n" (apparently by purpose because if it is invoked as "/bin/bash" it does). Use "/bin/echo -n" instead.

--- mk/platform/Darwin.mk
+++ mk/platform/Darwin.mk
@@ -1,11 +1,15 @@
-# $NetBSD: Darwin.mk,v 1.24 2007/07/29 05:19:44 jlam Exp $
+# $NetBSD: Darwin.mk,v 1.26 2007/10/19 13:41:35 rillig Exp $
 #
 # Variable definitions for the Darwin operating system.
 
 .if !defined(CPP) || ${CPP} == "cpp"
 CPP=		${CC} -E ${CPP_PRECOMP_FLAGS}
 .endif
+.if empty(MACHINE_PLATFORM:MDarwin-[0-8].*-i386)
+ECHO_N?=	/bin/echo -n
+.else
 ECHO_N?=	${ECHO} -n
+.endif
 LDD?=		/usr/bin/otool -L
 IMAKE_MAKE?=	${MAKE}		# program which gets invoked by imake
 PKGLOCALEDIR?=	share
