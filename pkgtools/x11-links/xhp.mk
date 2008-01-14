00a233b36179744e358e97287c7e1af3c1d74a3b
tnn <tnn>
2008-01-14 08:27:32 +0000

    Retire support for builtin X11 on HP-UX. The builtin libs are simply too broken. (Missing dynamic Xaw libs, header issues and so on ...) I'll make X11_TYPE=modular the default on HP-UX instead.

--- pkgtools/x11-links/xhp.mk
+++
@@ -1,9 +0,0 @@
-# $NetBSD$
-
-.if ${OS_VERSION} == "11.11" && ${OBJECT_FMT} == "SOM"
-FILES_LIST=	${FILESDIR}/xhp
-.else
-PKG_FAIL_REASON+=	"No native X11 support for this HP-UX release."
-PKG_FAIL_REASON+=	"Try X11_TYPE=modular"
-.endif
-
