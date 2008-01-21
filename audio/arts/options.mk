dc21b1127924d1680c9aefedd6b1ab32f8496d9b
markd <markd>
2008-01-21 00:43:50 +0000

    Add optional nas and jack support and make esound optional (on by default, for backward compatibility) note user-destdir support.

---
+++ audio/arts/options.mk
@@ -0,0 +1,22 @@
+$NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.arts
+PKG_SUPPORTED_OPTIONS=	esound nas jack
+PKG_SUGGESTED_OPTIONS=	esound
+
+.include "../../mk/bsd.options.mk"
+
+# ESOUND support
+.if !empty(PKG_OPTIONS:Mesound)
+.include "../../audio/esound/buildlink3.mk"
+.endif
+
+# NAS support
+.if !empty(PKG_OPTIONS:Mnas)
+.include "../../audio/nas/buildlink3.mk"
+.endif
+
+# JACK support
+.if !empty(PKG_OPTIONS:Mjack)
+.include "../../audio/jack/buildlink3.mk"
+.endif
