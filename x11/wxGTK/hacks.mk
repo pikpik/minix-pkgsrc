91005f1b92b6bf36392667d5d48268525015d5db
jdc <jdc>
2008-01-22 22:54:01 +0000

    Disable the precompiled headers on NetBSD sparc64 too, as this won't compile there with them enabled.

--- x11/wxGTK/hacks.mk
+++ x11/wxGTK/hacks.mk
@@ -1,12 +1,17 @@
-# $NetBSD$
+# $NetBSD: hacks.mk,v 1.1 2007/04/19 01:08:51 xtraeme Exp $
 #
-# Disable the option precomp headers, seems to barf on
-# NetBSD mipseb machines... reported by jmcneill@NetBSD.org.
+# Disable the precompiled headers on NetBSD mipseb and sparc64
+# machines, otherwise wxGTK will not compile there.
+#   mipseb reported by jmcneill@NetBSD.org.
+#   sparc64 reported by jdc@NetBSD.org.
+
 
 .if !defined(WXGTK_HACKS_MK)
 WXGTK_HACKS_MK=		# defined
 
-.if ${OPSYS} == "NetBSD" && ${MACHINE_ARCH} == "mipseb"
+.if ${OPSYS} == "NetBSD" && \
+    (${MACHINE_ARCH} == "mipseb" || \
+    ${MACHINE_ARCH} == "sparc64")
 PKG_HACKS+=		disable-precomp-headers
 CONFIGURE_ARGS+=        --disable-precomp-headers
 .endif
