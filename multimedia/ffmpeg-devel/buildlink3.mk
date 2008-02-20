b13d8260e1e8b693309ab04858d226e9d4a20e72
xtraeme <xtraeme>
2008-02-20 10:08:41 +0000

    Import a recent snapshot of ffmpeg (2007-10-04) based on a snapshot made by folks at FreeBSD/ports and packaged by Adam Hoka via pkgsrc-wip.
    This will provide a newer ffmpeg for packages that need it.

--- multimedia/ffmpeg-devel/buildlink3.mk
+++ multimedia/ffmpeg-devel/buildlink3.mk
@@ -1,18 +1,21 @@
-# $NetBSD: buildlink3.mk,v 1.2 2004/10/03 00:13:02 tv Exp $
+# $NetBSD$
 
-BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 FFMPEG_DEVEL_BUILDLINK3_MK:=	${FFMPEG_DEVEL_BUILDLINK3_MK}+
 
-.if !empty(BUILDLINK_DEPTH:M+)
+.if ${BUILDLINK_DEPTH} == "+"
 BUILDLINK_DEPENDS+=	ffmpeg-devel
 .endif
 
 BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nffmpeg-devel}
 BUILDLINK_PACKAGES+=	ffmpeg-devel
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}ffmpeg-devel
 
-.if !empty(FFMPEG_DEVEL_BUILDLINK3_MK:M+)
-BUILDLINK_DEPENDS.ffmpeg-devel+=	ffmpeg-devel>=0.4.9pre1
+.if ${FFMPEG_DEVEL_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.ffmpeg-devel+=	ffmpeg-devel>=20071004
 BUILDLINK_PKGSRCDIR.ffmpeg-devel?=	../../multimedia/ffmpeg-devel
-.endif	# FFMPEG_BUILDLINK3_MK
+.endif	# FFMPEG_DEVEL_BUILDLINK3_MK
 
-BUILDLINK_DEPTH:=     ${BUILDLINK_DEPTH:S/+$//}
+.include "../../devel/libgetopt/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
