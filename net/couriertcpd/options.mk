0fbde1e7a0e1dd959cca946ef327249ec911a9e2
jlam <jlam>
2008-02-21 15:50:29 +0000

    Get rid of the "gnutls" and "ssl" options and replace with a "courier-gnutls" option/toggle that allows using GNU TLS instead of defaulting to OpenSSL.
    Bump the PKGREVISION of couriertcpd, courier-imap, and courier-mta to 1.

--- net/couriertcpd/options.mk
+++ net/couriertcpd/options.mk
@@ -1,21 +1,20 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2008/02/19 18:45:41 jlam Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.couriertcpd
-PKG_SUPPORTED_OPTIONS=		inet6
-PKG_OPTIONS_REQUIRED_GROUPS=	tls
-PKG_OPTIONS_GROUP.tls=		gnutls ssl
-PKG_SUGGESTED_OPTIONS=		inet6 ssl
+PKG_SUPPORTED_OPTIONS=		courier-gnutls inet6
+PKG_SUGGESTED_OPTIONS=		inet6
 
 .include "../../mk/bsd.options.mk"
 
 ###
-### Build with OpenSSL or GNU TLS as the underlying crypto library.
+### Support using GNU TLS as the underlying crypto library; otherwise,
+### default to using OpenSSL.
 ###
-.if !empty(PKG_OPTIONS:Mssl)
-.  include "../../security/openssl/buildlink3.mk"
-.elif !empty(PKG_OPTIONS:Mgnutls)
+.if !empty(PKG_OPTIONS:Mcourier-gnutls)
 .  include "../../security/gnutls/buildlink3.mk"
 CONFIGURE_ARGS+=	--with-gnutls
+.else
+.  include "../../security/openssl/buildlink3.mk"
 .endif
 
 ###
