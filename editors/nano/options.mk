01d6fb74405b06137384ecdc5b7fc39e56106fd1
jlam <jlam>
2008-02-21 21:56:29 +0000

    + Rename the "ncursesw" option to "wide-curses".  This should be supported   under NetBSD-current without actually needing ncursesw.
    Bump the PKGREVISION to 1.

--- editors/nano/options.mk
+++ editors/nano/options.mk
@@ -1,11 +1,15 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2007/10/24 15:28:38 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.nano
-PKG_SUPPORTED_OPTIONS=	ncursesw
+PKG_SUPPORTED_OPTIONS=	wide-curses
+PKG_LEGACY_OPTS+=	ncursesw:wide-curses
 
 .include "../../mk/bsd.options.mk"
 
-.if !empty(PKG_OPTIONS:Mncursesw)
+###
+### Wide curses support; otherwise, default to using narrow curses.
+###
+.if !empty(PKG_OPTIONS:Mwide-curses)
 .  include "../../devel/ncursesw/buildlink3.mk"
 .else
 .  include "../../mk/curses.buildlink3.mk"
