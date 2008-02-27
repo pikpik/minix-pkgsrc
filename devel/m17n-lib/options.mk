3fd46947608e8d72cf9982d295a3e76140ba78bb
obache <obache>
2008-02-27 13:48:28 +0000

    Import m17n-lib version 1.5.1.
    The m17n library is a multilingual text processing library for the C language.

---
+++ devel/m17n-lib/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.m17n-lib
+PKG_SUPPORTED_OPTIONS=	anthy x11 libthai
+PKG_SUGGESTED_OPTIONS=	x11
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Manthy)
+.include "../../inputmethod/anthy/buildlink3.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../fonts/Xft2/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+.include "../../graphics/gd/buildlink3.mk"
+.include "../../graphics/libotf/buildlink3.mk"
+.include "../../mk/xaw.buildlink3.mk"
+PLIST_SUBST+=	X11=""
+.else
+CONFIGURE_ARGS+=	--without-gui
+PLIST_SUBST+=	X11="@comment "
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibthai)
+.include "../../devel/libthai/buildlink3.mk"
+.endif
