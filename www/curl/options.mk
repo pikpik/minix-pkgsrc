11f1c3addf62f96631fb244ec8aa0b251e726ac1
drochner <drochner>
2008-03-03 18:48:21 +0000

    add an option to enable gssapi, for GSS-Negotiate, from Todd Kover per PR pkg/36144 (just compile-tested because I don't have a Kerberos installation)

--- www/curl/options.mk
+++ www/curl/options.mk
@@ -1,7 +1,7 @@
-# $NetBSD: options.mk,v 1.1 2006/08/10 14:18:16 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2008/03/02 14:40:26 bjs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.curl
-PKG_SUPPORTED_OPTIONS=	inet6 libssh2
+PKG_SUPPORTED_OPTIONS=	inet6 libssh2 gssapi
 
 .include "../../mk/bsd.options.mk"
 
@@ -16,4 +16,12 @@ CONFIGURE_ARGS+= 	--with-libssh2=${BUILDLINK_PREFIX.libssh2:Q}
 .  include "../../security/libssh2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-libssh2
+.endif
+
+.if !empty(PKG_OPTIONS:Mgssapi)
+.include "../../mk/krb5.buildlink3.mk"
+CONFIGURE_ARGS+=	--with-gssapi=${KRB5BASE}
+CONFIGURE_ARGS+=	--with-gssapi-includes=${KRB5BASE}/include/gssapi
+.else
+CONFIGURE_ARGS+=	--without-gssapi
 .endif
