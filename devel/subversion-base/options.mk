de61eff9f2e9580b9f9449552d274c3ce62083d3
gdt <gdt>
2008-03-08 20:56:37 +0000

    Make apr1 be the default.  Proposed on tech-pkg on 1/31 with no objections, and approved by epg@.  PKGREVISION++.

--- devel/subversion-base/options.mk
+++ devel/subversion-base/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2007/01/25 18:15:49 epg Exp $
+# $NetBSD: options.mk,v 1.3 2008/01/29 19:04:28 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.subversion
 PKG_SUPPORTED_OPTIONS=	apache22 apr1 serf
+PKG_SUGGESTED_OPTIONS=	apr1
 
 # Note that this file is included as part of several packages.
 # Therefore this file only defines options, but does not include the
@@ -14,6 +15,7 @@ PKG_SUPPORTED_OPTIONS=	apache22 apr1 serf
 PKG_OPTIONS+=		apr1
 .endif
 
+# Why is this here?  apache22 is not used
 .if !empty(PKG_OPTIONS:Mapr1) && empty(PKG_OPTIONS:Mapache22)
 PKG_OPTIONS+=		apache22
 .endif
