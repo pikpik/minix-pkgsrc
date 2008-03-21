89e5109d95c7e8a4c7fc518b0657b31717d5aa21
jlam <jlam>
2008-03-21 15:07:28 +0000

    Substitute for ${RUBY_ARCH} in PLISTs.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.37 2008/01/13 09:09:13 obache Exp $
+# $NetBSD: rubyversion.mk,v 1.38 2008/03/09 13:31:18 taca Exp $
 #
 
 .if !defined(_RUBYVERSION_MK)
@@ -243,6 +243,7 @@ PLIST_SUBST+=		RUBY=${RUBY:Q} RUBY_VER=${RUBY_VER:Q} \
 			RUBY_VERSION=${RUBY_VERSION:Q} \
 			RUBY_VER_DIR=${RUBY_VER_DIR:Q} \
 			RUBY_DLEXT=${RUBY_DLEXT:Q} \
+			RUBY_ARCH=${RUBY_ARCH:Q} \
 			${PLIST_RUBY_DIRS:S,DIR="${PREFIX}/,DIR=",}
 
 #
