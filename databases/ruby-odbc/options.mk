d57ac9a866b9725907fb8500b220ed90756382df
jlam <jlam>
2008-04-04 15:15:50 +0000

    Initial import of ruby18-odbc-0.9995 as databases/ruby-odbc.
    This package contains a Ruby extension for accessing ODBC data sources.

---
+++ databases/ruby-odbc/options.mk
@@ -0,0 +1,24 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.ruby-odbc
+PKG_OPTIONS_REQUIRED_GROUPS=	odbc
+PKG_OPTIONS_GROUP.odbc=		iodbc unixodbc
+PKG_SUGGESTED_OPTIONS=		iodbc
+
+.include "../../mk/bsd.options.mk"
+
+###
+### iODBC
+###
+.if !empty(PKG_OPTIONS:Miodbc)
+.  include "../../databases/iodbc/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-odbc-dir=${BUILDLINK_PREFIX.iodbc}
+.endif
+
+###
+### unixODBC
+###
+.if !empty(PKG_OPTIONS:Munixodbc)
+.  include "../../databases/unixodbc/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-odbc-dir=${BUILDLINK_PREFIX.unixodbc}
+.endif
