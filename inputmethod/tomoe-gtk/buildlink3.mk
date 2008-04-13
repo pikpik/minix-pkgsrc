a8ffafcee3138b347a09c2735a7f1545da732473
obache <obache>
2008-04-13 06:06:40 +0000

    Import tomoe-gtk version 0.6.0.
    tomoe-gtk is a GUI library for tomoe that online handwriting recognition engine.
    It offers GTK+ GUI parts and is useful for making another applications using
    tomoe.

---
+++ inputmethod/tomoe-gtk/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+TOMOE_GTK_BUILDLINK3_MK:=	${TOMOE_GTK_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	tomoe-gtk
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Ntomoe-gtk}
+BUILDLINK_PACKAGES+=	tomoe-gtk
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}tomoe-gtk
+
+.if ${TOMOE_GTK_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.tomoe-gtk+=	tomoe-gtk>=0.6.0
+BUILDLINK_PKGSRCDIR.tomoe-gtk?=	../../inputmethod/tomoe-gtk
+.endif	# TOMOE_GTK_BUILDLINK3_MK
+
+.include "../../fonts/gucharmap/buildlink3.mk"
+.include "../../inputmethod/tomoe/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
