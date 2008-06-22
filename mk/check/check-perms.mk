c870ba77707ffbdf7462ff9c518cec4a93ea063d
joerg <joerg>
2008-06-22 22:05:19 +0000

    "Downgrade" checkperms dependency to a normal build dependency. This way it doesn't have to be installed for make checksum etc. to run.

--- mk/check/check-perms.mk
+++ mk/check/check-perms.mk
@@ -1,4 +1,4 @@
-# $NetBSD: check-perms.mk,v 1.10 2007/08/14 23:58:24 joerg Exp $
+# $NetBSD: check-perms.mk,v 1.11 2008/02/05 08:17:59 tnn Exp $
 #
 # This file checks that after installation of a package, all files and
 # directories of that package have sensible permissions set.
@@ -55,7 +55,7 @@ _CHECK_PERMS_FLAGS=	-c
 .endif
 
 .if !empty(CHECK_PERMS:M[Yy][Ee][Ss])
-BOOTSTRAP_DEPENDS+=	checkperms>=1.1:../../sysutils/checkperms
+BUILD_DEPENDS+=	checkperms>=1.1:../../sysutils/checkperms
 
 privileged-install-hook: _check-perms
 .endif
