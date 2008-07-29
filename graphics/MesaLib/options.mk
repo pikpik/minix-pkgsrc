f54f4e637603369f2d4eb94fbb9aeeb8cf596022
tnn <tnn>
2008-07-29 13:56:15 +0000

    Since PKG_SUPPORTED_OPTIONS is conditionally added to, initialise it to empty first. Should fix PR pkg/39246.

--- graphics/MesaLib/options.mk
+++ graphics/MesaLib/options.mk
@@ -1,6 +1,7 @@
-# $NetBSD: options.mk,v 1.10 2008/07/27 00:03:42 bjs Exp $
+# $NetBSD: options.mk,v 1.11 2008/07/28 02:12:57 bjs Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.MesaLib
+PKG_SUPPORTED_OPTIONS=
 
 # Assembler code build configurations
 .if (${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "x86_64") && \
