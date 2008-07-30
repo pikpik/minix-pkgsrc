0b0b79c3852490033d9018352654dc2b49b53358
wiz <wiz>
2008-07-30 09:53:36 +0000

    Update to 0.3.15, changes undocumented. Add debug option.

---
+++ devel/liboil/options.mk
@@ -0,0 +1,10 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.liboil
+PKG_SUPPORTED_OPTIONS=	debug
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mdebug)
+CONFIGURE_ARGS+=	--enable-debug
+.endif
