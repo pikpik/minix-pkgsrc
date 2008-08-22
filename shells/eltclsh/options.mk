60cc4ada1ec7faac3e98a46667f76af6b70cdd4f
obache <obache>
2008-08-22 07:44:00 +0000

    Update eltclsh to 1.9 Based on PR 37283 (update to 1.8).
    eltclsh 1.9 released august 8, 2008
    
     30. Fix libtool usage and install target to allow cross compilation in OpenEmbedded.
     29. Honor TCL_DBGX to allow  compiling  against a  tcl library  with debugging
         symbols enabled.
     28. Honor CFLAGS from the environment during the build process
     27. Fix history command that was not working.
     26. Switch repository to git.
    
    eltclsh 1.8 released august 20, 2007
    
     25. Add wrappedputs procedure that print a string wrapped to a given width.
     24. Stop scanning arguments in interactive mode when user press C-d.
    
    eltclsh 1.7 released december 19, 2006
    
     23. Add new function "interactive" which starts the interactive
         interpreter (useful inside scripts).
     22. Add support for installation in $(DESTDIR)

---
+++ shells/eltclsh/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+PKG_OPTIONS_VAR=        PKG_OPTIONS.eltclsh
+PKG_SUPPORTED_OPTIONS=  tk
+PKG_SUGGESTED_OPTIONS=  tk
+
+.include "../../mk/bsd.options.mk"
+
+##
+## Support TK, i.e. the edit-line wish shell
+##
+PLIST_VARS+=	tk
+.if !empty(PKG_OPTIONS:Mtk)
+.include "../../x11/tk/buildlink3.mk"
+CONFIGURE_ARGS+=        --with-tk=${BUILDLINK_PREFIX.tk}/lib
+PLIST.tk=		yes
+.else
+CONFIGURE_ARGS+=        --without-tk
+.endif
