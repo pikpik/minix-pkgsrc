247e69dea4b3737c586fc5d84965ce1275cdbe61
ahoka <ahoka>
2008-09-12 12:21:07 +0000

    Patch libavcodec to build with our slightly older version of x264. This reenables the x264 option and makes it default again.
    This patch should be removed after an x264 update.

--- multimedia/ffmpeg/options.mk
+++ multimedia/ffmpeg/options.mk
@@ -1,10 +1,10 @@
-# $NetBSD: options.mk,v 1.7 2008/09/07 23:39:37 ahoka Exp $
+# $NetBSD: options.mk,v 1.8 2008/09/08 17:57:02 ahoka Exp $
 
 # Global and legacy options
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.ffmpeg
-PKG_SUPPORTED_OPTIONS=	sdl theora xvid faad faac #x264
-PKG_SUGGESTED_OPTIONS=	faac theora xvid
+PKG_SUPPORTED_OPTIONS=	sdl theora xvid faad faac x264
+PKG_SUGGESTED_OPTIONS=	faac theora xvid x264
 #PKG_OPTIONS_OPTIONAL_GROUPS=	aac-decoder
 #PKG_OPTIONS_GROUP.aac-decoder=	faad faac
 
