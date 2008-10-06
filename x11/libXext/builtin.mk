ee3317366fe6907dd43d64f2fc84cb5a40b5db32
tron <tron>
2008-10-06 18:59:33 +0000

    Fix problems in previous commit which I missed because I didn't use the right package to test them.

--- x11/libXext/builtin.mk
+++ x11/libXext/builtin.mk
@@ -1,7 +1,7 @@
-# $NetBSD$
+# $NetBSD: builtin.mk,v 1.3 2008/10/06 18:20:41 tron Exp $
 
-BUILTIN_PKG:=	xext
-PKGCONFIG_FILE.pixman=	${X11BASE}/lib/pkgconfig/xext.pc
+BUILTIN_PKG:=	libXext
+PKGCONFIG_FILE.libXext=	${X11BASE}/lib/pkgconfig/xext.pc
 
 .include "../../mk/buildlink3/pkgconfig-builtin.mk"
 .include "../../mk/x11.builtin.mk"
