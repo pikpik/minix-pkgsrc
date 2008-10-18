6b6aec0d99fae600e3afb860818d7d875eb09fb7
hira <hira>
2008-10-18 01:05:08 +0000

    - Correct CDEFS on NetBSD/amd64 (-DX86 -> -DX86_64).  - Fix PLIST without gtk2 option.

--- misc/openoffice3/options.mk
+++ misc/openoffice3/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2008/10/14 11:02:24 hira Exp $
+# $NetBSD: options.mk,v 1.2 2008/10/15 23:17:29 hira Exp $
 
 PKG_OPTIONS_VAR=		PKG_OPTIONS.openoffice3
 PKG_SUPPORTED_OPTIONS=		cups gnome gtk2 java kde ooo-external-libwpd
@@ -68,10 +68,13 @@ CONFIGURE_ARGS+=	--enable-gnome-vfs --enable-evolution2
 CONFIGURE_ARGS+=	--disable-gnome-vfs --disable-evolution2
 .endif
 
+PLIST_VARS+=		gtk2
 .if !empty(PKG_OPTIONS:Mgtk2)
+PLIST.gtk2=		yes
 CONFIGURE_ARGS+=	--enable-gtk
 .include "../../x11/gtk2/buildlink3.mk"
 .else
+PLIST.gtk2=		no
 CONFIGURE_ARGS+=	--disable-gtk
 .endif
 
