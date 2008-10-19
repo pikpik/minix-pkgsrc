e620370113ebc67a6b8ce9cbd15075d1058f58d7
joerg <joerg>
2008-10-19 21:02:07 +0000

    Include nbcompat/types.h early to make sure that uintX_t are defined. Reported by Aleksey Cheusov and Joern Clausen in PR 39738.

--- pkgtools/libnbcompat/files/nbcompat.h
+++ pkgtools/libnbcompat/files/nbcompat.h
@@ -1,4 +1,4 @@
-/*	$NetBSD: nbcompat.h,v 1.40 2008/05/19 12:52:57 joerg Exp $	*/
+/*	$NetBSD: nbcompat.h,v 1.41 2008/10/06 12:36:20 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2003 The NetBSD Foundation, Inc.
@@ -29,9 +29,9 @@
 #include <nbcompat/nbconfig.h>
 
 #include <nbcompat/cdefs.h>
+#include <nbcompat/types.h>
 #include <nbcompat/limits.h>
 #include <nbcompat/endian.h>
-#include <nbcompat/types.h>
 #include <nbcompat/param.h>
 
 #if HAVE_ERRNO_H
