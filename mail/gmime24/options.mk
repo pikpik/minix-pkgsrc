278960324a359986a30b63fa22cba81a83f17192
wiz <wiz>
2008-10-24 16:37:49 +0000

    Import gmime24-2.4.3 as mail/gmime24.
    GMime is a set of utilities for parsing and creating messages using
    the Multipurpose Internet Mail Extension (MIME) as defined by the
    following RFCs:
    
     * 0822: Standard for the Format of Arpa Internet Text Messages
     * 1521: MIME (Multipurpose Internet Mail Extensions) Part One:
             Mechanisms for Specifying and Describing the Format of
             Internet Message Bodies
     * 1847: Security Multiparts for MIME: Multipart/Signed and
             Multipart/Encrypted
     * 1864: The Content-MD5 Header Field (Obsoletes rfc1544)
     * 2015: MIME Security with Pretty Good Privacy (PGP)
     * 2045: Multipurpose Internet Mail Extensions (MIME) Part One:
             Format of Internet Message Bodies
     * 2046: Multipurpose Internet Mail Extensions (MIME) Part Two:
             Media Types
     * 2047: Multipurpose Internet Mail Extensions (MIME) Part Three:
             Message Header Extensions for Non-ASCII Text
     * 2048: Multipurpose Internet Mail Extensions (MIME) Part Four:
             Registration Procedures
     * 2049: Multipurpose Internet Mail Extensions (MIME) Part Five:
             Conformance Criteria and Examples
     * 2183: Communicating Presentation Information in Internet Messages:
             The Content-Disposition Header Field
     * 2184: MIME Parameter Value and Encoded Word Extensions: Character
             Sets, Languages, and Continuations
     * 2231: MIME Parameter Value and Encoded Word Extensions: Character
             Sets, Languages, and Continuations (Obsoletes rfc2184)
     * 3156: MIME Security with OpenPGP (Updates rfc2015)

---
+++ mail/gmime24/options.mk
@@ -0,0 +1,10 @@
+# $NetBSD: options.mk,v 1.2 2005/01/10 20:59:43 adrianp Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gmime24
+PKG_SUPPORTED_OPTIONS=	inet6
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Minet6)
+CONFIGURE_ARGS+=	--enable-ipv6
+.endif
