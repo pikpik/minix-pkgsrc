d368c180f66da50ca4f4b98e9659659efcda413d
wiz <wiz>
2008-10-24 20:51:11 +0000

    Update to 2.24.0:
    2008-08-06  Mike Kestner  <mkestner@novell.com>
    
    	* *: Update to Gnome 2.23.5 release API.  Remove print and panel
    	APIs from gnome-sharp.dll into their own assemblies now located in
    	gnome-desktop-sharp.
    
    2008-05-14  Stephane Delcroix  <sdelcroix@novell.com>
    
    	* gconf/GConf/gconf-sharp.dll.config.in:
    	* gconf/GConf/ClientBase.cs: use GType.Init () instead of
    	g_type_init ();

--- x11/gnome-sharp/buildlink3.mk
+++ x11/gnome-sharp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2008/04/27 23:09:53 wiz Exp $
+# $NetBSD$
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 GNOME_SHARP_BUILDLINK3_MK:=	${GNOME_SHARP_BUILDLINK3_MK}+
@@ -12,18 +12,15 @@ BUILDLINK_PACKAGES+=	gnome-sharp
 BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}gnome-sharp
 
 .if ${GNOME_SHARP_BUILDLINK3_MK} == "+"
-BUILDLINK_API_DEPENDS.gnome-sharp+=	gnome-sharp>=2.20.1
-BUILDLINK_ABI_DEPENDS.gnome-sharp?=	gnome-sharp>=2.20.1nb1
+BUILDLINK_API_DEPENDS.gnome-sharp+=	gnome-sharp>=2.24.0
 BUILDLINK_PKGSRCDIR.gnome-sharp?=	../../x11/gnome-sharp
 .endif	# GNOME_SHARP_BUILDLINK3_MK
 
 .include "../../devel/libgnomeui/buildlink3.mk"
 .include "../../graphics/libart/buildlink3.mk"
+.include "../../graphics/libgnomecanvas/buildlink3.mk"
 .include "../../lang/mono/buildlink3.mk"
-.include "../../lang/monodoc/buildlink3.mk"
-.include "../../print/libgnomeprintui/buildlink3.mk"
 .include "../../sysutils/gnome-vfs/buildlink3.mk"
-.include "../../x11/gnome-panel/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .include "../../x11/gtk-sharp/buildlink3.mk"
 
