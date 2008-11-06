285d8a2a29cb6c71135c971ab1264da03c9a2758
obache <obache>
2008-11-06 14:21:10 +0000

    Add buildlink3.mk

---
+++ multimedia/p5-GStreamer/buildlink3.mk
@@ -0,0 +1,23 @@
+# $NetBSD$
+#
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+P5_GSTREAMER_BUILDLINK3_MK:=	${P5_GSTREAMER_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	p5-GStreamer
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Np5-GStreamer}
+BUILDLINK_PACKAGES+=	p5-GStreamer
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}p5-GStreamer
+
+.if ${P5_GSTREAMER_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.p5-GStreamer+=	p5-GStreamer>=0.11
+BUILDLINK_PKGSRCDIR.p5-GStreamer?=	../../multimedia/p5-GStreamer
+.endif	# P5_GSTREAMER_BUILDLINK3_MK
+
+.include "../../devel/p5-glib2/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
