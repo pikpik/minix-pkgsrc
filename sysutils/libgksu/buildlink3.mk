b94a5273209cb57821878e290046819e094439c2
wiz <wiz>
2008-11-15 19:36:09 +0000

    Initial import of libgksu-2.0.7. I packaged it for Gnome, but currently it looks like it's not really needed, so it's untested.
    GKSu is a library that provides a Gtk+ frontend to su and sudo. It
    supports login shells and preserving environment when acting as a
    su frontend. It is useful to menu items or other graphical programs
    that need to ask a user's password to run another program as another
    user.

---
+++ sysutils/libgksu/buildlink3.mk
@@ -0,0 +1,25 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBGKSU_BUILDLINK3_MK:=	${LIBGKSU_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libgksu
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgksu}
+BUILDLINK_PACKAGES+=	libgksu
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libgksu
+
+.if ${LIBGKSU_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libgksu+=	libgksu>=2.0.7
+BUILDLINK_PKGSRCDIR.libgksu?=	../../zzz/libgksu
+.endif	# LIBGKSU_BUILDLINK3_MK
+
+.include "../../security/gnome-keyring/buildlink3.mk"
+.include "../../sysutils/libgtop/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/startup-notification/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
