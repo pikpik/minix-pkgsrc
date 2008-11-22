2d24f10d1a01d66e5cffb6f4b9b9f3e434821d7c
jmcneill <jmcneill>
2008-11-22 16:16:26 +0000

    VARBASE/run/hal -> VARBASE/run/hald

--- sysutils/hal/files/hal.sh
+++ sysutils/hal/files/hal.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD$
+# $NetBSD: hal.sh,v 1.1.1.1 2008/11/22 15:20:51 jmcneill Exp $
 #
 # PROVIDE: hal
 # REQUIRE: DAEMON dbus
@@ -14,7 +14,7 @@ command="@PREFIX@/sbin/hald"
 start_precmd=hal_prestart
 
 hal_prestart() {
-	dir="@VARBASE@/run/hal"
+	dir="@VARBASE@/run/hald"
 	if @TEST@ ! -d $dir; then
 		@MKDIR@ $dir
 		@CHMOD@ 0755 $dir
