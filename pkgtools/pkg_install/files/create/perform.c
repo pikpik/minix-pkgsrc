b03ea241d8516affd9e23176b1cadd3311889008
joerg <joerg>
2009-02-03 13:15:34 +0000

    Include fcntl.h for O_RDONLY, it is included indirectly on NetBSD, but not on Solaris. Part of PR 40544.

--- pkgtools/pkg_install/files/create/perform.c
+++ pkgtools/pkg_install/files/create/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.19.2.5 2009/02/02 11:55:16 joerg Exp $	*/
+/*	$NetBSD: perform.c,v 1.23 2009/02/02 12:35:01 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -7,7 +7,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: perform.c,v 1.19.2.5 2009/02/02 11:55:16 joerg Exp $");
+__RCSID("$NetBSD: perform.c,v 1.23 2009/02/02 12:35:01 joerg Exp $");
 
 /*
  * FreeBSD install - a package for the installation and maintainance
@@ -34,6 +34,9 @@ __RCSID("$NetBSD: perform.c,v 1.19.2.5 2009/02/02 11:55:16 joerg Exp $");
 
 #if HAVE_ERR_H
 #include <err.h>
+#endif
+#if HAVE_FCNTL_H
+#include <fcntl.h>
 #endif
 #if HAVE_UNISTD_H
 #include <unistd.h>
