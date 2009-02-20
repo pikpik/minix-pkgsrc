a83140b5898ea9bba2d3e24c2101da2141f6b3a8
reed <reed>
2009-02-20 20:02:30 +0000

    Remove dependency on pango and libgnomeui. These haven't been used by this since 2004. (So now gimp via librsvg doesn't depend on gnome.)
    Also update to 0.6.2 version.
    
    ChangeLog at
    http://mail.gnome.org/archives/libcroco-list/2009-February/msg00000.html
    
    Improve DESCR while here too.

--- textproc/libcroco/buildlink3.mk
+++ textproc/libcroco/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.19 2007/06/05 05:37:12 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.20 2008/03/06 14:53:55 wiz Exp $
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
 LIBCROCO_BUILDLINK3_MK:=	${LIBCROCO_BUILDLINK3_MK}+
@@ -18,8 +18,6 @@ BUILDLINK_PKGSRCDIR.libcroco?=	../../textproc/libcroco
 .endif	# LIBCROCO_BUILDLINK3_MK
 
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libgnomeui/buildlink3.mk"
-.include "../../devel/pango/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 
 BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
