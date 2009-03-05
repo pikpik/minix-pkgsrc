e74b3604119bc97d9f86f175b528c67f6db8c711
ahoka <ahoka>
2009-03-05 21:59:43 +0000

    Import libgee-0.1.5 as devel/libgee.
    libgee is a collection library providing GObject-based interfaces and classes
    for commonly used data structures.
    
    libgee provides the following interfaces:
    
        * Iterable
              o Collection
                    + List
                    + Set
        * Iterator
        * Map
    
    The ArrayList, HashSet, and HashMap classes provide a reasonable sample
    implementation of the List, Set, and Map interfaces. ReadOnlyCollection,
    ReadOnlyList, ReadOnlySet, and ReadOnlyMap are read-only wrapper classes that
    prevent modification of the underlying collection.
    
    libgee is written in Vala and can be used like any GObject-based C library.
    It's planned to provide bindings for further languages.

---
+++ devel/libgee/buildlink3.mk
@@ -0,0 +1,22 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH}+
+LIBGEE_BUILDLINK3_MK:=	${LIBGEE_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libgee
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibgee}
+BUILDLINK_PACKAGES+=	libgee
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libgee
+
+.if ${LIBGEE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libgee+=	libgee>=0.1.5
+BUILDLINK_PKGSRCDIR.libgee?=	../../devel/libgee
+.endif	# LIBGEE_BUILDLINK3_MK
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../lang/vala/buildlink3.mk"
+
+BUILDLINK_DEPTH:=	${BUILDLINK_DEPTH:S/+$//}
