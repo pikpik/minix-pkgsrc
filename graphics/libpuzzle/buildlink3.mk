79bb82b6aa02275096a3fdd0afd364744650ab1f
imil <imil>
2009-03-05 00:23:46 +0000

    added buildlink3.mk for libpuzzle

---
+++ graphics/libpuzzle/buildlink3.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH}+
+LIBPUZZLE_BUILDLINK3_MK:=	${LIBPUZZLE_BUILDLINK3_MK}+
+
+.if ${BUILDLINK_DEPTH} == "+"
+BUILDLINK_DEPENDS+=	libpuzzle
+.endif
+
+BUILDLINK_PACKAGES:=	${BUILDLINK_PACKAGES:Nlibpuzzle}
+BUILDLINK_PACKAGES+=	libpuzzle
+BUILDLINK_ORDER:=	${BUILDLINK_ORDER} ${BUILDLINK_DEPTH}libpuzzle
+
+.if ${LIBPUZZLE_BUILDLINK3_MK} == "+"
+BUILDLINK_API_DEPENDS.libpuzzle+=	libpuzzle>=0.9
+BUILDLINK_PKGSRCDIR.libpuzzle?=	../../graphics/libpuzzle
+.endif	# LIBPUZZLE_BUILDLINK3_MK
+
+.include "../../graphics/gd/buildlink3.mk"
+
+BUILDLINK_DEPTH:=		${BUILDLINK_DEPTH:S/+$//}
