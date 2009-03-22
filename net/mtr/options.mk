57a7f58b7c8552c278779a011f03d3a9307b95e6
smb <smb>
2009-03-22 16:21:44 +0000

    Fix gtk version reference

--- net/mtr/options.mk
+++ net/mtr/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2006/04/03 18:38:58 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2007/08/16 23:12:21 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mtr
 PKG_SUPPORTED_OPTIONS=	gtk
@@ -6,7 +6,7 @@ PKG_SUPPORTED_OPTIONS=	gtk
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgtk)
-.include "../../x11/gtk/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--without-gtk
 .endif
