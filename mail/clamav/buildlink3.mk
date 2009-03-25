37e2320059a95751d0715d50a9bc1a3ac35d9c94
adam <adam>
2009-03-25 09:21:20 +0000

    Changes 0.95: * Google Safe Browsing support. * New clamav-milter. * Clamd extensions. * Improved API. * ClamdTOP. * Memory Pool Allocator. * Unified Option Parser.

--- mail/clamav/buildlink3.mk
+++ mail/clamav/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.18 2008/11/27 06:07:49 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.19 2009/03/20 19:24:54 joerg Exp $
 
 BUILDLINK_TREE+=	clamav
 
@@ -9,9 +9,9 @@ BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.94.2
 BUILDLINK_PKGSRCDIR.clamav?=	../../mail/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/libltdl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
-.include "../../security/tcp_wrappers/buildlink3.mk"
 .endif # CLAMAV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clamav
