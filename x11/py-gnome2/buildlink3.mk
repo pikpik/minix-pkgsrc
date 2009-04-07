34d97f6ca447a4bfc28daae21e2df1ee91be3221
drochner <drochner>
2009-04-07 10:28:55 +0000

    export gnome-vfs dependency (header files installed by us refer to it)

--- x11/py-gnome2/buildlink3.mk
+++ x11/py-gnome2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.21 2008/01/05 20:45:59 rillig Exp $
+# $NetBSD: buildlink3.mk,v 1.22 2009/03/20 19:25:49 joerg Exp $
 
 BUILDLINK_TREE+=	pygnome2
 
@@ -13,6 +13,7 @@ BUILDLINK_PKGSRCDIR.pygnome2?=	../../x11/py-gnome2
 
 .include "../../net/py-ORBit/buildlink3.mk"
 .include "../../x11/py-gtk2/buildlink3.mk"
+.include "../../sysutils/gnome-vfs/buildlink3.mk"
 .endif # PYGNOME2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pygnome2
