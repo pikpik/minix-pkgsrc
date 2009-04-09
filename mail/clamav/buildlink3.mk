e59854412dcb199f822c826b2f514beb3b6e8590
martti <martti>
2009-04-09 08:03:44 +0000

    Regenerated.

--- mail/clamav/buildlink3.mk
+++ mail/clamav/buildlink3.mk
@@ -1,17 +1,18 @@
-# $NetBSD: buildlink3.mk,v 1.19 2009/03/20 19:24:54 joerg Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	clamav
 
 .if !defined(CLAMAV_BUILDLINK3_MK)
 CLAMAV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.94.2
+BUILDLINK_API_DEPENDS.clamav+=	clamav>=0.95.1
 BUILDLINK_PKGSRCDIR.clamav?=	../../mail/clamav
 
 .include "../../archivers/bzip2/buildlink3.mk"
 .include "../../devel/libltdl/buildlink3.mk"
+.include "../../devel/ncurses/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/gmp/buildlink3.mk"
-.endif # CLAMAV_BUILDLINK3_MK
+.endif	# CLAMAV_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-clamav
