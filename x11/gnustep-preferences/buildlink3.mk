2f77299d6ce41bd9d478f8350a43b2566251fc1d
rh <rh>
2009-04-13 12:26:38 +0000

    Update gnustep-preferences to 1.2.0nb18.  This changes the filesystem layout to be more hier(7) compliant.

--- x11/gnustep-preferences/buildlink3.mk
+++ x11/gnustep-preferences/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.10 2008/12/28 07:56:38 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:25:41 joerg Exp $
 
 BUILDLINK_TREE+=	gnustep-preferences
 
 .if !defined(GNUSTEP_PREFERENCES_BUILDLINK3_MK)
 GNUSTEP_PREFERENCES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-preferences+=	gnustep-preferences>=1.2.0
-BUILDLINK_ABI_DEPENDS.gnustep-preferences+=	gnustep-preferences>=1.2.0nb16
+BUILDLINK_API_DEPENDS.gnustep-preferences+=	gnustep-preferences>=1.2.0nb18
+BUILDLINK_ABI_DEPENDS.gnustep-preferences+=	gnustep-preferences>=1.2.0nb18
 BUILDLINK_PKGSRCDIR.gnustep-preferences?=	../../x11/gnustep-preferences
 
 .include "../../x11/gnustep-back/buildlink3.mk"
