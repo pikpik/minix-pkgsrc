0187d76498a64f709b334b6271678838fce3513a
reed <reed>
2009-04-15 03:54:04 +0000

    Since gtkmm now depends on pangomm, then bump the API requirement as a hack to force reinstall to get pangomm. (Or would get buildlink failure due to gtkmm installed already so didn't bring in pangomm.)

--- x11/gtkmm/buildlink3.mk
+++ x11/gtkmm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.14 2008/10/16 21:34:08 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/03/20 19:25:42 joerg Exp $
 
 BUILDLINK_TREE+=	gtkmm
 
 .if !defined(GTKMM_BUILDLINK3_MK)
 GTKMM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.6.3
-BUILDLINK_ABI_DEPENDS.gtkmm+=	gtkmm>=2.14.1
+# increase API_DEPENDS as a hack because now depends on pangomm
+BUILDLINK_API_DEPENDS.gtkmm+=	gtkmm>=2.14.1
 BUILDLINK_PKGSRCDIR.gtkmm?=	../../x11/gtkmm
 
 .include "../../devel/pangomm/buildlink3.mk"
