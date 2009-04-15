3633904bb7e686591c7f4351a3b9b785c4811446
manu <manu>
2009-04-15 03:51:03 +0000

    Add buildlink3 support, from Fredrik Pettai <pettai@nordu.net>, missing from previous commit

---
+++ mail/dkim-milter/buildlink3.mk
@@ -0,0 +1,16 @@
+$NetBSD$
+
+BUILDLINK_TREE+=	dkim-milter
+
+.if !defined(DKIM_MILTER_BUILDLINK3_MK)
+DKIM_MILTER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dkim-milter+=	dkim-milter>=2.8.2
+BUILDLINK_PKGSRCDIR.dkim-milter?=	../../mail/dkim-milter
+BUILDLINK_DEPMETHOD.dkim-milter?=	build
+
+.include "../../security/openssl/buildlink3.mk"
+.include "../../mail/libmilter/buildlink3.mk"
+.endif # DKIM_MILTER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dkim-milter
