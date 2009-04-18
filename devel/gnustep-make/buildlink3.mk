15c3e1119c41ec42f655d2e49071a98276132c8a
rh <rh>
2009-04-18 03:10:33 +0000

    Allow gnustep packages to override INSTALL variables to enable unprivileged DESTDIR builds.  Bump PKGREVISION.

--- devel/gnustep-make/buildlink3.mk
+++ devel/gnustep-make/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:14 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2009/04/13 12:09:25 rh Exp $
 
 BUILDLINK_TREE+=	gnustep-make
 
 .if !defined(GNUSTEP_MAKE_BUILDLINK3_MK)
 GNUSTEP_MAKE_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gnustep-make+=	gnustep-make>=2.0.8
-BUILDLINK_ABI_DEPENDS.gnustep-make?=	gnustep-make>=2.0.8
+BUILDLINK_API_DEPENDS.gnustep-make+=	gnustep-make>=2.0.8nb1
+BUILDLINK_ABI_DEPENDS.gnustep-make?=	gnustep-make>=2.0.8nb1
 BUILDLINK_PKGSRCDIR.gnustep-make?=	../../devel/gnustep-make
 
 .include "../../devel/gnustep-make/gnustep.mk"
