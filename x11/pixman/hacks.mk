afda801eee0969c520d7a9fb8c3baee077dd3e6e
sno <sno>
2009-04-23 08:30:53 +0000

    Update to pixman-0.15.2 from 0.12.0
    Upstream changes (since 0.12.0):
      - Fix MMX & SSE intrinsics to work with Sun compilers & Solaris
      - More SSE2 optimizations
      - Optimize rectilinear nearest-neighbour scaling
      - Add support for BGRA and BGRx formats
      and a lot of minor bug fixes and optimizations
    
    Oked by wiz@

--- x11/pixman/hacks.mk
+++ x11/pixman/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.1 2008/07/18 03:34:10 bjs Exp $
+# $NetBSD: hacks.mk,v 1.2 2008/09/06 20:09:15 bjs Exp $
 #
 .if !defined(PIXMAN_HACKS_MK)
 PIXMAN_HACKS_MK=	# empty
@@ -8,8 +8,9 @@ PIXMAN_HACKS_MK=	# empty
 ### XXX SSE2 intrinsics require gcc-4.2+ to build unless
 ###	${MACHINE_ARCH} == "x86_64".
 ###
-.  if !empty(PKGSRC_COMPILER:Msunpro) || \
-   (empty(MACHINE_ARCH:Mx86_64) && empty(CC_VERSION:Mgcc-[4-9].[2-9]*))
+.  if empty(PKGSRC_COMPILER:Msunpro) && \
+   (empty(MACHINE_ARCH:Mx86_64) && \
+     (empty(CC_VERSION:Mgcc-4.[2-9]*) || empty(CC_VERSION:Mgcc-[5-9].*) || empty(CC_VERSION:Mgcc-[1-9][0-9]*)))
 CONFIGURE_ARGS+=	--disable-sse2
 .  endif
 .endif
