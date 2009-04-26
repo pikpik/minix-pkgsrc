4acd5cb4b18df3e45536bc19fbb67ef1788559f2
rillig <rillig>
2009-04-26 10:13:31 +0000

    Fixed pkglint warning.

--- graphics/silgraphite-xft/buildlink3.mk
+++ graphics/silgraphite-xft/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/12/15 00:44:52 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:45 joerg Exp $
 
 BUILDLINK_TREE+=	silgraphite-xft
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	silgraphite-xft
 SILGRAPHITE_XFT_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.silgraphite-xft+=	silgraphite-xft>=2.3
-BUILDLINK_PKGSRCDIR.silgraphite-xft?=	../../wip/silgraphite-xft
+BUILDLINK_PKGSRCDIR.silgraphite-xft?=	../../graphics/silgraphite-xft
 
 .include "../../graphics/silgraphite-ft/buildlink3.mk"
 .include "../../x11/libXft/buildlink3.mk"
