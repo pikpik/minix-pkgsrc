c109d0badcda84c9145cebf2c0fafc836a56d7f7
reed <reed>
2009-05-02 16:14:37 +0000

    Add new metadata: PREV_PKGPATH and SUPERSEDES. These will be available from pkg_summary(5) database. To be used to find previous packages when a package is moved or renamed. (Even though that is not recommended.)
    Documented in pkgsrc Guide and in the pkg_summary(5) manual page.
    
    Upcoming commit will have some packages modified to add them.

--- pkgtools/pkg_install/files/info/show.c
+++ pkgtools/pkg_install/files/info/show.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: show.c,v 1.27 2009/04/23 19:35:52 joerg Exp $	*/
+/*	$NetBSD: show.c,v 1.28 2009/04/24 14:00:25 joerg Exp $	*/
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -7,7 +7,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: show.c,v 1.27 2009/04/23 19:35:52 joerg Exp $");
+__RCSID("$NetBSD: show.c,v 1.28 2009/04/24 14:00:25 joerg Exp $");
 
 /*
  * FreeBSD install - a package for the installation and maintainance
@@ -334,6 +334,8 @@ show_summary(struct pkg_meta *meta, package_t *plist, const char *binpkgfile)
 		"HOMEPAGE",
 		"PKGTOOLS_VERSION",
 		"BUILD_DATE",
+		"PREV_PKGPATH",
+		"SUPERSEDES",
 		NULL
 	};
 	
