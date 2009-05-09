ef791739acc202b69c4a0258e106bcd90db16ab6
joerg <joerg>
2009-05-09 20:09:33 +0000

    Always use the paths.h from libnbcompat.

--- devel/nbpatch/files/pathnames.h
+++ devel/nbpatch/files/pathnames.h
@@ -1,7 +1,7 @@
 /*
  * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
  * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.2 2008/08/11 00:04:12 joerg Exp $
- * $NetBSD$
+ * $NetBSD: pathnames.h,v 1.1.1.1 2008/09/10 11:03:21 joerg Exp $
  */
 
 /*
@@ -9,6 +9,6 @@
  * on July 29, 2003.
  */
 
-#include <paths.h>
+#include <nbcompat/paths.h>
 
 #define	_PATH_ED		"/bin/ed"
