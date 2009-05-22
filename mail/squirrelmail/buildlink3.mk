4d017a491b47e88e1287ec4874f5fbca229d145a
martti <martti>
2009-05-22 07:05:14 +0000

    Updated

--- mail/squirrelmail/buildlink3.mk
+++ mail/squirrelmail/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.20 2008/12/04 07:18:46 martti Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2009/03/20 19:24:56 joerg Exp $
 
 BUILDLINK_TREE+=	squirrelmail
 
 .if !defined(SQUIRRELMAIL_BUILDLINK3_MK)
 SQUIRRELMAIL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.17
+BUILDLINK_API_DEPENDS.squirrelmail+=	squirrelmail>=1.4.19
 BUILDLINK_PKGSRCDIR.squirrelmail?=	../../mail/squirrelmail
 .endif # SQUIRRELMAIL_BUILDLINK3_MK
 
