6f0d43f734a1f118073dde53bc674723acc18ecf
obache <obache>
2009-05-30 00:16:10 +0000

    Import libnxml-0.18.3 as textproc/libnxml from pkgsrc-wip. Packaged by Kamel Derouiche.
    nXML is a C library for parsing, writing and creating XML 1.0 and
    1.1 files or streams. It supports utf-8, utf-16be and utf-16le,
    ucs-4 (1234, 4321, 2143, 2312).

---
+++ textproc/libnxml/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libnxml
+
+.if !defined(LIBNXML_BUILDLINK3_MK)
+LIBNXML_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libnxml+=	libnxml>=0.18.3
+BUILDLINK_PKGSRCDIR.libnxml?=	../../textproc/libnxml
+
+
+.include "../../www/curl/buildlink3.mk"
+.endif	# LIBNXML_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libnxml
