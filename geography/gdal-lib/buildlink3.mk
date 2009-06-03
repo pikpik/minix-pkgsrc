9d948c953d15e53cafb8b8fb158d3d3f531f7df8
hasso <hasso>
2009-06-03 17:28:31 +0000

    Update to 1.6.1. Upstream changes are too massive to list here (it's more than two years of development). http://trac.osgeo.org/gdal/wiki/DownloadSource has full changelogs.

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2008/02/16 01:29:00 gdt Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:35 joerg Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
 .if !defined(GDAL_LIB_BUILDLINK3_MK)
 GDAL_LIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gdal-lib+=	gdal-lib>=1.3.1
-BUILDLINK_ABI_DEPENDS.gdal-lib?=	gdal-lib>=1.4.0nb4
+BUILDLINK_API_DEPENDS.gdal-lib+=	gdal-lib>=1.6.1
+BUILDLINK_ABI_DEPENDS.gdal-lib?=	gdal-lib>=1.6.1
 BUILDLINK_PKGSRCDIR.gdal-lib?=	../../geography/gdal-lib
 
 pkgbase := gdal-lib
