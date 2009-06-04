bcce920adf5190c26779ac8aeb52da052bc86d7a
drochner <drochner>
2009-06-04 11:30:15 +0000

    update to 0.20.4 changes: - New enum value VTE_ERASE_TTY - Make VTE_ERASE_AUTO send \H for backspace if terminal erase is undefined -bugfixes

--- x11/vte/buildlink3.mk
+++ x11/vte/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2009/02/03 12:13:37 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2009/03/20 19:25:51 joerg Exp $
 
 BUILDLINK_TREE+=	vte
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	vte
 VTE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.vte+=	vte>=0.14.0
-BUILDLINK_ABI_DEPENDS.vte?=	vte>=0.17.4nb1
+BUILDLINK_ABI_DEPENDS.vte?=	vte>=0.20.4
 BUILDLINK_PKGSRCDIR.vte?=	../../x11/vte
 
 .include "../../devel/glib2/buildlink3.mk"
