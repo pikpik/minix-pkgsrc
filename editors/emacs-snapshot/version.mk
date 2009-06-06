a1d18f0aa08925aeca3b74a2d5d2ca626b28fb4e
minskim <minskim>
2009-06-06 05:06:12 +0000

    Set _EMACS_VERSION_MICRO for emacs-snapshot.
    Suggested by Makoto Fujiwara in PR 40023.

--- editors/emacs-snapshot/version.mk
+++ editors/emacs-snapshot/version.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: version.mk,v 1.1 2009/06/05 21:18:02 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
 _EMACS_REQD=	emacs>=23
@@ -7,3 +7,4 @@ _EMACS_PKGDEP.leim=
 
 _EMACS_VERSION_MAJOR=	23
 _EMACS_VERSION_MINOR=	0
+_EMACS_VERSION_MICRO=	94
