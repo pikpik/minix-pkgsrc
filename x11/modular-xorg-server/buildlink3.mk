f922a251021aa7175322392f0ef9a1be5e84d6c2
wiz <wiz>
2009-06-07 11:23:47 +0000

    Update to 1.6.1.901, first 1.6.2 prerelease. Lots of changes since last pkgsrc version 1.4.x. Based on patches provided by Hasso Tepper on pkgsrc-users. Enable dri by default.
    NOTE: You must install new versions of all dependencies, old ones
    won't work!

--- x11/modular-xorg-server/buildlink3.mk
+++ x11/modular-xorg-server/buildlink3.mk
@@ -1,13 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.5 2008/09/18 20:56:01 bjs Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:25:47 joerg Exp $
 
 BUILDLINK_TREE+=	modular-xorg-server
 
 .if !defined(MODULAR_XORG_SERVER_BUILDLINK3_MK)
 MODULAR_XORG_SERVER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.4.2
+BUILDLINK_API_DEPENDS.modular-xorg-server+=	modular-xorg-server>=1.6.0
 BUILDLINK_PKGSRCDIR.modular-xorg-server?=	../../x11/modular-xorg-server
 
+.include "../../sysutils/libpciaccess/buildlink3.mk"
 .include "../../x11/pixman/buildlink3.mk"
 
 .include "../../x11/compositeproto/buildlink3.mk"
