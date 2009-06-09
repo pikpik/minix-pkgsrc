7f9e7cdfae8bcbbf00ae51d8932a6b54c028f307
minskim <minskim>
2009-06-09 21:29:17 +0000

    Update kpathsea to 5.0.0.
    Changes:
    * Make shared library version and general package version the same.
    * Make library re-entrant.

--- print/kpathsea/buildlink3.mk
+++ print/kpathsea/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/01/03 22:12:42 minskim Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:14 joerg Exp $
 
 BUILDLINK_TREE+=	kpathsea
 
 .if !defined(KPATHSEA_BUILDLINK3_MK)
 KPATHSEA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.kpathsea+=	kpathsea>=3.5.7
+BUILDLINK_API_DEPENDS.kpathsea+=	kpathsea>=5.0.0
 BUILDLINK_PKGSRCDIR.kpathsea?=	../../print/kpathsea
 BUILDLINK_FILES.kpathsea+=	bin/kpsewhich
 .endif # KPATHSEA_BUILDLINK3_MK
