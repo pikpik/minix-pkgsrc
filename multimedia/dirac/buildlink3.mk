b83076400edc85642a3068976917abda844e293a
joerg <joerg>
2009-06-11 17:28:42 +0000

    Require stable version and make it a normal dependency, shared libraries are present now.

--- multimedia/dirac/buildlink3.mk
+++ multimedia/dirac/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/07/08 23:11:00 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:25:02 joerg Exp $
 
 BUILDLINK_TREE+=	dirac
 
 .if !defined(DIRAC_BUILDLINK3_MK)
 DIRAC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.dirac+=	dirac>=0.2.0
+BUILDLINK_API_DEPENDS.dirac+=	dirac>=1.0.2
 BUILDLINK_PKGSRCDIR.dirac?=	../../multimedia/dirac
-BUILDLINK_DEPMETHOD.dirac?=	build
 .endif # DIRAC_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-dirac
