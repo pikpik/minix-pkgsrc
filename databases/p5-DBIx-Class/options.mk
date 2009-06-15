4ae4c62ca9c52a94d531bf5d0533e2f25d0d9307
sno <sno>
2009-06-15 20:30:03 +0000

    pkgsrc changes:   - Updating package of p5 module DBIx::Class from 0.08103 to 0.08107   - Adjusting dependencies according to META.yaml   - Adjusting LICENSE according to META.yaml   - Setting module type to Module::Install
    Upstream changes:
    0.08107 2009-06-14 08:21:00 (UTC)
            - Fix serialization regression introduced in 0.08103 (affects
              Cursor::Cached)
            - POD fixes
            - Fixed incomplete ::Replicated debug output
    
    0.08106 2009-06-11 21:42:00 (UTC)
            - Switched SQLite storage driver to DateTime::Format::SQLite
              (proper timezone handling)
            - Fix more test problems
    
    0.08105 2009-06-11 19:04:00 (UTC)
            - Update of numeric columns now properly uses != to determine
              dirtyness instead of the usual eq
            - Fixes to IC::DT tests
            - Fixed exception when undef_if_invalid and timezone are both set on
              an invalid datetime column
    
    0.08104 2009-06-10 13:38:00 (UTC)
            - order_by now can take \[$sql, @bind] as in
              order_by => { -desc => \['colA LIKE ?', 'somestring'] }
            - SQL::Abstract errors are now properly croak()ed with the
              correct trace
            - populate() now properly reports the dataset slice in case of
              an exception
            - Fixed corner case when populate() erroneously falls back to
              create()
            - Work around braindead mysql when doing subquery counts on
              resultsets containing identically named columns from several
              tables
            - Fixed m2m add_to_$rel to invoke find_or_create on the far
              side of the relation, to avoid duplicates
            - DBIC now properly handles empty inserts (invoking all default
              values from the DB, normally via INSERT INTO tbl DEFAULT VALUES
            - Fix find_or_new/create to stop returning random rows when
              default value insert is requested (RT#28875)
            - Make IC::DT extra warning state the column name too
            - It is now possible to transparrently search() on columns
              requiring DBI bind (i.e. PostgreSQL BLOB)
            - as_query is now a Storage::DBI method, so custom cursors can
              be seamlessly used
            - Fix search_related regression introduced in 0.08103

--- databases/p5-DBIx-Class/options.mk
+++ databases/p5-DBIx-Class/options.mk
@@ -1,7 +1,8 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2009/04/24 17:28:05 sno Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.p5-DBIx-Class
 PKG_SUPPORTED_OPTIONS=	sql-translator
+PKG_SUGGESTED_OPTIONS=	sql-translator
 
 .include "../../mk/bsd.options.mk"
 
