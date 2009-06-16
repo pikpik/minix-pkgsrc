67d7ad4b9df3cdb8d66eee17f111b99abc7911ae
taca <taca>
2009-06-16 15:08:38 +0000

    Update Ruby packages to 1.8.7.174 (1.8.7-p174).
    * Fix critical problem of BigDecimal class in 1.8.7-p173.
    
    
    Fri Jun 12 16:36:44 2009  Yukihiro Matsumoto  <matz@ruby-lang.org>
    
    	* ext/bigdecimal/bigdecimal.c (VpToString): fixed a bug introduced
    	  in r23613.  [ruby-talk:338957]

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.46 2009/06/10 12:45:34 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.47 2009/06/14 22:58:03 joerg Exp $
 #
 
 .if !defined(_RUBYVERSION_MK)
@@ -10,7 +10,7 @@ _RUBYVERSION_MK=	# defined
 RUBY18_VERSION=		1.8.7
 
 # patch
-RUBY18_PATCHLEVEL=	173
+RUBY18_PATCHLEVEL=	174
 
 # RUBY_VERSION_DEFAULT defines default version for Ruby related
 #	packages and user can define in mk.conf.  (1.6 or 1.8)
