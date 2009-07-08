5a31dcf7c3906e9a245c98502920512e9563cfa1
minskim <minskim>
2009-07-08 12:51:16 +0000

    Add xzcat as a tool and update lzcat's TOOLS_DEPENDS to avoid conflicts.
    archivers/xz provides both lzcat and xzcat.

--- mk/tools/defaults.mk
+++ mk/tools/defaults.mk
@@ -1,4 +1,4 @@
-# $NetBSD: defaults.mk,v 1.55 2008/11/15 20:20:44 minskim Exp $
+# $NetBSD: defaults.mk,v 1.56 2009/02/05 19:03:29 joerg Exp $
 #
 # Copyright (c) 2005 The NetBSD Foundation, Inc.
 # All rights reserved.
@@ -145,6 +145,7 @@ _TOOLS_VARNAME.wc=		WC
 _TOOLS_VARNAME.wish=		WISH
 _TOOLS_VARNAME.xargs=		XARGS
 _TOOLS_VARNAME.xmkmf=		XMKMF_CMD
+_TOOLS_VARNAME.xzcat=		XZCAT
 _TOOLS_VARNAME.yacc=		YACC
 
 ######################################################################
