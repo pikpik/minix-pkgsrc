5963e0f8aad13b61e4d929167406e60b822ca4ff
drochner <drochner>
2009-07-09 19:44:50 +0000

    add lensfun-0.2.3, a library to correct photographic lens distortions, from wiz per pkgsrc-wip

---
+++ graphics/lensfun/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	lensfun
+
+.if !defined(LENSFUN_BUILDLINK3_MK)
+LENSFUN_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.lensfun+=	lensfun>=0.2.2b
+BUILDLINK_PKGSRCDIR.lensfun?=	../../graphics/lensfun
+
+.include "../../devel/glib2/buildlink3.mk"
+.endif # LENSFUN_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-lensfun
