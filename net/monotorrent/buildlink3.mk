de62311221e4c1f7d7469f4ec1c3ceb0cab363ce
kefren <kefren>
2009-07-16 07:50:26 +0000

    Update to 0.72. From the release announcement:
    This is a bugfix release to address a few reported issues and also a few
    issues that were discovered via my own testing.
    
        * Add a helper method which ensures all data is flushed to disk
        * Added additional error handling to prevent malformed DHT messages
          crashing the library
        * Fixed issue when zeroing unused bits for torrents with an exact
          multiple of 32 pieces
        * Fixed issue where data could be written to the wrong file if a file
          with the same name existed in multiple torrents
        * Fixed the handling of torrents where the last file(s) are of zero
          length
        * Fixed regression with global download rate limiting
        * Fixed a performance regression with the new piece picking pipeline
          which resulted in lots of CPU cycles being used up on peers which
          have not sent an unchoke message

--- net/monotorrent/buildlink3.mk
+++ net/monotorrent/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:11 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/25 22:37:13 he Exp $
 
 BUILDLINK_TREE+=	monotorrent
 
 .if !defined(MONOTORRENT_BUILDLINK3_MK)
 MONOTORRENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.monotorrent+=	monotorrent>=0.62
-BUILDLINK_ABI_DEPENDS.monotorrent?=	monotorrent>=0.62nb1
+BUILDLINK_API_DEPENDS.monotorrent+=	monotorrent>=0.72
+BUILDLINK_ABI_DEPENDS.monotorrent?=	monotorrent>=0.72
 BUILDLINK_PKGSRCDIR.monotorrent?=	../../net/monotorrent
 
 .include "../../lang/mono/buildlink3.mk"
