b654dd390a699d2a9bdfc9965a1739616fe2aafb
wiz <wiz>
2009-07-18 10:39:44 +0000

    Remove support code for SDL option; option was removed during last update, since ffplay is a separate package now. Addresses point raised by David A. Holland in PR 41729.

--- multimedia/ffmpeg/options.mk
+++ multimedia/ffmpeg/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.11 2008/09/15 21:24:00 sborrill Exp $
+# $NetBSD: options.mk,v 1.12 2009/06/12 16:25:34 ahoka Exp $
 
 # Global and legacy options
 
@@ -9,7 +9,6 @@ PKG_SUGGESTED_OPTIONS=	theora xvid x264
 #PKG_OPTIONS_GROUP.aac-decoder=	faad faac
 
 .include "../../mk/bsd.options.mk"
-.include "../../mk/bsd.prefs.mk"
 
 ###
 ### faad option
@@ -28,20 +27,6 @@ CONFIGURE_ARGS+=  --enable-libfaad
 CONFIGURE_ARGS+=	--enable-libfaac \
 			--enable-nonfree
 .include "../../audio/faac/buildlink3.mk"
-.endif
-
-###
-### SDL support
-###
-### You can build the frontend with SDL support enabled
-###
-
-.if !empty(PKG_OPTIONS:Msdl)
-CONFIGURE_ARGS+=	--enable-ffplay
-PLIST.sdl=		yes
-.include "../../devel/SDL/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--disable-ffplay
 .endif
 
 ###
