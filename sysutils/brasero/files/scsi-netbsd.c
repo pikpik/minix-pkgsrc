6a259be4227e5a5145a769116521bd02bb277d1e
drochner <drochner>
2009-07-26 18:25:01 +0000

    update brasero to 2.26.3 changes: minor bugfixes, translation updates
    pkgsrc changes:
    -in scsi io code, remove the code which builds a raw device path
     from a block device - use the raw_device hal property instead
    -split out the nautilus plugin, allows to use brasero (and its
     clients, eg sound-juicer) with less gnome desktop overhead
    -support playlist parsing as an option -- unfortunately this
     pulls in evolution-data-server indirectly which is pretty large
     for that limited gain of functionality (on per default, as in
     other pkgs able to use the evolution database)
    -cleanup

--- sysutils/brasero/files/scsi-netbsd.c
+++ sysutils/brasero/files/scsi-netbsd.c
@@ -1,4 +1,4 @@
-/* $NetBSD: scsi-netbsd.c,v 1.1.1.1 2009/03/16 10:49:06 jmcneill Exp $ */
+/* $NetBSD: scsi-netbsd.c,v 1.2 2009/03/22 09:30:39 wiz Exp $ */
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /*
  * Libbrasero-media
@@ -164,14 +164,11 @@ brasero_device_handle_open (const gchar *path,
 	int fd;
 	int flags = OPEN_FLAGS;
 	BraseroDeviceHandle *handle;
-	gchar *rdevnode;
 
 	if (exclusive)
 		flags |= O_EXCL;
 
-	rdevnode = g_strdup_printf ("/dev/r%s", path + strlen ("/dev/"));
-	fd = open (rdevnode, flags);
-	g_free (rdevnode);
+	fd = open (path, flags);
 	if (fd < 0) {
 		if (code) {
 			if (errno == EAGAIN
