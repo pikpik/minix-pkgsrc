582b2f761a309fd7277328a9fdf5ecf4136b2396
drochner <drochner>
2009-07-29 17:34:16 +0000

    the dri module wants intel_bufmgr.h which we got with libdrm-2.4.9

--- graphics/MesaLib/dri.mk
+++ graphics/MesaLib/dri.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dri.mk,v 1.1 2008/04/24 07:40:24 bjs Exp $
+# $NetBSD: dri.mk,v 1.2 2009/06/05 11:10:35 wiz Exp $
 #
 # Currently, this is for convenience only.
 #
@@ -10,6 +10,7 @@ DRI_MK=		# defined
 .  include "../../x11/glproto/buildlink3.mk"
 .  include "../../x11/libXdamage/buildlink3.mk"
 .  include "../../x11/libXfixes/buildlink3.mk"
+BUILDLINK_API_DEPENDS.libdrm+= libdrm>=2.4.9
 .  include "../../x11/libdrm/buildlink3.mk"
 .  include "../../x11/libXxf86vm/buildlink3.mk"
 .  include "../../x11/xf86driproto/buildlink3.mk"
