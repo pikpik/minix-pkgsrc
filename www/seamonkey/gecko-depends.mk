6c96bda68ea270a618cfab425d1d58255f6e3c0e
tnn <tnn>
2009-08-05 15:04:40 +0000

    mark as deprecated

--- www/seamonkey/gecko-depends.mk
+++ www/seamonkey/gecko-depends.mk
@@ -1,12 +1,3 @@
-# $NetBSD: gecko-depends.mk,v 1.1 2006/09/24 16:21:33 salo Exp $
+# $NetBSD: gecko-depends.mk,v 1.2 2008/05/31 23:02:40 tnn Exp $
 
-.if !empty(PKG_OPTIONS:Mfirefox)
-# needs .pc files
-BUILDLINK_API_DEPENDS.firefox+=		firefox>=1.5nb2
-BUILDLINK_API_DEPENDS.firefox+=		firefox<=2.99
-.include "../../www/firefox/buildlink3.mk"
-.else
-# needs .pc files
-BUILDLINK_API_DEPENDS.seamonkey+=	seamonkey>=1.0.5nb1
-.include "../../www/seamonkey/buildlink3.mk"
-.endif
+BROKEN=	"gecko-depends.mk" is deprecated. Use xulrunner instead.
--- www/seamonkey/gecko-options.mk
+++ www/seamonkey/gecko-options.mk
@@ -1,5 +1,3 @@
-# $NetBSD$
+# $NetBSD: gecko-options.mk,v 1.1 2006/09/24 16:21:33 salo Exp $
 
-PKG_OPTIONS_REQUIRED_GROUPS+=	gecko
-PKG_OPTIONS_GROUP.gecko=	firefox seamonkey
-PKG_SUGGESTED_OPTIONS+=		firefox
+BROKEN= "gecko-options.mk" is deprecated. Use xulrunner instead.
