f5ccfb33824c565702f36c0c7b2df6245bc6f8a6
tnn <tnn>
2009-08-06 12:21:37 +0000

    Switch to xulrunner ... but unfortunately it doesn't work. There's a SEGV in glib2 and the backtrace doesn't have any browser engine references in it so I'm inclined to believe this package was broken by a glib2/gtk2 update.

--- www/kazehakase/options.mk
+++ www/kazehakase/options.mk
@@ -1,11 +1,10 @@
-# $NetBSD: options.mk,v 1.3 2007/10/27 13:03:50 wiz Exp $
+# $NetBSD: options.mk,v 1.4 2009/07/03 14:00:24 drochner Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.kazehakase
 PKG_SUPPORTED_OPTIONS=	gnutls
 PKG_SUGGESTED_OPTIONS+=	gnutls
 
-.include "../../www/seamonkey/gecko-options.mk"
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mgnutls)
