8641f73b49c16aa99b3f37336256e53bbf1f027c
tnn <tnn>
2009-08-06 12:37:41 +0000

    with exception of wip/, there are no remaining consumers of these.

--- www/seamonkey/gecko-depends.mk
+++
@@ -1,3 +0,0 @@
-# $NetBSD: gecko-depends.mk,v 1.2 2008/05/31 23:02:40 tnn Exp $
-
-BROKEN=	"gecko-depends.mk" is deprecated. Use xulrunner instead.
--- www/seamonkey/gecko-options.mk
+++
@@ -1,3 +0,0 @@
-# $NetBSD: gecko-options.mk,v 1.1 2006/09/24 16:21:33 salo Exp $
-
-BROKEN= "gecko-options.mk" is deprecated. Use xulrunner instead.
