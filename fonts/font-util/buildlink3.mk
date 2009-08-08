701c94576d36a637b5054a20111836e8b0fa1df3
wiz <wiz>
2009-08-08 23:18:58 +0000

    pkglint fix: reorder lines.

--- fonts/font-util/buildlink3.mk
+++ fonts/font-util/buildlink3.mk
@@ -1,6 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2006/11/14 20:15:05 joerg Exp $
-
-BUILDLINK_DEPMETHOD.font-util?=	build
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:34 joerg Exp $
 
 BUILDLINK_TREE+=	font-util
 
@@ -9,6 +7,8 @@ FONT_UTIL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.font-util+=	font-util>=1.0.1
 BUILDLINK_PKGSRCDIR.font-util?=	../../fonts/font-util
+
+BUILDLINK_DEPMETHOD.font-util?=	build
 .endif # FONT_UTIL_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-font-util
