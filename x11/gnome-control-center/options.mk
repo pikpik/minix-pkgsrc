a9e17473a643680141beed5703e5bed1adf2909a
wiz <wiz>
2009-08-15 14:48:34 +0000

    Fix build against libxklavier-4.0 and depend on it. Bump PKGREVISION. Set LICENSE. Split off options handling into options.mk.

---
+++ x11/gnome-control-center/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gnome-control-center
+PKG_SUPPORTED_OPTIONS=	evolution
+PKG_SUGGESTED_OPTIONS=	evolution
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		evolution
+
+.if !empty(PKG_OPTIONS:Mevolution)
+CONFIGURE_ARGS+=	--enable-aboutme
+PLIST.evolution=	yes
+.include "../../mail/evolution-data-server/buildlink3.mk"
+.endif
