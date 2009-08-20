305a88854d5064c939c4b0621fdc56e46844aeae
adam <adam>
2009-08-20 20:07:29 +0000

    The pano13 library is the backend library used by many applications. Also included are: * PTOptimizer, a utility for optimising control points. * panoinfo a utility for querying the library version. * PTmender a replacement for PTStitcher. * PTblender implements just the colour and brightness correction of PTmender. * PTtiff2psd Converts a set of TIFF files into a PSD file. * PTuncrop Converts 'cropped TIFF' files into 'normal' TIFF files.

---
+++ graphics/libpano13/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libpano13
+
+.if !defined(LIBPANO13_BUILDLINK3_MK)
+LIBPANO13_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libpano13+=	libpano13>=2.9.14
+BUILDLINK_PKGSRCDIR.libpano13?=		../../graphics/libpano13
+
+.include "../../graphics/jpeg/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.endif	# LIBPANO13_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libpano13
