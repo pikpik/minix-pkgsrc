d80d9bd265efb8fc467975646ce552a651b0aed0
wiz <wiz>
2009-08-21 22:47:30 +0000

    Add buildlink3.mk.

---
+++ chat/telepathy-farsight/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	telepathy-farsight
+
+.if !defined(TELEPATHY_FARSIGHT_BUILDLINK3_MK)
+TELEPATHY_FARSIGHT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.telepathy-farsight+=	telepathy-farsight>=0.0.8
+BUILDLINK_PKGSRCDIR.telepathy-farsight?=	../../chat/telepathy-farsight
+
+.include "../../chat/telepathy-glib/buildlink3.mk"
+.include "../../multimedia/farsight2/buildlink3.mk"
+.endif	# TELEPATHY_FARSIGHT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-telepathy-farsight
