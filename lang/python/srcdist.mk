acadd56463fa353cbaf87fdb6b59e84747f6c6f9
joerg <joerg>
2009-09-07 18:22:10 +0000

    Retire Python 2.3 support.

--- lang/python/srcdist.mk
+++ lang/python/srcdist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: srcdist.mk,v 1.26 2009/04/11 19:42:23 snj Exp $
+# $NetBSD: srcdist.mk,v 1.27 2009/04/19 14:46:21 wiz Exp $
 
 .include "../../lang/python/pyversion.mk"
 
@@ -31,16 +31,6 @@ PATCHDIR=	${.CURDIR}/../../lang/python24/patches
 PYSUBDIR=	Python-2.4.5
 WRKSRC=		${WRKDIR}/${PYSUBDIR}
 MASTER_SITES=	ftp://ftp.python.org/pub/python/2.4/
-
-.elif ${_PYTHON_VERSION} == "23"
-
-DISTNAME=	Python-2.3.7
-EXTRACT_SUFX=	.tgz
-DISTINFO_FILE=	${.CURDIR}/../../lang/python23/distinfo
-PATCHDIR=	${.CURDIR}/../../lang/python23/patches
-PYSUBDIR=	Python-2.3.7
-WRKSRC=		${WRKDIR}/${PYSUBDIR}
-MASTER_SITES=	ftp://ftp.python.org/pub/python/2.3.7/
 
 .endif
 
