471250dfa21c4cde1f1da98f9a8e055609bb711d
wiz <wiz>
2009-09-10 20:16:45 +0000

    Initial import of buzztard-0.5.0 and its components.
    Buzztard is a music composer similar to tracker applications.  It
    is roughly modeled after the windows only, closed source and now
    discontinued application called Buzz.

---
+++ audio/gst-buzztard/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	gst-buzztard
+
+.if !defined(GST_BUZZTARD_BUILDLINK3_MK)
+GST_BUZZTARD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gst-buzztard+=	gst-buzztard>=0.5.0
+BUILDLINK_PKGSRCDIR.gst-buzztard?=	../../nih/gst-buzztard
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../multimedia/gst-plugins0.10-base/buildlink3.mk"
+.include "../../multimedia/gstreamer0.10/buildlink3.mk"
+.endif # GST_BUZZTARD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gst-buzztard
