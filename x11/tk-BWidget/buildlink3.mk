fef568d510928cc4003571b875f8bd31e98836c6
plunky <plunky>
2009-09-12 20:49:44 +0000

    update to BWidget 1.9.0

--- x11/tk-BWidget/buildlink3.mk
+++ x11/tk-BWidget/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2006/07/08 23:11:16 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:25:51 joerg Exp $
 #
 
 BUILDLINK_TREE+=	tk-BWidget
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	tk-BWidget
 .if !defined(TK_BWIDGET_BUILDLINK3_MK)
 TK_BWIDGET_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tk-BWidget+=	tk-BWidget>=1.7.0
-BUILDLINK_ABI_DEPENDS.tk-BWidget?=	tk-BWidget>=1.7.0nb1
+BUILDLINK_API_DEPENDS.tk-BWidget+=	tk-BWidget>=1.9.0
+BUILDLINK_ABI_DEPENDS.tk-BWidget+=	tk-BWidget>=1.9.0
 BUILDLINK_PKGSRCDIR.tk-BWidget?=	../../x11/tk-BWidget
 .endif # TK_BWIDGET_BUILDLINK3_MK
 
