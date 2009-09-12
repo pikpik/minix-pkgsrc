5089f1c578d714541b79c90f3007b5fe30d5fac8
sno <sno>
2009-09-12 19:30:16 +0000

    Updating databases/p5-DBIx-Class from 0.08109 to 0.08111
    pkgsrc changes:
      - Updating dependencies
    
    Upstream changes:
    0.08111 2009-09-06 21:58:00 (UTC)
            - The hashref to connection_info now accepts a 'dbh_maker'
              coderef, allowing better intergration with Catalyst
            - Fixed a complex prefetch + regular join regression introduced
              in 0.08108
            - Fixed insert_bulk rebless handling
            - Fixed Storable roundtrip regression, and general serialization
              cleanup
            - SQLT related fixes:
              - sqlt_type is now called on the correct storage object
              - hooks can now see the correct producer_type (RT#47891)
              - optional SQLT requirements for e.g. deploy() bumped to 0.11002
            - Really fixed (and greatly cleaned up) postgresql autoinc sequence
              autodetection
            - Automatically detect MySQL v3 and use INNER JOIN instead of JOIN
            - POD improvements (including RT#48769)
            - Test suite tweaks (including fixes for recent CPANTS fails)
            - Better support for MSSQL IDENTITY_INSERT ON

--- databases/p5-DBIx-Class/options.mk
+++ databases/p5-DBIx-Class/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2009/04/24 17:28:05 sno Exp $
+# $NetBSD: options.mk,v 1.2 2009/06/15 20:30:03 sno Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.p5-DBIx-Class
 PKG_SUPPORTED_OPTIONS=	sql-translator
@@ -7,5 +7,5 @@ PKG_SUGGESTED_OPTIONS=	sql-translator
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Msql-translator)
-DEPENDS+=	p5-SQL-Translator>=0.09004:../../databases/p5-SQL-Translator
+DEPENDS+=	p5-SQL-Translator>=0.11002:../../databases/p5-SQL-Translator
 .endif
