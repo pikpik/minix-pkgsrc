aded33cb05888abb182d3b087ebb23ebb14dac76
tron <tron>
2009-09-13 13:31:41 +0000

    Update to "x11-links" package to version 0.44: Detect X.org even if "imake" is not installed. The package now works as expected under Mac OS X Snow Leopard. No regressions under NetBSD/amd64 5.0_STABLE with native X.org.

--- pkgtools/x11-links/buildlink3.mk
+++ pkgtools/x11-links/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.20 2009/03/08 00:03:37 snj Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2009/03/20 19:25:13 joerg Exp $
 #
 # Don't include this file manually!  It will be included as necessary
 # by bsd.buildlink3.mk.
@@ -12,7 +12,7 @@ BUILDLINK_TREE+=	x11-links
 .if !defined(X11_LINKS_BUILDLINK3_MK)
 X11_LINKS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.43
+BUILDLINK_API_DEPENDS.x11-links+=	x11-links>=0.44
 BUILDLINK_PKGSRCDIR.x11-links?=	../../pkgtools/x11-links
 BUILDLINK_DEPMETHOD.x11-links?=	build
 
