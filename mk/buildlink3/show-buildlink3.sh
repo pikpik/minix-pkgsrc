8de38a2344f3ac752a1ac788cabbe82390c15a38
obache <obache>
2009-10-05 02:40:06 +0000

    Fix indentation of show-buildlink3, came from a typo.

--- mk/buildlink3/show-buildlink3.sh
+++ mk/buildlink3/show-buildlink3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $NetBSD: show-buildlink3.sh,v 1.1 2006/07/08 23:11:17 jlam Exp $
+# $NetBSD: show-buildlink3.sh,v 1.2 2009/03/20 19:25:01 joerg Exp $
 #
 # This script is a helper for the show-buildlink3 target and outputs
 # the arguments as tree.
@@ -10,7 +10,7 @@ while test $# -gt 0; do
 	pkg="$1"
 	case $pkg in
 	-*)
-		indentation=${indendation#    }
+		indentation=${indentation#    }
 		;;
 	*)
 		echo "${indentation}${pkg}"
