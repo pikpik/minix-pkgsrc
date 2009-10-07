b45d2606ea0e95b73cdcd6d494c27ea46d509495
joerg <joerg>
2009-10-07 12:53:27 +0000

    pkg_install-20091006: - restore pkg_add -f functionality for missing dependencies (PR 42001) - pkg_admin rebuild should count packages correctly (he@), also count @pkgdb - fix gpg-sign-package syntax in pkg_admin(1) - change default URL for pkg-vulnerabilities to use HTTP

--- pkgtools/pkg_install/files/add/add.h
+++ pkgtools/pkg_install/files/add/add.h
@@ -1,4 +1,4 @@
-/* $NetBSD: add.h,v 1.14 2009/08/06 16:53:34 joerg Exp $ */
+/* $NetBSD: add.h,v 1.15 2009/08/16 14:26:46 joerg Exp $ */
 
 /* from FreeBSD Id: add.h,v 1.8 1997/02/22 16:09:15 peter Exp  */
 
@@ -38,6 +38,8 @@ extern Boolean Force;
 extern Boolean Automatic;
 extern int LicenseCheck;
 extern int Replace;
+
+extern Boolean ForceDepends;
 
 int     make_hierarchy(char *);
 void    apply_perms(char *, char **, int);
