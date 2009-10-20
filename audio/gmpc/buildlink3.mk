4936b51fc78d247565c78f487a8cc01ba4138129
joerg <joerg>
2009-10-20 20:26:58 +0000

    Update to gmpc-0.19.1: - bugfixes - usability improvements

--- audio/gmpc/buildlink3.mk
+++ audio/gmpc/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/06/04 16:44:43 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/08/26 19:56:56 sno Exp $
 
 BUILDLINK_TREE+=	gmpc
 
 .if !defined(GMPC_BUILDLINK3_MK)
 GMPC_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gmpc+=	gmpc>=0.18.0
-BUILDLINK_ABI_DEPENDS.gmpc?=	gmpc>=0.18.0nb1
+BUILDLINK_API_DEPENDS.gmpc+=	gmpc>=0.19.1
 BUILDLINK_PKGSRCDIR.gmpc?=	../../audio/gmpc
 BUILDLINK_DEPMETHOD.gmpc?=	build
 
