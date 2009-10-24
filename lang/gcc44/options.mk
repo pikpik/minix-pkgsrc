b1451a41ad90986034e57e2b627e20768e020f75
tnn <tnn>
2009-10-24 20:15:05 +0000

    Turn native language support into an option and disable it by default everywhere but on NetBSD (where we know it doesn't add any run-time dependencies)

---
+++ lang/gcc44/options.mk
@@ -0,0 +1,24 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gcc44
+PKG_SUPPORTED_OPTIONS=	nls
+PKG_SUGGESTED_OPTIONS=
+.if ${OPSYS} == "NetBSD"
+PKG_SUGGESTED_OPTIONS+=	nls
+.endif
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		nls
+
+###
+### Native Language Support
+###
+.if !empty(PKG_OPTIONS:Mnls)
+PLIST.nls=		yes
+CONFIGURE_ARGS+=	--enable-nls
+.include "../../converters/libiconv/buildlink3.mk"
+.include "../../devel/gettext-lib/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-nls
+.endif
