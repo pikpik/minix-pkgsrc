4bf00183fc21cea6790e3c5f701d4d81ce4e13ec
wiz <wiz>
2009-10-25 21:32:48 +0000

    Add 2-clause-bsd to DEFAULT_ACCEPTABLE_LICENSES.

--- pkgtools/pkg_install/files/lib/license.c
+++ pkgtools/pkg_install/files/lib/license.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: license.c,v 1.8 2009/07/26 09:18:29 wiz Exp $	*/
+/*	$NetBSD: license.c,v 1.9 2009/08/02 17:56:45 joerg Exp $	*/
 
 /*-
  * Copyright (c) 2009 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -50,7 +50,7 @@ const char *default_acceptable_licenses =
     "gnu-fdl-v1.1 gnu-fdl-v1.2 gnu-fdl-v1.3 "
     "gnu-gpl-v2 gnu-lgpl-v2 gnu-lgpl-v2.1 "
     "gnu-gpl-v3 gnu-lgpl-v3 "
-    "original-bsd modified-bsd "
+    "original-bsd modified-bsd 2-clause-bsd "
     "x11 mit miros "
     "apache-1.1 apache-2.0 "
     "artistic artistic-2.0 "
