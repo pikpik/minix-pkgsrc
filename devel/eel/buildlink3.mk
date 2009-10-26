bf4d86a49cfdcb88fe6008edbb18415168732e77
drochner <drochner>
2009-10-26 09:46:22 +0000

    update for dependency changes in 2.28

--- devel/eel/buildlink3.mk
+++ devel/eel/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:24:12 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/08/26 19:56:46 sno Exp $
 
 BUILDLINK_TREE+=	eel
 
@@ -12,10 +12,6 @@ BUILDLINK_PKGSRCDIR.eel?=	../../devel/eel
 .include "../../devel/GConf/buildlink3.mk"
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
-.include "../../devel/libglade/buildlink3.mk"
-.include "../../devel/libgnome/buildlink3.mk"
-.include "../../devel/libgnomeui/buildlink3.mk"
-.include "../../graphics/libart/buildlink3.mk"
 .include "../../sysutils/gnome-menus/buildlink3.mk"
 .include "../../textproc/libxml2/buildlink3.mk"
 .include "../../x11/gnome-desktop/buildlink3.mk"
