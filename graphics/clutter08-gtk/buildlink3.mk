96bd0ea5629361c3b31c04b7f5533bd23e418ac3
obache <obache>
2009-10-26 08:32:30 +0000

    Import clutter08-gtk-0.8.3 as graphics/clutter08-gtk.
    clutter-gtk for clutter-0.8 branch.

---
+++ graphics/clutter08-gtk/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD: buildlink3.mk,v 1.3 2009/08/26 19:56:57 sno Exp $
+#
+
+BUILDLINK_TREE+=	clutter08-gtk
+
+.if !defined(CLUTTER08_GTK_BUILDLINK3_MK)
+CLUTTER08_GTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.clutter08-gtk+=	clutter08-gtk>=0.8.0
+BUILDLINK_PKGSRCDIR.clutter08-gtk?=	../../graphics/clutter08-gtk
+
+.include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/clutter08/buildlink3.mk"
+.endif # CLUTTER_GTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-clutter08-gtk
