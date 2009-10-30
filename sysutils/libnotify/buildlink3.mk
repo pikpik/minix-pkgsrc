af4fe9eb757d3261ac4ad95993a7d44bf6fb5ec5
drochner <drochner>
2009-10-30 16:54:07 +0000

    propagate glib/gtk dependencies, needed to link

--- sysutils/libnotify/buildlink3.mk
+++ sysutils/libnotify/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:25:25 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.7 2009/08/26 19:56:56 sno Exp $
 
 BUILDLINK_TREE+=	libnotify
 
@@ -9,10 +9,10 @@ BUILDLINK_API_DEPENDS.libnotify+=	libnotify>=0.3.2
 BUILDLINK_ABI_DEPENDS.libnotify?=	libnotify>=0.4.5nb1
 BUILDLINK_PKGSRCDIR.libnotify?=	../../sysutils/libnotify
 
-#.include "../../devel/glib2/buildlink3.mk"
+.include "../../devel/glib2/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
 .include "../../sysutils/dbus-glib/buildlink3.mk"
-#.include "../../x11/gtk2/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
 .endif # LIBNOTIFY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libnotify
