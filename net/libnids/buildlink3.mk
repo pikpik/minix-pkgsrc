a2351acc74bbdffbb5ac134d1a3aad99f6b59344
tnn <tnn>
2009-11-01 16:29:24 +0000

    deal w/ conditional glib2 support. Fixes build of security/pakemon and security/dsniff.

--- net/libnids/buildlink3.mk
+++ net/libnids/buildlink3.mk
@@ -1,13 +1,24 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/07/08 23:11:03 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:25:10 joerg Exp $
 
 BUILDLINK_TREE+=	libnids
 
 .if !defined(LIBNIDS_BUILDLINK3_MK)
 LIBNIDS_BUILDLINK3_MK:=
 
+
 BUILDLINK_API_DEPENDS.libnids+=	libnids>=1.18
 BUILDLINK_PKGSRCDIR.libnids?=	../../net/libnids
 BUILDLINK_DEPMETHOD.libnids?=	build
+
+pkgbase := libnids
+.include "../../mk/pkg-build-options.mk"
+.if !empty(PKG_BUILD_OPTIONS.libnids:Mglib)
+.include "../../devel/glib2/buildlink3.mk"
+# This is needed because libnids is a static library
+LDFLAGS+=	${COMPILER_RPATH_FLAG}${PREFIX}/lib -L${PREFIX}/lib	\
+		  -lglib-2.0 -lgthread-2.0
+.endif
+
 .endif # LIBNIDS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libnids
