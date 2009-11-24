8c8a647e2f729f6dfcebeea2d7759be6f20b73ee
drochner <drochner>
2009-11-24 11:24:42 +0000

    afais there are no references to libexif in exported libraries, so don't propagate bl3

--- graphics/gimp/buildlink3.mk
+++ graphics/gimp/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.27 2009/03/20 19:24:39 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2009/08/26 19:56:44 sno Exp $
 
 BUILDLINK_TREE+=	gimp
 
@@ -32,7 +32,6 @@ pkgbase := gimp
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../graphics/lcms/buildlink3.mk"
 .include "../../graphics/libart/buildlink3.mk"
-.include "../../graphics/libexif/buildlink3.mk"
 .include "../../graphics/png/buildlink3.mk"
 .include "../../graphics/libwmf/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
