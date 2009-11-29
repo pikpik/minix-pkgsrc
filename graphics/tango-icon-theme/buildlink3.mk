cf4650a14a99824ed799adfbd5526399bc4b32a3
snj <snj>
2009-11-29 21:47:38 +0000

    Move tango-icon-theme from pkgsrc-wip to pkgsrc.  tango-icon-theme is, unsurprisingly, an icon theme set.  Packaged by Dieter Roelants.

---
+++ graphics/tango-icon-theme/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	tango-icon-theme
+
+.if !defined(TANGO_ICON_THEME_BUILDLINK3_MK)
+TANGO_ICON_THEME_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.tango-icon-theme+=	tango-icon-theme>=0.8.90
+BUILDLINK_PKGSRCDIR.tango-icon-theme?=	../../graphics/tango-icon-theme
+.endif # TANGO_ICON_THEME_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-tango-icon-theme
