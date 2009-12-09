59ee3f252abf40d15cce616f055e2d7254535e1b
obache <obache>
2009-12-09 12:27:28 +0000

    Some clean up realted to net/libnet{10,11}. * buildlink bin/libnet{10,11}-config to bin/libnet-config for comvenience   (they ware renamed in pkgsrc to avoid conflict) * remove -lnet from BUILDLINK_CPPFLAGS.libnet11.   linker flags should not be in CPPFLAGS, and it berak likage with libtool   as reported in PR 37300. * libnet11 install just a static library, so set defaut DEPMETHOD = build
    Bump PKGREVISION for libnet11 related packages (net/isic will be updated later).

--- devel/libnet10/buildlink3.mk
+++ devel/libnet10/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2007/02/18 18:36:21 adrianp Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:21 joerg Exp $
 
 BUILDLINK_TREE+=	libnet10
 
@@ -10,6 +10,8 @@ BUILDLINK_PKGSRCDIR.libnet10?=		../../devel/libnet10
 
 BUILDLINK_INCDIRS.libnet10+=		include/libnet10
 BUILDLINK_LIBDIRS.libnet10+=		lib/libnet10
+BUILDLINK_FILES.libnet10+=		bin/libnet10-config
+BUILDLINK_FNAME_TRANSFORM.libnet10+=	-e 's|bin/libnet10-config|bin/libnet-config|'
 .endif # LIBNET10_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libnet10
