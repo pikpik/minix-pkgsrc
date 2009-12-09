a6aa9f07c9828af7892642d1dbb102d1358eb1a7
drochner <drochner>
2009-12-09 19:01:06 +0000

    update to 2.28.2 this switches to the gnome-2.28 release branch

--- sysutils/brasero/buildlink3.mk
+++ sysutils/brasero/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/07/26 18:25:01 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 19:56:58 sno Exp $
 
 BUILDLINK_TREE+=	brasero
 
@@ -11,7 +11,6 @@ BUILDLINK_PKGSRCDIR.brasero?=	../../sysutils/brasero
 
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
-.include "../../sysutils/hal/buildlink3.mk"
 
 .endif # BRASERO_BUILDLINK3_MK
 
