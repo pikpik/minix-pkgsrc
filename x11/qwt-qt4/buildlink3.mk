37acd87eab106f34b4e56b0e8c5bb8264e8c617f
drochner <drochner>
2009-12-11 19:17:38 +0000

    add one

---
+++ x11/qwt-qt4/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	qwt
+
+.if !defined(QWT_BUILDLINK3_MK)
+QWT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qwt+=	qwt>=5.2.0
+BUILDLINK_PKGSRCDIR.qwt?=	../../x11/qwt-qt4
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QWT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qwt
