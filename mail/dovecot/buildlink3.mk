cadfbe46c72139f25541409d63dbdc16f8ba937e
ghen <ghen>
2009-12-11 20:52:22 +0000

    Update to Dovecot 1.2.8, imported from pkgsrc-wip and based on work of Edgar Fuß <ef@math.uni-bonn.de>.
    The package now contains Stephan Bosch' new Dovecot Sieve plugin as a pkgsrc
    option (instead of the old CMU Sieve plugin that was a separate package), as
    well as the ManageSieve extension.  The dovecot-sieve package will be removed.
    
    Upgrading from Dovecot 1.1.x may require changes to your configuration file,
    see this webpage for more information: http://wiki.dovecot.org/Upgrading/1.2
    
    Major changes since Dovecot 1.1:
    
    	* When creating files or directories to mailboxes, Dovecot now uses
    	  the mailbox directory's permissions and GID for them. Previous
    	  versions simply used 0600 mode always. For backwards compatibility
    	  dovecot-shared file's permissions still override these with Maildir.
    	* SQL dictionary (quota) configuration file is different than in v1.1.
    	  See doc/dovecot-dict-sql-example.conf for the new format.
    	* deliver -m: Mailbox name is now assumed to be in UTF-8 format,
    	  not modified-UTF7. Stephan Bosch's new Sieve implementation also
    	  assumes UTF-8 format in fileinto parameters.
    
    	+ Full support for shared mailboxes and IMAP ACL extension.
    	  The code is mainly from Sascha Wilde and Bernhard Herzog.
    	+ IMAP: Added support for extensions: CONDSTORE, QRESYNC, ESEARCH,
    	  ESORT, SEARCHRES, WITHIN, ID and CONTEXT=SEARCH.
    	+ SEARCH supports INTHREAD search key, but the rest of the INTHREAD
    	  draft isn't implemented yet so it's not advertised in capability.
    	+ THREAD REFS algorithm where threads are sorted by their latest
    	  message instead of the thread root message. There is also no base
    	  subject merging.
    	+ IMAP: Implemented imap-response-codes draft.
    	+ Thread indexes for optimizing IMAP THREAD command and INTHREAD
    	  search key.
    	+ Added userdb checkpassword (by Sascha Wilde)
    	+ Virtual mailboxes: http://wiki.dovecot.org/Plugins/Virtual
    	+ Autocreate plugin: http://wiki.dovecot.org/Plugins/Autocreate
    	+ Listescape plugin: http://wiki.dovecot.org/Plugins/Listescape

--- mail/dovecot/buildlink3.mk
+++ mail/dovecot/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/19 15:50:27 joerg Exp $
+# $NetBSD$
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	dovecot
 .if !defined(DOVECOT_BUILDLINK3_MK)
 DOVECOT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=1.1beta16
+BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=1.2.0
 BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot
 
 pkgbase := dovecot
