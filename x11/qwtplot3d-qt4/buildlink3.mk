c3f93e9b85812ceb53165d56c6773dbf32786dc9
drochner <drochner>
2009-12-11 19:22:10 +0000

    add qwtplot3d-0.2.7, a collection of 3d widgets for qt (this is the qt4 version)

---
+++ x11/qwtplot3d-qt4/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	qwtplot3d
+
+.if !defined(QWTPLOT3D_BUILDLINK3_MK)
+QWTPLOT3D_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.qwtplot3d+=	qwtplot3d>=0.2.7
+BUILDLINK_PKGSRCDIR.qwtplot3d?=	../../x11/qwtplot3d-qt4
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.endif	# QWTPLOT3D_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-qwtplot3d
