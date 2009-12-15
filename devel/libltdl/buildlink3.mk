0078c7e563a4de8dc469484b128510ec5397e7a0
joerg <joerg>
2009-12-15 17:35:37 +0000

    Update to libtool-2.2.6b. This switches libtool to the new main branch. Changes are many, important for pkgsrc: - much higher speed (up to 30% faster for the overall build of libX11) - less magic for detection of the tag
    The pkgsrc integration of f2c has been changed to be more sane.
    libtool itself now depends on the f2c frontend to use proper
    auto-configuration and only fakes up the library. This part is
    encapsulated in mk/compiler/f2c.mk.

--- devel/libltdl/buildlink3.mk
+++ devel/libltdl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2006/12/12 21:52:35 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:20 joerg Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 .if ${OPSYS} == "NetBSD"
@@ -14,7 +14,7 @@ BUILDLINK_TREE+=	libltdl
 .if !defined(LIBLTDL_BUILDLINK3_MK)
 LIBLTDL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libltdl+=	libltdl>=1.5.10
+BUILDLINK_API_DEPENDS.libltdl+=	libltdl>=2.2.6
 BUILDLINK_PKGSRCDIR.libltdl?=	../../devel/libltdl
 
 .include "../../mk/dlopen.buildlink3.mk"
