0923b8e6c3d606f87cb452c715a0c3d192c35269
obache <obache>
2010-01-05 02:10:13 +0000

    libuim is linked with libintl, so add buildlink to gettext-lib.

--- inputmethod/uim/buildlink3.mk
+++ inputmethod/uim/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.10 2008/06/08 13:01:48 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:47 joerg Exp $
 
 BUILDLINK_TREE+=	uim
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.uim+=	uim>=1.5.1
 BUILDLINK_ABI_DEPENDS.uim?=	uim>=1.5.1
 BUILDLINK_PKGSRCDIR.uim?=	../../inputmethod/uim
 
+.include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/libgcroots/buildlink3.mk"
 .endif # UIM_BUILDLINK3_MK
 
