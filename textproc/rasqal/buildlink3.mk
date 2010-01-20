556b709aea56989c9d5aac6e3d0569ff09bda451
wiz <wiz>
2010-01-20 10:40:30 +0000

    Update to 0.9.17. Bump API depends.
    2009-12-16 Rasqal Version 0.9.17 Released
    
       WARNING: ABI AND API CHANGED in this release. Rasqal 0.9.17 is binary
       incompatible with 0.9.16 or earlier.
    
       Added a new query engine that implements the SPARQL algebra better
       All constructors now take a rasqal_world argument
       Added LAQRS syntax support for SUM, AVG, MIN, MAX, COALESCE()
       experimental syntax
       Added query result formatters for CSV, TSV and ASCII tables
       Prefer pkg-config for configuring
       Many resilience and resource failure fixes by Lauri Aalto
       Many other bug fixes and improvements were made
       Fixed Issues: Issue#0000077, Issue#0000128, Issue#0000168,
       Issue#0000258, Issue#0000261, Issue#0000271, Issue#0000279 and
       Issue#0000305

--- textproc/rasqal/buildlink3.mk
+++ textproc/rasqal/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:32 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/01/17 12:02:47 wiz Exp $
 
 BUILDLINK_TREE+=	rasqal
 
 .if !defined(RASQAL_BUILDLINK3_MK)
 RASQAL_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.16
-BUILDLINK_ABI_DEPENDS.rasqal?=	rasqal>=0.9.16nb1
+BUILDLINK_API_DEPENDS.rasqal+=	rasqal>=0.9.17
 BUILDLINK_PKGSRCDIR.rasqal?=	../../textproc/rasqal
 
 .include "../../textproc/raptor/buildlink3.mk"
