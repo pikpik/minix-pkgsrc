32a83af6f62032d50c07c45a5acb3cfbd2f8c08f
drochner <drochner>
2010-01-21 12:12:27 +0000

    make the "avahi" option "off" per defauld, as in other pkgs, bump PKGREVISION

--- audio/pulseaudio/options.mk
+++ audio/pulseaudio/options.mk
@@ -1,8 +1,7 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2009/01/25 16:56:01 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pulseaudio
 PKG_SUPPORTED_OPTIONS=	avahi
-PKG_SUGGESTED_OPTIONS=	avahi
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 .include "../../mk/bsd.options.mk"
 
