e7b48e47d16f718a8be061e3a3f5b0f40b3aecc1
adam <adam>
2010-01-26 08:26:11 +0000

    Added dependency on jasper, libungif, png

--- geography/gdal-lib/buildlink3.mk
+++ geography/gdal-lib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2010/01/18 09:36:09 adam Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/01/18 10:01:49 wiz Exp $
 
 BUILDLINK_TREE+=	gdal-lib
 
@@ -22,6 +22,11 @@ pkgbase := gdal-lib
 
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../geography/geos/buildlink3.mk"
+.include "../../graphics/jasper/buildlink3.mk"
+.include "../../graphics/libungif/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../graphics/tiff/buildlink3.mk"
+.include "../../misc/proj/buildlink3.mk"
 .include "../../textproc/xerces-c/buildlink3.mk"
 .include "../../www/curl/buildlink3.mk"
 .endif # GDAL_LIB_BUILDLINK3_MK
