fe7bda1f5660b05ea3789726cb43318e56b2e67d
tnn <tnn>
2010-01-30 14:54:48 +0000

    Update to libgii-1.0.2. Needed for libggi update. No NEWS entry.

--- graphics/libgii/buildlink3.mk
+++ graphics/libgii/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.10 2007/02/02 19:17:31 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.11 2009/03/20 19:24:42 joerg Exp $
 
 BUILDLINK_TREE+=	libgii
 
 .if !defined(LIBGII_BUILDLINK3_MK)
 LIBGII_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgii+=	libgii>=0.9.0
-BUILDLINK_ABI_DEPENDS.libgii?=	libgii>=0.9.1nb1
+BUILDLINK_API_DEPENDS.libgii+=	libgii>=1.0.2
 BUILDLINK_PKGSRCDIR.libgii?=	../../graphics/libgii
 
 .include "../../x11/libX11/buildlink3.mk"
