f4ea1562f83ac2c6a91b7aa90b68698a3d6a0e59
tnn <tnn>
2010-01-31 20:54:44 +0000

    add some convenience variables for version handling

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.4 2010/01/19 10:07:49 tnn Exp $
+# $NetBSD: dist.mk,v 1.5 2010/01/26 16:22:59 tron Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -6,11 +6,13 @@
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	3.5.7
+FIREFOX_VER=	3.5.${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	1.9.1
+MOZ_BRANCH_MINOR=	7
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../devel/xulrunner/patches
 
-WRKSRC=		${WRKDIR}/mozilla-1.9.1
+WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
