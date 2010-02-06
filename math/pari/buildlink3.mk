5f0209130098e663eeae7595e301d763c6ea1dfa
sno <sno>
2010-02-06 10:37:06 +0000

    Updating math/pari from 2.1.6nb3 to 2.3.5 (newer than PR pkg/38116)
    pkgsrc changes:
    - Adding license (GPLv2)
    - Adding X11 and GMP support (latter via option)
    - Adding patches to support math/p5-Math-Pari can be build without
      rebuilding pari again
    
    Upstream changes:
    * Primarily a lot of bug fixes
    * Plotting API reworked
    * More targets are supported

--- math/pari/buildlink3.mk
+++ math/pari/buildlink3.mk
@@ -1,13 +1,23 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:10:59 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:58 joerg Exp $
 
 BUILDLINK_TREE+=	pari
 
 .if !defined(PARI_BUILDLINK3_MK)
 PARI_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pari+=	pari>=2.1.5nb1
-BUILDLINK_ABI_DEPENDS.pari+=	pari>=2.1.6nb1
+BUILDLINK_API_DEPENDS.pari+=	pari>=2.3.4
+BUILDLINK_ABI_DEPENDS.pari+=	pari>=2.3.4
 BUILDLINK_PKGSRCDIR.pari?=	../../math/pari
+
+pkgbase := pari
+.include "../../mk/pkg-build-options.mk"
+
+PARI_LIB?=	pari
+.  if !empty(PKG_BUILD_OPTIONS.pari:Mgmp)
+PARI_LIB=	pari-gmp
+.    include "../../devel/gmp/buildlink3.mk"
+.  endif
+
 .endif # PARI_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pari
