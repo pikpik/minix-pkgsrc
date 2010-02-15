73f04b5b051ef56d6ef5b136a3209982869d23ef
wiz <wiz>
2010-02-15 15:51:55 +0000

    Initial import of liblastfm-3.0nb2 from wip, packaged by myself.
    liblastfm is a collection of libraries to help you integrate Last.fm
    services into your rich desktop software.

---
+++ audio/liblastfm/buildlink3.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	liblastfm
+
+.if !defined(LIBLASTFM_BUILDLINK3_MK)
+LIBLASTFM_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.liblastfm+=	liblastfm>=0.3.0
+BUILDLINK_ABI_DEPENDS.liblastfm?=	liblastfm>=0.3.0nb1
+BUILDLINK_PKGSRCDIR.liblastfm?=	../../audio/liblastfm
+
+.include "../../audio/libsamplerate/buildlink3.mk"
+.include "../../math/fftwf/buildlink3.mk"
+.include "../../x11/qt4-libs/buildlink3.mk"
+.include "../../x11/qt4-tools/buildlink3.mk"
+.endif	# LIBLASTFM_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-liblastfm
