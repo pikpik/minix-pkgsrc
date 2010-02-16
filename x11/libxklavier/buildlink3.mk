a854a871376603e1183c4e5d325ef6c7ed466e3b
wiz <wiz>
2010-02-16 13:35:14 +0000

    Update to 5.0:
    The release adds ability to have different types of listeners in
    the same process (necessary for the latest unstable version of
    GNOME).
    
    The API/ABI (and soname) was changed (oops, I did it again).

--- x11/libxklavier/buildlink3.mk
+++ x11/libxklavier/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2007/01/15 19:26:33 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2009/03/20 19:25:47 joerg Exp $
 
 BUILDLINK_TREE+=	libxklavier
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	libxklavier
 LIBXKLAVIER_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libxklavier+=	libxklavier>=3.0
+BUILDLINK_ABI_DEPENDS.libxklavier?=	libxklavier>=5.0
 BUILDLINK_PKGSRCDIR.libxklavier?=	../../x11/libxklavier
 
 .include "../../textproc/libxml2/buildlink3.mk"
