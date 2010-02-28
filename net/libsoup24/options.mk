c0d62d569d376f427e148e6ba93a434390575d28
tonio <tonio>
2010-02-28 20:49:21 +0000

    Add a gnome option, enabled by default. Fix PR pkg/42854, applying a similar patch.
    Remove the dependency on gtk-doc, it wasn't used.

---
+++ net/libsoup24/options.mk
@@ -0,0 +1,18 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.libsoup24
+PKG_SUPPORTED_OPTIONS=	gnome
+PKG_SUGGESTED_OPTIONS=	gnome
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+= gnome
+
+.if !empty(PKG_OPTIONS:Mgnome)
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../security/gnome-keyring/buildlink3.mk"
+PKGCONFIG_OVERRIDE+=	libsoup-gnome-2.4.pc.in
+PLIST.gnome=	yes
+.else
+CONFIGURE_ARGS+= --without-gnome
+.endif
