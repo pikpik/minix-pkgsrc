7bc0978a4a1d857ad0de11769eab852272ab1309
obache <obache>
2010-03-11 06:11:51 +0000

    Add buildlink3.mk

---
+++ graphics/py-clutter/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+#
+
+BUILDLINK_TREE+=	py-clutter
+
+.if !defined(PY_CLUTTER_BUILDLINK3_MK)
+PY_CLUTTER_BUILDLINK3_MK:=
+
+.  include "../../lang/python/pyversion.mk"
+
+BUILDLINK_API_DEPENDS.py-clutter+=	${PYPKGPREFIX}-clutter>=1.0.0
+BUILDLINK_PKGSRCDIR.py-clutter?=	../../graphics/py-clutter
+
+.include "../../graphics/clutter/buildlink3.mk"
+.include "../../graphics/py-cairo/buildlink3.mk"
+.include "../../x11/py-gtk2/buildlink3.mk"
+.endif	# PY_CLUTTER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-py-clutter
