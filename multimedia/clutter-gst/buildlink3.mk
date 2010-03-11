05634b3cb8d76927c4b8c77f29a34c4d517c303d
obache <obache>
2010-03-11 04:07:58 +0000

    Update clutter-gst to 1.0.0.
    clutter-gst 1.0.0 (09/02/2010)
    ===============================================================================
    
      * list of changes since 0.10.0
    
        o Uses the playbin2 GStreamer element.
    
        o Autoloads subtitle files when they have the same name than the video
          being played and are in the same directory.
    
        o GObject introspection support.
    
        o Added a new clutter_gst_init_with_arg() symbol, changed *get_playbin()
          to a more generic *get_pipeline()
    
        o A lot of small bug fixes and code clean up.
    
    Many thanks to:
    
      Bastian Winkler
      Ole André Vadla Ravnås
      Roland Peffer

--- multimedia/clutter-gst/buildlink3.mk
+++ multimedia/clutter-gst/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.4 2009/10/26 08:52:43 obache Exp $
 #
 
 BUILDLINK_TREE+=	clutter-gst
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	clutter-gst
 .if !defined(CLUTTER_GST_BUILDLINK3_MK)
 CLUTTER_GST_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=0.10.0
+BUILDLINK_API_DEPENDS.clutter-gst+=	clutter-gst>=1.0.0
 BUILDLINK_PKGSRCDIR.clutter-gst?=	../../multimedia/clutter-gst
 
 .include "../../graphics/clutter/buildlink3.mk"
