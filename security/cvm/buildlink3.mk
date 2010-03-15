b6e783bada38bb1424c4b8393b746d967bfafce4
schmonz <schmonz>
2010-03-15 03:21:15 +0000

    Installs shlibs nowadays, so don't default to a build-only dependency.

--- security/cvm/buildlink3.mk
+++ security/cvm/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2008/04/13 06:51:19 schmonz Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:25:17 joerg Exp $
 
 BUILDLINK_TREE+=	cvm
 
@@ -7,7 +7,6 @@ CVM_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.cvm+=		cvm>=0.90
 BUILDLINK_PKGSRCDIR.cvm?=	../../security/cvm
-BUILDLINK_DEPMETHOD.cvm?=	build
 .endif # CVM_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-cvm
