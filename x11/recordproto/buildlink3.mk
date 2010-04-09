ad4665f456f189c10ff100349dbb5889d48150a7
tnn <tnn>
2010-04-09 08:27:31 +0000

    Update to recordproto-1.14.
    Recordproto has seen a similar cleanup as xextproto recently. Xlib headers
    have been removed and the remaining headers renamed for consistency.
    A stub recordstr.h is provided to avoid breakage in current clients,
    the Xlib header now resides in libXtst.

--- x11/recordproto/buildlink3.mk
+++ x11/recordproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2007/10/23 15:34:32 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:50 joerg Exp $
 
 BUILDLINK_DEPMETHOD.recordproto?=	build
 
@@ -7,7 +7,7 @@ BUILDLINK_TREE+=	recordproto
 .if !defined(RECORDPROTO_BUILDLINK3_MK)
 RECORDPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.recordproto+=	recordproto>=1.13.1
+BUILDLINK_API_DEPENDS.recordproto+=	recordproto>=1.14
 BUILDLINK_PKGSRCDIR.recordproto?=	../../x11/recordproto
 .endif # RECORDPROTO_BUILDLINK3_MK
 
