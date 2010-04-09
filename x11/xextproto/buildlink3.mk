ba9631752810fde58bed9743ad9ae120046a6fae
tnn <tnn>
2010-04-09 08:33:38 +0000

    Update to xextproto-7.1.1.
    "Many headers included in xextproto were combined library, server and
     protocol headers. This release splits those headers up and moves the
     client-side library headers into the libXext module.
     Clients should not be affected by this change.
     Drivers that included headers from this module will need to be fixed."
    
    "Note that as a result of this file removal and renaming, xextproto is only
     partially compatible to other, already released modules. An upgrade of
     xextproto from 7.0.x to 7.1.0 also requires upgrades of libXext, libXtst,
     the X server and possible others."
    
    xxx tnn note: I've added a compat shmstr.h to help old software,
    such as xorg-server 1.6 to build. More fixes coming.

--- x11/xextproto/buildlink3.mk
+++ x11/xextproto/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.8 2008/01/07 13:59:36 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:25:52 joerg Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -9,7 +9,7 @@ BUILDLINK_TREE+=	xextproto
 .if !defined(XEXTPROTO_BUILDLINK3_MK)
 XEXTPROTO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.xextproto+=	xextproto>=7.0
+BUILDLINK_API_DEPENDS.xextproto+=	xextproto>=7.1.1
 BUILDLINK_PKGSRCDIR.xextproto?=	../../x11/xextproto
 
 .include "../../x11/inputproto/buildlink3.mk"
---
+++ x11/xextproto/files/shmstr.h
@@ -0,0 +1,32 @@
+/* $NetBSD$ */
+
+#warning this file is deprecated and provided only for source compatibility
+
+#include "shmproto.h"
+ 
+#define XSHM_PUT_IMAGE_ARGS \
+    DrawablePtr		/* dst */, \
+    GCPtr		/* pGC */, \
+    int			/* depth */, \
+    unsigned int	/* format */, \
+    int			/* w */, \
+    int			/* h */, \
+    int			/* sx */, \
+    int			/* sy */, \
+    int			/* sw */, \
+    int			/* sh */, \
+    int			/* dx */, \
+    int			/* dy */, \
+    char *		/* data */
+
+#define XSHM_CREATE_PIXMAP_ARGS \
+    ScreenPtr	/* pScreen */, \
+    int		/* width */, \
+    int		/* height */, \
+    int		/* depth */, \
+    char *	/* addr */
+
+typedef struct _ShmFuncs {
+    PixmapPtr	(* CreatePixmap)(XSHM_CREATE_PIXMAP_ARGS);
+    void	(* PutImage)(XSHM_PUT_IMAGE_ARGS);
+} ShmFuncs, *ShmFuncsPtr;
