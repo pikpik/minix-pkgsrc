5edc36ccc8da151ebc9d9201aa085661f0048090
adam <adam>
2010-04-15 07:01:23 +0000

    Rev.2 changes: * Unlimit file descr. to 1024 for c2s, as in c2s configuration file. * Allow -Os (instead of -O1) as a work-around 64-bit compiler bug. * Router patch.

--- chat/jabberd2/files/c2s.sh
+++ chat/jabberd2/files/c2s.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: c2s.sh,v 1.1.1.1 2004/03/28 19:44:07 xtraeme Exp $
+# $NetBSD: c2s.sh,v 1.2 2004/06/26 11:21:46 abs Exp $
 #
 # PROVIDE: c2s
 # REQUIRE: DAEMON
@@ -31,6 +31,11 @@ remove_pidfile()
 	if [ -f @JABBERD_PIDDIR@/${name}.pid ]; then
 	    rm -f @JABBERD_PIDDIR@/${name}.pid
 	fi
+}
+
+limits_precmd()
+{
+	ulimit -n 1024
 }
 
 if [ -f /etc/rc.subr ]; then
