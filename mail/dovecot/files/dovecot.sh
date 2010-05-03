17d18a0bdd062c810212bbf4af4eafca5bb35e80
ghen <ghen>
2010-05-03 19:19:01 +0000

    load_rc_config prior to setting a default for dovecot_fdlimit in the rc.d script, so it can be overridden from rc.conf.  From Edgar Fuß. Bump PKGREVISION.

--- mail/dovecot/files/dovecot.sh
+++ mail/dovecot/files/dovecot.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: dovecot.sh,v 1.3 2008/06/22 17:41:23 ghen Exp $
+# $NetBSD: dovecot.sh,v 1.4 2008/07/10 00:54:30 jnemeth Exp $
 #
 
 # PROVIDE: dovecot
@@ -14,6 +14,7 @@ command="@PREFIX@/sbin/${name}"
 required_files="@PKG_SYSCONFDIR@/$name.conf"
 extra_commands="reload"
 
+load_rc_config $name
 dovecot_fdlimit=${dovecot_fdlimit-"768"}
 
 # A default limit of 64 (at least on NetBSD) may be too low for many people
@@ -28,5 +29,4 @@ if [ ${dovecot_fdlimit} -gt ${SOFT_FDLIMIT} ]; then
   fi
 fi
 
-load_rc_config $name
 run_rc_command "$1"
