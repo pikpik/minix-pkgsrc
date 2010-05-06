b75df7b3338cefb727c07d8f5f238645f66c19d7
pettai <pettai>
2010-05-06 12:50:17 +0000

    The OpenDNSSEC project announces the development of Open Source software that manages the security of domain names on the Internet. The project intends to drive adoption of Domain Name System Security Extensions (DNSSEC) to further enhance Internet security.

---
+++ security/opendnssec/options.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.opendnssec
+PKG_SUPPORTED_OPTIONS=		softhsm
+PKG_SUGGESTED_OPTIONS=		softhsm
+
+.include "../../mk/bsd.options.mk"
+
+###
+### SoftHSM
+###
+.if !empty(PKG_OPTIONS:Msofthsm)
+.include "../../security/softhsm/buildlink3.mk"
+.endif
