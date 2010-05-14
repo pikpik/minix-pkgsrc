43ffdc63c20ab6ad46540d5029f3980bd3cbf598
asau <asau>
2010-05-14 20:12:58 +0000

    Update to OpenMPI 1.4.2
    Changes in v1.4.2 as compared to v1.4.1:
    
    - Fixed problem when running in heterogeneous environments.
    - Update LSF support to ensure that the path is passed correctly.
    - Fixed some miscellaneous oversubscription detection bugs.
    - IBM re-licensed its LoadLeveler code to be BSD-compliant.
    - Various fixes for multithreading deadlocks, race conditions, and
      other nefarious things.
    - Fixed ROMIO's handling of "nearly" contiguous issues (e.g., with
      non-zero true_lb).
    - Bunches of Windows build fixes.
    - Now allow the graceful failover from MTLs to BTLs if no MTLs can
      initialize successfully.
    - Added "clobber" information to various atomic operations, fixing
      erroneous behavior in some newer versions of the GNU compiler suite.
    - Update various iWARP and InfiniBand device specifications in the
      OpenFabrics .ini support file.
    - Fix the use of hostfiles when a username is supplied.
    - Various fixes for rankfile support.
    - Updated the internal version of VampirTrace to 5.4.12.
    - Fixed OS X TCP wireup issues having to do with IPv4/IPv6 confusion
      (see https://svn.open-mpi.org/trac/ompi/changeset/22788 for more
      details).
    - Fixed some problems in processor affinity support, including when
      there are "holes" in the processor namespace (e.g., offline
      processors).
    - Ensure that Open MPI's "session directory" (usually located in /tmp)
      is cleaned up after process termination.
    - Fixed some problems with the collective "hierarch" implementation
      that could occur in some obscure conditions.
    - Various MPI_REQUEST_NULL, API parameter checking, and attribute
      error handling fixes.
    - Fix case where MPI_GATHER erroneously used datatypes on non-root nodes.
    - Patched ROMIO support for PVFS2 > v2.7 (patch taken from MPICH2
      version of ROMIO).
    - Fixed "mpirun --report-bindings" behavior when used with
      mpi_paffinity_alone=1.  Also fixed mpi_paffinity_alone=1 behavior
      with non-MPI applications.
    - Ensure that all OpenFabrics devices have compatible receive_queues
      specifications before allowing them to communicate.  See the lengthy
      comment in https://svn.open-mpi.org/trac/ompi/changeset/22592 for details.
    - Fix some issues with checkpoint/restart.
    - Improve the pre-MPI_INIT/post-MPI_FINALIZE error messages.
    - Ensure that loopback addresses are never advertised to peer
      processes for RDMA/OpenFabrics support.
    - Fixed a CSUM PML false positive.
    - Various fixes for Catamount support.
    - Minor update to wrapper compilers in how user-specific argv is
      ordered on the final command line.  Thanks to Jed Brown for the
      suggestions.

--- parallel/openmpi/options.mk
+++ parallel/openmpi/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1.1.1 2009/12/12 23:30:01 asau Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.openmpi
 PKG_SUPPORTED_OPTIONS=	debug openmpi-vampirtrace
@@ -14,6 +14,7 @@ CONFIGURE_ARGS+=	--enable-debug
 .if !empty(PKG_OPTIONS:Mopenmpi-vampirtrace)
 CONFLICTS+=	libotf-[0-9]*
 PLIST_SRC+=	PLIST.vt
+.include "../../graphics/freetype2/buildlink3.mk"
 .else
 CONFIGURE_ARGS+=	--enable-contrib-no-build=vt
 .endif
