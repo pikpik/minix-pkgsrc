352777972aedfd1f2c12c560e203a7915d1864cd
obache <obache>
2010-05-14 11:02:20 +0000

    Enable gucharmap support again. Bump PKGREVISION.

--- inputmethod/tomoe-gtk/buildlink3.mk
+++ inputmethod/tomoe-gtk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:24:47 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/08/26 19:56:57 sno Exp $
 
 BUILDLINK_TREE+=	tomoe-gtk
 
@@ -6,10 +6,11 @@ BUILDLINK_TREE+=	tomoe-gtk
 TOMOE_GTK_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.tomoe-gtk+=	tomoe-gtk>=0.6.0
-BUILDLINK_ABI_DEPENDS.tomoe-gtk?=	tomoe-gtk>=0.6.0nb2
+BUILDLINK_ABI_DEPENDS.tomoe-gtk+=	tomoe-gtk>=0.6.0nb3
 BUILDLINK_PKGSRCDIR.tomoe-gtk?=	../../inputmethod/tomoe-gtk
 
 .include "../../inputmethod/tomoe/buildlink3.mk"
+.include "../../fonts/gucharmap/buildlink3.mk"
 .include "../../x11/gtk2/buildlink3.mk"
 .endif # TOMOE_GTK_BUILDLINK3_MK
 
