e5185377b26443c781c7354c5ac98357bd0ce2d7
taca <taca>
2010-05-26 16:11:47 +0000

    Importing net/bind97 package 9.7.0pl2 package. (This is simply based on net/bind96).
    BIND 9.7.0pl2 (9.7.0-P2)
    
    New Features in BIND 9.7 - 'DNSSEC for Humans'
    
    BIND 9.7 introduces several improvements, especially for simplifying
    DNSSEC configuration and DNSSEC maintenance. This article lists some
    of the new features and significant changes in BIND 9.7.
    
    
    For more information please refer these webpage.
    
    http://www.isc.org/software/bind/new-features/9.7
    http://www.isc.org/files/release-notes/9.7.0-P2%20rel%20notes.txt

---
+++ net/bind97/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	bind
+
+.if !defined(BIND_BUILDLINK3_MK)
+BIND_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.bind+=	bind>=9.7.0
+BUILDLINK_PKGSRCDIR.bind?=	../../net/bind97
+.endif # BIND_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-bind
