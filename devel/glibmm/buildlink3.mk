01c5b35eeb1aa24954a463dab972a48913c45c63
adam <adam>
2010-06-02 16:43:26 +0000

    Changes 2.24.2: * Reference Documentation: Add main page text for index.html.
    Changes 2.24.1:
    * UnixOutputStream: Fix the constructor to use the correct property name.
    * Documentation: Strip out the gobject-introspection annotations, and use
      other improvements made to docextract_to_xml.py
    * Fix the Windows MSVC build.
    
    Changes 2.24.0:
    * Changes in glibmm 2.24 compared to glibmm 2.22:
    Glib:
    * BalanceTree: New class, wrapping GTree.
    * Fix ustring::format(non-const char*) and add regression test.
    Gio:
    * Added InetAddress, NetworkAddress, NetworkService, Resolver, Socket,
      SocketAddress, SocketAddressEnumerator, SocketClient, SocketConnectable,
      SocketConnection, SocketListener, SocketService, SrvTarget, TcpConnection,
      Threaded SocketService, UnixConnection
    * AsyncResult: Add get_source_object_base(), deprecating get_source_object()
      because it usually can never work.
    * Cancellable: Added release_fd() and make_polldf().

--- devel/glibmm/buildlink3.mk
+++ devel/glibmm/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2007/09/21 13:03:35 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:14 joerg Exp $
 
 BUILDLINK_TREE+=	glibmm
 
 .if !defined(GLIBMM_BUILDLINK3_MK)
 GLIBMM_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.6.1
-BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.8.3nb1
+BUILDLINK_API_DEPENDS.glibmm+=	glibmm>=2.24.2
+BUILDLINK_ABI_DEPENDS.glibmm+=	glibmm>=2.24.2
 BUILDLINK_PKGSRCDIR.glibmm?=	../../devel/glibmm
 
 .include "../../devel/glib2/buildlink3.mk"
