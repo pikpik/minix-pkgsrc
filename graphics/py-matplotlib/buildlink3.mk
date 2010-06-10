ae57f521d5af26795f7a563529ee6adc6391f6c5
wiz <wiz>
2010-06-10 20:51:05 +0000

    Make pkglint more happy.

--- graphics/py-matplotlib/buildlink3.mk
+++ graphics/py-matplotlib/buildlink3.mk
@@ -1,14 +1,14 @@
-# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 17:30:11 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.5 2009/03/20 19:24:44 joerg Exp $
 
 BUILDLINK_TREE+=	pymatplotlib
 
-.if !defined(PY_MATPLOTLIB_BUILDLINK3_MK)
-PY_MATPLOTLIB_BUILDLINK3_MK:=
+.if !defined(PYMATPLOTLIB_BUILDLINK3_MK)
+PYMATPLOTLIB_BUILDLINK3_MK:=
 
 .include "../../lang/python/pyversion.mk"
 
 BUILDLINK_API_DEPENDS.pymatplotlib+=	${PYPKGPREFIX}-matplotlib>=0.87.2
 BUILDLINK_PKGSRCDIR.pymatplotlib?=	../../graphics/py-matplotlib
-.endif # PY_MATPLOTLIB_BUILDLINK3_MK
+.endif # PYMATPLOTLIB_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pymatplotlib
