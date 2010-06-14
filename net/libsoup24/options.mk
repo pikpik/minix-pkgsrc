180437f5a3451962dabf49e59e756d418dbb9a29
drochner <drochner>
2010-06-14 18:00:54 +0000

    replace the gnome-keyring dependency by one on libgnome-keyring which is a subset of the former, ride on the libpng update

--- net/libsoup24/options.mk
+++ net/libsoup24/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2010/02/28 20:49:21 tonio Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.libsoup24
 PKG_SUPPORTED_OPTIONS=	gnome
@@ -10,7 +10,7 @@ PLIST_VARS+= gnome
 
 .if !empty(PKG_OPTIONS:Mgnome)
 .include "../../devel/GConf/buildlink3.mk"
-.include "../../security/gnome-keyring/buildlink3.mk"
+.include "../../security/libgnome-keyring/buildlink3.mk"
 PKGCONFIG_OVERRIDE+=	libsoup-gnome-2.4.pc.in
 PLIST.gnome=	yes
 .else
