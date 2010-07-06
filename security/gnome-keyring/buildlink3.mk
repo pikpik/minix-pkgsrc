5e0d77ecd1026e1b9d35aedb3d03d513fa21eac6
drochner <drochner>
2010-07-06 16:13:42 +0000

    update to 2.30.3 changes: -bugfixes -Add dbus activation service file for org.freedesktop.secrets
    pkgsrc note: removed dependency on libgnome-keyring; child pkgs
    should have been updated to list this dependency explicitely

--- security/gnome-keyring/buildlink3.mk
+++ security/gnome-keyring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2010/06/11 13:45:50 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2010/06/13 22:45:18 wiz Exp $
 
 BUILDLINK_TREE+=	gnome-keyring
 
@@ -8,9 +8,6 @@ GNOME_KEYRING_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gnome-keyring+=	gnome-keyring>=0.4.0
 BUILDLINK_ABI_DEPENDS.gnome-keyring?=	gnome-keyring>=2.30.1nb1
 BUILDLINK_PKGSRCDIR.gnome-keyring?=	../../security/gnome-keyring
-
-# XXX see Makefile
-.include "../../security/libgnome-keyring/buildlink3.mk"
 
 .include "../../devel/gettext-lib/buildlink3.mk"
 .include "../../devel/glib2/buildlink3.mk"
