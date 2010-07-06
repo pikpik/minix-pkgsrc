378823e93d1c0a12580ae8fb8f645a7a51ce2cd5
drochner <drochner>
2010-07-06 15:25:06 +0000

    update to 0.6.4 This switches to the gnome-2.30 release branch.

--- net/libgdata/buildlink3.mk
+++ net/libgdata/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/10/21 18:48:21 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2010/06/13 22:45:11 wiz Exp $
 
 BUILDLINK_TREE+=	libgdata
 
 .if !defined(LIBGDATA_BUILDLINK3_MK)
 LIBGDATA_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libgdata+=	libgdata>=0.1.1
-BUILDLINK_ABI_DEPENDS.libgdata?=	libgdata>=0.5.2nb1
+BUILDLINK_API_DEPENDS.libgdata+=	libgdata>=0.6.4
 BUILDLINK_PKGSRCDIR.libgdata?=	../../net/libgdata
 
 
