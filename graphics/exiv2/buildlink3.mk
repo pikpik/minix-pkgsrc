aa8d2058645c0c1fae69ad2c8b5e4083a01fcbec
drochner <drochner>
2010-07-14 18:46:30 +0000

    belatedly bump ABI rev - the last update changed the shlib major

--- graphics/exiv2/buildlink3.mk
+++ graphics/exiv2/buildlink3.mk
@@ -1,13 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:38 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/07/07 10:02:51 drochner Exp $
 
 BUILDLINK_TREE+=	exiv2
 
 .if !defined(EXIV2_BUILDLINK3_MK)
 EXIV2_BUILDLINK3_MK:=
 
-# 0.18 changes the signature of printTag
 BUILDLINK_API_DEPENDS.exiv2+=	exiv2>=0.18
-BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.18
+BUILDLINK_ABI_DEPENDS.exiv2+=	exiv2>=0.20
 BUILDLINK_PKGSRCDIR.exiv2?=	../../graphics/exiv2
 
 .include "../../textproc/expat/buildlink3.mk"
