89b3d1e0b41d541565fe0eece00d61905c9fbd93
drochner <drochner>
2010-07-18 12:22:25 +0000

    add orc-0.4.6, a replacement for liboil, used by future gstreamer

---
+++ devel/orc/buildlink3.mk
@@ -0,0 +1,12 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	orc
+
+.if !defined(ORC_BUILDLINK3_MK)
+ORC_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.orc+=	orc>=0.4.6
+BUILDLINK_PKGSRCDIR.orc?=	../../devel/orc
+.endif	# ORC_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-orc
