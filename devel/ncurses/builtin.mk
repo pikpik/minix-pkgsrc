5643d5e0d47de0a8eb900e5112301ec3954ee475
dholland <dholland>
2010-07-26 06:49:30 +0000

    The change in -r1.27 of this file failed to restore the old logic completely; rectify that. Now setting USE_NCURSES=yes won't install pkgsrc ncurses on e.g. Linux where it's really built in.
    Not sure if a version bump is wanted, but since I just did one we'll
    ride that.

--- devel/ncurses/builtin.mk
+++ devel/ncurses/builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: builtin.mk,v 1.26 2010/02/23 19:50:49 drochner Exp $
+# $NetBSD: builtin.mk,v 1.27 2010/02/24 12:08:07 drochner Exp $
 
 BUILTIN_PKG:=	ncurses
 
@@ -97,7 +97,8 @@ MAKEVARS+=	USE_BUILTIN.ncurses
 
 # If USE_NCURSES is set to yes, the use of an ncurses implementation
 # is forced.
-.if defined(USE_NCURSES) && !empty(USE_NCURSES:M[yY][eE][sS])
+.if defined(USE_NCURSES) && !empty(USE_NCURSES:M[yY][eE][sS]) \
+	&& !empty(IS_BUILTIN.ncurses:M[nN][oO])
 USE_BUILTIN.ncurses=	no
 .endif
 # If it is set to chgat, a curses implementation with chgat(3) support
