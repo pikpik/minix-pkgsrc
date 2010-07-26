988b9f50045f09de5882e41515d571dd7cd5ac98
drochner <drochner>
2010-07-26 11:13:43 +0000

    add one

---
+++ graphics/eog/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	eog
+
+.if !defined(EOG_BUILDLINK3_MK)
+EOG_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.eog+=	eog>=2.30.2
+BUILDLINK_PKGSRCDIR.eog?=	../../graphics/eog
+
+.include "../../devel/GConf/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# EOG_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-eog
