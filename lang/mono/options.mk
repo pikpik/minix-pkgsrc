3924e76a9bb7cd0e7a8f4580b7b948f037dcd907
kefren <kefren>
2010-07-31 08:40:37 +0000

    Update to 2.6.7 - XXX: need to make moonlight profile work Full update list: http://www.mono-project.com/Release_Notes_Mono_2.6.7

--- lang/mono/options.mk
+++ lang/mono/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2009/11/25 10:13:06 kefren Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mono
 PKG_SUPPORTED_OPTIONS=	moonlight
-PKG_SUGGESTED_OPTIONS=	moonlight
+# PKG_SUGGESTED_OPTIONS=	moonlight
 
 .include "../../mk/bsd.options.mk"
 
@@ -10,6 +10,7 @@ PLIST_VARS+=		moon
 
 .if !empty(PKG_OPTIONS:Mmoonlight)
 PLIST.moon=	yes
+CONFIGURE_ARGS+=	--with-mcs-docs=yes
 .else
 CONFIGURE_ARGS+=	--with-moonlight=no
 .endif
