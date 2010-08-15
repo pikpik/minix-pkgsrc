a078f826eb70286dbea3d289b18f4627871d0c37
obache <obache>
2010-08-15 12:01:32 +0000

    Fixes variable name to detect builtin.

--- sysutils/dtpstree/options.mk
+++ sysutils/dtpstree/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1.1.1 2010/08/15 09:07:11 obache Exp $
 #
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dtpstree
@@ -7,7 +7,7 @@ PKG_SUPPORTED_OPTIONS=	terminfo
 CHECK_BUILTIN.terminfo:=	yes
 .include "../../mk/terminfo.builtin.mk"
 CHECK_BUILTIN.terminfo:=	no
-.if !empty(CHECK_BUILTIN.terminfo:M[yY][eE][sS])
+.if !empty(USE_BUILTIN.terminfo:M[yY][eE][sS])
 PKG_SUGGESTED_OPTIONS=	terminfo
 .endif
 
