d10dd62fc490eaa4d181f1720ca169c9336640d3
gls <gls>
2010-08-25 02:17:27 +0000

    options.mk was left aside the previous commit

---
+++ textproc/asciidoc/options.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.asciidoc
+PKG_SUPPORTED_OPTIONS=	pdf epub
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mpdf)
+DEPENDS+=	dblatex-[0-9]*:../../textproc/dblatex
+.endif
+.if !empty(PKG_OPTIONS:Mepub)
+DEPENDS+=	libxml2-[0-9]*:../../textproc/libxml2
+DEPENDS+=	libxslt-[0-9]*:../../textproc/libxslt
+.endif
