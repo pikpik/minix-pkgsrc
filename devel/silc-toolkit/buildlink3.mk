95867d5b2a1284563a32a195da8891c4ee8ea877
adam <adam>
2010-08-30 11:17:16 +0000

    Changes 1.1.10 (toolkit): * More string format fixes in silcd and client libary * configure: changed AC_PROG_LIBTOOL order to fix disabling shared libs * configure: check threads support in OpenBSD * Fixed string format vulnerability in client entry handling * Reported and patch provided by William Cummings * silcd: Fixed IDENTIFY command reply handling for channels
    Changes 1.1.18 (server):
    * silcd: Added heartbeat support
    * Added support for sending SILC_PACKET_HEARTBEAT packets to connection,
      to make sure they keep alive and to detect if they have died
    * Set SO_KEEPALIVE for all accept()ed sockets
    * silcd: Fixed SIGUSR1 signal handling
    * Fixed the SIGUSR1 signal handling which can be used to dump the server
      internals to /tmp.
    * Changed also End of Stream handling to handle NULL idata pointer instead
      of ignoring the EOS in case it is NULL.
    * Changed also the DETACH timeout handling to use the packet stream
      directly instead of looking up client in the callback
    * More string format fixes in silcd and client libary

--- devel/silc-toolkit/buildlink3.mk
+++ devel/silc-toolkit/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/10/25 01:40:36 dmcmahill Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:24:29 joerg Exp $
 #
 
 BUILDLINK_TREE+=	silc-toolkit
@@ -6,8 +6,8 @@ BUILDLINK_TREE+=	silc-toolkit
 .if !defined(SILC_TOOLKIT_BUILDLINK3_MK)
 SILC_TOOLKIT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.silc-toolkit+=	silc-toolkit>=0.9.12
-BUILDLINK_ABI_DEPENDS.silc-toolkit+=	silc-toolkit>=0.9.12nb1
+BUILDLINK_API_DEPENDS.silc-toolkit+=	silc-toolkit>=1.1.10
+BUILDLINK_ABI_DEPENDS.silc-toolkit+=	silc-toolkit>=1.1.10
 BUILDLINK_PKGSRCDIR.silc-toolkit?=	../../devel/silc-toolkit
 
 .include "../../devel/libgetopt/buildlink3.mk"
