5155ea0220109fd2a2a9cd40a17f9ab8ec41b4e4
obache <obache>
2010-08-31 08:25:20 +0000

    Import input-pad-0.1.1 as inputmethod/input-pad.
    The input pad is a tool to send a character on button to text applications.

---
+++ inputmethod/input-pad/options.mk
@@ -0,0 +1,28 @@
+# $NetBSD$
+#
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.input-pad
+PKG_SUPPORTED_OPTIONS=	eekboard xtest
+PKG_SUGGESTED_OPTIONS=	eekboard
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	eek python xtest
+
+#
+# Draw keyboard using libeek
+#
+.if !empty(PKG_OPTIONS:Meek)
+.include "../../x11/eekboard/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-eek
+PLIST.eek=		yes
+.endif
+
+#
+# Send keyboard event with xtest
+#
+.if !empty(PKG_OPTIONS:Mxtest)
+.include "../../x11/libXtst/buildlink3.mk"
+CONFIGURE_ARGS+=	--enable-xtest=yes
+PLIST.xtest=		yes
+.endif
