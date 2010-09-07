4901babc8e6ca6d0f7e469a00c8dcec44ad17cc5
adam <adam>
2010-09-07 19:04:15 +0000

    Changes 2.4.18:
    GMime 2.4 has had a number of API changes since GMime 2.2. To start, ALL public
    APIs that used to use off_t in GMime 2.2 now use gint64 so that the API and ABI
    do not change based on whether or not large file support is enabled.
    
    In addition, all of the functions marked as deprecated in 2.0 and 2.2 were
    removed (usually they had equivalent functionality in a parent class).
    
    Many functions have also been renamed for better clarity and/or consistency.
    For convenience, GMime 2.4 source packages include a shell-script to aid in
    porting applications using GMime 2.2 (should work for most GMime 2.0
    applications as well) to the 2.4 API. You can find this script under the
    tools/ directory, named `gmime-port-2-2-to-2-4.sh'.
    
    This script won't fix everything, but it should help quite a bit.

--- mail/gmime/buildlink3.mk
+++ mail/gmime/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.11 2006/07/08 23:10:57 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.12 2009/03/20 19:24:54 joerg Exp $
 
 BUILDLINK_TREE+=	gmime
 
 .if !defined(GMIME_BUILDLINK3_MK)
 GMIME_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.1.7
+BUILDLINK_API_DEPENDS.gmime+=	gmime>=2.4.0
 BUILDLINK_PKGSRCDIR.gmime?=	../../mail/gmime
 
 .include "../../devel/glib2/buildlink3.mk"
