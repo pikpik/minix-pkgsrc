5923c03bb057a60b85e41e11d1a648ebeff8ecfb
obache <obache>
2010-09-11 08:26:04 +0000

    Update tinycdb to 0.77.
    pkgsrc changes:
    * simplify subst with SUBST_VARS.
    * build and install shared library.
    * install pkgconfig file.
    
    package changes:
    
    tinycdb-0.77
    
     - bugfix release: manpage typos, portability fixes and the like
    
     - bugfix: improper logic in EINTR handling in _cdb_make_full_write
       routine which may lead to corruped .cdb file.

--- databases/tinycdb/buildlink3.mk
+++ databases/tinycdb/buildlink3.mk
@@ -1,12 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.5 2006/07/08 23:10:40 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:24:08 joerg Exp $
 
 BUILDLINK_TREE+=	tinycdb
 
 .if !defined(TINYCDB_BUILDLINK3_MK)
 TINYCDB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.tinycdb+=	tinycdb>=0.75
-BUILDLINK_DEPMETHOD.tinycdb?=	build
+BUILDLINK_API_DEPENDS.tinycdb+=	tinycdb>=0.77
 BUILDLINK_PKGSRCDIR.tinycdb?=	../../databases/tinycdb
 .endif # TINYCDB_BUILDLINK3_MK
 
