707ce5948a0499c092e7b93846b384a62e8e8f72
tron <tron>
2010-09-14 19:07:00 +0000

    Set the required API version to 0.18.4 as well to enforce correct build time dependences.

--- x11/pixman/buildlink3.mk
+++ x11/pixman/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:25:48 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2010/09/14 11:03:10 wiz Exp $
 
 BUILDLINK_TREE+=	pixman
 
 .if !defined(PIXMAN_BUILDLINK3_MK)
 PIXMAN_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pixman+=	pixman>=0.9.6
+BUILDLINK_API_DEPENDS.pixman+=	pixman>=0.18.4
 BUILDLINK_ABI_DEPENDS.pixman+=	pixman>=0.18.4
 BUILDLINK_PKGSRCDIR.pixman?=	../../x11/pixman
 .endif # PIXMAN_BUILDLINK3_MK
