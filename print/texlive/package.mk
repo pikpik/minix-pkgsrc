f327c5bec5ede6a4c59943804b2f029344b710d4
minskim <minskim>
2010-09-24 16:04:19 +0000

    Set MASTER_SITES for TeX live packages to ${MASTER_SITE_BACKUP}, because distfiles on CTAN change frequently.

--- print/texlive/package.mk
+++ print/texlive/package.mk
@@ -31,7 +31,11 @@
 #	should not be installed.
 
 CATEGORIES?=	print
+.if empty(TEXLIVE_USE_CTAN)
+MASTER_SITES?=	${MASTER_SITE_BACKUP}
+.else
 MASTER_SITES?=	${MASTER_SITE_TEX_CTAN:=systems/texlive/tlnet/archive/}
+.endif
 .if empty(TEXLIVE_REV)
 DIST_SUBDIR?=	${PKGNAME_NOREV}
 .else
