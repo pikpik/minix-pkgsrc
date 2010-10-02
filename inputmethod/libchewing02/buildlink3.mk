a8bc054185303fdb1a3ee1c885c1205c0120ef4c
obache <obache>
2010-10-02 08:39:33 +0000

    Replace dependency patterns with one liner.

--- inputmethod/libchewing02/buildlink3.mk
+++ inputmethod/libchewing02/buildlink3.mk
@@ -1,14 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.6 2009/03/20 19:24:46 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2009/11/24 11:18:53 obache Exp $
 
 BUILDLINK_TREE+=	libchewing02
 
 .if !defined(LIBCHEWING02_BUILDLINK3_MK)
 LIBCHEWING02_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libchewing02+=	libchewing>=0.2.7
-BUILDLINK_API_DEPENDS.libchewing02+=	libchewing<0.3.0
-BUILDLINK_ABI_DEPENDS.libchewing02+=	libchewing>=0.2.7
-BUILDLINK_ABI_DEPENDS.libchewing02+=	libchewing<0.3.0
+BUILDLINK_API_DEPENDS.libchewing02+=	libchewing>=0.2.7<0.3.0
+BUILDLINK_ABI_DEPENDS.libchewing02+=	libchewing>=0.2.7<0.3.0
 BUILDLINK_PKGSRCDIR.libchewing02?=	../../inputmethod/libchewing02
 .endif # LIBCHEWING02_BUILDLINK3_MK
 
