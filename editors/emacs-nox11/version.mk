b3af5f3f6b45e13a280c60d00f9dcd956fb3003a
joerg <joerg>
2010-10-12 13:22:46 +0000

    Be consistent and provide upper limits for all EMACS_REQD cases.

--- editors/emacs-nox11/version.mk
+++ editors/emacs-nox11/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD: version.mk,v 1.2 2008/10/11 09:56:38 uebayasi Exp $
+# $NetBSD: version.mk,v 1.3 2009/08/05 22:04:51 minskim Exp $
 
 _EMACS_FLAVOR=	emacs
-_EMACS_REQD=	emacs-nox11>=23
+_EMACS_REQD=	emacs-nox11>=23<24
 
 _EMACS_VERSION_MAJOR=	23
 _EMACS_VERSION_MINOR=	1
