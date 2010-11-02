d7ec23aff302bc7124d736657c9023efc5ad0550
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2010-11-01 18:47:46 -0700

    Fixup fromcvs/togit conversion

--- www/apache22/buildlink3.mk
+++ www/apache22/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2009/03/20 19:25:35 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2010/01/17 12:02:48 wiz Exp $
 
 BUILDLINK_TREE+=	apache
 
@@ -9,16 +9,16 @@ BUILDLINK_API_DEPENDS.apache+=	apache>=2.2.3
 BUILDLINK_ABI_DEPENDS.apache?=	apache>=2.2.14nb1
 BUILDLINK_PKGSRCDIR.apache?=	../../www/apache22
 BUILDLINK_DEPMETHOD.apache?=	build
-.  if defined(APACHE_MODULE)
+.if defined(APACHE_MODULE)
 BUILDLINK_DEPMETHOD.apache+=	full
-.  endif
+.endif
 USE_TOOLS+=	perl			# for "apxs"
 CONFIGURE_ENV+=	APR_LIBTOOL=${LIBTOOL:Q}	# make apxs use the libtool we specify
 MAKE_ENV+=	APR_LIBTOOL=${LIBTOOL:Q}
 APXS?=		${BUILDLINK_PREFIX.apache}/sbin/apxs
-.  if defined(GNU_CONFIGURE)
+.if defined(GNU_CONFIGURE)
 CONFIGURE_ARGS+=	--with-apxs2=${APXS:Q}
-.  endif
+.endif
 
 .include "../../devel/apr/buildlink3.mk"
 .include "../../devel/apr-util/buildlink3.mk"
