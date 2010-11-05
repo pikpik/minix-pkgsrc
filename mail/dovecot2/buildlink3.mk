ec303d482c3c6610676269039ad6bf0b14fc0524
adam <adam>
2010-11-05 12:00:39 +0000

    Dovecot is an open source IMAP and POP3 server for Linux/UNIX-like systems, written with security primarily in mind. Dovecot is an excellent choice for both small and large installations. It's fast, simple to set up, requires no special administration and it uses very little memory.

---
+++ mail/dovecot2/buildlink3.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+BUILDLINK_TREE+=	dovecot
+
+.if !defined(DOVECOT_BUILDLINK3_MK)
+DOVECOT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.dovecot+=		dovecot>=2.0.6
+BUILDLINK_PKGSRCDIR.dovecot?=		../../mail/dovecot2
+
+pkgbase:=	dovecot
+.include "../../mk/pkg-build-options.mk"
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.endif # DOVECOT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-dovecot
