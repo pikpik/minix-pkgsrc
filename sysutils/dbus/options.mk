1cd019f08b6fbf27fb2d90a5f7ccd53874f55fe6
shattered <shattered>
2010-11-07 15:24:50 +0000

    kqueue is BSD-specific; add it to PKG_SUPPORTED_OPTIONS only on *BSD.

--- sysutils/dbus/options.mk
+++ sysutils/dbus/options.mk
@@ -1,12 +1,13 @@
-# $NetBSD: options.mk,v 1.2 2008/06/19 01:56:44 bjs Exp $
+# $NetBSD: options.mk,v 1.3 2008/12/21 12:09:42 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.dbus
-PKG_SUPPORTED_OPTIONS+=	debug kqueue x11
+PKG_SUPPORTED_OPTIONS+=	debug x11
 PKG_SUGGESTED_OPTIONS=	x11
 
 .if (${OPSYS} == "NetBSD"  ||	\
      ${OPSYS} == "FreeBSD" ||	\
      ${OPSYS} == "OpenBSD")
+PKG_SUPPORTED_OPTIONS+=	kqueue
 PKG_SUGGESTED_OPTIONS+=	kqueue
 .endif
 
