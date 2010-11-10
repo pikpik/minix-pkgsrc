c01c69f5071d690712f9ed440394bbfeeba99215
obache <obache>
2010-11-10 04:47:59 +0000

    Some ABI incompatibilites was introduced at Ruby/GLib2 0.90.3. (macro changes, need to recompile package using it) Bump BUILDLINK_ABI_DEPENDS and bump PKGREVISION of two packages using it (other packages depending on Ruby/GLib2 are part of Ruby/Gnome2 and  already depending on the version).

--- devel/ruby-gnome2-glib/buildlink3.mk
+++ devel/ruby-gnome2-glib/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2009/01/19 04:01:52 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2009/03/20 19:24:29 joerg Exp $
 
 BUILDLINK_TREE+=	ruby-gnome2-glib
 
@@ -6,6 +6,7 @@ BUILDLINK_TREE+=	ruby-gnome2-glib
 RUBY_GNOME2_GLIB_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ruby-gnome2-glib+=	${RUBY_PKGPREFIX}-gnome2-glib>=0.17.0
+BUILDLINK_ABI_DEPENDS.ruby-gnome2-glib+=	${RUBY_PKGPREFIX}-gnome2-glib>=0.90.3
 BUILDLINK_PKGSRCDIR.ruby-gnome2-glib?=		../../devel/ruby-gnome2-glib
 
 .include "../../devel/glib2/buildlink3.mk"
