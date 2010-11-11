1c689c2fa970fec415fe81b72cff3f2cd657c443
adam <adam>
2010-11-11 20:54:40 +0000

    Changes 1.2.9: * Restore binary compatibility again, 1.2.8 broke it
    Changes 1.2.8:
    * Fix bug 10025: Segfault trying to parse certain word documents
    * Fix bug 12746: Tables not imported from MS Word documents
    * Fix Bug 11433 wvWare hangs on this seemingly blank word doc
    * Buildfix: Remove unused LINK_WIN32_DLL check
    * Buildfix: link with -no-undefined
    
    Changes 1.2.7:
    * Bug 12353: Regression: newlines not imported anymore
    * Cross building improvements
    * Fix build issue: 'libtool: link: `-::-' is not valid version information'
    * Bug 12353: configure.ac in 1.2.6 release says 1.2.5
    
    Changes 1.2.6:
    Important:
     * Restore the soversion major to the value of 1.2.4 because ABI didn't change.
    Fixes:
    * Bug 11547: unportable test construct.
    * Fix typos in wvWare's help message.
    * Fix a few mis-matched wvMalloc/malloc/wvFree/free combinations. Use glib to
      allocate and free all of our memory.
    * Bug 9355: a NULL pointer was causing a crash.
    * Bug 9705: Default font alignment in paragraph was incorrect.
    * Bug 10125: Crash in the PAP handling.
    * Fix a memory leak in wvRTF.
    * Bug 12034: Fix a huge performance issue while loading images.
    * Fix invalid writes in wvConfig.c

--- converters/wv/buildlink3.mk
+++ converters/wv/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.20 2010/01/18 09:58:42 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.21 2010/06/13 22:44:00 wiz Exp $
 
 BUILDLINK_TREE+=	wv
 
 .if !defined(WV_BUILDLINK3_MK)
 WV_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.wv+=		wv>=1.0.0
+BUILDLINK_API_DEPENDS.wv+=	wv>=1.0.0
 BUILDLINK_ABI_DEPENDS.wv+=	wv>=1.2.5nb3
 BUILDLINK_PKGSRCDIR.wv?=	../../converters/wv
 
