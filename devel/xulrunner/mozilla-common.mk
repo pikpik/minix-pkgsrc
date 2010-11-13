64eadd033d9e8b1d58c5f33e3e6e0df84227261c
tnn <tnn>
2010-11-13 11:54:44 +0000

    Reluctantly switch over to bundled cairo and pixman for mozilla packages, like other distros have recently done. Bump package revisions.
    Background:
    The cairo-1.10 update caused multiple regressions in firefox, such as
    flickering gif animations and crashes.
    Mozilla doesn't seem interested in fixing it on the stable branches:
      https://bugzilla.mozilla.org/show_bug.cgi?id=610107
    Other references:
      https://bugzilla.redhat.com/show_bug.cgi?id=628331
      http://bugs.gentoo.org/show_bug.cgi?id=337813
      https://bugzilla.mozilla.org/show_bug.cgi?id=597174
    
    This workaround is guaranteed to cause other problems in the long run;
    so we should attempt to switch back when we move to the mozilla-2.0 branch.

--- devel/xulrunner/mozilla-common.mk
+++ devel/xulrunner/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.16 2010/09/09 09:41:21 tnn Exp $
+# $NetBSD: mozilla-common.mk,v 1.17 2010/11/03 01:22:36 joerg Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 1.9.1.
 # 
@@ -25,7 +25,7 @@ CONFIGURE_ARGS+=	--enable-optimize=-O2 --with-pthreads
 CONFIGURE_ARGS+=	--disable-javaxpcom
 CONFIGURE_ARGS+=	--enable-default-toolkit=cairo-gtk2
 CONFIGURE_ARGS+=	--enable-svg --enable-mathml
-CONFIGURE_ARGS+=	--enable-system-cairo
+CONFIGURE_ARGS+=	--disable-system-cairo
 CONFIGURE_ARGS+=	--with-system-jpeg
 CONFIGURE_ARGS+=	--with-system-zlib --with-system-bz2
 CONFIGURE_ARGS+=	--enable-system-sqlite
