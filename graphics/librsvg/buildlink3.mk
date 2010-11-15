a191eda7ac21f5d79ccbe366f5456fd569c523fa
drochner <drochner>
2010-11-15 18:31:01 +0000

    update to 2.32.1 This switches to the gnome-2.32 release branch. pkgsrc change: don't build command line tools and UI theme (can be built as seperate pkg), remove gtk2 dependency

--- graphics/librsvg/buildlink3.mk
+++ graphics/librsvg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.17 2010/06/13 22:44:38 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.18 2010/09/14 11:00:59 wiz Exp $
 
 BUILDLINK_TREE+=	librsvg
 
@@ -9,9 +9,8 @@ BUILDLINK_API_DEPENDS.librsvg+=		librsvg>=2.12.6nb1
 BUILDLINK_ABI_DEPENDS.librsvg+=		librsvg>=2.26.3nb2
 BUILDLINK_PKGSRCDIR.librsvg?=		../../graphics/librsvg
 
-.include "../../devel/libgsf/buildlink3.mk"
 .include "../../textproc/libcroco/buildlink3.mk"
-.include "../../x11/gtk2/buildlink3.mk"
+.include "../../graphics/gdk-pixbuf2/buildlink3.mk"
 .endif # LIBRSVG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-librsvg
