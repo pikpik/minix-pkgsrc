461c0653337bbe1c57ec326a1c46d5c279d57651
dholland <dholland>
2010-11-17 15:46:21 +0000

    Update graphviz to 2.26.3.
    pkgsrc changes: fix a bit of pkglint. Todo: add an option to enable smyrna.
    Upstream changelog:
    
    January 26, 2010
            - Release 2.26.3
            - libcgraph.so   version bumped from 4 to 5 due to API changes
            - Allow ranksep to specify multiple radial differences in twopi
            - Allow the user to specify all pairwise distances in neato with
            - Fixed bugs: 1280, 1409, 1567, 1583, 1624, 1631, 1655, 1708, 1709,
                1727, 1784, 1792, 1798, 1800, 1813, 1814, 1830, 1831, 1833, 1836,
                1839
    
    December 10, 2009
            - Release 2.26.0
            - Core features:
                - added: "smyrna" - a new opengl-based viewer for large graphs
                - added: rudimentary "gml2gv", "gv2gml" converters
                - extended support for various image formats in node images
                - removed vestiges of codegens, now all putput formats supported
                  through plugins.  Dropped some output formats for which
                  plugins have not been developed: -Tdia, -Tmif
                - gvpr converted to a library; additional array handling and
                  text processing functions added; language extended to allow
                  multiple BEG_G/N/E blocks.
                - allow background images specified via xdot
            - Fixed bugs:
                1683, 1713, 1718, 1720, 1738, 1747, 1759, 1770, 1776, 1786,
                1799, 1816, 1827

--- graphics/graphviz/buildlink3.mk
+++ graphics/graphviz/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2009/03/06 07:36:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.17 2009/03/20 19:24:40 joerg Exp $
 
 BUILDLINK_TREE+=	graphviz
 
 .if !defined(GRAPHVIZ_BUILDLINK3_MK)
 GRAPHVIZ_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=1.12
-BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.6nb5
+BUILDLINK_API_DEPENDS.graphviz+=	graphviz>=2.26.3
+BUILDLINK_ABI_DEPENDS.graphviz+=	graphviz>=2.26.3
 BUILDLINK_PKGSRCDIR.graphviz?=	../../graphics/graphviz
 
 .include "../../converters/libiconv/buildlink3.mk"
