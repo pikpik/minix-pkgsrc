485585b85055181ca62480d10e294f2579d2a9fb
drochner <drochner>
2010-11-24 18:19:26 +0000

    add clutter-mx-1.0.4, a widget set originating from Moblin (it can be used optionally by Gnome's "empathy" IM client)

---
+++ graphics/clutter-mx/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	clutter-mx
+
+.if !defined(CLUTTER_MX_BUILDLINK3_MK)
+CLUTTER_MX_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.clutter-mx+=	clutter-mx>=1.0.3
+BUILDLINK_PKGSRCDIR.clutter-mx?=	../../graphics/clutter-mx
+
+.include "../../graphics/clutter/buildlink3.mk"
+.include "../../x11/libXrandr/buildlink3.mk"
+.include "../../x11/gtk2/buildlink3.mk"
+.endif	# CLUTTER_MX_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-clutter-mx
