c891b3187a12433e1b0e4e6e4a3cb93d2017bbda
wiz <wiz>
2010-11-25 10:41:17 +0000

    Remove libgsf here as well.

--- converters/libwpd/buildlink3.mk
+++ converters/libwpd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.13 2009/03/20 19:24:03 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.14 2010/11/25 10:27:49 wiz Exp $
 
 BUILDLINK_TREE+=	libwpd
 
@@ -8,7 +8,6 @@ LIBWPD_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libwpd+=	libwpd>=0.9.0
 BUILDLINK_PKGSRCDIR.libwpd?=	../../converters/libwpd
 
-.include "../../devel/libgsf/buildlink3.mk"
 .endif # LIBWPD_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libwpd
