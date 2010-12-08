41cddea2e4554e632bba32070972377ec74bcbba
abs <abs>
2010-12-08 00:42:16 +0000

    use fixesproto4, not the "too new" fixesproto. May address PR/44120

--- x11/Xfixes/buildlink3.mk
+++ x11/Xfixes/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2008/01/07 13:59:35 tron Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2009/03/20 19:25:39 joerg Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -12,7 +12,7 @@ BUILDLINK_ABI_DEPENDS.Xfixes+=	Xfixes>=2.0.1nb2
 BUILDLINK_PKGSRCDIR.Xfixes?=	../../x11/Xfixes
 
 .include "../../mk/x11.buildlink3.mk"
-.include "../../x11/fixesproto/buildlink3.mk"
+.include "../../x11/fixesproto4/buildlink3.mk"
 .endif # XFIXES_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-Xfixes
