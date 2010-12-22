0e5333ca28b4981f594b23aff8b4bb41f51f9912
abs <abs>
2010-12-22 23:41:30 +0000

    Re-implement check for older native X11 systems to use renderproto9.
    (For making the unreasonable assumption that BUILTIN_X11_TYPE.native on
    a native NetBSD xorg system would be "xorg" I am awarded the Fail In
    Technology prize of... a full set of "Synergy on P/OS" install media.
    - - The correct answer was of course "unknown".)
    
    Also make the test a little more robust, if not more attractive.
    
    As further spice for this particular value equation; earlier this week I
    switched the only platform I had for which this was relevant (OS X 10.4)
    to modular X11, but I take solace from the fact this should make the lives
    of those building x11 apps from current pkgsrc on netbsd-4 no less painful
    than they were before...

--- x11/renderproto/buildlink3.mk
+++ x11/renderproto/buildlink3.mk
@@ -7,8 +7,20 @@ BUILDLINK_TREE+=	renderproto
 .if !defined(RENDERPROTO_BUILDLINK3_MK)
 RENDERPROTO_BUILDLINK3_MK:=
 
+.if ${X11_TYPE} == "native"
+. include "../../mk/x11.buildlink3.mk"
+. if ${BUILTIN_X11_TYPE.native} == "XFree86"
+RENDERPROTO_RENDERPROTO9=1
+. endif
+.endif
+
+.if defined(RENDERPROTO_RENDERPROTO9)
+BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
+BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto9
+.else
 BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
 BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto
+.endif
 
 BUILDLINK_DEPMETHOD.renderproto?=	build
 .endif # RENDERPROTO_BUILDLINK3_MK
