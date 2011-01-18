870e759ef13d4090f591e6ea391c456d13c605fc
repo-copy <repo-copy>
2011-01-18 20:57:16 +0000

    Repo copy files

---
+++ devel/glib2/schemas.mk
@@ -0,0 +1,23 @@
+# $NetBSD: modules.mk,v 1.1 2010/11/17 14:09:55 drochner Exp $
+#
+# This Makefile fragment is intended to be included by packages that install
+# GSettings schemas.  It takes care of rebuilding the corresponding databases
+# at (de)installation time.
+#
+
+.if !defined(GLIB_SCHEMAS_MK)
+GLIB_SCHEMAS_MK=	# defined
+
+GLIB_COMPILE_SCHEMAS=	${BUILDLINK_PREFIX.glib2}/bin/glib-compile-schemas
+
+FILES_SUBST+=		GLIB_SCHEMAS_DIR=${GLIB_SCHEMAS_DIR:Q}
+FILES_SUBST+=		GLIB_COMPILE_SCHEMAS=${GLIB_COMPILE_SCHEMAS:Q}
+GLIB_SCHEMAS_DIR=${BUILDLINK_PREFIX.glib2}/share/glib-2.0/schemas
+INSTALL_TEMPLATES+=	../../devel/glib2/files/schemas.tmpl
+DEINSTALL_TEMPLATES+=	../../devel/glib2/files/schemas.tmpl
+
+TOOLS_NOOP+=		glib-compile-schemas
+
+.include "../../devel/glib2/buildlink3.mk"
+
+.endif			# GLIB_SCHEMAS_MK
---
+++ www/librest/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	librest
+
+.if !defined(LIBREST_BUILDLINK3_MK)
+LIBREST_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.librest+=	librest>=0.6
+BUILDLINK_PKGSRCDIR.librest?=	../../www/librest
+
+.include "../../devel/glib2/buildlink3.mk"
+.include "../../net/libsoup24/buildlink3.mk"
+.include "../../textproc/libxml2/buildlink3.mk"
+.endif	# LIBREST_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-librest
