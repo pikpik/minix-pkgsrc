78124686bb5c8a6dea45e08825758646509875d6
Ben Gras <ben@minix3.org>
2011-01-31 03:07:46 +0000

    minor prettifications to pbulk.sh.

--- minix/pbulk.sh
+++ minix/pbulk.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 set -e
+
+# pkgsrc source tree, bootstrap subtree
+PKGSRC=/usr/pkgsrc
+BOOTSTRAP=$PKGSRC/bootstrap
+
+# New system prefix to install all results in
 PREFIX=/usr/pbulk
+
 VARBASE=/usr/pbulk/var
+
+# Compiling during bootstrap
 WORKDIR=/usr/tmp/pbulk-bootstrap
+
+# mk.conf pkgsrc read by these pkgsrc binaries
 MKCONF=$PREFIX/etc/mk.conf
+
+# Compiling packages
 WRKOBJDIR=/usr/tmp/pbulk-outer
-minixdir="`pwd`"
-MINIXMKCONF=$minixdir/mk.conf.minix
+
+# Extra minix mk.conf
+MINIXMKCONF=$PKGSRC/minix/mk.conf.minix
 
 if [ ! -f $MINIXMKCONF ]
-then	echo "$MINIXMKCONF not found. Please run me from the minix/ pkgsrc dir."
+then	echo "$MINIXMKCONF not found."
 	exit 1
 fi
 
@@ -23,8 +37,8 @@ do	if [ -d $d ]
 	fi
 done
 
-cd /usr/pkgsrc/bootstrap
-sh bootstrap --prefix=/usr/pbulk --varbase=/usr/pbulk/var --workdir=/usr/tmp/pbulk-bootstrap
+cd $BOOTSTRAP
+sh bootstrap --prefix=$PREFIX --varbase=$VARBASE --workdir=$WORKDIR
 
 if [ ! -f $MKCONF ]
 then	echo "$MKCONF not found. bootstrap should have generated this."
@@ -40,9 +54,9 @@ cat $MINIXMKCONF >>$MKCONF
 echo >>$MKCONF .endif
 
 echo "Ok. Now install the pbulk build tools in the new hierarchy. Type:"
-echo "# cd /usr/pkgsrc/pkgtools/pbulk"
+echo "# cd $PKGSRC/pkgtools/pbulk"
 echo "# $PREFIX/bin/bmake package"
 
 echo "When that is done, to make a binary kit, do:"
-echo "# cd /usr/pkgsrc/bootstrap"
-echo "# sh bootstrap --gzip-binary-kit /usr/pkgsrc/bootstrap/bootstrap.tgz"
+echo "# cd $BOOTSTRAP"
+echo "# sh bootstrap --gzip-binary-kit $BOOTSTRAP/bootstrap.tgz"
