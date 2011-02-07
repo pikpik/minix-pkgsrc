0b2c3322530a5ef45144f7c94266793dfa791a3a
wiz <wiz>
2011-02-07 10:33:27 +0000

    Add default binpkg site for DragonFly. From Justin Sherrill in PR 44526.

--- mk/install/bin-install.mk
+++ mk/install/bin-install.mk
@@ -1,4 +1,4 @@
-# $NetBSD: bin-install.mk,v 1.18 2008/02/20 09:58:04 wiz Exp $
+# $NetBSD: bin-install.mk,v 1.19 2010/04/18 21:48:39 sbd Exp $
 #
 
 # This file provides the following targets:
@@ -32,6 +32,9 @@
 .if ${OPSYS} == "NetBSD"
 BINPKG_SITES?= \
 	ftp://ftp.NetBSD.org/pub/pkgsrc/packages/NetBSD/$${arch}/$${rel}
+.elif ${OPSYS} == "DragonFly"
+BINPKG_SITES?= \
+	http://mirror-master.dragonflybsd.org/packages/$${arch}/DragonFly-$${rel}/stable
 .else
 BINPKG_SITES?=
 .endif
