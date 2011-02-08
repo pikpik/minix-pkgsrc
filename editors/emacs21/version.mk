60f294ba31a772c8d73734247937859af4441a8d
hiramatsu <hiramatsu>
2011-02-08 13:20:14 +0000

    Instead of extracting version string from PKGNAME, use ${_EMACS_VERSION_MAJOR} and ${_EMACS_VERSION_MINOR} to determine installed version of emacs.
    Also, set minimum version of _EMACS_REQD to the same as
    ${_EMACS_VERSION_MAJOR}.${_EMACS_VERSION_MINOR}. Otherwise, buildlink
    does not work for emacs lisp libraries.
    
    Should fix PR/42763.

--- editors/emacs21/version.mk
+++ editors/emacs21/version.mk
@@ -1,7 +1,7 @@
-# $NetBSD$
+# $NetBSD: version.mk,v 1.1 2008/10/11 09:31:56 uebayasi Exp $
 
 _EMACS_FLAVOR=	emacs
-_EMACS_REQD=	emacs>=21<22
+_EMACS_REQD=	emacs>=21.4<22
 _EMACS_PKGDEP.base=
 _EMACS_PKGDEP.leim=	leim>=21.${EMACS_VERSION_MINOR}<22:../../editors/leim21
 
