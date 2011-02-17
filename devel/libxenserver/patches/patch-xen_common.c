5fa126afa921bab0b1b7261e740035403e12970d
sborrill <sborrill>
2011-02-17 09:54:02 +0000

    SDK for Citrix XenServer 5.6 FP1
    Citrix XenServer includes a XML-RPC based API, providing programmatic access
    to the extensive set of XenServer management features and tools. The
    XenServer API can be called from a remote system as well as local to the
    XenServer host. Remote calls are generally made securely over HTTPS, using
    port 443.

---
+++ devel/libxenserver/patches/patch-xen_common.c
@@ -0,0 +1,20 @@
+$NetBSD$
+
+--- src/xen_common.c.orig	2011-02-11 12:59:37.000000000 +0000
++++ src/xen_common.c	2011-02-11 13:00:20.000000000 +0000
+@@ -1131,6 +1131,7 @@
+     default:
+         assert(false);
+     }
++    return 0;
+ }
+ 
+ 
+@@ -1638,6 +1639,7 @@
+     default:
+         assert(false);
+     }
++    return NULL;
+ }
+ 
+ 
