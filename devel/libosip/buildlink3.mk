1bd1882b201295cde030cc043ff2c0b19da27427
schwarz <schwarz>
2011-02-19 17:02:57 +0000

    Updated libosip to 3.5.0

--- devel/libosip/buildlink3.mk
+++ devel/libosip/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.9 2008/11/11 22:02:42 schwarz Exp $
+# $NetBSD: buildlink3.mk,v 1.10 2009/03/20 19:24:21 joerg Exp $
 
 BUILDLINK_TREE+=	libosip2
 
 .if !defined(LIBOSIP2_BUILDLINK3_MK)
 LIBOSIP2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libosip2+=	libosip2>=2.2.0
-BUILDLINK_ABI_DEPENDS.libosip2+=	libosip2>=3.2.0
+BUILDLINK_API_DEPENDS.libosip2+=	libosip2>=3.5.0
+BUILDLINK_ABI_DEPENDS.libosip2+=	libosip2>=3.5.0
 BUILDLINK_PKGSRCDIR.libosip2?=	../../devel/libosip
 .endif # LIBOSIP2_BUILDLINK3_MK
 
