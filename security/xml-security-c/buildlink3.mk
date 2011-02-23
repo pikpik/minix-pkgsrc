cdd7c3128568a5ce3c69958124e2f50773d84cf8
adam <adam>
2011-02-23 09:02:08 +0000

    Changes 1.6.0: * Fix: wrong namespace in encryption DigestMethod * Fix: RetrievalMethod handler * Fix: support for >1 CRL per KeyInfo * Fix: buffer initialization issue * Fix: vector index bug * Fix: stylesheet append bug * Fix: header guard in XPath transform header * Fix: string release crash * Fix: improper c14n of XSLT * Fix: setters for Reference Type/Id * Fix: skip comments in X509Certificate elements * Fix: more header guards * Fix: NSS verification of RSA broken * Expose algorithm URI on Signature and Reference objects * White/blacklisting of otherwise registered algorithms * Add selected XML Signature 1.1 KeyInfo extensions * Add elliptic curve keys and signatures via ECDSA * Support debugging of Reference/SignedInfo data * Clean up tests for SHA2 algorithms in OpenSSL * Updated autoconf script, added NSS support, removed pre-automake material * Add methods for Reference removal to DSIGSignature/DSIGSignedInfo classes

--- security/xml-security-c/buildlink3.mk
+++ security/xml-security-c/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.3 2010/01/17 12:02:45 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.4 2010/12/17 07:36:12 obache Exp $
 
 BUILDLINK_TREE+=	xml-security-c
 
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.xml-security-c+=	xml-security-c>=1.5.1nb3
 BUILDLINK_PKGSRCDIR.xml-security-c?=	../../security/xml-security-c
 
 .include "../../security/openssl/buildlink3.mk"
-.include "../../textproc/xalan-c/buildlink3.mk"
 .include "../../textproc/xerces-c/buildlink3.mk"
 .endif # XML_SECURITY_C_BUILDLINK3_MK
 
