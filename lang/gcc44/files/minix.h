3e960523022d2ff57f8ab5f4fda4e638a3691559
Ben Gras <ben@minix3.org>
2011-03-03 01:34:30 +0000

    elf-generating gcc

--- lang/gcc44/files/minix.h
+++ lang/gcc44/files/minix.h
@@ -1,42 +1,5 @@
-/* MINIX OS header file */
-
-#undef  TARGET_VERSION
-#define TARGET_VERSION fprintf (stderr, " (i386 MINIX)");
-
-#undef  TARGET_OS_CPP_BUILTINS
-#define TARGET_OS_CPP_BUILTINS()				\
-	do							\
-	{							\
-		builtin_define ("__minix");			\
-		builtin_define ("__i386");			\
-								\
-		builtin_define_with_int_value ("_EM_WSIZE", 4);	\
-		builtin_define_with_int_value ("_EM_PSIZE", 4);	\
-		builtin_define_with_int_value ("_EM_SSIZE", 2);	\
-		builtin_define_with_int_value ("_EM_LSIZE", 4);	\
-		builtin_define_with_int_value ("_EM_FSIZE", 4);	\
-		builtin_define_with_int_value ("_EM_DSIZE", 8);	\
-	} while ( 0 )
-
-#undef  LIB_SPEC
-#define LIB_SPEC \
-	"-lc"
-
-#undef  STANDARD_STARTFILE_PREFIX_1
-#define STANDARD_STARTFILE_PREFIX_1 "/usr/lib/"
-
-#undef  STANDARD_STARTFILE_PREFIX_2
-
-#undef  STARTFILE_SPEC
-#define STARTFILE_SPEC \
-	"crtso.o%s"
-
-#undef  ENDFILE_SPEC
-#define ENDFILE_SPEC \
-	"libend.a%s"
-
-/* Definitions for Unix assembler syntax for the Intel 80386.
-   Copyright (C) 1988, 1994, 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+/* Base configuration file for all FreeBSD targets.
+   Copyright (C) 1999, 2000, 2001 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
@@ -52,76 +15,57 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING.  If not, write to
-the Free Software Foundation, 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.  */
 
-/* This file defines the aspects of assembler syntax
-   that are the same for all the i386 Unix systems
-   (though they may differ in non-Unix systems).  */
+/* Common MINIX configuration. 
+   All FreeBSD architectures should include this file, which will specify
+   their commonalities.
+*/
 
-/* Define macro used to output shift-double opcodes when the shift
-   count is in %cl.  Some assemblers require %cl as an argument;
-   some don't.  This macro controls what to do: by default, don't
-   print %cl.  */
-#undef  SHIFT_DOUBLE_OMITS_COUNT
-#define SHIFT_DOUBLE_OMITS_COUNT 1
-
-/* Define the syntax of pseudo-ops, labels and comments.  */
+/* In case we need to know.  */
+#define USING_CONFIG_MINIX 1
 
-/* String containing the assembler's comment-starter.  */
+/* This defines which switch letters take arguments.  On MINIX, most of
+   the normal cases (defined in gcc.c) apply, and we also have -h* and
+   -z* options (for the linker) (coming from SVR4).
+   We also have -R (alias --rpath), no -z, --soname (-h), --assert etc.  */
 
-#undef  ASM_COMMENT_START
-#define ASM_COMMENT_START "/"
+#undef  SWITCH_TAKES_ARG
+#define SWITCH_TAKES_ARG(CHAR) (MINIX_SWITCH_TAKES_ARG(CHAR))
 
-/* Output to assembler file text saying following lines
-   may contain character constants, extra white space, comments, etc.  */
+#undef  WORD_SWITCH_TAKES_ARG
+#define WORD_SWITCH_TAKES_ARG(STR) (MINIX_WORD_SWITCH_TAKES_ARG(STR))
 
-#define ASM_APP_ON "/APP\n"
+#undef  TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS() MINIX_TARGET_OS_CPP_BUILTINS()
 
-/* Output to assembler file text saying following lines
-   no longer contain unusual constructs.  */
+#undef  CPP_SPEC
+#define CPP_SPEC MINIX_CPP_SPEC
 
-#define ASM_APP_OFF "/NO_APP\n"
+#undef  STARTFILE_SPEC
+#define STARTFILE_SPEC MINIX_STARTFILE_SPEC
 
-/* Output before read-only data.  */
+#undef  ENDFILE_SPEC
+#define ENDFILE_SPEC MINIX_ENDFILE_SPEC
 
-#define TEXT_SECTION_ASM_OP "\t.text"
-
-/* Output before writable (initialized) data.  */
-
-#define DATA_SECTION_ASM_OP "\t.data"
-
-/* Output before writable (uninitialized) data.  */
-
-#define BSS_SECTION_ASM_OP "\t.bss"
-
-/* Globalizing directive for a label.  */
-#define GLOBAL_ASM_OP ".globl "
-
-/* By default, target has a 80387, uses IEEE compatible arithmetic,
-   and returns float values in the 387.  */
-#undef  TARGET_SUBTARGET_DEFAULT
-#define TARGET_SUBTARGET_DEFAULT (MASK_IEEE_FP | MASK_80387 | MASK_FLOAT_RETURNS)
-
-#undef  LONG_DOUBLE_TYPE_SIZE
-#define LONG_DOUBLE_TYPE_SIZE 64
-
-/* The string value for __SIZE_TYPE__.  */
-
-#undef  SIZE_TYPE
-#define SIZE_TYPE "unsigned int"
+#undef  LIB_SPEC
+#define LIB_SPEC MINIX_LIB_SPEC
 
 
-/* This is how we tell the assembler that a symbol is weak.  */
+/************************[  Target stuff  ]***********************************/
 
-#undef ASM_WEAKEN_LABEL
-#define ASM_WEAKEN_LABEL(FILE,NAME)					\
-  do 									\
-    {									\
-      fputs ("\t" GLOBAL_ASM_OP "\t", FILE); assemble_name (FILE, NAME);		\
-      fputc ('\n', FILE);						\
-      fputs ("\t.weak\t", FILE); assemble_name (FILE, NAME);		\
-      fputc ('\n', FILE);						\
-    }									\
-  while (0)
+/* All MINIX Architectures support the ELF object file format.  */
+#undef  OBJECT_FORMAT_ELF
+#define OBJECT_FORMAT_ELF
 
+/* Don't assume anything about the header files.  */
+#undef  NO_IMPLICIT_EXTERN_C
+#define NO_IMPLICIT_EXTERN_C	1
+
+/* Code generation parameters.  */
+
+/* Use periods rather than dollar signs in special g++ assembler names.
+   This ensures the configuration knows our system correctly so we can link
+   with libraries compiled with the native cc.  */
+#undef NO_DOLLAR_IN_LABEL
