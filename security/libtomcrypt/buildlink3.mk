96e40eaa054d63f3570cb7cc5ff1b5652deba9fd
drochner <drochner>
2011-03-10 10:02:34 +0000

    build shared lib, bump PKGREV

--- security/libtomcrypt/buildlink3.mk
+++ security/libtomcrypt/buildlink3.mk
@@ -1,11 +1,9 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/11/27 06:11:57 agc Exp $
 
 BUILDLINK_TREE+=	libtomcrypt
 
 .if !defined(LIBTOMCRYPT_BUILDLINK3_MK)
 LIBTOMCRYPT_BUILDLINK3_MK:=
-
-BUILDLINK_DEPMETHOD.libtomcrypt?=	build
 
 BUILDLINK_API_DEPENDS.libtomcrypt+=	libtomcrypt>=1.17
 BUILDLINK_PKGSRCDIR.libtomcrypt?=	../../security/libtomcrypt
