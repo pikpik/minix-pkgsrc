485134fb999e170f159b2ea6076afcf822343374
gdt <gdt>
2011-03-30 19:05:22 +0000

    Bump ABI_DEPENDS due to shlib major change.

--- geography/gpsd/buildlink3.mk
+++ geography/gpsd/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:35 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/12/07 20:24:22 gdt Exp $
 
 BUILDLINK_TREE+=	gpsd
 
@@ -8,7 +8,7 @@ GPSD_BUILDLINK3_MK:=
 # 2.90 is the beginning of withdrawing of interfaces.  Plus
 # it's an ABI.
 BUILDLINK_API_DEPENDS.gpsd+=	gpsd>=2.90
-BUILDLINK_ABI_DEPENDS.gpsd?=	gpsd>=2.90
+BUILDLINK_ABI_DEPENDS.gpsd?=	gpsd>=2.96
 BUILDLINK_PKGSRCDIR.gpsd?=	../../geography/gpsd
 .endif # GPSD_BUILDLINK3_MK
 
