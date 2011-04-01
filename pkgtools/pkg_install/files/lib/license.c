dfca91352823ca6cfbfbbd9a62c0c845b335c34d
wiz <wiz>
2011-04-01 13:11:08 +0000

    Allow GNU GPL v1 by default.

--- pkgtools/pkg_install/files/lib/license.c
+++ pkgtools/pkg_install/files/lib/license.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: license.c,v 1.16 2011/02/06 18:54:50 schnoebe Exp $	*/
+/*	$NetBSD: license.c,v 1.17 2011/03/17 23:07:52 bad Exp $	*/
 
 /*-
  * Copyright (c) 2009 Joerg Sonnenberger <joerg@NetBSD.org>.
@@ -48,6 +48,7 @@
 const char *default_acceptable_licenses =
     "public-domain unlicense"
     "gnu-fdl-v1.1 gnu-fdl-v1.2 gnu-fdl-v1.3 "
+    "gnu-gpl-v1 "
     "gnu-gpl-v2 gnu-lgpl-v2 gnu-lgpl-v2.1 "
     "gnu-gpl-v3 gnu-lgpl-v3 "
     "original-bsd modified-bsd 2-clause-bsd "
