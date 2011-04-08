2af22a7559381fb536d2e538b85b7c101c3f7b9a
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2011-04-08 14:53:48 -0700

    Fixup fromcvs/togit conversion

--- net/rdesktop/options.mk
+++ net/rdesktop/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1 2008/06/08 16:29:28 tron Exp $
+# $NetBSD: options.mk,v 1.2 2008/09/12 16:00:56 sborrill Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.rdesktop
 PKG_SUPPORTED_OPTIONS+=	inet6 ssl
@@ -10,7 +10,7 @@ PKG_SUGGESTED_OPTIONS+=	inet6 ssl
 ### Support both IPv6 and IPv4 connections.
 ###
 .if !empty(PKG_OPTIONS:Minet6)
-CONFIGURE_ARGE+=	--with-ipv6
+CONFIGURE_ARGS+=	--with-ipv6
 .endif
 
 .if !empty(PKG_OPTIONS:Mssl)
