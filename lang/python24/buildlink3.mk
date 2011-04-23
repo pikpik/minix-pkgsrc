ee9c21cca255943ea6660647a8e47ff30fd95e9e
obache <obache>
2011-04-23 06:06:30 +0000

    remove tail enmty line.

--- lang/python24/buildlink3.mk
+++ lang/python24/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.12 2011/04/22 13:42:00 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.13 2011/04/23 06:04:37 obache Exp $
 
 BUILDLINK_TREE+=	python24
 
@@ -23,4 +23,3 @@ BUILDLINK_DEPMETHOD.python24?=	${BUILDLINK_DEPMETHOD.python}
 .endif # PYTHON24_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-python24
-
