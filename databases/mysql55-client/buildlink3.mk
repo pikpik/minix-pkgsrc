475cb4f655d9bb7503361c4e1894b829f4e17ac9
adam <adam>
2011-04-27 08:49:01 +0000

    Removed USE_LIBTOOL and BUILDLINK_LIBDIRS

--- databases/mysql55-client/buildlink3.mk
+++ databases/mysql55-client/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/03/04 16:27:18 taca Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/04/25 21:12:52 adam Exp $
 
 BUILDLINK_TREE+=	mysql-client
 
@@ -8,11 +8,10 @@ MYSQL_CLIENT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mysql-client+=	mysql-client>=5.5.10
 BUILDLINK_ABI_DEPENDS.mysql-client+=	mysql-client>=5.5.10
 BUILDLINK_PKGSRCDIR.mysql-client?=	../../databases/mysql55-client
-BUILDLINK_LIBDIRS.mysql-client?=	lib/mysql
 BUILDLINK_INCDIRS.mysql-client?=	include/mysql
 
-.include "../../security/openssl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
 .endif # MYSQL_CLIENT_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-mysql-client
