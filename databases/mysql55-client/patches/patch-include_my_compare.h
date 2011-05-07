94f8101d0049248cb8b933210274feca6739e4c6
adam <adam>
2011-05-07 04:03:08 +0000

    Changes 5.5.12: * Fixed bugs: Illegal mix of collations * Problem: comparison of a DATETIME sp variable and NOW() led to Illegal mix of   collations error when character_set_connection=utf8. * Error happened in Arg_comparator::set_compare_func(), because the first   argument was errouneously converted to utf8, while the second argument was   not. * Fix: separate agg_arg_charsets_for_comparison() into two functions:   - agg_arg_charsets_for_comparison() - for pure comparison, when we don't need     to return any string result and therefore don't need to convert arguments     to @@character_set_connection:           SELECT a = b;   - agg_arg_charsets_for_string_results_with_comparison() - when we need to     return a string result, but we also need to do comparison internally:           SELECT REPLACE(a,b,c)     If all arguments are numbers:           SELECT REPLACE(123,2,3) -> 133     we convert arguments to @@character_set_connection.

--- databases/mysql55-client/patches/patch-include_my_compare.h
+++ databases/mysql55-client/patches/patch-include_my_compare.h
@@ -1,10 +1,10 @@
-$NetBSD: patch-al,v 1.1.1.1 2010/03/04 16:27:18 taca Exp $
+$NetBSD$
 
 Adjust HA_MAX_KEY_LENGTH x4.
 
---- include/my_handler.h.orig	2010-02-04 11:37:06.000000000 +0000
-+++ include/my_handler.h
-@@ -41,7 +41,7 @@ extern "C" {
+--- include/my_compare.h.orig	2010-02-04 11:37:06.000000000 +0000
++++ include/my_compare.h
+@@ -39,7 +39,7 @@ extern "C" {
    But beware the dependency of MI_MAX_POSSIBLE_KEY_BUFF and HA_MAX_KEY_LENGTH.
  */
  
