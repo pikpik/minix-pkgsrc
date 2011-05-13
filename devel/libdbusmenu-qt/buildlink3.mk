c5c59fd3bd0924fe56c6089f2e877e50b4eb220f
markd <markd>
2011-05-13 21:21:24 +0000

    Import libdbusmenu-qt 0.8.2
    This library provides a Qt implementation of the DBusMenu protocol.
    
    The DBusMenu protocol makes it possible for applications to export and import
    their menus over DBus.

---
+++ devel/libdbusmenu-qt/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libdbusmenu-qt
+
+.if !defined(LIBDBUSMENU_QT_BUILDLINK3_MK)
+LIBDBUSMENU_QT_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libdbusmenu-qt+=	libdbusmenu-qt>=0.8.2
+BUILDLINK_PKGSRCDIR.libdbusmenu-qt?=	../../devel/libdbusmenu-qt
+
+.include "../../x11/qt4-libs/buildlink3.mk"
+.include "../../x11/qt4-qdbus/buildlink3.mk"
+.endif	# LIBDBUSMENU_QT_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libdbusmenu-qt
