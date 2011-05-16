aced604fe0976164dab1616358d642c6aa7f882c
obache <obache>
2011-05-16 06:18:13 +0000

    Since mono-2.8, --with-glib=system is not supported (embeded on will be used) Drop dependency on devel/glib2.
    Bump PKGREVISION.

--- lang/mono/buildlink3.mk
+++ lang/mono/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.38 2010/12/17 07:43:54 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.39 2011/04/22 13:42:28 obache Exp $
 
 BUILDLINK_TREE+=	mono
 
@@ -11,7 +11,6 @@ BUILDLINK_PKGSRCDIR.mono?=	../../lang/mono
 ALL_ENV+=			MONO_SHARED_DIR=${WRKDIR:Q}
 BUILDLINK_CONTENTS_FILTER.mono+=	${EGREP} '(^include/|^lib/)'
 
-.include "../../devel/glib2/buildlink3.mk"
 .include "../../textproc/icu/buildlink3.mk"
 .endif # MONO_BUILDLINK3_MK
 
