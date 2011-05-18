f9c87bf7375f4303cfc88db231f8bf56171e4fc8
drochner <drochner>
2011-05-18 10:11:47 +0000

    this pkg only installs binaries -- no need to propagate library dependencies

--- security/gnupg2/buildlink3.mk
+++ security/gnupg2/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2009/12/18 17:49:51 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2011/04/22 13:42:48 obache Exp $
 
 BUILDLINK_TREE+=	gnupg2
 
@@ -10,11 +10,6 @@ BUILDLINK_ABI_DEPENDS.gnupg2?=	gnupg2>=2.0.17nb1
 BUILDLINK_PKGSRCDIR.gnupg2?=	../../security/gnupg2
 BUILDLINK_PREFIX.gnupg2?=	${LOCALBASE}
 
-.include "../../security/libgpg-error/buildlink3.mk"
-.include "../../security/libgcrypt/buildlink3.mk"
-.include "../../security/libksba/buildlink3.mk"
-.include "../../security/pinentry/buildlink3.mk"
-.include "../../devel/zlib/buildlink3.mk"
 .endif # GNUPG2_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gnupg2
