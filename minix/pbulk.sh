04d3ea79398da4f6fde2948502e3d9170f4dee66
Thomas Cort <tcort@minix3.org>
2011-05-25 13:18:53 +0000

    Add command line options and general clean-up.
    Make the script more modular by splitting up the functionality
    into functions. Add command line arguments to call the different
    functions. Also do some of the TODO items.

--- minix/pbulk.sh
+++ minix/pbulk.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 #####
 ##
@@ -7,61 +6,150 @@ set -e
 ##
 #####
 
+# Stop execution when an error occurs
+set -e
+
 # Set output verbosity (0=quiet, 1=normal, 2=debug)
 PKG_DEBUG_LEVEL=2
 
-# TODO consider splitting this up into several scripts or adding
-# command line arguments to enable someone to run one specific part
-# (i.e. just bootstrap or just run a bulkbuild if a bootstrap has been done).
-
-# TODO add check for compilers and other things needed for the build
-
-# TODO double check that this list is complete and accurate
-dirs="/usr/pbulk /usr/pbulk_build /usr/pbulk-packages /usr/tmp/pbulk-bootstrap /usr/tmp/pbulk-outer /usr/tmp/pbulk_build-bootstrap /usr/tmp/pbulk_build-outer"
-for d in $dirs
-do      if [ -d $d ]
-        then    echo "$d exists."
-                echo "please remove all of $dirs."
-                echo "Then re-run me."
-                exit 1
-        fi
-done
+pbulksh_help() {
+	echo "Usage: "
+	echo "  $0 --bootstrap   Do the initial bootstrap"
+	echo "  $0 --backup      Backup /usr/pkg to a safe place"
+	echo "  $0 --bin-kit     Create a binary kit"
+	echo "  $0 --build       Execute bulkbuild"
+	echo "  $0 --restore     Restore /usr/pkg"
+	echo "  $0 --all         Do all steps mentioned above"
+	echo "  $0 --help        Display this message"
+}
 
 # Preparation -- bootstraps a pkgsrc installation into /usr/pbulk
-# Defining variables for these paths would be nice, but it pollutes the
-# environment and some of the later commands get confused.
-cd /usr/pkgsrc
-sh ./bootstrap/bootstrap --prefix=/usr/pbulk --varbase=/usr/pbulk/var --workdir=/usr/tmp/pbulk-bootstrap --pkgdbdir=/usr/pbulk/.pkgdb --mk-fragment=/usr/pkgsrc/minix/mk.conf.minix.pbulk
-rm -rf /usr/tmp/pbulk-bootstrap
+pbulksh_bootstrap() {
 
-# Install pbulk into /usr/pbulk
-cd /usr/pkgsrc/pkgtools/pbulk
-env PATH=/usr/pbulk/bin:/usr/pbulk/sbin:${PATH} bmake install package
+	dirs="/usr/pbulk /usr/pbulk-packages /usr/tmp/pbulk-bootstrap /usr/tmp/pbulk-outer /usr/pbulk-logs"
+	for d in $dirs
+	do      if [ -d $d ]
+	        then    echo "$d exists."
+	                echo "please remove all of $dirs."
+	                echo "Then re-run me."
+	                exit 1
+	        fi
+	done
+
+	cd /usr/pkgsrc
+
+	sh ./bootstrap/bootstrap --prefix=/usr/pbulk \
+			--varbase=/usr/pbulk/var \
+			--workdir=/usr/tmp/pbulk-bootstrap \
+			--pkgdbdir=/usr/pbulk/.pkgdb \
+			--mk-fragment=/usr/pkgsrc/minix/mk.conf.minix.pbulk
+
+	rm -rf /usr/tmp/pbulk-bootstrap
+
+	# Install pbulk into /usr/pbulk
+	cd /usr/pkgsrc/pkgtools/pbulk
+	env PATH=/usr/pbulk/bin:/usr/pbulk/sbin:${PATH} bmake install package
+	cp /usr/pkgsrc/minix/pbulk.conf /usr/pbulk/etc/pbulk.conf
+}
 
 # In order to build proper packages, we need to work in /usr/pkg.
 # Here, we back up /usr/pkg as well as VARBASE.
-mv /usr/pkg /usr/pkg.sav
-mkdir /usr/pkg
-mv /usr/var /usr/var.sav
-mkdir /usr/var
+pbulksh_backup() {
+
+	dirs="/usr/pkg.sav /usr/var.sav"
+	for d in $dirs
+	do      if [ -d $d ]
+	        then    echo "$d exists."
+	                echo "Did you already run --backup?"
+	                exit 1
+	        fi
+	done
+
+	mv /usr/pkg /usr/pkg.sav
+	mkdir /usr/pkg
+	mv /usr/var /usr/var.sav
+	mkdir /usr/var
+}
 
 # Create a bootstrap a binary kit. pbulk needs this.
-cd /usr/pkgsrc
-sh ./bootstrap/cleanup
-env PATH=/usr/pbulk/bin:/usr/pbulk/sbin:/usr/pkg.sav/bin:/usr/pkg.sav/sbin:${PATH} sh ./bootstrap/bootstrap \
-	--mk-fragment=/usr/pkgsrc/minix/mk.conf.minix \
-	--gzip-binary-kit=/usr/pkgsrc/bootstrap/bootstrap.tar.gz \
-	--varbase=/usr/var \
-	--pkgdbdir=/usr/var/db/pkg
+pbulksh_bin_kit() {
 
-rm -rf /usr/pbulk-packages
+	dirs="/usr/pkg.sav /usr/var.sav"
+	for d in $dirs
+	do      if [ ! -d $d ]
+	        then    echo "$d doesn't exist."
+	                echo "Re-run with --backup to back it up"
+	                exit 1
+	        fi
+	done
+
+	cd /usr/pkgsrc
+	rm -f /usr/pkgsrc/bootstrap/bootstrap.tar.gz
+	sh ./bootstrap/cleanup
+	env PATH=/usr/pbulk/bin:/usr/pbulk/sbin:/usr/pkg.sav/bin:/usr/pkg.sav/sbin:${PATH} sh ./bootstrap/bootstrap \
+		--mk-fragment=/usr/pkgsrc/minix/mk.conf.minix \
+		--gzip-binary-kit=/usr/pkgsrc/bootstrap/bootstrap.tar.gz \
+		--varbase=/usr/var \
+		--pkgdbdir=/usr/var/db/pkg
+
+	rm -rf /usr/pbulk-packages
+}
 
 # Perform the bulk build. Most configuration is in pbulk.conf
-cp /usr/pkgsrc/minix/pbulk.conf /usr/pbulk/etc/pbulk.conf
-env PATH=/usr/pkg.sav/bin:/usr/pkg.sav/sbin:${PATH} /usr/pbulk/bin/bulkbuild
+pbulksh_build() {
+
+
+	if [ ! -f /usr/pkgsrc/bootstrap/bootstrap.tar.gz ]
+	then
+		echo "/usr/pkgsrc/bootstrap/bootstrap.tar.gz doesn't exist"
+		echo "Re-run with --bin-kit to create it"
+		exit 1
+	fi
+
+	if [ ! -d /usr/pbulk ]
+	then
+		echo "/usr/pbulk doesn't exist"
+		echo "Re-run with --bootstrap to create it"
+		exit 1
+	fi
+
+	env PATH=/usr/pkg.sav/bin:/usr/pkg.sav/sbin:${PATH} /usr/pbulk/bin/bulkbuild
+}
 
 # Restore the backed up /usr/pkg and /usr/var
-rm -rf /usr/pkg
-mv /usr/pkg.sav /usr/pkg
-rm -rf /usr/var
-mv /usr/var.sav /usr/var
+pbulksh_restore() {
+
+	dirs="/usr/pkg.sav /usr/var.sav"
+	for d in $dirs
+	do      if [ ! -d $d ]
+	        then    echo "$d doesn't exist."
+	                echo "Can't restore without it."
+	                exit 1
+	        fi
+	done
+
+	rm -rf /usr/pkg
+	mv /usr/pkg.sav /usr/pkg
+	rm -rf /usr/var
+	mv /usr/var.sav /usr/var
+}
+
+pbulksh_all() {
+
+	pbulksh_bootstrap
+	pbulksh_backup
+	pbulksh_bin_kit
+	pbulksh_build
+	pbulksh_restore
+}
+
+case $1 in
+	"--bootstrap") pbulksh_bootstrap; break;;
+	"--backup") pbulksh_backup; break;;
+	"--bin-kit") pbulksh_bin_kit; break;;
+	"--build") pbulksh_build; break;;
+	"--restore") pbulksh_restore; break;;
+	"--all") pbulksh_all; break;;
+	"--help") pbulksh_help; break;;
+	*) echo pbulksh_help; break;;
+esac
