c6e492aeba6a8402376f61ac97ce6b4fbf650478
Thomas Cort <tcort@minix3.org>
2011-05-25 01:18:48 +0000

    Simple bulk building with pbulk.
    Update the pbulk.sh script and add some supporting files to enable
    bulk building. More work needs to be done, but this code meets the
    first milestone (bulk building misc/figlet).

--- minix/pbulk.sh
+++ minix/pbulk.sh
@@ -1,62 +1,67 @@
 #!/bin/sh
 set -e
 
-# pkgsrc source tree, bootstrap subtree
-PKGSRC=/usr/pkgsrc
-BOOTSTRAP=$PKGSRC/bootstrap
+#####
+##
+## Minix pBulk Bootstrap Script
+##
+#####
 
-# New system prefix to install all results in
-PREFIX=/usr/pbulk
+# Set output verbosity (0=quiet, 1=normal, 2=debug)
+PKG_DEBUG_LEVEL=2
 
-VARBASE=/usr/pbulk/var
+# TODO consider splitting this up into several scripts or adding
+# command line arguments to enable someone to run one specific part
+# (i.e. just bootstrap or just run a bulkbuild if a bootstrap has been done).
 
-# Compiling during bootstrap
-WORKDIR=/usr/tmp/pbulk-bootstrap
+# TODO add check for compilers and other things needed for the build
 
-# mk.conf pkgsrc read by these pkgsrc binaries
-MKCONF=$PREFIX/etc/mk.conf
-
-# Compiling packages
-WRKOBJDIR=/usr/tmp/pbulk-outer
-
-# Extra minix mk.conf
-MINIXMKCONF=$PKGSRC/minix/mk.conf.minix
-
-if [ ! -f $MINIXMKCONF ]
-then	echo "$MINIXMKCONF not found."
-	exit 1
-fi
-
-dirs="$PREFIX $VARBASE $WORKDIR $WRKOBJDIR"
+# TODO double check that this list is complete and accurate
+dirs="/usr/pbulk /usr/pbulk_build /usr/pbulk-packages /usr/tmp/pbulk-bootstrap /usr/tmp/pbulk-outer /usr/tmp/pbulk_build-bootstrap /usr/tmp/pbulk_build-outer"
 for d in $dirs
-do	if [ -d $d ]
-	then	echo "$d exists."
-		echo "please remove all of $dirs."
-		echo "Then re-run me."
-		exit 1
-	fi
+do      if [ -d $d ]
+        then    echo "$d exists."
+                echo "please remove all of $dirs."
+                echo "Then re-run me."
+                exit 1
+        fi
 done
 
-cd $BOOTSTRAP
-sh bootstrap --prefix=$PREFIX --varbase=$VARBASE --workdir=$WORKDIR
+# Preparation -- bootstraps a pkgsrc installation into /usr/pbulk
+# Defining variables for these paths would be nice, but it pollutes the
+# environment and some of the later commands get confused.
+cd /usr/pkgsrc
+sh ./bootstrap/bootstrap --prefix=/usr/pbulk --varbase=/usr/pbulk/var --workdir=/usr/tmp/pbulk-bootstrap --pkgdbdir=/usr/pbulk/.pkgdb --mk-fragment=/usr/pkgsrc/minix/mk.conf.minix.pbulk
+rm -rf /usr/tmp/pbulk-bootstrap
 
-if [ ! -f $MKCONF ]
-then	echo "$MKCONF not found. bootstrap should have generated this."
-	exit 1
-fi
+# Install pbulk into /usr/pbulk
+cd /usr/pkgsrc/pkgtools/pbulk
+env PATH=/usr/pbulk/bin:/usr/pbulk/sbin:${PATH} bmake install package
 
-echo Adding to $MKCONF
+# In order to build proper packages, we need to work in /usr/pkg.
+# Here, we back up /usr/pkg as well as VARBASE.
+mv /usr/pkg /usr/pkg.sav
+mkdir /usr/pkg
+mv /usr/var /usr/var.sav
+mkdir /usr/var
 
-echo "# These settings added by Minix pbulk.sh" >>$MKCONF
-echo >>$MKCONF .ifdef BSD_PKG_MK
-echo "WRKOBJDIR=$WRKOBJDIR" >>$MKCONF
-cat $MINIXMKCONF >>$MKCONF
-echo >>$MKCONF .endif
+# Create a bootstrap a binary kit. pbulk needs this.
+cd /usr/pkgsrc
+sh ./bootstrap/cleanup
+env PATH=/usr/pbulk/bin:/usr/pbulk/sbin:/usr/pkg.sav/bin:/usr/pkg.sav/sbin:${PATH} sh ./bootstrap/bootstrap \
+	--mk-fragment=/usr/pkgsrc/minix/mk.conf.minix \
+	--gzip-binary-kit=/usr/pkgsrc/bootstrap/bootstrap.tar.gz \
+	--varbase=/usr/var \
+	--pkgdbdir=/usr/var/db/pkg
 
-echo "Ok. Now install the pbulk build tools in the new hierarchy. Type:"
-echo "# cd $PKGSRC/pkgtools/pbulk"
-echo "# $PREFIX/bin/bmake package"
+rm -rf /usr/pbulk-packages
 
-echo "When that is done, to make a binary kit, do:"
-echo "# cd $BOOTSTRAP"
-echo "# sh bootstrap --gzip-binary-kit $BOOTSTRAP/bootstrap.tgz"
+# Perform the bulk build. Most configuration is in pbulk.conf
+cp /usr/pkgsrc/minix/pbulk.conf /usr/pbulk/etc/pbulk.conf
+env PATH=/usr/pkg.sav/bin:/usr/pkg.sav/sbin:${PATH} /usr/pbulk/bin/bulkbuild
+
+# Restore the backed up /usr/pkg and /usr/var
+rm -rf /usr/pkg
+mv /usr/pkg.sav /usr/pkg
+rm -rf /usr/var
+mv /usr/var.sav /usr/var
