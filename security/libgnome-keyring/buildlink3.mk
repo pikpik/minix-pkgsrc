6c0b8a7b5bc1eac3d5525ce24bfcb44d177ead9b
hans <hans>
2011-05-27 14:02:57 +0000

    libgnome-keyring 2.32.0 does not depend on eggdbus anymore, according to http://mail.gnome.org/archives/gnome-announce-list/2010-September/msg00101.html

--- security/libgnome-keyring/buildlink3.mk
+++ security/libgnome-keyring/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/06/01 10:56:27 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/04/22 13:42:19 obache Exp $
 
 BUILDLINK_TREE+=	libgnome-keyring
 
@@ -12,7 +12,6 @@ BUILDLINK_PKGSRCDIR.libgnome-keyring?=	../../security/libgnome-keyring
 .include "../../devel/glib2/buildlink3.mk"
 .include "../../security/libgcrypt/buildlink3.mk"
 .include "../../sysutils/dbus/buildlink3.mk"
-.include "../../sysutils/eggdbus/buildlink3.mk"
 .endif	# LIBGNOME_KEYRING_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libgnome-keyring
