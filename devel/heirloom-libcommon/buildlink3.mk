fe48cfcebd016657cbcd8b1d220cbe20f19cf218
cheusov <cheusov>
2011-05-28 08:45:35 +0000

    heirloom part

---
+++ devel/heirloom-libcommon/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	heirloom-libcommon
+
+.if !defined(HEIRLOOM_LIBCOMMON_BUILDLINK3_MK)
+HEIRLOOM_LIBCOMMON_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.heirloom-libcommon+=	heirloom-libcommon>=070227
+BUILDLINK_PKGSRCDIR.heirloom-libcommon?=	../../devel/heirloom-libcommon
+BUILDLINK_DEPMETHOD.heirloom-libcommon?=	build
+BUILDLINK_INCDIRS.heirloom-libcommon+=		include/heirloom
+.endif # HEIRLOOM_LIBCOMMON_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-heirloom-libcommon
