d89402cc2a032684e3a8956612079d77ca2cbd84
cheusov <cheusov>
2011-05-28 09:48:07 +0000

    Regexp library from heirloom collection aware of utf-8

---
+++ devel/libuxre/buildlink3.mk
@@ -0,0 +1,13 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libuxre
+
+.if !defined(LIBUXRE_BUILDLINK3_MK)
+LIBUXRE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libuxre+=	libuxre>=060122
+BUILDLINK_PKGSRCDIR.libuxre?=	../../devel/libuxre
+BUILDLINK_INCDIRS.libuxre+=	include/uxre
+.endif # LIBUXRE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libuxre
