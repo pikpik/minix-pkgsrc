24b0bc81b79d75bfc4760cb963ee269728a05227
shattered <shattered>
2011-06-11 18:49:31 +0000

    PR/39663: enable 256 color support (only with ncurses and PDCurses).

--- misc/screen/options.mk
+++ misc/screen/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2009/03/02 21:11:49 adrianp Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.screen
 
@@ -18,8 +18,12 @@ CURSES_DEFAULT=	curses
 
 .if !empty(PKG_OPTIONS:Mncurses)
 CURSES_DEFAULT=	ncurses
+CONFIGURE_ARGS+=	--enable-colors256
 .endif
 
 .if !empty(PKG_OPTIONS:Mpdcurses)
 CURSES_DEFAULT=	pdcurses
+CONFIGURE_ARGS+=	--enable-colors256
 .endif
+
+.include "../../mk/curses.buildlink3.mk"
