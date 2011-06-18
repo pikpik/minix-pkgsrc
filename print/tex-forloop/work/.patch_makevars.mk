5830117e3f5ae023338b528026d9cd65183df5b8
brook <brook>
2011-06-18 16:05:24 +0000

    Initial revision.

---
+++ print/tex-forloop/work/.patch_makevars.mk
@@ -0,0 +1,15 @@
+.if !defined(_MAKEVARS_MK)
+_MAKEVARS_MK=	defined
+
+KPATHSEA_PREFIX=	/usr/pkg
+TOOLS_PREFIX.digest=	/usr/pkg
+TOOLS_PREFIX.xzcat=	/usr/pkg
+_BLNK_PHYSICAL_PATH.LOCALBASE=	/usr/pkg
+_BLNK_PHYSICAL_PATH.WRKDIR=	/usr/pkgsrc/wip-local/tex-forloop/work
+_IGNORE_INFO_PATH=	
+_MANCOMPRESSED=	no
+_MANZ=	no
+_USE_TOOLS=	[ awk basename cat chgrp chmod chown cmp cp cut date diff digest dirname echo egrep env expr false file find ftp grep head hostname id install ln ls mkdir mv pax printf pwd readelf rm rmdir sed sh sort tail tar test touch tr true uname wc xargs xzcat
+_WRAP_PATH=	/usr/pkgsrc/wip-local/tex-forloop/work/.buildlink/bin:/usr/pkgsrc/wip-local/tex-forloop/work/.tools/bin:/usr/pkg/bin:/usr/pkg/bin:/usr/pkg/sbin:/usr/pkg/qt4/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11R7/bin:/usr/X11R6/bin:/usr/pkg/EMBOSS/bin
+
+.endif # _MAKEVARS_MK
