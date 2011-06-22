3e00f23eaddb31552f1d2d284d866b9f803f9134
Thomas Cort <tcort@minix3.org>
2011-06-22 17:48:25 +0000

    minix/pkgs_requiring_minix_libc.mk: add list of packages
    This is a list of packages that either haven't been tested with
    NetBSD libc yet or are known not to work with NetBSD libc.
    Initially this list includes everything from limited_list.pbulk
    with the exception of games/sex (that is the test case for
    the code that sets up compiling with NetBSD libc).

---
+++ minix/pkgs_requiring_minix_libc.mk
@@ -0,0 +1,32 @@
+PKGS_REQUIRING_MINIX_LIBC = \
+	archivers/9e archivers/bzip2 archivers/gtar-base archivers/gtar-info \
+	archivers/gtar archivers/gzip archivers/libarchive archivers/libzip \
+	archivers/pax archivers/unzip archivers/zip chat/naim \
+	converters/libiconv converters/p5-MIME-Base64 databases/db4 \
+	databases/sqlite3 devel/apr devel/apr-util devel/autoconf213 \
+	devel/autoconf devel/automake devel/automake14 devel/binutils \
+	devel/bison devel/bmake devel/cmake devel/diffutils devel/flex \
+	devel/gettext-lib devel/gettext-tools devel/gindent devel/gmake \
+	devel/gmp devel/gtexinfo devel/lcov devel/libiberty devel/libidn \
+	devel/libtool-base devel/m4 devel/ncurses devel/p5-enum \
+	devel/p5-Error devel/patch devel/pkg-config devel/pth \
+	devel/readline devel/scmcvs devel/scmgit-base devel/subversion-base \
+	devel/topgit editors/mg editors/nano editors/vilearn editors/vim \
+	editors/vim-share editors/zile games/bastet games/cmatrix \
+	games/doom1 games/dungeon games/gnugo games/ninvaders \
+	graphics/png ham/locator lang/f2c lang/gawk lang/gcc44 lang/lua \
+	lang/nawk lang/perl5 math/mpfr math/units minix/clang \
+	minix/compiler-rt minix/x11 misc/ascii misc/cowsay misc/figlet \
+	net/rsync pkgtools/bootstrap-mk-files pkgtools/digest \
+	pkgtools/libnbcompat pkgtools/osabi pkgtools/p5-pkgsrc-Dewey \
+	pkgtools/pbulk pkgtools/pkgdep pkgtools/pkgdepgraph \
+	pkgtools/pkgdiff pkgtools/pkgin pkgtools/pkg_install \
+	pkgtools/pkg_install-info pkgtools/pkg_leaves pkgtools/pkglint \
+	pkgtools/url2pkg pkgtools/x11-links security/bcrypt \
+	security/openssh security/openssl security/p5-Digest \
+	security/p5-Digest-SHA1 security/rats security/sudo shells/bash \
+	shells/bash2 shells/pdksh shells/zsh sysutils/bchunk sysutils/cpuid \
+	sysutils/mdf2iso sysutils/tree sysutils/wtail textproc/expat \
+	textproc/grep textproc/groff textproc/gsed textproc/wdiff \
+	textproc/xmlrpc-c wm/fluxbox www/curl www/libwww www/lynx \
+	www/neon www/urlget x11/inputproto 
