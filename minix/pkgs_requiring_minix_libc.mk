088d21554dd44170b11d2a9a680aebbf9e5648ac
Ben Gras <ben@minix3.org>
2011-06-28 14:33:13 +0200

    mark 20 packages as not needing minix libc
      . they build without changes

--- minix/pkgs_requiring_minix_libc.mk
+++ minix/pkgs_requiring_minix_libc.mk
@@ -2,7 +2,6 @@ PKGS_REQUIRING_MINIX_LIBC = \
 	archivers/9e \
 	archivers/bzip2 \
 	archivers/gtar-base \
-	archivers/gtar-info \
 	archivers/gtar \
 	archivers/gzip \
 	archivers/libarchive \
@@ -12,15 +11,10 @@ PKGS_REQUIRING_MINIX_LIBC = \
 	archivers/zip \
 	chat/naim \
 	converters/libiconv \
-	converters/p5-MIME-Base64 \
 	databases/db4 \
 	databases/sqlite3 \
 	devel/apr \
 	devel/apr-util \
-	devel/autoconf213 \
-	devel/autoconf \
-	devel/automake \
-	devel/automake14 \
 	devel/binutils \
 	devel/bison \
 	devel/bmake \
@@ -33,31 +27,25 @@ PKGS_REQUIRING_MINIX_LIBC = \
 	devel/gmake \
 	devel/gmp \
 	devel/gtexinfo \
-	devel/lcov \
 	devel/libgetopt \
 	devel/libiberty \
 	devel/libidn \
 	devel/libtool-base \
 	devel/m4 \
 	devel/ncurses \
-	devel/p5-enum \
-	devel/p5-Error \
 	devel/pkg-config \
 	devel/pth \
 	devel/readline \
 	devel/scmcvs \
 	devel/scmgit-base \
 	devel/subversion-base \
-	devel/topgit \
 	editors/mg \
 	editors/nano \
-	editors/vilearn \
 	editors/vim \
 	editors/vim-share \
 	editors/zile \
 	games/bastet \
 	games/cmatrix \
-	games/doom1 \
 	games/dungeon \
 	games/gnugo \
 	games/ninvaders \
@@ -75,29 +63,21 @@ PKGS_REQUIRING_MINIX_LIBC = \
 	minix/compiler-rt \
 	minix/x11 \
 	misc/ascii \
-	misc/cowsay \
 	misc/figlet \
 	net/rsync \
-	pkgtools/bootstrap-mk-files \
 	pkgtools/digest \
 	pkgtools/libnbcompat \
-	pkgtools/osabi \
 	pkgtools/p5-pkgsrc-Dewey \
 	pkgtools/pbulk \
 	pkgtools/pkgdep \
 	pkgtools/pkgdepgraph \
-	pkgtools/pkgdiff \
 	pkgtools/pkgin \
 	pkgtools/pkg_install \
 	pkgtools/pkg_install-info \
-	pkgtools/pkg_leaves \
-	pkgtools/pkglint \
-	pkgtools/url2pkg \
 	pkgtools/x11-links \
 	security/bcrypt \
 	security/openssh \
 	security/openssl \
-	security/p5-Digest \
 	security/p5-Digest-SHA1 \
 	security/rats \
 	security/sudo \
