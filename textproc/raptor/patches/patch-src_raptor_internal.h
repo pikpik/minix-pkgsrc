44fb8f0def338cca922e7c9d8cd2c64d0c05d44a
repo-copy <repo-copy>
2011-07-02 01:45:21 +0000

    Repo copy files

---
+++ textproc/raptor/patches/patch-src_raptor_internal.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+curl/types.h is not used (empty) since curl-7.12.0 and removed at curl-7.21.7.
+
+--- src/raptor_internal.h.orig	2010-01-29 23:54:42.000000000 +0000
++++ src/raptor_internal.h
+@@ -852,7 +852,6 @@ int raptor_utf8_is_nfc(const unsigned ch
+ 
+ #ifdef RAPTOR_WWW_LIBCURL
+ #include <curl/curl.h>
+-#include <curl/types.h>
+ #include <curl/easy.h>
+ #endif
+ 
