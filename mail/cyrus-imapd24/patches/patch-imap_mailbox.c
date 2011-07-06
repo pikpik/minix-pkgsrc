af2894fbbb1d2d47889acb0c1cbf95e0f94ba0c7
obache <obache>
2011-07-06 09:45:45 +0000

    Update cyrus-imapd24 to 2.4.10.
    While here,
    * Exactly enable/disable PCRE support with package option, enabled by default.
    * Add workaround patches for PR#44275, sizeof(time_t) > sizeof(unsigned long).
    
    Changes to the Cyrus IMAP Server since 2.4.9
    
          * fixed handling of unparsable emails during append (which would
            cause invalid cyrus.index records otherwise)
          * quota: fix a pile of bugs. #1801, virtdomain support; #2728, slow
            user delete; #3178, "file name too long" with big mailbox names;
            #3179, quota -f doubles usage.
          * Bug #3043 - parse multiple groups in headers correctly
          * Bug #3158 - lmtp backend connection timeout
          * Bug #3223 - limit MIME parsing depth to avoid stack overflows
          * Bug #3273 - add SORT=DISPLAY support (but note: still questions
            about correctness of unicode sorting)
          * Bug #3504 - convert all sieve scripts to \r\n line endings on
            upload
          * Bug #3402 - options to munge 8bit characters in headers during lmtp
            delivery to avoid backscatter
          * sync_client: fix broken keepalive TCP options (I doubt anyone ever
            tried to use it)
          * Bug #3482 - add "-o" option to ipurge to only purge messages with
            \Deleted flag set

---
+++ mail/cyrus-imapd24/patches/patch-imap_mailbox.c
@@ -0,0 +1,19 @@
+$NetBSD$
+
+* cast to unsigned long for platform sizeof(time_t) > sizeof(unsigned long).
+  https://bugzilla.cyrusimap.org/show_bug.cgi?id=3376
+
+--- imap/mailbox.c.orig	2011-07-04 15:05:01.000000000 +0000
++++ imap/mailbox.c
+@@ -1918,9 +1918,9 @@ bit32 make_sync_crc(struct mailbox *mail
+     }
+ 
+     snprintf(buf, 4096, "%u " MODSEQ_FMT " %lu (%u) %lu %s",
+-	    record->uid, record->modseq, record->last_updated,
++	    record->uid, record->modseq, (unsigned long)record->last_updated,
+ 	    flagcrc,
+-	    record->internaldate,
++	    (unsigned long)record->internaldate,
+ 	    message_guid_encode(&record->guid));
+ 
+     return crc32_cstring(buf);
