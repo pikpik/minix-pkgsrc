f9985bdb99be515048c04a6a9d2cd83e97301238
tnn <tnn>
2011-07-07 11:04:35 +0000

    Add a hacks file to drop user-supplied -march values on GCC<4.4 i386. Workaround for PR pkg/44912: gcc generates unaliged SSE2 references.

---
+++ devel/xulrunner/hacks.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+.if !defined(XULRUNNER_HACKS_MK)
+XULRUNNER_HACKS_MK=	defined
+
+.include "../../mk/bsd.fast.prefs.mk"
+.include "../../mk/compiler.mk"
+
+# PR pkg/44912: gcc generates unaliged SSE2 references
+.if !empty(PKGSRC_COMPILER:Mgcc) && !empty(CC_VERSION:Mgcc-4.[0-3]*) && !empty(MACHINE_PLATFORM:M*-*-i386)
+PKG_HACKS+=		optimization
+BUILDLINK_TRANSFORM+=	rm:-march=[-_A-Za-z0-9]*
+.endif
+
+.endif	# XULRUNNER_HACKS_MK
