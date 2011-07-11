69e007939e19f53be4886ada408d5ad2358471b2
tnn <tnn>
2011-07-11 12:46:14 +0000

    Update to firefox-5.0. Changes:   Added support for CSS animations   The Do-Not-Track header preference has been moved to increase discoverability   Tuned HTTP idle connection logic for increased performance   Improved canvas, JavaScript, memory, and networking performance   Improved standards support for HTML5, XHR, MathML, SMIL, and canvas   Improved spell checking for some locales   Improved desktop environment integration for Linux users   WebGL content can no longer load cross-domain textures   Background tabs have setTimeout and setInterval clamped to 1000ms to improve     performance   Fixed several stability issues   Fixed several security issues

--- devel/xulrunner/dist.mk
+++ devel/xulrunner/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.20 2011/04/26 14:16:36 tnn Exp $
+# $NetBSD: dist.mk,v 1.2 2009/11/23 14:16:22 tnn Exp $
 #
 # used by devel/nspr/Makefile
 # used by devel/nss/Makefile
@@ -6,13 +6,13 @@
 # used by www/firefox/Makefile
 
 DISTNAME=	firefox-${FIREFOX_VER}.source
-FIREFOX_VER=	4.0${MOZ_BRANCH_MINOR}
-MOZ_BRANCH=	2.0
-MOZ_BRANCH_MINOR=	.1
+FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
+MOZ_BRANCH=	5.0
+MOZ_BRANCH_MINOR=	# empty
 MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner/distinfo
 PATCHDIR=	${.CURDIR}/../../devel/xulrunner/patches
 
-WRKSRC=		${WRKDIR}/mozilla-${MOZ_BRANCH}
+WRKSRC=		${WRKDIR}/mozilla-release
