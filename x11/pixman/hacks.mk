b29dda3af9b47a2b975b49a0da4fe81a23358147
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2011-07-12 18:44:29 -0700

    Fixup fromcvs/togit conversion

--- x11/pixman/hacks.mk
+++ x11/pixman/hacks.mk
@@ -1,4 +1,4 @@
-# $NetBSD: hacks.mk,v 1.4 2009/12/16 22:28:32 tnn Exp $
+# $NetBSD: hacks.mk,v 1.5 2011/07/12 18:23:09 tron Exp $
 #
 .if !defined(PIXMAN_HACKS_MK)
 PIXMAN_HACKS_MK=	# empty
@@ -9,7 +9,7 @@ PIXMAN_HACKS_MK=	# empty
 ###
 .if empty(CC_VERSION:Mgcc-4.[2-9]*) && \
     empty(CC_VERSION:Mgcc-[5-9].*) && \
-    empty(CC_VERSION:Mgcc-[1-9][0-9].*)
+    empty(CC_VERSION:Mgcc-[1-9][0-9]*)
 CONFIGURE_ARGS+=	--disable-sse2
 .  endif
 .endif
