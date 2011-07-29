2d0ce08c66d0a3a1679be4e7f0498f9ad0122f4b
ryoon <ryoon>
2011-07-29 15:18:29 +0000

    Import gmock-1.6.0 (googlemock) as devel/gmock from wip/gmock
    Inspired by jMock, EasyMock, and Hamcrest, and designed with C++'s
    specifics in mind, Google C++ Mocking Framework (or Google Mock for
    short) is a library for writing and using C++ mock classes. Google Mock:
    
    * lets you create mock classes trivially using simple macros,
    * supports a rich set of matchers and actions,
    * handles unordered, partially ordered, or completely ordered expectations,
    * is extensible by users, and
    * works on Linux, Mac OS X, Windows, Windows Mobile, minGW, and Symbian.

---
+++ devel/gmock/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	gmock
+
+.if !defined(GMOCK_BUILDLINK3_MK)
+GMOCK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gmock+=	gmock>=1.6.0
+BUILDLINK_PKGSRCDIR.gmock?=	../../devel/gmock
+
+.include "../../devel/googletest/buildlink3.mk"
+.endif	# GMOCK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gmock
