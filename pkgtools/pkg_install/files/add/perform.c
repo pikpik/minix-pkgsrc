521e2e3b7178d3c586b901c6d552dd0644e97688
agc <agc>
2011-08-05 07:04:28 +0000

    Fix for pkg_delete on NFS from Anthony Mallet.
    Problem analysis from Anthony:
    
    	On Wednesday, at 23:52, Anthony Mallet wrote:
    	> # rm -rf ~tmp && mkdir ~/tmp
    	> # pkg_add -K ~/tmp/var/db/pkg -p ~/tmp /usr/pkgsrc/packages/All/digest-20080510.tgz
    	> # pkg_add -U -K ~/tmp/var/db/pkg -p ~/tmp /usr/pkgsrc/packages/All/digest-20080510.tgz
    	> pkg_delete: Couldn't remove package directory in `/home/tho/tmp/var/db/pkg/digest-20080510'
    	> Also, the dir var/db/pkg/digest-20080510.xxxxxxx is never cleaned.
    	>
    	> The problem apparently is that pkg_delete finds some stalled NFS entries
    	> (.nfs*) in the var/db/pkg/digest-20080510 dir, so it does not delete the
    	> directory. Is this due to pkg_add not correctly closing file descriptors before
    	> exec'ing pkg_delete? For instance, I really don't understand the logic in
    	> check_already_installed() (add/perform.c:375) regarding the open() of +CONTENTS
    	> which is almost never closed (and never used as well...). Shouldn't this be
    	> closed before running pkg_delete?
    
    	ktrace shows that the +CONTENTS file is open() by pkg_add, I believe this is in
    	check_already_installed(), add/perform.c:381. Then pkg_delete is run and when
    	it comes to deleting the pkgdir entry, it finds that .nfs file and aborts.
    
    Bump version to 20110805

--- pkgtools/pkg_install/files/add/perform.c
+++ pkgtools/pkg_install/files/add/perform.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: perform.c,v 1.98 2010/09/14 22:26:18 gdt Exp $	*/
+/*	$NetBSD: perform.c,v 1.99 2010/12/12 13:18:38 wiz Exp $	*/
 #if HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -6,7 +6,7 @@
 #if HAVE_SYS_CDEFS_H
 #include <sys/cdefs.h>
 #endif
-__RCSID("$NetBSD: perform.c,v 1.98 2010/09/14 22:26:18 gdt Exp $");
+__RCSID("$NetBSD: perform.c,v 1.99 2010/12/12 13:18:38 wiz Exp $");
 
 /*-
  * Copyright (c) 2003 Grant Beattie <grant@NetBSD.org>
@@ -391,6 +391,7 @@ check_already_installed(struct pkg_task *pkg)
 	free(filename);
 	if (fd == -1)
 		return 1;
+	close(fd);
 
 	if (ReplaceSame) {
 		struct stat sb;
@@ -420,7 +421,6 @@ check_already_installed(struct pkg_task *pkg)
 		warnx("package `%s' already recorded as installed",
 		      pkg->pkgname);
 	}
-	close(fd);
 	return 0;
 
 }
