8d5c339a1a1df13e3ed45ebedeaae5b5c8b35545
adam <adam>
2011-08-05 18:06:15 +0000

    Changes 5.14.1: * Several modules were updated to fix minor bugs. * Tests were fixed. * Documentation was improved.
    Changes 5.14.0:
    * Unicode version 6.0 is now supported.
    * Regular expressions can now be reset to the default modifiers.
    * Four new regular expression modifiers, "/d", "/l" , "/u", and "/a", were
      introduced.
    * The substitution operators now support the modifier "/r" to work
      non-destructively, leaving the original unmodified.
    * Larger octals can now be specified.
    * Array and hash operators can now experimentally operate directly on hard
      references.
    * printf-like functions now understand post-1980 size modifiers.
    * Exception handling was made more reliable and consistent.
    * Other enhancements and many bug fixes were made
    
    PkgSrc:
    * Fixed configuring on Mac OS X 10.7.

--- lang/perl5/module.mk
+++ lang/perl5/module.mk
@@ -1,4 +1,4 @@
-# $NetBSD: module.mk,v 1.62 2010/05/09 18:46:34 morr Exp $
+# $NetBSD: module.mk,v 1.63 2010/08/27 05:27:37 sno Exp $
 #
 # This Makefile fragment is intended to be included by packages that build
 # and install perl5 modules.
@@ -214,7 +214,7 @@ PERL5_MAKE_FLAGS.makemaker+=	${_var_}=${PERL5_${_var_}:Q}
 # so override its value to the module's ${PREFIX}.
 # Also, set VENDORARCHEXP, so existing .packlist won't be read.
 #
-PERL5_MAKE_FLAGS.makemaker+=   PREFIX=${PREFIX:Q} VENDORARCHEXP=${DESTDIR}${PERL5_INSTALLVENDORARCH}
+PERL5_MAKE_FLAGS.makemaker+=	PREFIX=${PREFIX:Q} VENDORARCHEXP=${DESTDIR}${PERL5_INSTALLVENDORARCH}
 
 PERL5_MAKE_FLAGS+=	${PERL5_MAKE_FLAGS.${PERL5_MODTYPE}}
 MAKE_FLAGS+=		${PERL5_MAKE_FLAGS}
