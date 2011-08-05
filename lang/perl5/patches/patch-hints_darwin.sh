4da170f49c2be7281ccb537510884e991d294bc8
ryoon <ryoon>
2011-08-05 13:12:18 +0000

    Fix build on MacOS X Lion (Darwin Kernel Version 11.0.1).
    From upstream comment:
    nm isn't known to work after Snow Leopard and XCode 4; testing with OS X 10.5
    and Xcode 3 shows a working nm, but pretending it doesn't work produces no
    problems.
    
    From upstream commit log:
    Testing from 10.5 with Xcode 3 to the latest OS X works with usenm='false' but
    not always with usenm='true'

---
+++ lang/perl5/patches/patch-hints_darwin.sh
@@ -0,0 +1,19 @@
+$NetBSD$
+
+Patch from http://perl5.git.perl.org/perl.git/commitdiff/60a655a1ee05c577268377c1135ffabc34dbff43
+
+--- /usr/tmp/lang/perl5/work/perl-5.12.2/hints/darwin.sh.orig	2010-09-05 15:14:33.000000000 +0000
++++ /usr/tmp/lang/perl5/work/perl-5.12.2/hints/darwin.sh
+@@ -68,8 +68,10 @@ esac
+ # Since we can build fat, the archname doesn't need the processor type
+ archname='darwin';
+ 
+-# nm works.
+-usenm='true';
++# nm isn't known to work after Snow Leopard and XCode 4; testing with OS X 10.5
++# and Xcode 3 shows a working nm, but pretending it doesn't work produces no
++# problems.
++usenm='false';
+ 
+ case "$optimize" in
+ '')
