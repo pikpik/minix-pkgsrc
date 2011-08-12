2c99264176056dc33753f0aa9bc93e3d3a60e5ce
taca <taca>
2011-08-12 15:56:38 +0000

    Avoid to define constant Deprecate since it is conflict with rubygems. Newer rubygems poorly defines module Deprecate which could be Gem::Deprecate.
    Bump PKGREVISION.

---
+++ devel/ruby-deprecated/patches/patch-lib_deprecated.rb
@@ -0,0 +1,12 @@
+$NetBSD$
+
+* Avoid to use Deprecate since it would be conflict with newer rubygems.
+
+--- lib/deprecated.rb.orig	2011-08-09 00:21:03.000000000 +0000
++++ lib/deprecated.rb
+@@ -195,5 +195,3 @@ Module.send(:define_method, :deprecate, 
+             end)
+ 
+ Deprecated.set_action(:warn)
+-
+-Deprecate = Deprecated
