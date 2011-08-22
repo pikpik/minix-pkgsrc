4f1f8988f04101678da39a3a5d8017e82c9db9b5
wiz <wiz>
2011-08-22 12:18:19 +0000

    Fix for python-3.x. Mark as not-for 2.4 and 2.5.

---
+++ x11/py-Xlib/patches/patch-Xlib_support_vms__connect.py
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Python-3.1 compatibility.
+
+--- Xlib/support/vms_connect.py.orig	2007-06-10 14:11:58.000000000 +0000
++++ Xlib/support/vms_connect.py
+@@ -62,7 +62,7 @@ def get_socket(dname, host, dno):
+         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         s.connect((host, 6000 + dno))
+ 
+-    except socket.error, val:
++    except socket.error as val:
+         raise error.DisplayConnectionError(dname, str(val))
+ 
+     return s
