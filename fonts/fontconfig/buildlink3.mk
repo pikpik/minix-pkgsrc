7a29efdd07248425362a478ea48f44ce2104ec55
Thomas Cort <tcort@minix3.org>
2011-08-23 01:34:52 +0000

    wm/jwm: initial work to get it building
    mk.conf changes contributed by Jan Weick

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,5 +1,11 @@
 # $NetBSD: buildlink3.mk,v 1.25 2007/10/16 18:12:56 reed Exp $
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+.if ${X11_TYPE} != "modular" && !exists(${X11BASE}/lib/pkgconfig/fontconfig.pc)
+.include "../../mk/x11.buildlink3.mk"
+.else
+
 BUILDLINK_TREE+=	fontconfig
 
 .if !defined(FONTCONFIG_BUILDLINK3_MK)
@@ -15,3 +21,5 @@ BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 .endif # FONTCONFIG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-fontconfig
+
+.endif
