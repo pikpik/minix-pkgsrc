0271209cbeade94651d1ddec3f526a7a278551c6
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2011-08-29 12:43:03 -0700

    Fixup fromcvs/togit conversion

--- net/flickcurl/patches/patch-src_flickcurl__internal.h
+++ net/flickcurl/patches/patch-src_flickcurl__internal.h
@@ -1,4 +1,6 @@
-$NetBSD$
+$NetBSD: patch-src_flickcurl__internal.h,v 1.1.1.1 2011/08/25 13:30:23 jakllsch Exp $
+
+Compatibility with latest curl.
 
 --- src/flickcurl_internal.h.orig	2010-07-24 19:33:34.000000000 +0000
 +++ src/flickcurl_internal.h
