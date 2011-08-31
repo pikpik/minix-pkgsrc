e9f836041c0869d6c9ac6d8f144984b9018d90b3
apb <apb>
2011-08-31 11:43:17 +0000

    Add PythonTidy 1.20 "Cleans up, regularizes, and reformats the text of Python scripts."

---
+++ devel/pythontidy/patches/patch-pythontidy.py
@@ -0,0 +1,19 @@
+$NetBSD$
+
+Replace hardcoded references to /usr/bin/python
+with references to the pkgsrc python interpreter.
+This patch changes /usr/bin/python to %PYTHONBIN%,
+and the SUBST_STAGES.pythonbin rules in the Makefile
+changes %PYTHONBIN% to the value of ${PYTHONBIN}.
+
+--- pythontidy.py.orig	2011-08-31 11:00:17.000000000 +0000
++++ pythontidy.py
+@@ -296,7 +296,7 @@ SUBSCRIPT_SEP = ', '
+ DICT_COLON = ': '
+ SLICE_COLON = ':'
+ COMMENT_PREFIX = '# '  # 2007 May 25
+-SHEBANG = '#!/usr/bin/python'
++SHEBANG = '#!%PYTHONBIN%'
+ CODING = 'utf-8'
+ CODING_SPEC = '# -*- coding: %s -*-' % CODING
+ BOILERPLATE = NULL  # 2007 Mar 06
