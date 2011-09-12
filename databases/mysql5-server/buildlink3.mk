894fda309de5086873e14142e8c58b6a03c1de55
joerg <joerg>
2011-09-12 10:56:10 +0000

    Add b3.mk file for consistency with the other MySQL server packages

---
+++ databases/mysql5-server/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	mysql-server
+
+.if !defined(MYSQL_SERVER_BUILDLINK3_MK)
+MYSQL_SERVER_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.mysql-server+=	mysql-server>=5.0<5.1
+BUILDLINK_PKGSRCDIR.mysql-server?=	../../databases/mysql5-server
+BUILDLINK_LIBDIRS.mysql-server?=	lib/mysql
+
+.include "../../databases/mysql51-client/buildlink3.mk"
+.endif	# MYSQL_SERVER_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-mysql-server
