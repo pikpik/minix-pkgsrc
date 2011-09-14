b3e735809ad9336fea9565d836c6130301601044
hans <hans>
2011-09-14 17:14:09 +0000

    Explicitly depend on gmp>=4.3.2 and mpfr>=2.4.2.

--- math/mpcomplex/buildlink3.mk
+++ math/mpcomplex/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/07/27 17:09:45 asau Exp $
 
 BUILDLINK_TREE+=	mpcomplex
 
@@ -8,6 +8,9 @@ MPCOMPLEX_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.mpcomplex+=	mpcomplex>=0.8.2
 BUILDLINK_PKGSRCDIR.mpcomplex?=	../../math/mpcomplex
 
+BUILDLINK_API_DEPENDS.gmp+=     gmp>=4.3.2
+.include "../../devel/gmp/buildlink3.mk"
+BUILDLINK_API_DEPENDS.mpfr+=    mpfr>=2.4.2
 .include "../../math/mpfr/buildlink3.mk"
 .endif	# MPCOMPLEX_BUILDLINK3_MK
 
