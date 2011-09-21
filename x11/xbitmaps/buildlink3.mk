a3e750681ca11bea14fc1b7ba28addb44545d258
joerg <joerg>
2011-09-21 14:58:37 +0000

    Add support for builtin xbitmaps. Bump x11-links to 0.68.

--- x11/xbitmaps/buildlink3.mk
+++ x11/xbitmaps/buildlink3.mk
@@ -1,10 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2006/11/06 10:29:33 joerg Exp $
-
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${X11_TYPE} != "modular"
-.include "../../mk/x11.buildlink3.mk"
-.else
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:25:52 joerg Exp $
 
 BUILDLINK_DEPMETHOD.xbitmaps?=	build
 
@@ -18,5 +12,3 @@ BUILDLINK_PKGSRCDIR.xbitmaps?=	../../x11/xbitmaps
 .endif # XBITMAPS_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xbitmaps
-
-.endif
