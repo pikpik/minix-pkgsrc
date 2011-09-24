2433e45c8452032d444a5a477b5b5183b043a363
joerg <joerg>
2011-09-24 13:01:29 +0000

    Fix fallout from include guard rename in libX11

---
+++ wm/blackbox70/patches/patch-lib-Util.hh
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- lib/Util.hh.orig	2011-09-22 13:58:57.000000000 +0000
++++ lib/Util.hh
+@@ -94,7 +94,7 @@ namespace bt {
+ 
+   std::string tolower(const std::string &string);
+ 
+-#ifdef _XUTIL_H_
++#if defined(_XUTIL_H_) || defined(_X11_XUTIL_H_)
+   std::string textPropertyToString(::Display *display,
+                                    ::XTextProperty& text_prop);
+ #endif
