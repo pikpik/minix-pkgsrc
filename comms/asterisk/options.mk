6677ba3c37cf3f4d7da1a7c8a0fb5f72e19a4990
wiz <wiz>
2011-10-06 08:35:01 +0000

    Remove zaptel option, zaptel-netbsd was removed.

--- comms/asterisk/options.mk
+++ comms/asterisk/options.mk
@@ -1,19 +1,11 @@
-# $NetBSD: options.mk,v 1.2 2006/01/13 20:32:38 riz Exp $
+# $NetBSD: options.mk,v 1.3 2008/04/12 22:42:58 jlam Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.asterisk
-PKG_SUPPORTED_OPTIONS=	zaptel gtk
+PKG_SUPPORTED_OPTIONS=	gtk
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		zaptel gtk
-
-.if !empty(PKG_OPTIONS:Mzaptel)
-# zaptel only supported under NetBSD at the moment
-.  include "../../comms/zaptel-netbsd/buildlink3.mk"
-PLIST.zaptel=		yes
-.else
-MAKE_FLAGS+=		WITHOUT_ZAPTEL=1
-.endif
+PLIST_VARS+=		gtk
 
 .if !empty(PKG_OPTIONS:Mgtk)
 .  include "../../x11/gtk/buildlink3.mk"
