94599df767d7b8de51e438114030b197e69272b1
drochner <drochner>
2011-10-20 16:32:57 +0000

    build poppler's "cpp" wrapper as a seperate pkg, addresses PR pkg/45498 by Francois Tigeot

---
+++ print/poppler-cpp/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	poppler-cpp
+
+.if !defined(POPPLER_CPP_BUILDLINK3_MK)
+POPPLER_CPP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.poppler-cpp+=	poppler-cpp>=0.16.7
+BUILDLINK_PKGSRCDIR.poppler-cpp?=	../../print/poppler-cpp
+
+.include "../../print/poppler/buildlink3.mk"
+.endif	# POPPLER_CPP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-poppler-cpp
--- print/poppler/buildlink3.mk
+++ print/poppler/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.23 2011/01/31 16:55:28 drochner Exp $
+# $NetBSD: buildlink3.mk,v 1.24 2011/03/09 13:06:49 drochner Exp $
 
 BUILDLINK_TREE+=	poppler
 
@@ -17,6 +17,7 @@ pkgbase := poppler
 
 .include "../../fonts/fontconfig/buildlink3.mk"
 .include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
 .endif # POPPLER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-poppler
