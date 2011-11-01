e6849d065bc19f1b6ae5b2ce0e0bc0b2f6fda528
sbd <sbd>
2011-11-01 06:43:16 +0000

    Add missing archivers/xz buildlink
    Bump PKGREVISION

--- archivers/libarchive/buildlink3.mk
+++ archivers/libarchive/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.2 2007/07/29 17:35:14 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.3 2009/03/20 19:23:50 joerg Exp $
 
 BUILDLINK_TREE+=	libarchive
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libarchive+=	libarchive>=2.2
 BUILDLINK_PKGSRCDIR.libarchive?=	../../archivers/libarchive
 
 .include "../../archivers/bzip2/buildlink3.mk"
+.include "../../archivers/xz/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .endif # LIBARCHIVE_BUILDLINK3_MK
 
