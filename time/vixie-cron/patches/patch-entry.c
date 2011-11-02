f4adfc5552f63d79cbbed89c6937eff9b6d18fc5
sbd <sbd>
2011-11-02 23:47:37 +0000

    Make the crontab entry '-q' option work.
    Bump PKGREVISION.

---
+++ time/vixie-cron/patches/patch-entry.c
@@ -0,0 +1,16 @@
+$NetBSD$
+
+Make the crontab entry '-q' option work.
+
+--- entry.c.orig	2011-05-01 12:04:30.412434092 +0000
++++ entry.c
+@@ -336,7 +336,8 @@ load_entry(FILE *file, void (*error_func
+
+ 	/* If the first character of the command is '-' it is a cron option.
+ 	 */
+-	while ((ch = get_char(file)) == '-') {
++	Skip_Blanks(ch, file)
++	while (ch == '-') {
+ 		switch (ch = get_char(file)) {
+ 		case 'q':
+ 			e->flags |= DONT_LOG;
