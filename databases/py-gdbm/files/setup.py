64a2c504ab2ffe042477d465974dfb25dfa1bcb5
obache <obache>
2011-11-03 12:21:41 +0000

    Support Python3.

--- databases/py-gdbm/files/setup.py
+++ databases/py-gdbm/files/setup.py
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: setup.py,v 1.1 2002/01/28 09:30:10 drochner Exp $
 
 import distutils
 from distutils.core import setup, Extension
@@ -10,8 +10,8 @@ gdbmlib = gdbmprefix + '/lib'
 setup(
 	ext_modules = [
 		Extension(
-			'gdbm',
-			['Modules/gdbmmodule.c'],
+			'@GDBM_MODNAME@',
+			['Modules/@GDBM_MODNAME@module.c'],
 			include_dirs=[gdbmincl],
 			library_dirs=[gdbmlib],
 			runtime_library_dirs=[gdbmlib],
