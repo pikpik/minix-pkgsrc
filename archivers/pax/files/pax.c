681898f000d2cc4ac7de12459658f53a66cec442
Thomas Cort <tcort@minix3.org>
2011-11-05 20:43:10 +0000

    archivers/pax: use ifdef HAVE_SETRLIMIT instead of ifndef __minix

--- archivers/pax/files/pax.c
+++ archivers/pax/files/pax.c
@@ -405,7 +405,7 @@ gen_init(void)
 	struct sigaction n_hand;
 	struct sigaction o_hand;
 
-#ifndef __minix
+#if defined(HAVE_GETRLIMIT) && defined(HAVE_SETRLIMIT)
 	/*
 	 * Really needed to handle large archives. We can run out of memory for
 	 * internal tables really fast when we have a whole lot of files...
@@ -441,7 +441,7 @@ gen_init(void)
 		(void)setrlimit(RLIMIT_RSS , &reslimit);
 	}
 #endif
-#endif /* __minix */
+#endif /* defined(HAVE_GETRLIMIT) && defined(HAVE_SETRLIMIT) */
 	/*
 	 * Handle posix locale
 	 *
