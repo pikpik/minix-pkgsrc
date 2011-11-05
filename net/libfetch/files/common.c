f62d1fd5aa9b31fa7c24880159f778205eeb348f
Thomas Cort <tcort@minix3.org>
2011-11-05 19:23:31 +0000

    net/libfetch: remove some minix specific bits not needed after libc switch

--- net/libfetch/files/common.c
+++ net/libfetch/files/common.c
@@ -150,15 +150,11 @@ fetch_syserr(void)
 	case EADDRNOTAVAIL:
 	case ENETDOWN:
 	case ENETUNREACH:
-#if defined(ENETRESET)
 	case ENETRESET:
-#endif
 	case EHOSTUNREACH:
 		fetchLastErrCode = FETCH_NETWORK;
 		break;
-#if defined(ECONNABORTED)
 	case ECONNABORTED:
-#endif
 	case ECONNRESET:
 		fetchLastErrCode = FETCH_ABORT;
 		break;
@@ -166,9 +162,7 @@ fetch_syserr(void)
 		fetchLastErrCode = FETCH_TIMEOUT;
 		break;
 	case ECONNREFUSED:
-#if defined(EHOSTDOWN)
 	case EHOSTDOWN:
-#endif
 		fetchLastErrCode = FETCH_DOWN;
 		break;
 default:
