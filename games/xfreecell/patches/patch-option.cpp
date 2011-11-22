7efd98eaa177547f3df8754be9a18d55c09c4e7b
joerg <joerg>
2011-11-22 18:11:14 +0000

    Fix build with newer GCC versions

---
+++ games/xfreecell/patches/patch-option.cpp
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- option.cpp.orig	2011-11-22 17:06:36.000000000 +0000
++++ option.cpp
+@@ -4,6 +4,8 @@
+ #include <dirent.h>
+ #include <unistd.h>
+ #include <stdio.h>
++#include <stdlib.h>
++#include <string.h>
+ 
+ #include "general.h"
+ #include "option.h"
