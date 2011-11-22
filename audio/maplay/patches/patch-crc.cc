1a71bf66a0c3e252176ab3c69553af9107df262b
wiz <wiz>
2011-11-22 20:57:35 +0000

    Unconditionalize the DragonFly patches for newer c++. Use patches instead of sed. Set LICENSE while here.

---
+++ audio/maplay/patches/patch-crc.cc
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- crc.cc.orig	1994-06-23 12:14:35.000000000 +0000
++++ crc.cc
+@@ -18,7 +18,7 @@
+  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  */
+ 
+-#include <iostream.h>
++#include <iostream>
+ #include <stdlib.h>
+ #include "crc.h"
+ 
