83eec9d4539d5b39b3e2abaa51ab3e03ed443bb2
tez <tez>
2011-11-22 22:23:13 +0000

    Cacti is a complete frontend to rrdtool, it stores all of the necessary information to create graphs and populates them with data in a MySQL database. The frontend is completely PHP driven. Along with being able to maintain Graphs, Data Sources, and Round Robin Archives in a database, cacti handles the data gathering also. There is also SNMP support for those used to creating traffic graphs with MRTG.
    The Plugin Architecture for Cacti was designed to be both simple in nature
    and robust enough to allow freedom to do almost anything in Cacti.  The
    Plugin Architecture for Cacti is integrated into this package.
    
    (created from wip/cacti by pettai)

---
+++ net/cacti/patches/patch-include_global.php
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Make rra directory configurable by package variable
+
+--- include/global.php.orig	2011-09-26 20:41:03.000000000 +0000
++++ include/global.php
+@@ -103,7 +103,7 @@ if ($config["cacti_server_os"] == "win32
+ 	$config["library_path"] = preg_replace("/(.*[\/])include/", "\\1lib", dirname(__FILE__));
+ }
+ $config["include_path"] = dirname(__FILE__);
+-$config["rra_path"] = $config["base_path"] . '/rra';
++$config["rra_path"] = '@CACTI_RRADIR@';
+ 
+ /* colors */
+ $colors["dark_outline"] = "454E53";
