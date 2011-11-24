e24083ee5dfaec38c04782682bb2394577173c40
joerg <joerg>
2011-11-24 14:24:34 +0000

    Fix build with GCC 4.5

---
+++ biology/mummer/patches/patch-src_tigr_show-diff.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/tigr/show-diff.cc.orig	2011-11-24 02:32:28.000000000 +0000
++++ src/tigr/show-diff.cc
+@@ -14,6 +14,7 @@
+ #include <cstdlib>
+ #include <cassert>
+ #include <climits>
++#include <algorithm>
+ using namespace std;
+ 
+ 
