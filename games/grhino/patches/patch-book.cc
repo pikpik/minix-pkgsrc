0bd03de2f47e34175a820f5f7f56cef7d69f06d8
joerg <joerg>
2011-11-24 14:08:05 +0000

    Fix build with newer GCC

---
+++ games/grhino/patches/patch-book.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- book.cc.orig	2011-11-24 01:11:40.000000000 +0000
++++ book.cc
+@@ -25,6 +25,7 @@
+ #include "rand.h"
+ #include "gtstream.h"
+ 
++#include <cstdlib>
+ #include <vector>
+ #include <iostream>
+ #include <stdexcept>
