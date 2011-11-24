6b70e6d7162051567567b1426e8731f1e8bb5db0
joerg <joerg>
2011-11-24 13:56:02 +0000

    Fix build with newer GCC

---
+++ mail/fix-mime-charset/patches/patch-src_argp-local.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/argp-local.h.orig	2011-11-24 00:53:41.000000000 +0000
++++ src/argp-local.h
+@@ -10,6 +10,7 @@
+ //
+ 
+ #include <getopt.h>
++#include <stdlib.h>
+ #include <string.h>
+ #include <iostream>
+ 
---
+++ mail/fix-mime-charset/patches/patch-src_decoder.cc
@@ -0,0 +1,8 @@
+$NetBSD$
+
+--- src/decoder.cc.orig	2011-11-24 00:54:13.000000000 +0000
++++ src/decoder.cc
+@@ -2,2 +2,3 @@
+ 
++#include <string.h>
+ #include "decoder.h"
