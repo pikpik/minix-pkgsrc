af255692c2bce723739eb31a0225e67b93b63935
joerg <joerg>
2011-11-24 14:07:12 +0000

    Fix build with GCC 4.5

---
+++ games/six/patches/patch-six_main.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- six/main.cpp.orig	2011-11-24 03:21:53.000000000 +0000
++++ six/main.cpp
+@@ -11,6 +11,7 @@
+ #include <klocale.h>
+ #include <kmessagebox.h>
+ #include <qfile.h>
++#include <cstdlib>
+ 
+ using std::cin;
+ 
