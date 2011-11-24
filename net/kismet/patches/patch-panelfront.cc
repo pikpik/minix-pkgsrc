df74dcebc04d17ac8a92c2bbd195e915f4890ab8
joerg <joerg>
2011-11-24 13:43:11 +0000

    Fix build with newer GCC

---
+++ net/kismet/patches/patch-panelfront.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- panelfront.cc.orig	2011-11-24 01:48:35.000000000 +0000
++++ panelfront.cc
+@@ -21,6 +21,7 @@
+ #include <math.h>
+ #include <sys/types.h>
+ #include <dirent.h>
++#include <limits.h>
+ 
+ #if defined(SYS_OPENBSD) && defined(HAVE_MACHINE_APMVAR_H)
+ #include <machine/apmvar.h>
