d1481abe8b44cd38ff4d4677fa6816cbdcf94321
joerg <joerg>
2011-11-24 13:59:50 +0000

    Fix build with GCC 4.5

---
+++ graphics/metacam/patches/patch-edittiff.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- edittiff.h.orig	2011-11-24 02:20:47.000000000 +0000
++++ edittiff.h
+@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suit
+ #define EDITTIFF_H_INCLUDED
+ 
+ #include "metatiff.h"
++#include <cstdlib>
+ 
+ class _EditIFD;
+ 
