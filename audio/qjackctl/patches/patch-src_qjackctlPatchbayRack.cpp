c0a7d81d1126bddf027dea13de5b50926054b37a
joerg <joerg>
2011-11-24 14:25:17 +0000

    Fix build with modern GCC

---
+++ audio/qjackctl/patches/patch-src_qjackctlPatchbayRack.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/qjackctlPatchbayRack.cpp.orig	2011-11-24 03:02:27.000000000 +0000
++++ src/qjackctlPatchbayRack.cpp
+@@ -22,6 +22,7 @@
+ #include "qjackctlPatchbayRack.h"
+ 
+ #include <QRegExp>
++#include <cstdlib>
+ 
+ 
+ //----------------------------------------------------------------------
