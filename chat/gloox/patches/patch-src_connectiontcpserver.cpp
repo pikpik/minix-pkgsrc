30774491222c2e29cce980fc881378b58c912dc4
joerg <joerg>
2011-11-25 21:31:13 +0000

    Always include string.h

--- chat/gloox/patches/patch-src_connectiontcpserver.cpp
+++ chat/gloox/patches/patch-src_connectiontcpserver.cpp
@@ -1,14 +1,12 @@
-$NetBSD$
+$NetBSD: patch-src_connectiontcpserver.cpp,v 1.1 2011/11/23 19:37:56 marino Exp $
 
 --- src/connectiontcpserver.cpp.orig	2009-10-12 16:40:41.000000000 +0000
 +++ src/connectiontcpserver.cpp
-@@ -23,6 +23,10 @@
+@@ -23,6 +23,8 @@
  #include "mutexguard.h"
  #include "util.h"
  
-+#if defined(__DragonFly__)
 +#include <string.h>
-+#endif
 +
  #ifdef __MINGW32__
  # include <winsock.h>
