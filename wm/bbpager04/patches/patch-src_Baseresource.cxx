ef1f2433baaadeae105a4673b239c82c1f7a4e00
joerg <joerg>
2011-11-25 22:22:05 +0000

    Fix build with modern GCC.

---
+++ wm/bbpager04/patches/patch-src_Baseresource.cxx
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/Baseresource.cxx.orig	2011-11-25 17:02:36.000000000 +0000
++++ src/Baseresource.cxx
+@@ -19,6 +19,7 @@
+ // (See the included file COPYING / GPL-2.0)
+ //
+ 
++#include <strings.h>
+ #include <string>
+ #include "Baseresource.h"
+ #include <stdio.h>
