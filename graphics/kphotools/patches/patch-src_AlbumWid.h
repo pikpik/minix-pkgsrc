89154d0bcf551373dba096b8b0e2553bcac2131b
joerg <joerg>
2011-11-25 21:58:42 +0000

    Fix build with GCC 4.5

---
+++ graphics/kphotools/patches/patch-src_AlbumWid.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- src/AlbumWid.h.orig	2011-11-25 19:16:22.000000000 +0000
++++ src/AlbumWid.h
+@@ -15,7 +15,7 @@
+ #include <qwidget.h>
+ #include <qdir.h>
+ #include <qfiledialog.h>
+-#include <vector.h>
++#include <vector>
+ #include <qwmatrix.h>
+ #include <qtabwidget.h>
+ #include <qcheckbox.h>
