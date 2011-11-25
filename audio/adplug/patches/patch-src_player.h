3c0eeec017d07eef76eb91e9e2507600d0b23a8e
joerg <joerg>
2011-11-25 21:29:32 +0000

    Fix build with newer GCC

---
+++ audio/adplug/patches/patch-src_player.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/player.h.orig	2011-11-25 16:52:54.000000000 +0000
++++ src/player.h
+@@ -22,6 +22,7 @@
+ #ifndef H_ADPLUG_PLAYER
+ #define H_ADPLUG_PLAYER
+ 
++#include <cstring>
+ #include <string>
+ 
+ #include "fprovide.h"
