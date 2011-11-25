17d530a6f70b8fcb4716d57d83f6899000ce4c01
joerg <joerg>
2011-11-25 22:14:17 +0000

    Fix build with newer GCC.

---
+++ net/latd/patches/patch-llogin.cc
@@ -0,0 +1,19 @@
+$NetBSD$
+
+--- llogin.cc.orig	2011-11-25 19:18:49.000000000 +0000
++++ llogin.cc
+@@ -46,10 +46,13 @@
+ #include <queue>
+ #include <map>
+ #include <string>
+-#include <algo.h>
++#include <algorithm>
+ #include <iterator>
+ #include <string>
+ #include <strstream>
++#include <iostream>
++
++using namespace std;
+ 
+ #include "lat.h"
+ #include "latcp.h"
