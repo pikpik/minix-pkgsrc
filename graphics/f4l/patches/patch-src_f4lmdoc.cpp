24ad7f7671d798b93eb52a949d64a4bf6f42faaf
joerg <joerg>
2011-11-25 21:59:26 +0000

    Fix build with newer GCC

---
+++ graphics/f4l/patches/patch-src_f4lmdoc.cpp
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- src/f4lmdoc.cpp.orig	2011-11-25 17:34:16.000000000 +0000
++++ src/f4lmdoc.cpp
+@@ -21,7 +21,7 @@
+ #include <qwidget.h>
+ #include <qmessagebox.h>
+ #include <qfiledialog.h>
+-#include <iostream.h>
++#include <iostream>
+ #include <qfiledialog.h>
+ #include <qprocess.h> // reading ttf files
+ #include <qimage.h>
---
+++ graphics/libvideogfx/patches/patch-libvideogfx_utility_math.cc
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- libvideogfx/utility/math.cc.orig	2011-11-25 19:39:49.000000000 +0000
++++ libvideogfx/utility/math.cc
+@@ -19,7 +19,7 @@
+ 
+ #include "libvideogfx/utility/math.hh"
+ #include <assert.h>
+-#include <iostream.h>
++#include <iostream>
+ using namespace std;
+ 
+ namespace videogfx {
