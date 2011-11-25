441fd1a3b25dd2555c392c525b03f36c7e7a8837
joerg <joerg>
2011-11-25 21:44:06 +0000

    Fix build with newer GCC

---
+++ devel/gtl/patches/patch-src_graph.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/graph.cpp.orig	2011-11-25 18:11:10.000000000 +0000
++++ src/graph.cpp
+@@ -16,6 +16,7 @@
+ 
+ #include <cassert>
+ #include <cstdio>
++#include <cstring>
+ 
+ #include <algorithm>
+ #include <queue>
