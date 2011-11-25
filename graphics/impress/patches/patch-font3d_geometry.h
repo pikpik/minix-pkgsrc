36e387bf970301d802521633ba902c493c50cf45
joerg <joerg>
2011-11-25 21:57:36 +0000

    Fix build with modern GCC versions

---
+++ graphics/impress/patches/patch-font3d_geometry.h
@@ -0,0 +1,14 @@
+$NetBSD$
+
+--- font3d/geometry.h.orig	2011-11-25 18:27:25.000000000 +0000
++++ font3d/geometry.h
+@@ -31,7 +31,8 @@
+ #ifndef __GEOMETRY_H__
+ #define __GEOMETRY_H__
+ 
+-#include <iostream.h>
++#include <iostream>
++using namespace std;
+ 
+ #include "vector.h"
+ 
