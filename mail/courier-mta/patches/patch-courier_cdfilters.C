068015c9bc48a0c3cff3f1aeccca57324a626e01
joerg <joerg>
2011-11-26 17:14:42 +0000

    Fix build with modern GCC

---
+++ mail/courier-mta/patches/patch-courier_cdfilters.C
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- courier/cdfilters.C.orig	2011-11-26 15:35:04.000000000 +0000
++++ courier/cdfilters.C
+@@ -13,6 +13,7 @@
+ #include	<unistd.h>
+ #endif
+ #include	<stdlib.h>
++#include	<string.h>
+ #include	<errno.h>
+ #include	<ctype.h>
+ 
