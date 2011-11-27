7d4a3e7dbf04b701372b9f14c97a59c8e824c43a
joerg <joerg>
2011-11-27 19:39:09 +0000

    Fix build with modern GCC

---
+++ emulators/vmips/patches/patch-terminalcontroller.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- terminalcontroller.cc.orig	2011-11-27 02:51:21.000000000 +0000
++++ terminalcontroller.cc
+@@ -21,6 +21,7 @@ with VMIPS; if not, write to the Free So
+ #include "error.h"
+ #include "terminalcontroller.h"
+ #include <cassert>
++#include <cstring>
+ #include <errno.h>
+ #include <sys/time.h>
+ #include <unistd.h>
---
+++ emulators/vmips/patches/patch-vmips.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- vmips.cc.orig	2011-11-27 02:51:01.000000000 +0000
++++ vmips.cc
+@@ -50,6 +50,7 @@ with VMIPS; if not, write to the Free So
+ #include <cassert>
+ #include <csignal>
+ #include <cstdarg>
++#include <cstring>
+ #include <string>
+ #include <exception>
+ 
