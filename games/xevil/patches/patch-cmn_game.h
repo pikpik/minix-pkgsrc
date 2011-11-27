a206bfaee78613b89f238389b81450587c18f027
joerg <joerg>
2011-11-27 19:43:33 +0000

    Fix build with newer GCC.

---
+++ games/xevil/patches/patch-cmn_game.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- cmn/game.h.orig	2011-11-27 02:22:18.000000000 +0000
++++ cmn/game.h
+@@ -35,7 +35,7 @@
+ extern "C" {
+ #include <time.h>
+ }
+-#include <iostream.h>
++#include <iostream>
+ #include "utils.h"
+ #include "ui.h"
+ #include "world.h"
---
+++ games/xevil/patches/patch-cmn_world.cpp
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- cmn/world.cpp.orig	2011-11-27 02:22:11.000000000 +0000
++++ cmn/world.cpp
+@@ -35,7 +35,7 @@
+ #endif
+ 
+ // Include Files.
+-#include <iostream.h>
++#include <iostream>
+ 
+ #include "utils.h"
+ #include "coord.h"
