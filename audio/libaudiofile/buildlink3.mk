9279529ab1eced040e69be69541cd4b05dcf76ee
wiz <wiz>
2011-11-29 19:39:30 +0000

    Update to 0.3.1:
    Changes for Audio File Library version 0.3.1:
    
    * Fix installation of man pages.
    * Add support for Creative Voice File format.
    * Support u-law and A-law compression in Core Audio Format files.
    
    Changes for Audio File Library version 0.3.0:
    
    * Define AFframecount and AFfileoffset as 64-bit integers regardless of
      whether system specifies off_t as 64 bits.
    * Added support for Core Audio Format.
    * Added support for extensible WAVE format files.
    * Fixed leak of miscellaneous data buffers. (Thanks to Stefano Magni
      for finding and fixing this problem.)
    * Fixed default mapping between integer and floating-point audio data.
    * Fix handling of NeXT sound files with unspecified or inconsistent length.
    * Added support for miscellaneous data in IFF/8SVX files.
    * Added support for byte-swapped IRCAM sound files.
    * Refactored file parsing and writing.
    * Refactored audio conversion.
    * Updated and expanded documentation.

--- audio/libaudiofile/buildlink3.mk
+++ audio/libaudiofile/buildlink3.mk
@@ -1,13 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.14 2009/03/20 19:23:54 joerg Exp $
+# $NetBSD: buildlink3.mk,v 1.15 2011/04/21 21:06:25 hans Exp $
 
 BUILDLINK_TREE+=	libaudiofile
 
-.if !defined(AUDIOFILE_BUILDLINK3_MK)
-AUDIOFILE_BUILDLINK3_MK:=
+.if !defined(LIBAUDIOFILE_BUILDLINK3_MK)
+LIBAUDIOFILE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libaudiofile+=	libaudiofile>=0.2.1
 BUILDLINK_ABI_DEPENDS.libaudiofile+=	libaudiofile>=0.2.6nb1
 BUILDLINK_PKGSRCDIR.libaudiofile?=	../../audio/libaudiofile
-.endif # AUDIOFILE_BUILDLINK3_MK
+.endif # LIBAUDIOFILE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libaudiofile
