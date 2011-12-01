aadd5b339f6008a062099af1d8d7dcf8c65467f1
repo-copy <repo-copy>
2011-12-01 17:59:07 +0000

    Repo copy files

---
+++ x11/fltk13/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD: buildlink3.mk,v 1.21 2011/01/13 13:36:09 wiz Exp $
+
+BUILDLINK_TREE+=	fltk
+
+.if !defined(FLTK_BUILDLINK3_MK)
+FLTK_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.fltk+=	fltk>=1.3.0
+BUILDLINK_PKGSRCDIR.fltk?=	../../x11/fltk
+BUILDLINK_FILES.fltk+=		include/Fl/*
+
+.include "../../graphics/MesaLib/buildlink3.mk"
+.include "../../graphics/glu/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.include "../../x11/libXext/buildlink3.mk"
+.endif # FLTK_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-fltk
