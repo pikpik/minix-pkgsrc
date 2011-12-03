139edd1ad902d8e75b5c74ae2c0280b5282d7490
sbd <sbd>
2011-12-03 09:12:03 +0000

    Make gdbm_compat look like real ndbm.

--- databases/gdbm_compat/buildlink3.mk
+++ databases/gdbm_compat/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/10/17 08:44:32 obache Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/12/03 08:42:37 sbd Exp $
 #
 
 BUILDLINK_TREE+=	gdbm_compat
@@ -9,6 +9,9 @@ GDBM_COMPAT_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.gdbm_compat+=	gdbm_compat>=1.8.3
 BUILDLINK_ABI_DEPENDS.gdbm_compat+=	gdbm_compat>=1.9.1
 BUILDLINK_PKGSRCDIR.gdbm_compat?=	../../databases/gdbm_compat
+
+# Look like real ndbm.
+BUILDLINK_TRANSFORM+=	l:ndbm:gdbm_compat:gdbm
 
 .include "../../databases/gdbm/buildlink3.mk"
 .endif	# GDBM_COMPAT_BUILDLINK3_MK
