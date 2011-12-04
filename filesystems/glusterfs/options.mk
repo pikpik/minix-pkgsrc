0429b4f317bda91493ecac8d4770c40f63010c12
brook <brook>
2011-12-04 00:24:35 +0000

    Georeplication is apparently enabled by default, so it must be explicitly disabled if that option is not selected.

--- filesystems/glusterfs/options.mk
+++ filesystems/glusterfs/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.4 2011/09/27 12:45:02 manu Exp $
 
 PKG_OPTIONS_VAR=        PKG_OPTIONS.glusterfs
 PKG_SUPPORTED_OPTIONS=  georeplication
@@ -8,4 +8,6 @@ PKG_SUPPORTED_OPTIONS=  georeplication
 .if !empty(PKG_OPTIONS:Mgeoreplication)
 CONFIGURE_ARGS+=	--enable-georeplication
 PLIST_SRC+=		${PKGDIR}/PLIST.georeplication
+.else
+CONFIGURE_ARGS+=	--disable-georeplication
 .endif
