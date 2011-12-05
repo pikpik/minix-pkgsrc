31e0343f749b91ce66c003be1516033243d6a15c
repo-copy <repo-copy>
2011-12-05 17:41:32 +0000

    Repo copy files

---
+++ sysutils/edbus/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	edbus
+
+.if !defined(EDBUS_BUILDLINK3_MK)
+EDBUS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.edbus+=	edbus>=1.1.0
+BUILDLINK_PKGSRCDIR.edbus?=	../../sysutils/edbus
+
+.include "../../devel/ecore/buildlink3.mk"
+.include "../../sysutils/dbus/buildlink3.mk"
+.include "../../sysutils/efreet/buildlink3.mk"
+.endif # EDBUS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-edbus
