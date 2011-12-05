dba7dacc9c6e418d1d259199ba5860f499d17479
joerg <joerg>
2011-12-05 22:53:45 +0000

    Fix build with modern GCC

---
+++ multimedia/subtitleeditor/patches/patch-plugins_subtitleformats_advancedsubstationalpha_advancedsubstationalpha.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- plugins/subtitleformats/advancedsubstationalpha/advancedsubstationalpha.cc.orig	2011-12-05 21:08:30.000000000 +0000
++++ plugins/subtitleformats/advancedsubstationalpha/advancedsubstationalpha.cc
+@@ -24,6 +24,7 @@
+ #include <utility.h>
+ #include <iomanip>
+ #include <document.h>
++#include <cstdio>
+ 
+ /*
+  *
