18cac34867ef4fbddfcbed67d945f41dc5b25c06
sbd <sbd>
2011-12-06 00:19:26 +0000

    Recursive bump for lang/ocaml buildlink addition.

--- x11/ocaml-graphics/buildlink3.mk
+++ x11/ocaml-graphics/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.7 2006/07/08 23:11:15 jlam Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2009/03/20 19:25:48 joerg Exp $
 
 BUILDLINK_TREE+=	ocaml-graphics
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ocaml-graphics
 OCAML_GRAPHICS_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ocaml-graphics+=	ocaml-graphics>=3.08.2
-BUILDLINK_ABI_DEPENDS.ocaml-graphics?=	ocaml-graphics>=3.09.1
+BUILDLINK_ABI_DEPENDS.ocaml-graphics?=	ocaml-graphics>=3.12.0nb5
 BUILDLINK_PKGSRCDIR.ocaml-graphics?=	../../x11/ocaml-graphics
 
 .include "../../lang/ocaml/buildlink3.mk"
