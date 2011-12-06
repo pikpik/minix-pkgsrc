947e99bed214776ce53057773fd39d35fcad1c2f
joerg <joerg>
2011-12-06 19:47:32 +0000

    Fix build with newer GCC

---
+++ security/steghide/patches/patch-src_Graph.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/Graph.cc.orig	2011-12-06 17:01:21.000000000 +0000
++++ src/Graph.cc
+@@ -22,6 +22,7 @@
+ #include <list>
+ #include <map>
+ #include <vector>
++#include <climits>
+ 
+ #include "BitString.h"
+ #include "CvrStgFile.h"
