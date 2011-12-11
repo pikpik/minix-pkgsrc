acabc5e22b33b929d76e20446326525ccff55d95
marino <marino>
2011-12-11 11:59:06 +0000

    graphics/osg: Fix all platforms
    1) Modified current patch to support DragonFly
    2) Fixed includes for ffmeg headers
    3) The API for ffmpeg's sample format changed, so tell it to use old API.
       This can be removed when OpenSceneGraph version is upgraded
    4) The PLIST was missing ffmpeg plugin.  I guess it never built before.

---
+++ graphics/osg/patches/patch-src_osgPlugins_ffmpeg_FFmpegHeaders.hpp
@@ -0,0 +1,20 @@
+$NetBSD$
+
+--- src/osgPlugins/ffmpeg/FFmpegHeaders.hpp.orig	2010-03-19 08:03:02.000000000 +0000
++++ src/osgPlugins/ffmpeg/FFmpegHeaders.hpp
+@@ -6,11 +6,12 @@
+ extern "C"
+ {
+ #define __STDC_CONSTANT_MACROS
++#define FF_API_OLD_SAMPLE_FMT 0
+ #include <errno.h>    // for error codes defined in avformat.h
+ #include <stdint.h>
+-#include <avcodec.h>
+-#include <avformat.h>
+-#include <avdevice.h>
++#include <libavcodec/avcodec.h>
++#include <libavformat/avformat.h>
++#include <libavdevice/avdevice.h>
+ 
+ #ifdef USE_SWSCALE    
+     #include <swscale.h>
