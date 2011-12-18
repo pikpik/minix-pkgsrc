b244161cbd03af4e09ea09e11c68032d69d6e4c8
ryoon <ryoon>
2011-12-18 08:38:14 +0000

    Make php-sqlite and php-mysql conditional. Default is sqlite. Fix typo in MESSAGE. Bump PKGREVISION.
    Tnanks you, gdt@.

---
+++ www/php-owncloud/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.php-owncloud
+
+PKG_OPTIONS_REQUIRED_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql sqlite
+
+PKG_SUGGESTED_OPTIONS=	sqlite
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Use mysql or sqlite backend
+###
+.if !empty(PKG_OPTIONS:Mmysql)
+DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=5.2.0:../../databases/php-mysql
+.elif !empty(PKG_OPTIONS:Msqlite)
+DEPENDS+=	${PHP_PKG_PREFIX}-sqlite>=5.2.0:../../databases/php-sqlite
+.endif
