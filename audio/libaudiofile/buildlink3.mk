8f7ed552250a745f746579bf5d91856d9f7a56a7
sbd <sbd>
2011-12-22 07:47:43 +0000

    Include audio/alsa-lib/buildlink3.mk on Linux.

--- audio/libaudiofile/buildlink3.mk
+++ audio/libaudiofile/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2011/04/21 21:06:25 hans Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2011/11/29 19:39:30 wiz Exp $
 
 BUILDLINK_TREE+=	libaudiofile
 
@@ -8,6 +8,12 @@ LIBAUDIOFILE_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.libaudiofile+=	libaudiofile>=0.2.1
 BUILDLINK_ABI_DEPENDS.libaudiofile+=	libaudiofile>=0.2.6nb1
 BUILDLINK_PKGSRCDIR.libaudiofile?=	../../audio/libaudiofile
+
+.include "../../mk/bsd.prefs.mk"
+.if ${OPSYS} == "Linux"
+.  include "../../audio/alsa-lib/buildlink3.mk"
+.endif
+
 .endif # LIBAUDIOFILE_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-libaudiofile
