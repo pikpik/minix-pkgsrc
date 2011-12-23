1da9bf09b5cfacfd5cbfd322d4cc35cbf13238d6
joerg <joerg>
2011-12-23 01:19:16 +0000

    Newer GCC doesn't allow template instantiation with incomplete types, so make sure that the class definitions are present.

---
+++ finance/kmymoney2/patches/patch-kmymoney2_plugins_viewinterface.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- kmymoney2/plugins/viewinterface.h.orig	2008-10-29 12:01:13.000000000 +0000
++++ kmymoney2/plugins/viewinterface.h
+@@ -39,6 +39,8 @@ class KPopupMenu;
+ // Project Includes
+ 
+ #include <kmymoney/mymoneyaccount.h>
++#include <kmymoney/mymoneysplit.h>
++#include <kmymoney/mymoneytransaction.h>
+ #include <kmymoney/mymoneyinstitution.h>
+ #include <kmymoney/export.h>
+ class KMyMoneyViewBase;
