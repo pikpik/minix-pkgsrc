5f172c1c712f3116154a0f50cde95e6fd12ef714
marino <marino>
2012-01-07 20:44:32 +0000

    www/seamonkey: PR#45629 (webm assy optimization for DragonFly)
    These patches enable optimizations that allow video play w/o stuttering.
    Other i386 and x86_64 platforms need the same optimizations, so this is
    only a partial fix of the PR.

---
+++ www/seamonkey/patches/patch-mozilla_media_libvpx_vpx_config.h
@@ -0,0 +1,18 @@
+$NetBSD$
+
+--- mozilla/media/libvpx/vpx_config.h.orig	2011-11-07 21:08:15.000000000 +0000
++++ mozilla/media/libvpx/vpx_config.h
+@@ -12,11 +12,11 @@
+ /* 32 bit MacOS. */
+ #include "vpx_config_x86-darwin9-gcc.h"
+ 
+-#elif defined(__linux__) && defined(__i386__)
++#elif (defined(__linux__) || defined(__DragonFly__)) && defined(__i386__)
+ /* 32 bit Linux. */
+ #include "vpx_config_x86-linux-gcc.h"
+ 
+-#elif defined(__linux__) && defined(__x86_64__)
++#elif (defined(__linux__) || defined(__DragonFly__)) && defined(__x86_64__)
+ /* 64 bit Linux. */
+ #include "vpx_config_x86_64-linux-gcc.h"
+ 
