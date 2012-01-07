d88c746292b074be6802264ed0ba88599cb48ec1
marino <marino>
2012-01-07 18:47:39 +0000

    devel/nspr, devel/xulrunner: PR#45638 (DragonFly support)
    DragonFly has diverged from FreeBSD to the point where NSPR will
    not build LibreOffice on DragonFly due to being configured as DragonFly.
    These patches split out DragonFly as its own platform, and should not
    affect other platforms.

---
+++ devel/xulrunner/patches/patch-nsprpub_pr_include_md_prosdep.h
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- nsprpub/pr/include/md/prosdep.h.orig	2011-09-02 20:15:44.000000000 +0000
++++ nsprpub/pr/include/md/prosdep.h
+@@ -66,7 +66,7 @@ PR_BEGIN_EXTERN_C
+ #if defined(AIX)
+ #include "md/_aix.h"
+ 
+-#elif defined(FREEBSD)
++#elif defined(FREEBSD) || defined(__DragonFly__)
+ #include "md/_freebsd.h"
+ 
+ #elif defined(NETBSD)
