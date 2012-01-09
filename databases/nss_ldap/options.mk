723e6b2e7a60e34d213a2af3be53ecc7c4787b55
manu <manu>
2012-01-09 05:16:29 +0000

    Make Kerberos support optionnal and enabled by default in nss_ldap. It is useful to disable it if OpenLDAP was linked with pkgsrc's OpenSSL, since nss_ldap will pull the base system OpenSSL dependency through Kerberos libraries, leading to crashes because of multiple incomaptible libcrypto linked.

---
+++ databases/nss_ldap/options.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.nss_ldap
+PKG_SUPPORTED_OPTIONS=	krb5
+PKG_SUGGESTED_OPTIONS=	krb5
+
+.include "../../mk/bsd.options.mk"
+
+.if empty(PKG_OPTIONS:Mkrb5)
+SUBST_CLASSES+=		krb5
+SUBST_STAGE.krb5=	post-configure
+SUBST_FILES.krb5=	Makefile
+SUBST_SED.krb5=		-e "s/-lkrb5//; s/-lcom_err//; s/-lgssapi//;"
+.endif
