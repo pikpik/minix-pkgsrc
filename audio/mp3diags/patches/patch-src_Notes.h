d04648bc82c98f2b4891c2c3c7ad6792fd6eb9a5
hans <hans>
2012-01-11 19:25:08 +0000

    Fix build on SunOS.

---
+++ audio/mp3diags/patches/patch-src_Notes.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- src/Notes.h.orig	2010-02-13 07:58:04.000000000 +0100
++++ src/Notes.h	2012-01-02 11:26:42.140106730 +0100
+@@ -32,6 +32,10 @@
+ #include  "SerSupport.h"
+ #include  <boost/serialization/split_member.hpp>
+ 
++#ifdef ERR
++#undef ERR
++#endif
++
+ /*
+ Stores a message that is added while parsing a file. Multiple messages may be added, depending on the issues found.
+ 
