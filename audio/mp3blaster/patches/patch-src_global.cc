f9ea45852064a39287c8c445b2079e89ea7401a3
hans <hans>
2012-01-11 19:20:13 +0000

    Fix build on SunOS. Needs ncurses.

---
+++ audio/mp3blaster/patches/patch-src_global.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/global.cc.orig	2009-01-24 16:25:10.000000000 +0100
++++ src/global.cc	2011-12-30 18:09:44.289850163 +0100
+@@ -23,6 +23,7 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
++#include <strings.h>
+ #include <ctype.h>
+ #include <sys/types.h>
+ #include <dirent.h>
