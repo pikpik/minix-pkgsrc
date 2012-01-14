e7214d8160baa03a23e3a3528a4553a6e9aba28e
hans <hans>
2012-01-14 02:09:38 +0000

    Convert to USE_TOOLS=zip.

--- devel/xulrunner/mozilla-common.mk
+++ devel/xulrunner/mozilla-common.mk
@@ -1,4 +1,4 @@
-# $NetBSD: mozilla-common.mk,v 1.27 2011/11/15 17:23:25 tnn Exp $
+# $NetBSD: mozilla-common.mk,v 1.28 2011/11/27 10:01:05 tnn Exp $
 #
 # common Makefile fragment for mozilla packages based on gecko 2.0.
 # 
@@ -8,11 +8,9 @@
 # used by www/seamonkey/Makefile
 
 GNU_CONFIGURE=		yes
-USE_TOOLS+=		pkg-config perl gmake autoconf213 unzip
+USE_TOOLS+=		pkg-config perl gmake autoconf213 unzip zip
 USE_LANGUAGES+=		c99 c++
 UNLIMIT_RESOURCES+=	datasize
-
-BUILD_DEPENDS+=		zip>=2.3:../../archivers/zip
 
 PKG_DESTDIR_SUPPORT=	user-destdir
 CHECK_PORTABILITY_SKIP+=${MOZILLA_DIR}security/nss/tests/libpkix/libpkix.sh
--- graphics/zphoto/options.mk
+++ graphics/zphoto/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2008/04/12 22:43:02 jlam Exp $
+# $NetBSD: options.mk,v 1.4 2008/09/22 20:21:03 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.zphoto
 PKG_SUPPORTED_OPTIONS=	zip wx
@@ -25,7 +25,7 @@ CONFIGURE_ARGS+=	--disable-imlib2
 .endif
 
 .if !empty(PKG_OPTIONS:Mzip)
-DEPENDS+=	zip-[0-9]*:../../archivers/zip
+USE_TOOLS+=	zip:run
 .else
 CONFIGURE_ARGS+=	--disable-zip
 .endif
