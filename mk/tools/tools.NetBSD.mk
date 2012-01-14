01cf845d127c9ada4e0fd2ffe39acd61862c99e8
obache <obache>
2012-01-14 07:44:35 +0000

    gsed related clean up. * Stop to treat NetBSD's sed as GNU sed, not full compatible.   * Then, no need to reset TOOLS_PLATFORM.gsed for NetBSD if USE_TOOLS+=gsed and     real GNU sed is required.   * In addition, convert simple USE_TOOLS+=gsed to conditionally, without NetBSD. * convert {BUILD_,}DEPENDS+=gsed to USE_TOOLS, all tools from gsed are real gsed.

--- mk/tools/tools.NetBSD.mk
+++ mk/tools/tools.NetBSD.mk
@@ -1,4 +1,4 @@
-# $NetBSD: tools.NetBSD.mk,v 1.43 2010/11/10 19:59:06 drochner Exp $
+# $NetBSD: tools.NetBSD.mk,v 1.44 2011/11/17 19:01:31 joerg Exp $
 #
 # System-supplied tools for the NetBSD operating system.
 
@@ -37,7 +37,6 @@ TOOLS_PLATFORM.gawk?=		${TOOLS_PLATFORM.awk}
 .endif
 TOOLS_PLATFORM.grep?=		/usr/bin/grep
 TOOLS_PLATFORM.groff?=		/usr/bin/groff
-TOOLS_PLATFORM.gsed?=		${TOOLS_PLATFORM.sed}		# GNUish
 TOOLS_PLATFORM.gsoelim?=	${TOOLS_PLATFORM.soelim}	# GNUish
 TOOLS_PLATFORM.gunzip?=		/usr/bin/gunzip -f
 TOOLS_PLATFORM.gzcat?=		/usr/bin/gzcat
