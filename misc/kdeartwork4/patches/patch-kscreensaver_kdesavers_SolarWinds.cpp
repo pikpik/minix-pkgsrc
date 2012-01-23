a0443952d8c25dad372a49e4b1a01e66133f9c70
joerg <joerg>
2012-01-23 20:51:10 +0000

    Newer Qt requires a few more explicit GL/glu.h includes.

---
+++ misc/kdeartwork4/patches/patch-kscreensaver_kdesavers_SolarWinds.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- kscreensaver/kdesavers/SolarWinds.cpp.orig	2012-01-23 15:14:00.000000000 +0000
++++ kscreensaver/kdesavers/SolarWinds.cpp
+@@ -24,6 +24,7 @@
+ #include <stdlib.h>
+ #include <time.h>
+ #include <qtimer.h>
++#include <GL/glu.h>
+ #include "SolarWinds.h"
+ #include "SolarWinds.moc"
+ 
