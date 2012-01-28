8dadda5df0728a405dca33f7b12a45a40472ba91
marino <marino>
2012-01-28 11:59:06 +0000

    x11/efltk: Fix DragonFly take II
    Select the right prototype this time.

--- x11/efltk/patches/patch-src_core_filename_list.cpp
+++ x11/efltk/patches/patch-src_core_filename_list.cpp
@@ -2,12 +2,12 @@ $NetBSD$
 
 --- src/core/filename_list.cpp.orig	2003-01-26 01:16:49.000000000 +0000
 +++ src/core/filename_list.cpp
-@@ -49,7 +49,7 @@ int fl_filename_list(const char *dir, di
- #if defined(__hpux)
+@@ -46,7 +46,7 @@ int fl_casealphasort(struct dirent **a,
+ 
+ int fl_filename_list(const char *dir, dirent ***list, Fl_File_Sort_F *sort) 
+ {
+-#if defined(__hpux)
++#if defined(__hpux) || defined(__DragonFly__)
      // HP-UX defines the comparison function like this:
      return scandir(dir, list, 0, (int(*)(const dirent **, const dirent **))sort);
--#elif defined(__osf__)
-+#elif defined(__osf__) || defined(__DragonFly__)
-     // OSF, DU 4.0x
-     return scandir(dir, list, 0, (int(*)(dirent **, dirent **))sort);
- #elif defined(_AIX)
+ #elif defined(__osf__)
