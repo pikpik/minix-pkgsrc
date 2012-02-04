c409bdf55a5e1da32ab82912756138e6253ce7dc
sbd <sbd>
2012-02-04 07:53:26 +0000

    Add a cast to a strrchr call.

---
+++ audio/darkice/patches/patch-src_Util.cpp
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- src/Util.cpp.orig	2005-04-17 10:58:09.000000000 +0000
++++ src/Util.cpp
+@@ -288,7 +288,7 @@ Util :: fileAddDate ( const char * str )
+     strftime( strdate, 128, "[%m-%d-%Y-%H-%M-%S]", localtime (&now));
+ 
+     // search for the part before the extension of the file name
+-    if ( !(last = strrchr( str, '.')) ) {
++    if ( !(last = (char *) strrchr( str, '.')) ) {
+         last = (char *) str + strlen( str);
+     }
+ 
