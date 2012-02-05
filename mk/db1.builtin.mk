dc0c47d5ed296b2c41c3c0797fe4c0db43a17ebf
obache <obache>
2012-02-05 12:16:56 +0000

    Don't set BUILDLINK_LIBS for db1, db2 and db3, same as db4.
    It will add BDB libraries to LIBS, and may introduce unwanted linked against BDB
    libraries, whether needed or not.

--- mk/db1.builtin.mk
+++ mk/db1.builtin.mk
@@ -1,4 +1,4 @@
-# $NetBSD: db1.builtin.mk,v 1.20 2007/11/16 05:44:56 bjs Exp $
+# $NetBSD: db1.builtin.mk,v 1.21 2007/11/16 08:14:20 rillig Exp $
 
 BUILTIN_PKG:=	db1
 
@@ -76,7 +76,7 @@ BUILDLINK_LDADD.db1=	-ldb
 .    else
 BUILDLINK_LDADD.db1=	# empty
 .    endif
-BUILDLINK_LIBS.db1=	${BUILDLINK_LDADD.db1}
+#BUILDLINK_LIBS.db1=	${BUILDLINK_LDADD.db1}
 .  endif
 
 BUILDLINK_TARGETS+=	buildlink-db1-db185-h
