bb8a12582adb03c4cd006002ec77a2b0c955b2a4
sbd <sbd>
2012-02-05 20:05:56 +0000

    Include <cstdio> where needed for thinks like snprintf, fopen....

---
+++ audio/libspiff/patches/patch-src_SpiffPropsWriter.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/SpiffPropsWriter.cpp.orig	2008-09-21 18:21:45.000000000 +0000
++++ src/SpiffPropsWriter.cpp
+@@ -52,6 +52,7 @@
+ #include <spiff/SpiffToolbox.h>
+ #include <list>
+ #include <cstdlib>
++#include <cstdio>
+ 
+ namespace Spiff {
+ 
---
+++ audio/libspiff/patches/patch-src_SpiffXmlFormatter.cpp
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/SpiffXmlFormatter.cpp.orig	2008-09-21 18:21:45.000000000 +0000
++++ src/SpiffXmlFormatter.cpp
+@@ -48,6 +48,7 @@
+ #include <map>
+ #include <list>
+ #include <set>
++#include <cstdio>
+ 
+ namespace Spiff {
+ 
