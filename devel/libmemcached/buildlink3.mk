48ab812b7addfb405274fc8dd8e31660299eb74a
fhajny <fhajny>
2012-02-07 11:34:00 +0000

    Update libmemcached to 1.0.4.
    pkgsrc changes:
    * Fix build on SunOS
    * Add proper libtool support
    
    Upstream changes:
    
    1.0.4
    * Fix for memcached_dump().
    * Additional testing for memcached_stat_execute().
    
    1.0.3
    * Increased size of sort buffer used during Ketama.
    * Added support for new behavior to handle dead servers.
    * Overall haul of UDP IO.
    * Fixed C compile issue with memcached_exist()
    * Numerous bug fixes.
    * Clang support for OSX.
    * All commands now using vector send support.
    
    1.0.2
    * Dropped libmemcached/memcached_util.h (undocumented header file)
    * Added memcached_touch() and memcached_touch_by_key()
    * UDP support restructured to toggle on a complete memcached_st structure.

--- devel/libmemcached/buildlink3.mk
+++ devel/libmemcached/buildlink3.mk
@@ -1,8 +1,8 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2011/10/16 05:33:56 manu Exp $
 
 BUILDLINK_TREE+=	libmemcached
 
-.if !defined(libmemcached_BUILDLINK3_MK)
+.if !defined(LIBMEMCACHED_BUILDLINK3_MK)
 LIBMEMCACHED_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libmemcached+=	libmemcached>=0.53
