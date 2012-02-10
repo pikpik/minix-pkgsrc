09ec534923c407bf6744f4f0aa8580cf6013deca
drochner <drochner>
2012-02-10 17:19:02 +0000

    propagate xextproto dependency, installed header uses it

--- x11/libXtst/buildlink3.mk
+++ x11/libXtst/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.5 2010/04/09 08:41:23 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.6 2010/04/10 21:09:21 tnn Exp $
 
 .include "../../mk/bsd.fast.prefs.mk"
 
@@ -11,6 +11,7 @@ BUILDLINK_API_DEPENDS.libXtst+=	libXtst>=1.0.1
 BUILDLINK_PKGSRCDIR.libXtst?=	../../x11/libXtst
 
 .include "../../x11/recordproto/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
 .include "../../x11/libX11/buildlink3.mk"
 .include "../../x11/libXext/buildlink3.mk"
 .include "../../x11/libXi/buildlink3.mk"
