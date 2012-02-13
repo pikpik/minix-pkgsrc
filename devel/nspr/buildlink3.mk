6bce10e6c1ba672fd562f002b6b51f33cb11bc9d
drochner <drochner>
2012-02-13 12:54:43 +0000

    BUILDLINK_LDFLAGS must die. It causes nothing but damage, in particular makes random programs link against unwanted libraries.

--- devel/nspr/buildlink3.mk
+++ devel/nspr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.15 2010/12/23 12:24:00 fhajny Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2011/04/26 14:19:39 tnn Exp $
 
 BUILDLINK_TREE+=	nspr
 
@@ -15,8 +15,6 @@ BUILDLINK_LIBDIRS.nspr+=	lib/nspr
 BUILDLINK_RPATHDIRS.nspr+=	lib/nspr
 
 BUILDLINK_INCDIRS.nspr+=	include/nspr
-
-BUILDLINK_LDFLAGS.nspr+=	-lnspr4
 
 .endif	# NSPR_BUILDLINK3_MK
 
