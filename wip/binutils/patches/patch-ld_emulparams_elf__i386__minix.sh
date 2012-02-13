4c81a8b3d0760a0835dfc283e2c30caee84b507b
Ben Gras <ben@minix3.org>
2012-02-13 23:29:52 +0100

    no wip

--- wip/binutils/patches/patch-ld_emulparams_elf__i386__minix.sh
+++
@@ -1,8 +0,0 @@
-$NetBSD$
-
---- ld/emulparams/elf_i386_minix.sh.orig	Mon Jul 25 16:24:37 2011
-+++ ld/emulparams/elf_i386_minix.sh
-@@ -0,0 +1,3 @@
-+. ${srcdir}/emulparams/elf_i386.sh
-+. ${srcdir}/emulparams/elf_minix.sh
-+OUTPUT_FORMAT="elf32-i386-minix"
--- wip/binutils/patches/patch-ld_emulparams_elf__minix.sh
+++
@@ -1,6 +0,0 @@
-$NetBSD$
-
---- ld/emulparams/elf_minix.sh.orig	Mon Jul 25 16:24:37 2011
-+++ ld/emulparams/elf_minix.sh
-@@ -0,0 +1 @@
-+ELF_INTERPRETER_NAME=\"/usr/libexec/ld-elf.so.1\"
