71f3cdb5d661a5e73e48f476adc2718e521c017b
reinoud <reinoud>
2012-02-20 15:39:37 +0000

    Bump devel/allegro to version 4.2.2 from pkgsrc-wip.

--- devel/allegro/buildlink3.mk
+++ devel/allegro/buildlink3.mk
@@ -1,19 +1,13 @@
-# $NetBSD: buildlink3.mk,v 1.9 2007/01/20 14:16:29 joerg Exp $
+# $NetBSD$
 
 BUILDLINK_TREE+=	allegro
 
 .if !defined(ALLEGRO_BUILDLINK3_MK)
 ALLEGRO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.allegro+=	allegro>=4.1.13
-BUILDLINK_ABI_DEPENDS.allegro?=	allegro>=4.2.0nb1
+BUILDLINK_API_DEPENDS.allegro+=	allegro>=4.4.2
 BUILDLINK_PKGSRCDIR.allegro?=	../../devel/allegro
 
-.include "../../x11/libX11/buildlink3.mk"
-.include "../../x11/libXcursor/buildlink3.mk"
-.include "../../x11/libXext/buildlink3.mk"
-.include "../../x11/libXxf86vm/buildlink3.mk"
-.include "../../x11/xextproto/buildlink3.mk"
-.endif # ALLEGRO_BUILDLINK3_MK
+.endif	# ALLEGRO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-allegro
