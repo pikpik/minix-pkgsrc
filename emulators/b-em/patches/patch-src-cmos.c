efeafb7608f24693395a4da24f84f784bc8f4da7
reinoud <reinoud>
2012-02-20 15:45:19 +0000

    Add emulators/b-em, a Freeware Acorn BBC Micro Emulator with 2nd processors

---
+++ emulators/b-em/patches/patch-src-cmos.c
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- src/cmos.c.orig	2010-10-25 13:51:42.000000000 +0000
++++ src/cmos.c
+@@ -5,7 +5,7 @@
+ 
+ uint8_t cmos[64];
+ 
+-int cmosrw,cmosstrobe,cmosold,cmosaddr,cmosena;
++uint8_t cmosrw,cmosstrobe,cmosold,cmosaddr,cmosena;
+ 
+ void cmosupdate(uint8_t IC32, uint8_t sdbval)
+ {
---
+++ emulators/b-em/patches/patch-src-tapenoise.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/tapenoise.c.orig	2010-10-25 13:53:16.000000000 +0000
++++ src/tapenoise.c
+@@ -2,6 +2,7 @@
+   Tape noise (not very good)*/
+ 
+ #include <stdio.h>
++#include <math.h>
+ #include <allegro.h>
+ #include "b-em.h"
+ 
