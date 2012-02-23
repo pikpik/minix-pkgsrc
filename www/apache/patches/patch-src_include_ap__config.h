c3793f974eb03b993bfbee257acf57c5b49ec3f6
Ben Gras <ben@minix3.org>
2012-02-23 19:00:59 +0100

    apache fix

--- www/apache/patches/patch-src_include_ap__config.h
+++ www/apache/patches/patch-src_include_ap__config.h
@@ -1,8 +1,22 @@
-$NetBSD: patch-am,v 1.8 2005/08/22 16:19:00 reed Exp $
+$NetBSD$
 
---- src/include/ap_config.h.orig	2006-07-12 08:16:05.000000000 +0000
+--- src/include/ap_config.h.orig	Wed Jul 12 08:16:05 2006
 +++ src/include/ap_config.h
-@@ -700,8 +700,8 @@ extern char *crypt();
+@@ -73,6 +73,13 @@ stat() properly */
+ 
+ #include "os.h"
+ 
++
++#if defined(__minix)
++#define HAVE_FCNTL_SERIALIZED_ACCEPT
++#define USE_FCNTL_SERIALIZED_ACCEPT
++#warning mnix
++#endif
++
+ /* Define one of these according to your system. */
+ #if defined(MINT)
+ typedef int rlim_t;
+@@ -700,8 +707,8 @@ extern char *crypt();
  #undef NO_SETSID
  #define HAVE_SYSLOG 1
  
@@ -13,7 +27,7 @@ $NetBSD: patch-am,v 1.8 2005/08/22 16:19:00 reed Exp $
  #include <osreldate.h>
  #endif
  #define HAVE_GMTOFF 1
-@@ -1305,7 +1305,7 @@ extern int ap_execve(const char *filenam
+@@ -1305,7 +1312,7 @@ extern int ap_execve(const char *filename, char * cons
   * so we don't have to.  Sigh...
   */
  
