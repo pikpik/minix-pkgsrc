4c8072e3cb8d1ee728677cbca3542c23ccca3bac
adam <adam>
2012-02-26 23:19:02 +0000

    Changes 1.7.4: * Fix5010 Line intersection non fatal error * Fix2992 Use native Separators in Filename-edit-widget * Fix4633 'Save as...DXF' does not add file extension * Fix4412 Ftools: Input layers are not presented in alphabetical order * Fix4326 Use project title as default filename in Save and Save as dialogs * Fix4623 GPS plugin: 'Save as...' does not add file extension * Fix4652 Remove "unsynced" EPSG CRS that are not listed in GDAL * Fix4625 Enable wait cursor when WMS search is ongoing * Fix3845 Add support for modulo operator (%) in rule based rendering * Fix4193 GRASS Rasters are not drawn with user-defined style * Fix4557 Fix crash when project is not yet saved * Fix4315 'Stretch Histogram to full dataset' toolbar button does not work with   GRASS rasters * Fix4632 More precise composer map rotation * Fix4664 Fix label orientation with line dependent position * Fix2435 Fix Query builder from layer properties when layer name in cyrrilic * Fix4684 Skip attribute values with invalid variant type * Fix4549 Update QgsGenericProjectionSelector bindings * Fix4129 GDAL provider: add statusChanged signal and fix debugging output * Fix4379 If set 'prompt for CRS', CRS is asked when creating new shapefile   layer * Fix4694 Hourglass not shown when classifying * Fix4478 Actions using %% do not work from identify results * Fix4646 Editing of table of attributes goes Wild * Fix3963 Crash when applying a graduated vector style * Fix3185,4689 Gps Tools: Allow user to add new ports * Fix4402 QGIS crashes displaying overview if diagrams are used * Fix4371 Identify tool does not work with reprojected WMS layers * Fix4305 Query builder: added ' ' around numeric fields (wrong) * Fix4217 Allow up to 5 decimal places for width/size Scale Field * Fix4452 Support for cyrillic symbols in raster metadata * Fix4558 Enhancements in OGR Provider * Fix3201 Show warning when user tried to create field 'shape' in shapefile

---
+++ geography/qgis/patches/patch-src_core_qgsapplication.cpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Use UNIX prefix path on Mac OS X.
+
+--- src/core/qgsapplication.cpp.orig	2012-02-26 22:09:01.000000000 +0000
++++ src/core/qgsapplication.cpp
+@@ -60,7 +60,7 @@ QString QgsApplication::mConfigPath = QD
+ QgsApplication::QgsApplication( int & argc, char ** argv, bool GUIenabled, QString customConfigPath )
+     : QApplication( argc, argv, GUIenabled )
+ {
+-#if defined(Q_WS_MACX) || defined(Q_WS_WIN32) || defined(WIN32)
++#if defined(Q_WS_WIN32) || defined(WIN32)
+   setPrefixPath( applicationDirPath(), true );
+ #else
+   QDir myDir( applicationDirPath() );
