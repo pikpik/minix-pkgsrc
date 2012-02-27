e3eeb794e36b41023a76096ed89c830618d66934
adam <adam>
2012-02-27 13:58:05 +0000

    Changes 1.4.1: * Support for the Beneath a Steel Sky Enhanced Soundtrack by James Woodcock * Slight graphical improvement for the PlayStation version of Broken Sword 2 * Several bugfixes for Lands of Lore * More logical sound settings for SCI games * A fixed crash in the VGA version of Quest for Glory 1

--- games/scummvm/options.mk
+++ games/scummvm/options.mk
@@ -1,10 +1,14 @@
-# $NetBSD: options.mk,v 1.5 2006/03/09 21:04:44 jlam Exp $
+# $NetBSD: options.mk,v 1.6 2007/02/02 17:58:02 joerg Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.scummvm
-PKG_SUPPORTED_OPTIONS=	sdl x11
+PKG_SUPPORTED_OPTIONS=	sdl x11 fluidsynth
 PKG_SUGGESTED_OPTIONS=	sdl
 
 .include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mfluidsynth)
+.include "../../audio/fluidsynth/buildlink3.mk"
+.endif
 
 .if !empty(PKG_OPTIONS:Msdl)
 CONFIGURE_ARGS+=	--backend=sdl
