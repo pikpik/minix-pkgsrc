6fd0ea043020ed3e8817b8196ce7b3e77a01ead5
adam <adam>
2012-03-07 16:27:35 +0000

    Changes 3.0.1: * OpenGL ES 1.1, and OpenGL ES 2.0 support * OpenGL 3.x and 4.x support along with associated OpenGL extensions * Supoort for Android on tablets and phones * Support for IOS on tablets and phones (end users applications have already   been accepted on the App Store) * Introduction of Present3D application for immersive 3D presentations * New extensible serializers that provide new .osgb binary, .osgt ascii and   .osgx xml file native formats * New generalized serializable metadata architecture * New osgQt library that makes it straight forward to integrate !OpenSceneGraph   with Qt, including web browsing via QWebKit. * New FBX plugin based on the Audodesk FBX DSK that enables reading of the the   .fbx 3D authoring and interchange format * New directshow plugin for handling movies under Windows * New pov plugin for exporting a scene to POV-Ray format, enabling use of   POV-Ray to do photo-realistic ray tracing * New ply plugin for reading ply files * Improvements to osgText that enable bevelling of 3D text and highter quality   2D fonts * Improvements to osgVolume enabling high quality ray traced volume rendering * Improvements to osgManipulator making it more flexible * Improvements to osgParticle to support shader based particle effects * Revamped 3ds plugin for reading and writing of .3ds files * Revamped dxf plugin for reading and writing of .dxf files * Support for Cocoa and 64bit OSX build * Improved thread safety and efficiency of database paging * New Texture and buffer object pools for efficiently managing GL object   resources

--- graphics/osg/patches/patch-src_osgPlugins_ffmpeg_FFmpegHeaders.hpp
+++ graphics/osg/patches/patch-src_osgPlugins_ffmpeg_FFmpegHeaders.hpp
@@ -1,12 +1,11 @@
 $NetBSD$
 
---- src/osgPlugins/ffmpeg/FFmpegHeaders.hpp.orig	2010-03-19 08:03:02.000000000 +0000
+Point to correct header files.
+
+--- src/osgPlugins/ffmpeg/FFmpegHeaders.hpp.orig	2012-03-05 21:20:14.000000000 +0000
 +++ src/osgPlugins/ffmpeg/FFmpegHeaders.hpp
-@@ -6,11 +6,12 @@
- extern "C"
- {
- #define __STDC_CONSTANT_MACROS
-+#define FF_API_OLD_SAMPLE_FMT 0
+@@ -9,9 +9,9 @@ extern "C"
+ #define FF_API_OLD_SAMPLE_FMT 0
  #include <errno.h>    // for error codes defined in avformat.h
  #include <stdint.h>
 -#include <avcodec.h>
