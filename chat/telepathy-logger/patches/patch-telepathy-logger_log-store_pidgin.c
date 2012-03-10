d8c85d1a98f59d075d8b4cf070ce5f7bd557bc3c
obache <obache>
2012-03-10 08:38:34 +0000

    Add missing RCS id to patch file.

--- chat/telepathy-logger/patches/patch-telepathy-logger_log-store_pidgin.c
+++ chat/telepathy-logger/patches/patch-telepathy-logger_log-store_pidgin.c
@@ -1,3 +1,5 @@
+$NetBSD$
+
 --- telepathy-logger/log-store-pidgin.c.orig	2011-03-25 00:13:54.000000000 +0100
 +++ telepathy-logger/log-store-pidgin.c	2012-01-22 14:26:33.292976356 +0100
 @@ -21,7 +21,9 @@
