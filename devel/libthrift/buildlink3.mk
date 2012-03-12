b9cb237b2099133cd986d0bc3715b032aa06c988
sbd <sbd>
2012-03-12 07:08:40 +0000

    Add security/openssl buildlink to the buildlink3.mk file.

--- devel/libthrift/buildlink3.mk
+++ devel/libthrift/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2010/05/01 02:49:10 tonnerre Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2011/02/11 21:22:02 tnn Exp $
 
 BUILDLINK_TREE+=	libthrift
 
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.libthrift+=	libthrift>=0.2.0
 BUILDLINK_ABI_DEPENDS.libthrift+=	libthrift>=0.5.0nb1
 BUILDLINK_PKGSRCDIR.libthrift?=		../../devel/libthrift
 
+.include "../../security/openssl/buildlink3.mk"
 .include "../../devel/zlib/buildlink3.mk"
 .include "../../devel/libevent/buildlink3.mk"
 
