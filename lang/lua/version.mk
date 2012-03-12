8db6bcdf9293b75a9083b5605d92bc1b868ebdd4
wiz <wiz>
2012-03-12 15:49:27 +0000

    Update to 5.1.5. Lua 5.1.5 released. This is a bug-fix release. (no further changelog found)
    Remove master site that doesn't have new tarball.
    Fix pkglint warning in patch-ac.

--- lang/lua/version.mk
+++ lang/lua/version.mk
@@ -1,6 +1,6 @@
-# $NetBSD$
+# $NetBSD: version.mk,v 1.1 2009/01/11 23:15:55 minskim Exp $
 
-LUA_VERSION=	5.1.4
+LUA_VERSION=	5.1.5
 
 LUA_VERSION_MAJOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\1/}
 LUA_VERSION_MINOR=	${LUA_VERSION:C/([0-9]+)\.([0-9]+)\.([0-9]+)/\2/}
