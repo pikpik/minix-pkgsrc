310c3c2bc828589366cbe6ba84e136766bb09b15
markd <markd>
2012-03-19 10:07:52 +0000

    Update to 4.5.1
    Required for KDE SC 4.8

--- multimedia/phonon/buildlink3.mk
+++ multimedia/phonon/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.12 2012/02/06 12:41:06 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.8 2011/01/13 13:36:53 wiz Exp $
 
 BUILDLINK_TREE+=	phonon
 
 .if !defined(PHONON_BUILDLINK3_MK)
 PHONON_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.phonon+=	phonon>=4.1.83
-BUILDLINK_ABI_DEPENDS.phonon?=	phonon>=4.4.2nb7
+BUILDLINK_API_DEPENDS.phonon+=	phonon>=4.5.0
+BUILDLINK_ABI_DEPENDS.phonon?=	phonon>=4.5.0
 BUILDLINK_PKGSRCDIR.phonon?=	../../multimedia/phonon
 
 .include "../../x11/qt4-libs/buildlink3.mk"
