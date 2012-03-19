d4cdea140da54c2fb01df21fa99e4b0a0beca443
ryoon <ryoon>
2012-03-19 20:33:32 +0000

    Add MASTER_SITE_MOZILLA_ALL to MASTER_SITES because ESR version is hosted at ftp.mozilla.org (only?).

--- devel/xulrunner10/dist.mk
+++ devel/xulrunner10/dist.mk
@@ -1,4 +1,4 @@
-# $NetBSD: dist.mk,v 1.28 2012/03/06 12:34:08 ryoon Exp $
+# $NetBSD: dist.mk,v 1.1.1.1 2012/03/15 08:58:26 ryoon Exp $
 #
 # used by devel/xulrunner10/Makefile
 # used by www/firefox10/Makefile
@@ -7,7 +7,8 @@ DISTNAME=	firefox-${FIREFOX_VER}esr.source
 FIREFOX_VER=	${MOZ_BRANCH}${MOZ_BRANCH_MINOR}
 MOZ_BRANCH=	10.0
 MOZ_BRANCH_MINOR=	.3
-MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}/source/}
+MASTER_SITES=	${MASTER_SITE_MOZILLA:=firefox/releases/${FIREFOX_VER}esr/source/} \
+		${MASTER_SITE_MOZILLA_ALL:=firefox/releases/${FIREFOX_VER}esr/source/}
 EXTRACT_SUFX=	.tar.bz2
 
 DISTINFO_FILE=	${.CURDIR}/../../devel/xulrunner10/distinfo
