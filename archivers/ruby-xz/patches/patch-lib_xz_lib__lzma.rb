63db4a5a331003dfbbcd0c07f6dabe96f1231b65
taca <taca>
2012-03-20 07:26:05 +0000

    Update ruby-xz package to 0.1.1.
    == 0.1.0
    
    * <b>Add XZ::StreamReader and XZ::StreamWriter for io-like behaviour.</b>
    * New dependency on the +io-like+ gem.
    * <b>Add Ruby 1.8 compatibility.</b> Thanks to Christoph Plank.
    * We now have proper unit tests.

---
+++ archivers/ruby-xz/patches/patch-lib_xz_lib__lzma.rb
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Use an absolute path for a library to load.
+
+--- lib/xz/lib_lzma.rb.orig	2012-03-19 09:42:58.000000000 +0000
++++ lib/xz/lib_lzma.rb
+@@ -71,7 +71,7 @@ module XZ
+     :lzma_buf_error,
+     :lzma_prog_error
+     
+-    ffi_lib ['lzma.so.2', 'lzma.so', 'lzma']
++    ffi_lib ['lzma.@SHLIB_EXT@.2', 'lzma.@SHLIB_EXT@', 'lzma'].map! { |f|  File.join('@LOCALBASE@/lib', f) }
+     
+     attach_function :lzma_easy_encoder, [:pointer, :uint32, :int], :int
+     attach_function :lzma_code, [:pointer, :int], :int
