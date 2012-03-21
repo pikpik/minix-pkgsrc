338a0b7b07fdb047677ad464a1cee8519eb42322
drochner <drochner>
2012-03-21 10:40:25 +0000

    -make libxul.so link against libkvm on NetBSD because it contains  references to its functions  (patch-xulrunner_app_Makefile.in is probably unnecessary now, didn't   test) -propagate dependency on nspr -- at least the .pc file refers to it bump PKGREV

--- devel/xulrunner/buildlink3.mk
+++ devel/xulrunner/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.22 2012/03/06 13:37:32 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.23 2012/03/15 08:30:06 ryoon Exp $
 
 BUILDLINK_TREE+=	xulrunner
 
@@ -22,6 +22,7 @@ pkgbase := xulrunner
 . include "../../devel/libgnomeui/buildlink3.mk"
 . include "../../sysutils/gnome-vfs/buildlink3.mk"
 .endif
+.include "../../devel/nspr/buildlink3.mk"
 .endif # XULRUNNER_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-xulrunner
