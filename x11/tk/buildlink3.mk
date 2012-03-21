041bb86ac1ddcd711686cb087379b104af567387
obache <obache>
2012-03-21 15:32:28 +0000

    Set WISH here, same as PERL5 in lang/perl5/buildlink3.mk.

--- x11/tk/buildlink3.mk
+++ x11/tk/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2011/11/13 13:55:59 shattered Exp $
+# $NetBSD: buildlink3.mk,v 1.25 2012/03/05 13:36:37 obache Exp $
 
 BUILDLINK_TREE+=	tk
 
@@ -20,6 +20,10 @@ BUILDLINK_TRANSFORM+=	l:tk8.4:tk84
 TKCONFIG_SH?=	${BUILDLINK_PREFIX.tk}/lib/tkConfig.sh
 
 _TOOLS_USE_PKGSRC.wish=	yes
+
+FIND_PREFIX:=		TOOLS_PREFIX.wish=tk
+.include "../../mk/find-prefix.mk"
+WISH=			${TOOLS_PREFIX.wish}/bin/wish
 
 .include "../../lang/tcl/buildlink3.mk"
 .include "../../mk/pthread.buildlink3.mk"
