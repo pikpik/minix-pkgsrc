30b32c92ac20df209521cd42a97ad870b2f016e8
markd <markd>
2012-03-21 20:53:44 +0000

    Import libkface 2.5.0
    Libkface is a Qt/C++ wrapper around LibFace library to perform face
    recognition and detection over pictures 
    
    This library is used by kipi-plugins, digiKam and others kipi host programs.

---
+++ graphics/libkface/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	libkface
+
+.if !defined(LIBKFACE_BUILDLINK3_MK)
+LIBKFACE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.libkface+=	libkface>=2.5.0
+BUILDLINK_PKGSRCDIR.libkface?=	../../graphics/libkface
+
+.include "../../graphics/opencv/buildlink3.mk"
+.include "../../x11/kdelibs4/buildlink3.mk"
+.endif	# LIBKFACE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-libkface
