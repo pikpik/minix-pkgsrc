d36fce909d1a8d1e3b8f6560f3b7e573440d89d9
markd <markd>
2012-03-26 09:15:04 +0000

    fix build failure that occurs when random order of includes in a generated file did not happen to include <KSharedConfig> prior to its need here.

---
+++ print/kbibtex/patches/patch-src_gui_bibtex_bibtexfileview.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- src/gui/bibtex/bibtexfileview.h.orig	2011-11-20 20:36:54.000000000 +0000
++++ src/gui/bibtex/bibtexfileview.h
+@@ -21,6 +21,7 @@
+ #define KBIBTEX_GUI_BIBTEXFILEVIEW_H
+ 
+ #include <QTreeView>
++#include <KSharedConfig>
+ 
+ #include <kbibtexgui_export.h>
+ 
