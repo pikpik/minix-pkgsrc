521bea8699b6df013214641fe23747e1ed697bba
ryoon <ryoon>
2012-03-31 16:06:56 +0000

    Bump PKGREVISION
    * Fix build on FreeBSD 9.0
    ** Patches are borrowed from FreeBSD ports
    ** Split FreeBSD support from DragonFly/FreeBSD support

---
+++ sysutils/hal/patches/patch-hald_freebsd_hf-volume.c
@@ -0,0 +1,20 @@
+$NetBSD$
+
+* From FreeBSD ports repository to fix build on FreeBSD 9.0 RELEASE.
+
+--- hald/freebsd/hf-volume.c.orig	2009-08-24 12:42:29.000000000 +0000
++++ hald/freebsd/hf-volume.c
+@@ -86,9 +86,12 @@ hf_volume_resolve_fuse (const char *spec
+         {
+           if (strcmp(fields[0], special) == 0)
+ 	    {
++	      char *ret;
++
++	      ret = g_strdup(fields[1]);
+ 	      g_strfreev(fields);
+ 	      g_strfreev(lines);
+-	      return g_strdup(fields[1]);
++	      return ret;
+ 	    }
+ 	}
+       g_strfreev(fields);
