0fe66a8048079c26d2ab1f612c3a228bbc26e068
dholland <dholland>
2012-03-31 23:47:51 +0000

    Restrict depends to avoid matching the kde4 package

--- graphics/libkdcraw-kde3/buildlink3.mk
+++ graphics/libkdcraw-kde3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.16 2012/03/03 00:12:34 wiz Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/19 22:29:41 markd Exp $
 
 BUILDLINK_TREE+=	libkdcraw
 
 .if !defined(LIBKDCRAW_BUILDLINK3_MK)
 LIBKDCRAW_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=0.1.9nb11
+BUILDLINK_API_DEPENDS.libkdcraw+=	libkdcraw>=0.1.1<4.0
+BUILDLINK_ABI_DEPENDS.libkdcraw?=	libkdcraw>=0.1.9nb11<4.0
 BUILDLINK_PKGSRCDIR.libkdcraw?=	../../graphics/libkdcraw-kde3
 
 .include "../../x11/qt3-libs/buildlink3.mk"
--- graphics/libkexiv2-kde3/buildlink3.mk
+++ graphics/libkexiv2-kde3/buildlink3.mk
@@ -1,12 +1,12 @@
-# $NetBSD: buildlink3.mk,v 1.11 2010/07/14 11:11:14 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/03/19 23:01:43 markd Exp $
 
 BUILDLINK_TREE+=	libkexiv2
 
 .if !defined(LIBKEXIV2_BUILDLINK3_MK)
 LIBKEXIV2_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=0.1.1
-BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.9nb4
+BUILDLINK_API_DEPENDS.libkexiv2+=	libkexiv2>=0.1.1<4.0
+BUILDLINK_ABI_DEPENDS.libkexiv2+=	libkexiv2>=0.1.9nb4<4.0
 BUILDLINK_PKGSRCDIR.libkexiv2?=	../../graphics/libkexiv2-kde3
 
 .include "../../graphics/exiv2/buildlink3.mk"
