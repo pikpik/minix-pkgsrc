1cf79faaa43add99727b55f25ee8cf2333f6ced2
wiz <wiz>
2012-04-08 20:58:05 +0000

    Fix paths after import.

--- math/ppl/buildlink3.mk
+++ math/ppl/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.9 2010/07/15 21:35:10 asau Exp $
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/04/08 20:52:33 wiz Exp $
 
 BUILDLINK_TREE+=	ppl
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	ppl
 PPL_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.ppl+=	ppl>=0.11.2
-BUILDLINK_PKGSRCDIR.ppl?=	../../wip/ppl
+BUILDLINK_PKGSRCDIR.ppl?=	../../math/ppl
 
 .endif # PPL_BUILDLINK3_MK
 
