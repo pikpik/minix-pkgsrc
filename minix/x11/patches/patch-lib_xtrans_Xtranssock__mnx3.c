1c6dfbd5a8e050b3d1a35788397972cf63bc59b0
Ben Gras <ben@minix3.org>
2012-04-09 15:53:28 +0200

    fix for _RESRTICT for x11

--- minix/x11/patches/patch-lib_xtrans_Xtranssock__mnx3.c
+++ minix/x11/patches/patch-lib_xtrans_Xtranssock__mnx3.c
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- lib/xtrans/Xtranssock_mnx3.c.orig	Thu Jul 14 12:00:30 2011
+--- lib/xtrans/Xtranssock_mnx3.c.orig	Sun Aug  1 11:51:53 2010
 +++ lib/xtrans/Xtranssock_mnx3.c
-@@ -2661,7 +2661,10 @@ static char* unix_nolisten[] = { "local" , NULL };
+@@ -2661,17 +2661,20 @@ static char* unix_nolisten[] = { "local" , NULL };
  
  /* Starting point of a socket library */
  #include <net/netlib.h>
@@ -14,3 +14,36 @@ $NetBSD$
  #include <net/gen/in.h>
  #include <net/gen/tcp.h>
  #include <net/gen/tcp_io.h>
+ 
+-static int _tcp_getsockname(int socket, struct sockaddr *_RESTRICT address,
+-	socklen_t *_RESTRICT address_len, nwio_tcpconf_t *tcpconfp);
+-
+ #if 0
+-static int getsockname(int socket, struct sockaddr *_RESTRICT address,
+-	socklen_t *_RESTRICT address_len)
++static int _tcp_getsockname(int socket, struct sockaddr *_restrict address,
++	socklen_t *_restrict address_len, nwio_tcpconf_t *tcpconfp);
++
++static int getsockname(int socket, struct sockaddr *_restrict address,
++	socklen_t *_restrict address_len)
+ {
+ 	int r;
+ 	nwio_tcpconf_t tcpconf;
+@@ -2689,10 +2692,9 @@ static int getsockname(int socket, struct sockaddr *_R
+ 	}
+ 	assert(0);
+ }
+-#endif
+ 
+-static int _tcp_getsockname(int socket, struct sockaddr *_RESTRICT address,
+-	socklen_t *_RESTRICT address_len, nwio_tcpconf_t *tcpconfp)
++static int _tcp_getsockname(int socket, struct sockaddr *_restrict address,
++	socklen_t *_restrict address_len, nwio_tcpconf_t *tcpconfp)
+ {
+ 	socklen_t len;
+ 	struct sockaddr_in sin;
+@@ -2710,3 +2712,4 @@ static int _tcp_getsockname(int socket, struct sockadd
+ 
+ 	return 0;
+ }
++#endif
