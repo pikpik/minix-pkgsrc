ebd7693f49b1022a3a640db4db725ba35308c895
Thomas Cort <tcort@minix3.org>
2012-04-11 10:08:28 +0000

    lang/gcc44: version bump to 4.4.7

--- lang/gcc44/patches/patch-gcc_ginclude_stddef.h
+++ lang/gcc44/patches/patch-gcc_ginclude_stddef.h
@@ -3,9 +3,9 @@ $NetBSD$
 Fix build on NetBSD i386/amd64 after the ansi.h header include protection
 name change.
 
---- gcc/ginclude/stddef.h.orig	2009-04-09 23:23:07.000000000 +0000
+--- gcc/ginclude/stddef.h.orig	Wed Apr 11 02:32:21 2012
 +++ gcc/ginclude/stddef.h
-@@ -53,6 +53,11 @@ see the files COPYING3 and COPYING.RUNTI
+@@ -53,6 +53,11 @@ see the files COPYING3 and COPYING.RUNTIME respectivel
     one less case to deal with in the following.  */
  #if defined (__BSD_NET2__) || defined (____386BSD____) || (defined (__FreeBSD__) && (__FreeBSD__ < 5)) || defined(__NetBSD__)
  #include <machine/ansi.h>
