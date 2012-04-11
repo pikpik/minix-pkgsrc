e4de8d7165362aa20a7ead9fa14ef35e33cc345c
darcy <darcy>
2012-04-11 19:32:17 +0000

    Forgot to add in the fixes to actually run it.

---
+++ net/gitso/patches/patch-GitsoThread.py
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Test for BSD systems
+
+--- share/gitso/GitsoThread.py.orig	2010-02-22 02:48:04.000000000 +0000
++++ share/gitso/GitsoThread.py
+@@ -122,7 +122,7 @@ class GitsoThread(threading.Thread):
+ 		
+ 		connection = []
+ 		listen = []
+-		if sys.platform == 'darwin' or sys.platform.find('linux') != -1:
++		if sys.platform == 'darwin' or sys.platform.find('linux') != -1 or sys.platform.find('bsd'):
+ 			if self.host <> "":
+ 				connection = os.popen('LANG=C netstat -an | grep 5500 | grep ESTABLISHED').readlines()
+ 			else:
