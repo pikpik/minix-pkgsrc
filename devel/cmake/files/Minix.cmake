d0cb499c79c5df7d83884b6310777cf0c8c82dde
Ben Gras <ben@minix3.org>
2012-04-18 01:36:45 +0200

    dynamicify clang further

--- devel/cmake/files/Minix.cmake
+++ devel/cmake/files/Minix.cmake
@@ -1,22 +1,11 @@
-# File by Ivan Galin
-
-# Minix doesn't support shared libs
-SET_PROPERTY(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
-
-SET(CMAKE_CXX_LINK_SHARED_LIBRARY )
-SET(CMAKE_CXX_LINK_MODULE_LIBRARY )
-SET(CMAKE_C_LINK_SHARED_LIBRARY )
-SET(CMAKE_C_LINK_MODULE_LIBRARY )
-
-SET(CMAKE_DL_LIBS "" )
-SET(CMAKE_SHARED_LIBRARY_C_FLAGS "")            # -pic
-SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "")       # -shared
+SET(CMAKE_DL_LIBS "")
+SET(CMAKE_SHARED_LIBRARY_C_FLAGS "-fPIC")            # -pic 
+SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-shared")       # -shared
 SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")         # +s, flag for exe link to use shared lib
-SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "")       # -rpath
-SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP "")   # : or empty
+SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "-Wl,-rpath,")       # -rpath
+SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")   # : or empty
+SET(CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG "-Wl,-rpath-link,")
+SET(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-Wl,-soname,")
+SET(CMAKE_EXE_EXPORTS_C_FLAG "-Wl,--export-dynamic")
 
-# small Hack while compile CMake - GCC in Minix not use /usr/pkg/include
-# as default path for includes.
-SET(CMAKE_CXX_LINK_FLAGS "-lssl -lbz2 -lcrypto ${CMAKE_CXX_LINK_FLAGS}")
-
-INCLUDE(Platform/UnixPaths) 
+INCLUDE(Platform/UnixPaths)
