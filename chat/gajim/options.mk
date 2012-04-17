1187cf62bcc728dea81824b84336b0aef9b35489
gls <gls>
2012-04-17 20:57:09 +0000

    Update chat/gajim to 0.15.
    pkgsrc changes:
    ---------------
    - Add a patch to fix CVE-2012-2093, taken from upstream repository.
    - s/py-amkCrypto/py-crypto (py-amkCrypto is not maintained anymore upstream)
    
    upstream changes:
    -----------------
    
    Gajim 0.15 (18 March 2012)
    
    * Plugin system
    * Whiteboard (via a plugin)
    * Message archiving
    * Stream managment
    * IBB
    * Nested roster group
    * Roster filtrering
    * UPower support
    * GPG support for windows
    * Spell checking support for windows
    
    Gajim 0.14.4 (22 July 2011)
    
    * Fix translation issue
    * other minor fixes
    
    Gajim 0.14.3 (19 June 2011)
    
    * Fix history viewer
    * Fix closing roster window
    * Prevent some erros with metacontacts
    
    Gajim 0.14.2 (07 June 2011)
    
    * Fix CPU usage when testing file transfer proxies
    * Fix invalid XML char regex
    * Fix subscription request window handling
    * Fix URL display in chat message banner
    * Other minor bugfixes

--- chat/gajim/options.mk
+++ chat/gajim/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.1.1.1 2010/12/24 00:02:29 wiz Exp $
+# $NetBSD: options.mk,v 1.2 2011/05/14 14:28:07 obache Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.gajim
 PKG_SUPPORTED_OPTIONS=	avahi gnome gnome-keyring ssl
@@ -9,7 +9,7 @@ PKG_SUGGESTED_OPTIONS=	ssl
 # ssl
 .if !empty(PKG_OPTIONS:Mssl)
 DEPENDS+=	${PYPKGPREFIX}-OpenSSL>=0.9:../../security/py-OpenSSL
-DEPENDS+=	${PYPKGPREFIX}-amkCrypto-[0-9]*:../../security/py-amkCrypto
+DEPENDS+=	${PYPKGPREFIX}-crypto-[0-9]*:../../security/py-crypto
 .endif
 
 # avahi
