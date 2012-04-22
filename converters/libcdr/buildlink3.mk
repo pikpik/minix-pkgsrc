fd5309d887a9fc30d94bb86693c82081cb28f16a
wiz <wiz>
2012-04-22 17:48:06 +0000

    Fix path after import.

--- converters/libcdr/buildlink3.mk
+++ converters/libcdr/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: buildlink3.mk,v 1.1.1.1 2012/04/22 17:47:42 wiz Exp $
 
 BUILDLINK_TREE+=	libcdr
 
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	libcdr
 LIBCDR_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.libcdr+=	libcdr>=0.0.6
-BUILDLINK_PKGSRCDIR.libcdr?=	../../wip/libcdr
+BUILDLINK_PKGSRCDIR.libcdr?=	../../converters/libcdr
 
 .include "../../converters/libwpd/buildlink3.mk"
 .include "../../converters/libwpg/buildlink3.mk"
