527a78654934395d034fc22008e4d6d0792136fe
ryoon <ryoon>
2012-04-28 16:56:59 +0000

    Update to 12.0
    * Remove unused option.
    * Update enigmail to 1.4.1
    
    Changelog:
    * Global Search results now include message extracts in the results
    * Various security fixes
    * Various improvements to RSS feed subscription and general feed handling
    * Thunderbird now supports add-ons that provide different types of
      local mail storage

--- mail/thunderbird/patches/patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h
+++ mail/thunderbird/patches/patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h
@@ -1,6 +1,6 @@
-$NetBSD: patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h,v 1.1 2011/08/19 14:39:10 tnn Exp $
+$NetBSD: patch-ipc_chromium_src_base_third__party_nspr_prcpucfg.h,v 1.2 2012/03/10 11:42:38 ryoon Exp $
 
---- mozilla/ipc/chromium/src/base/third_party/nspr/prcpucfg.h.orig	2012-02-16 10:25:01.000000000 +0000
+--- mozilla/ipc/chromium/src/base/third_party/nspr/prcpucfg.h.orig	2012-04-20 22:40:06.000000000 +0000
 +++ mozilla/ipc/chromium/src/base/third_party/nspr/prcpucfg.h
 @@ -34,7 +34,7 @@
  #include "base/third_party/nspr/prcpucfg_win.h"
