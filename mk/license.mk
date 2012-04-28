7fdd628353a146b5f0e4165c5442e823768cd348
christos <christos>
2012-04-28 22:43:37 +0000

    Add IBM public license 1.0

--- mk/license.mk
+++ mk/license.mk
@@ -1,4 +1,4 @@
-# $NetBSD: license.mk,v 1.50 2012/01/28 10:46:08 wiz Exp $
+# $NetBSD: license.mk,v 1.51 2012/04/15 20:52:28 brook Exp $
 #
 # This file handles everything about the LICENSE variable. It is
 # included automatically by bsd.pkg.mk.
@@ -123,6 +123,7 @@ DEFAULT_ACCEPTABLE_LICENSES= \
 	zpl \
 	python-software-foundation \
 	ipafont \
+	ibm-public-license-1.0 \
 	isc \
 	boost-license \
 	mplusfont \
