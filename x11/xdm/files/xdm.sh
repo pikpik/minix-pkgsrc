31a9f80be900ae3de6813aaa3d8a28b87f152834
sbd <sbd>
2012-04-29 08:04:56 +0000

    Put xdm's config files in ${PKG_SYSCONFBASE}/xdm, install them using CONF_FILES, and adjust the rc.d script. (As requested in PR#41102).
    Bump PKGREVISION.

--- x11/xdm/files/xdm.sh
+++ x11/xdm/files/xdm.sh
@@ -1,6 +1,6 @@
 #!@RCD_SCRIPTS_SHELL@
 #
-# $NetBSD: xdm.sh,v 1.1.1.1 2007/08/06 14:56:33 tron Exp $
+# $NetBSD: xdm.sh,v 1.2 2007/08/06 16:43:40 tron Exp $
 #
 
 # PROVIDE: xdm
@@ -11,11 +11,9 @@ $_rc_subr_loaded . /etc/rc.subr
 
 name="xdm"
 rcvar=$name
-xdm_config="/etc/X11/${name}/${name}-config"
 command="@PREFIX@/bin/${name}"
-command_args="-config ${xdm_config}"
-pidfile="/var/run/${name}.pid"
-required_files="${xdm_config}"
+pidfile="@VARBASE@/run/${name}.pid"
+required_files="@PKG_SYSCONFDIR@/xdm-config"
 extra_commands="reload"
 
 load_rc_config $name
