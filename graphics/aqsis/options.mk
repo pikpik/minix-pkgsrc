575e95035832e7b5c0598a5b60febe567ea8bcc1
dholland <dholland>
2012-04-30 02:54:28 +0000

    Switch the qt4 option on by default, as it turns out it works and (to my surprise) doesn't require PLIST additions. Ride the update as it was less than an hour ago.

--- graphics/aqsis/options.mk
+++ graphics/aqsis/options.mk
@@ -1,11 +1,12 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.1 2012/04/30 01:59:12 dholland Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.aqsis
 PKG_SUPPORTED_OPTIONS=	qt4
-PKG_SUGGESTED_OPTIONS=
+PKG_SUGGESTED_OPTIONS=	qt4
 
 .include "../../mk/bsd.options.mk"
 
+# not actually used, at least for now
 PLIST_VARS+=	qt4
 
 .if !empty(PKG_OPTIONS:Mqt4)
