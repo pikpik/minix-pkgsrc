fbd48a19553bcee076ac18a30d833e64211e7951
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-05-18 06:33:23 -0700

    Fixup fromcvs/togit conversion

--- inputmethod/ibus/patches/patch-setup_i18n.py
+++ inputmethod/ibus/patches/patch-setup_i18n.py
@@ -1,4 +1,4 @@
-$NetBSD$
+$NetBSD: patch-setup_i18n.py,v 1.1 2012/05/02 09:11:44 obache Exp $
 
 * fix translations fail
  http://code.google.com/p/ibus/issues/detail?id=1453
@@ -13,7 +13,7 @@ $NetBSD$
  import gettext
  import os
  
-@@ -30,5 +31,12 @@ N_ = lambda a: a
+@@ -30,5 +31,13 @@ N_ = lambda a: a
  
  def init():
      localedir = os.getenv("IBUS_LOCALEDIR")
@@ -22,6 +22,7 @@ $NetBSD$
 +    try:
 +        # for non-standard localedir
 +        locale.bindtextdomain(DOMAINNAME, localedir)
++        locale.bind_textdomain_codeset(DOMAINNAME, "UTF-8")
 +    except AttributeError:
 +        pass
      gettext.bindtextdomain(DOMAINNAME, localedir)
