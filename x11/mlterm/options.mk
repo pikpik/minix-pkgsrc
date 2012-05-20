c4227ef1995e1b9300609fa33b1daad897d50f24
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-05-20 12:27:37 -0700

    Fixup fromcvs/togit conversion

--- x11/mlterm/options.mk
+++ x11/mlterm/options.mk
@@ -1,12 +1,18 @@
-# $NetBSD: options.mk,v 1.4 2010/09/22 09:33:41 obache Exp $
+# $NetBSD: options.mk,v 1.5 2012/05/20 06:07:31 tsutsui Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.mlterm
-PKG_SUPPORTED_OPTIONS=	m17nlib uim xft2
+PKG_SUPPORTED_OPTIONS=	libind m17nlib uim xft2
 PKG_SUGGESTED_OPTIONS=
 
 .include "../../mk/bsd.options.mk"
 
-PLIST_VARS+=		m17nlib uim xft2
+PLIST_VARS+=		ind m17nlib uim xft2
+
+.if !empty(PKG_OPTIONS:Mlibind)
+CONFIGURE_ARGS+=	--enable-ind
+PLIST.ind=		yes
+LICENSE+=		AND gnu-lgpl-v2
+.endif
 
 .if !empty(PKG_OPTIONS:Mm17nlib)
 .include "../../devel/m17n-lib/buildlink3.mk"
