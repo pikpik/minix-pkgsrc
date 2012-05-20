1283b8be780be21c4e3beb74858d13a68d9c0ff8
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-05-20 06:33:38 -0700

    Fixup fromcvs/togit conversion

--- sysutils/amanda-common/options.mk
+++ sysutils/amanda-common/options.mk
@@ -1,9 +1,13 @@
-# $NetBSD$
+# $NetBSD: options.mk,v 1.2 2012/05/20 11:13:35 sbd Exp $
 
 # Since amanda's ipv6 usage is broken, turn it off by default.
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.amanda
-PKG_SUPPORTED_OPTIONS=	inet6
+PKG_SUPPORTED_OPTIONS=	inet6 amanda-fqdn amanda-ssh
+PKG_SUGGESTED_OPTIONS=	amanda-ssh
+
+PKG_OPTIONS_LEGACY_VARS+=	AMANDA_FQDN:amanda-fqdn
+PKG_OPTIONS_LEGACY_VARS+=	AMANDA_SSH:amanda-ssh
 
 .include "../../mk/bsd.options.mk"
 
@@ -11,4 +15,19 @@ PKG_SUPPORTED_OPTIONS=	inet6
 CONFIGURE_ARGS+=	--with-ipv6
 .else
 CONFIGURE_ARGS+=	--without-ipv6
+.endif
+
+.if !empty(PKG_OPTIONS:Mamanda-fqdn)
+CONFIGURE_ARGS+=        --with-fqdn
+.endif
+
+.if !empty(PKG_OPTIONS:Mamanda-ssh)
+CONFIGURE_ARGS+=        --with-ssh-security
+
+.  if !exists(/usr/bin/ssh)
+DEPENDS+=		openssh-[0-9]*:../../security/openssh
+FIND_PREFIX:= 		SSHPREFIX=openssh
+.include "../../mk/find-prefix.mk"
+CONFIGURE_ENV+=		ac_cv_path_SSH=${SSHPREFIX}/bin/ssh
+.  endif
 .endif
