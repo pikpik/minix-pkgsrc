3c147637d3203f9b08143a6a9f8ea80fccd4a01f
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-05-20 18:26:30 -0700

    Fixup fromcvs/togit conversion

--- graphics/osg/patches/patch-applications_present3D_Cluster.cpp
+++ graphics/osg/patches/patch-applications_present3D_Cluster.cpp
@@ -4,7 +4,7 @@ Support more platforms.
 
 --- applications/present3D/Cluster.cpp.orig	2012-03-05 16:33:33.000000000 +0000
 +++ applications/present3D/Cluster.cpp
-@@ -33,7 +33,7 @@
+@@ -33,9 +33,12 @@
  #if defined(__linux)
      #include <unistd.h>
      #include <linux/sockios.h>
@@ -12,13 +12,19 @@ Support more platforms.
 +#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
      #include <unistd.h>
      #include <sys/sockio.h>
++#elif defined(__DragonFly__)
++    #include <unistd.h>
++    #include <sys/sockio.h>
  #elif defined(__sgi)
-@@ -336,7 +336,7 @@ void Receiver::sync( void )
+     #include <unistd.h>
+     #include <net/soioctl.h>
+@@ -336,7 +339,8 @@ void Receiver::sync( void )
          return;
      }
  
 -#if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ )
-+#if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ ) || defined(__NetBSD__) || defined(__OpenBSD__)
++#if defined(__linux) || defined(__FreeBSD__) || defined( __APPLE__ ) \
++ || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
      socklen_t 
  #else
      int
