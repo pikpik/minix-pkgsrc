78e99b4fe55f7cf3eb83c6c4b614796f26bd5290
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-05-23 08:00:13 -0700

    Fixup fromcvs/togit conversion

--- games/qonk/patches/patch-src_lisp_lexer.cpp
+++ games/qonk/patches/patch-src_lisp_lexer.cpp
@@ -1,12 +1,15 @@
-$NetBSD$
+$NetBSD: patch-src_lisp_lexer.cpp,v 1.1 2011/12/05 22:45:19 joerg Exp $
 
---- src/lisp/lexer.cpp.orig	2011-12-05 21:37:43.000000000 +0000
+Add missing headers.
+
+--- src/lisp/lexer.cpp.orig	2007-09-14 05:43:03.000000000 +0000
 +++ src/lisp/lexer.cpp
-@@ -19,6 +19,7 @@
+@@ -19,6 +19,8 @@
  //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  #include <sstream>
  #include <stdexcept>
 +#include <cstring>
++#include <cstdio>
  
  #include "lexer.hpp"
  #include "translation.hpp"
