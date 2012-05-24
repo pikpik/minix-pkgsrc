6516dfd22448a8ec13fbee5b95c43f765af1d331
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-05-23 18:26:59 -0700

    Fixup fromcvs/togit conversion

---
+++ textproc/p5-Text-BibTeX/patches/patch-inc_MyBuilder.pm
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Fix linking of btparse library into xscode object
+
+--- inc/MyBuilder.pm.orig	2012-01-11 15:01:26.000000000 +0000
++++ inc/MyBuilder.pm
+@@ -120,7 +120,7 @@ sub ACTION_compile_xscode {
+         my $btparselibdir = $self->install_path('usrlib');
+         $cbuilder->link(
+                         module_name => 'Text::BibTeX',
+-                        extra_linker_flags => "-Lbtparse/src -lbtparse ",
++                        extra_linker_flags => "-Lbtparse/src -Wl,-R${btparselibdir} -lbtparse ",
+                         objects     => $objects,
+                         lib_file    => $lib_file,
+                        );
