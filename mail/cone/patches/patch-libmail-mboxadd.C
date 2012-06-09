2e3ac372aaace6faef007a57586edb24b17460ae
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-06-09 14:20:04 -0700

    Fixup fromcvs/togit conversion

--- mail/cone/patches/patch-libmail-mboxadd.C
+++ mail/cone/patches/patch-libmail-mboxadd.C
@@ -1,10 +1,10 @@
-$NetBSD$
+$NetBSD: patch-libmail-mboxadd.C,v 1.1 2011/11/22 18:12:35 joerg Exp $
 
 Use stdio functions to ensure that the mail::file cast to FILE * happens.
 
 --- libmail/mboxadd.C.orig	2011-11-22 16:29:21.000000000 +0000
 +++ libmail/mboxadd.C
-@@ -16,6 +16,10 @@
+@@ -15,6 +15,10 @@
  #include <sys/types.h>
  #include <sys/stat.h>
  
--- mail/cone/patches/patch-libmail-mboxread.C
+++ mail/cone/patches/patch-libmail-mboxread.C
@@ -1,10 +1,10 @@
-$NetBSD$
+$NetBSD: patch-libmail-mboxread.C,v 1.1 2011/11/22 18:12:35 joerg Exp $
 
 Use stdio functions to ensure that the mail::file cast to FILE * happens.
 
 --- libmail/mboxread.C.orig	2011-11-22 16:32:57.000000000 +0000
 +++ libmail/mboxread.C
-@@ -12,6 +12,8 @@
+@@ -11,6 +11,8 @@
  #include <ctype.h>
  #include <errno.h>
  
