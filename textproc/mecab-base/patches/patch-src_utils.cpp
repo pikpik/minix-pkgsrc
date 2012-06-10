b23140ab72599a5b84c6ac9bcf9e09b4e924a287
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-06-10 07:16:01 -0700

    Fixup fromcvs/togit conversion

---
+++ textproc/mecab-base/patches/patch-src_utils.cpp
@@ -0,0 +1,15 @@
+$NetBSD$
+
+* `function not inlinable' with gcc-4.1.2 -O2 on NetBSD-5.1.2.
+
+--- src/utils.cpp.orig	2012-04-12 15:46:14.000000000 +0000
++++ src/utils.cpp
+@@ -408,7 +408,7 @@ namespace {
+ 
+ #else   // defined(_MSC_VER)
+ 
+-#define FORCE_INLINE __attribute__((always_inline))
++#define FORCE_INLINE inline __attribute__((always_inline))
+ 
+ inline uint32_t rotl32 ( uint32_t x, uint8_t r ) {
+   return (x << r) | (x >> (32 - r));
