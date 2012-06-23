73952afc93e358aa80bca429b9728fd44e4f70bd
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-06-22 18:27:06 -0700

    Fixup fromcvs/togit conversion

---
+++ audio/libsndfile/patches/patch-programs_sndfile-play.c
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- programs/sndfile-play.c.orig	2012-06-22 19:48:12.000000000 +0000
++++ programs/sndfile-play.c
+@@ -58,7 +58,6 @@
+ 	#include 	<sys/soundcard.h>
+ 
+ #elif (defined (__MACH__) && defined (__APPLE__))
+-	#include <Carbon.h>
+ 	#include <CoreAudio/AudioHardware.h>
+ 
+ #elif defined (HAVE_SNDIO_H)
