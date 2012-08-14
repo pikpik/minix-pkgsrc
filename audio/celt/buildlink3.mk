7f992c043120320629d528ed047c62a203730aaa
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-08-14 06:55:59 -0700

    Fixup fromcvs/togit conversion

--- audio/celt/buildlink3.mk
+++ audio/celt/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1.1.1 2008/12/03 00:44:49 bjs Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:23:52 joerg Exp $
 
 BUILDLINK_TREE+=	celt
 
 .if !defined(CELT_BUILDLINK3_MK)
 CELT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.celt+=	celt>=0.5.0
+BUILDLINK_API_DEPENDS.celt+=	celt>=0.11.3
 BUILDLINK_PKGSRCDIR.celt?=	../../audio/celt
 pkgbase:= celt
 .  include "../../mk/pkg-build-options.mk"
--- graphics/librsvg/buildlink3.mk
+++ graphics/librsvg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.24 2012/06/14 07:43:24 sbd Exp $
+# $NetBSD: buildlink3.mk,v 1.26 2012/08/14 08:46:17 wiz Exp $
 
 BUILDLINK_TREE+=	librsvg
 
@@ -9,8 +9,10 @@ BUILDLINK_API_DEPENDS.librsvg+=		librsvg>=2.12.6nb1
 BUILDLINK_ABI_DEPENDS.librsvg+=		librsvg>=2.36.1nb2
 BUILDLINK_PKGSRCDIR.librsvg?=		../../graphics/librsvg
 
+.include "../../devel/pango/buildlink3.mk"
 .include "../../graphics/cairo/buildlink3.mk"
 .include "../../graphics/gdk-pixbuf2/buildlink3.mk"
+.include "../../textproc/libcroco/buildlink3.mk"
 .endif # LIBRSVG_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-librsvg
