561b2079e6765486f66fa4bf3cfda77b211ebeb6
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-08-23 15:03:50 -0700

    Fixup fromcvs/togit conversion

---
+++ sysutils/gsettings-desktop-schemas/buildlink3.mk
@@ -0,0 +1,14 @@
+# $NetBSD$
+
+# Just for the pkg_config .pc file
+BUILDLINK_TREE+=	gsettings-desktop-schemas
+
+.if !defined(GSETTINGS_DESKTOP_SCHEMAS_BUILDLINK3_MK)
+GSETTINGS_DESKTOP_SCHEMAS_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gsettings-desktop-schemas+=	gsettings-desktop-schemas>=3.4.2
+BUILDLINK_PKGSRCDIR.gsettings-desktop-schemas?=	../../sysutils/gsettings-desktop-schemas
+
+.endif # GSETTINGS_DESKTOP_SCHEMAS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gsettings-desktop-schemas
