f68a00565b5184d6a33f550d3bc3271ba84ec347
Ben Gras <ben@minix3.org>
2012-08-30 17:23:24 +0200

    little gcc47 cleanup - specify nojava option externally

--- lang/gcc47/options.mk
+++ lang/gcc47/options.mk
@@ -12,9 +12,7 @@ PKG_SUGGESTED_OPTIONS+= nls
 .elif ${OPSYS} == "SunOS"
 PKG_SUGGESTED_OPTIONS+=	gcc-inplace-math
 .else
-.if ${OPSYS} != "Minix"
 PKG_SUGGESTED_OPTIONS+= gcc-java
-.endif
 .endif
 
 ###
