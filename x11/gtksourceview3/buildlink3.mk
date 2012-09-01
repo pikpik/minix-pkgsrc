8480615401c833a3755a98e5b667d104d800b74f
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-08-31 18:27:02 -0700

    Fixup fromcvs/togit conversion

---
+++ x11/gtksourceview3/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	gtksourceview3
+
+.if !defined(GTKSOURCEVIEW3_BUILDLINK3_MK)
+GTKSOURCEVIEW3_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gtksourceview3+=	gtksourceview3>=3.4.2
+BUILDLINK_PKGSRCDIR.gtksourceview3?=	../../x11/gtksourceview3
+
+.include "../../textproc/libxml2/buildlink3.mk"
+.include "../../x11/gtk3/buildlink3.mk"
+.endif	# GTKSOURCEVIEW3_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gtksourceview3
