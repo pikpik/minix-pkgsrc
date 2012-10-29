81c2be8cff706b34641a98a3b6255563e7e01e22
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-10-28 18:27:13 -0700

    Fixup fromcvs/togit conversion

--- devel/SDL/options.mk
+++ devel/SDL/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.4 2008/12/19 11:41:25 tron Exp $
+# $NetBSD: options.mk,v 1.5 2008/12/19 13:05:06 tron Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.SDL
 PKG_SUPPORTED_OPTIONS=	aalib arts esound nas pulseaudio
-PKG_SUGGESTED_OPTIONS=	aalib arts esound nas pulseaudio
+PKG_SUGGESTED_OPTIONS=	esound nas pulseaudio
 
 .include "../../mk/bsd.options.mk"
 
