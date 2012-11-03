85907d7ce3078e3bd2a78f35b653be57b946b5cd
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-11-02 20:18:19 -0700

    Fixup fromcvs/togit conversion

--- audio/pulseaudio/options.mk
+++ audio/pulseaudio/options.mk
@@ -1,8 +1,8 @@
-# $NetBSD: options.mk,v 1.2 2010/01/21 12:12:27 drochner Exp $
+# $NetBSD: options.mk,v 1.3 2011/04/01 15:46:04 ahoka Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pulseaudio
-PKG_SUPPORTED_OPTIONS=	avahi
-PKG_SUGGESTED_OPTIONS=	avahi
+PKG_SUPPORTED_OPTIONS=	avahi x11
+PKG_SUGGESTED_OPTIONS=	avahi x11
 PLIST_VARS+=		${PKG_SUPPORTED_OPTIONS}
 .include "../../mk/bsd.options.mk"
 
@@ -15,3 +15,18 @@ PLIST.avahi=		yes
 .else
 CONFIGURE_ARGS+=	--disable-avahi
 .endif
+
+###
+### X11
+###
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libICE/buildlink3.mk"
+.include "../../x11/libSM/buildlink3.mk"
+.include "../../x11/libX11/buildlink3.mk"
+.include "../../x11/libXtst/buildlink3.mk"
+.include "../../x11/xextproto/buildlink3.mk"
+PLIST.x11=		yes
+.else
+CONFIGURE_ARGS+=	--disable-x11
+.endif
+
