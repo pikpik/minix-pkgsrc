8f6dc72e5952cb3c62f837346051ee00a86941d8
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-11-24 07:37:50 -0800

    Fixup fromcvs/togit conversion

---
+++ www/SOGo/patches/patch-SoObjects_SOGo_NSData+Crypto.m
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- SoObjects/SOGo/NSData+Crypto.m.orig	2012-11-22 18:43:09.000000000 +0100
++++ SoObjects/SOGo/NSData+Crypto.m	2012-11-22 18:43:47.000000000 +0100
+@@ -22,9 +22,9 @@
+  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+  * Boston, MA 02111-1307, USA.
+  */
+ 
+-#ifndef __OpenBSD__
++#ifdef __sun
+ #include <crypt.h>
+ #endif
+ 
+ #include <sys/types.h>
