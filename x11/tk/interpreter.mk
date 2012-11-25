250f57309f2eb73742f0bd4fdacddb1efa49eb35
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-11-25 06:21:04 -0800

    Fixup fromcvs/togit conversion

--- x11/tk/interpreter.mk
+++ x11/tk/interpreter.mk
@@ -1,4 +1,4 @@
-# $NetBSD$
+# $NetBSD: interpreter.mk,v 1.1 2012/08/31 11:16:12 he Exp $
 #
 # Package-settable variables:
 #
@@ -7,10 +7,9 @@
 #
 # See also:
 #	replace-interpreter
-#
 
 .if !defined(TK_INTERPRETER_MK)
-TK_INTERPRETER_MK=  # defined
+TK_INTERPRETER_MK=	# defined
 
 REPLACE_INTERPRETER+=	wish
 REPLACE.wish.old=	.*/wish
@@ -18,4 +17,3 @@ REPLACE.wish.new=	${LOCALBASE}/bin/wish
 REPLACE_FILES.wish=	${REPLACE_WISH}
 
 .endif
-
