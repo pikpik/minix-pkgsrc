47b45b678e60432526754120f0b42c34c66c8e7a
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-12-03 18:30:23 -0800

    Fixup fromcvs/togit conversion

--- security/opensc/options.mk
+++ security/opensc/options.mk
@@ -1,10 +1,11 @@
-# $NetBSD: options.mk,v 1.1.1.1 2009/02/26 10:50:05 hasso Exp $
+# $NetBSD: options.mk,v 1.2 2012/11/30 14:44:34 gdt Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.opensc
 
-# Really this should be an options group; exactly one of openct,
-# pcsc-lite, or ctapi must be specified.
-PKG_SUPPORTED_OPTIONS=	pcsc-lite openct
+# Arguably these should be selectable individually, but upstream requires
+# that exactly one be chosen.
+PKG_OPTIONS_REQUIRED_GROUPS=	cardreader
+PKG_OPTIONS_GROUP.cardreader=	pcsc-lite openct
 PKG_SUGGESTED_OPTIONS=	pcsc-lite
 
 .include "../../mk/bsd.options.mk"
