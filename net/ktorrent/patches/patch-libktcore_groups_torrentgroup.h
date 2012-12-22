d5516498f9815be95d88292863a97698f600c968
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-12-22 13:33:52 -0800

    Fixup fromcvs/togit conversion

---
+++ net/ktorrent/patches/patch-libktcore_groups_torrentgroup.h
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- libktcore/groups/torrentgroup.h.orig	2012-12-22 19:31:00.000000000 +0000
++++ libktcore/groups/torrentgroup.h
+@@ -29,7 +29,6 @@ namespace kt
+ {
+ 
+ 	class QueueManager;
+-	class TorrentInterface;
+ 
+ 	/**
+ 		@author Joris Guisson <joris.guisson@gmail.com>
