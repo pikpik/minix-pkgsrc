0442b0284ee3a3e9bbf64fbbb91032ba693fa9e3
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2012-12-27 06:36:44 -0800

    Fixup fromcvs/togit conversion

---
+++ editors/mflteco/patches/patch-te__data.c
@@ -0,0 +1,27 @@
+$NetBSD$
+
+- use const for string constants
+- silence warning about initializer
+
+--- te_data.c.orig	1993-08-05 22:29:33.000000000 +0000
++++ te_data.c
+@@ -12,7 +12,7 @@
+ #include "te_defs.h"
+ 
+ /* error message text */
+-char *errors[] =
++const char *errors[] =
+ {
+ 	"> not in iteration",
+ 	"Can't pop Q register",
+@@ -77,8 +77,8 @@ char *errors[] =
+ /* the text buffer headers */
+ 	struct bh buffs[2] =
+ {
+-		{ NULL, NULL, 0, 0, 0 },
+-		{ NULL, NULL, 0, 0, 0 }
++		{ NULL, NULL, 0, 0, 0, 0 },
++		{ NULL, NULL, 0, 0, 0, 0 }
+ } ;
+ 	struct bh *pbuff = buffs;
+ 
