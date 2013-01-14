d8484b6ba8a080bc811a782603b171069df128f3
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-01-13 18:30:34 -0800

    Fixup fromcvs/togit conversion

--- emulators/xtrs/patches/patch-trs_cassette.c
+++ emulators/xtrs/patches/patch-trs_cassette.c
@@ -2,7 +2,7 @@ $NetBSD$
 
 --- trs_cassette.c.orig	2008-06-26 04:40:13.000000000 +0000
 +++ trs_cassette.c
-@@ -234,15 +234,17 @@ static long wave_dataid_offset = WAVE_DA
+@@ -234,15 +234,19 @@ static long wave_dataid_offset = WAVE_DA
  static long wave_datasize_offset = WAVE_DATASIZE_OFFSET;
  static long wave_data_offset = WAVE_DATA_OFFSET;
  
@@ -15,13 +15,14 @@ $NetBSD$
  /* ioport of the SoundBlaster command register. 0 means none */
  static unsigned char sb_cassette_volume[4];
  static unsigned char sb_sound_volume[2];
--#endif /*SB_SOUND*/
+ #endif /*SB_SOUND*/
++#if SB_SOUND || OSS_SOUND
  static unsigned int sb_address=0;
-+#endif /*SB_SOUND*/
++#endif
  static int sb_volume = 0;
  
  /* Put a 2-byte quantity to a file in little-endian order */
-@@ -1221,11 +1223,13 @@ trs_sound_out(int value)
+@@ -1221,11 +1225,13 @@ trs_sound_out(int value)
  #endif
  }
  
