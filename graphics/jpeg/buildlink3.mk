db17a682a8f1b48c806ad52295b3a2f1a3c836f2
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-01-24 19:36:48 -0800

    Fixup fromcvs/togit conversion

--- graphics/jpeg/buildlink3.mk
+++ graphics/jpeg/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.14 2010/01/25 08:45:34 tnn Exp $
+# $NetBSD: buildlink3.mk,v 1.16 2013/01/24 21:49:18 adam Exp $
 
 BUILDLINK_TREE+=	jpeg
 
@@ -6,8 +6,7 @@ BUILDLINK_TREE+=	jpeg
 JPEG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.jpeg+=	jpeg>=8nb1
-#BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8nb1
-BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=8nb1
+BUILDLINK_ABI_DEPENDS.jpeg+=	jpeg>=9
 BUILDLINK_PKGSRCDIR.jpeg?=	../../graphics/jpeg
 .endif # JPEG_BUILDLINK3_MK
 
