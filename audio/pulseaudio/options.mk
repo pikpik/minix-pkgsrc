c1b35f07492da9a9bd752b3706f6e1cc51ca338b
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-01-29 12:29:53 -0800

    Fixup fromcvs/togit conversion

--- audio/pulseaudio/options.mk
+++ audio/pulseaudio/options.mk
@@ -1,4 +1,4 @@
-# $NetBSD: options.mk,v 1.3 2011/04/01 15:46:04 ahoka Exp $
+# $NetBSD: options.mk,v 1.4 2012/11/03 01:54:55 ryoon Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pulseaudio
 PKG_SUPPORTED_OPTIONS=	avahi x11
@@ -29,4 +29,3 @@ PLIST.x11=		yes
 .else
 CONFIGURE_ARGS+=	--disable-x11
 .endif
-
