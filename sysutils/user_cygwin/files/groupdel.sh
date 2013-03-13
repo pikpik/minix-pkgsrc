4fdef7051d0967e99c0338341e5169e8bb4614d4
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-03-13 13:20:45 -0700

    Fixup fromcvs/togit conversion

--- sysutils/user_cygwin/files/groupdel.sh
+++ sysutils/user_cygwin/files/groupdel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $NetBSD: groupdel.sh,v 1.1 2013/03/06 12:37:16 obache Exp $
+# $NetBSD: groupdel.sh,v 1.2 2013/03/12 05:41:39 obache Exp $
 
 export PATH=/bin:"$(/bin/cygpath -S)"
 
@@ -10,7 +10,7 @@ show_usage () {
 
 verbose=false
 run_cmd () {
-	if $verbose; then printf '%s\n' "+ $*"; fi
+	if $verbose; then printf '%s\n' "+ $*" >&2; fi
 	"$@"
 }
 
