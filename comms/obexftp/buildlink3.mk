c2a3cec0b2c86b4950549c7ea0d45c0eb7cb57c6
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-03-15 08:01:22 -0700

    Fixup fromcvs/togit conversion

--- comms/obexftp/buildlink3.mk
+++ comms/obexftp/buildlink3.mk
@@ -1,11 +1,11 @@
-# $NetBSD: buildlink3.mk,v 1.1 2006/11/30 00:07:03 xtraeme Exp $
+# $NetBSD: buildlink3.mk,v 1.2 2009/03/20 19:24:02 joerg Exp $
 
 BUILDLINK_TREE+=	obexftp
 
 .if !defined(OBEXFTP_BUILDLINK3_MK)
 OBEXFTP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.obexftp+=	obexftp>=0.20nb1
+BUILDLINK_API_DEPENDS.obexftp+=	obexftp>=0.24
 BUILDLINK_PKGSRCDIR.obexftp?=	../../comms/obexftp
 
 .include "../../comms/openobex/buildlink3.mk"
