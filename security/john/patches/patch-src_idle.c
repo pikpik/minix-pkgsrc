a94eae00ff83b7aeac5abd88407a20b3b64e7b08
Thomas Cort <tcort@minix3.org>
2013-03-15 00:13:29 +0000

    security/john: initial minix port - works on /etc/master.passwd

---
+++ security/john/patches/patch-src_idle.c
@@ -0,0 +1,12 @@
+--- src/idle.c.orig	Fri Dec 25 20:32:45 2009
++++ src/idle.c	Fri Mar 15 00:03:41 2013
+@@ -91,7 +91,9 @@
+ 	do {
+ 		if (event_pending) break;
+ 		last_check = current;
++#ifdef __minix
+ 		sched_yield();
++#endif
+ 		yield_calls++;
+ 		current = times(&buf);
+ 	} while (current - last_check > 1 && current - last_adj < clk_tck);
