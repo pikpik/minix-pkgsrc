e69a2f4a49df5189000a0aa5db065551922995dd
pkgsrc fixup <pkgsrc@dragonflybsd.org>
2013-03-18 12:30:16 -0700

    Fixup fromcvs/togit conversion

--- sysutils/gio-fam/hacks.mk
+++ sysutils/gio-fam/hacks.mk
@@ -1,15 +1,20 @@
-# $NetBSD$
+# $NetBSD: hacks.mk,v 1.1 2012/05/01 18:45:53 adam Exp $
 
 .if !defined(GIOFAM_HACKS_MK)
 GIOFAM_HACKS_MK=	# defined
 
 .if ${OPSYS} == "Darwin"
+CHECK_BUILTIN.libiconv:=	yes
+.  include "../../converters/libiconv/builtin.mk"
+CHECK_BUILTIN.libiconv:=	no
+.  if !empty(USE_BUILTIN.iconv:M[Yy][Ee][Ss])
 PKG_HACKS+=		darwin-iconv
 SUBST_CLASSES+=		iconv
 SUBST_STAGE.iconv=	pre-configure
 SUBST_MESSAGE.iconv=	Changing libiconv_open to iconv_open.
 SUBST_FILES.iconv=	configure
 SUBST_SED.iconv=	-e 's,libiconv_open,iconv_open,g'
+.  endif
 .endif
 
 .endif
