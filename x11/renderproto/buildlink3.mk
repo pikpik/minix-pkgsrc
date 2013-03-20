92b5fa1a3ef460ddd3e6376ee02c6f9e33cb07ba
Thomas Cort <tcort@minix3.org>
2013-03-20 14:58:34 +0000

    x11/renderproto: fix up buildlink3

--- x11/renderproto/buildlink3.mk
+++ x11/renderproto/buildlink3.mk
@@ -9,20 +9,10 @@ RENDERPROTO_BUILDLINK3_MK:=
 
 .if ${X11_TYPE} == "native" || ${X11_TYPE} == "monolithic"
 . include "../../mk/x11.buildlink3.mk"
-#. if ${BUILTIN_X11_TYPE.native} == "XFree86" || ${BUILTIN_X11_TYPE.native} == "xorg"
-# && empty(BUILTIN_X11_VERSION.xorg)
-RENDERPROTO_RENDERPROTO9=1
-#. endif
 .endif
 
-.if defined(RENDERPROTO_RENDERPROTO9)
-BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
-BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto9
-BUILDLINK_BUILTIN_MK.renderproto=	../../x11/renderproto/builtin.mk
-.else
 BUILDLINK_API_DEPENDS.renderproto+=	renderproto>=0.9.3nb1
 BUILDLINK_PKGSRCDIR.renderproto?=	../../x11/renderproto
-.endif
 
 BUILDLINK_DEPMETHOD.renderproto?=	build
 .endif # RENDERPROTO_BUILDLINK3_MK
