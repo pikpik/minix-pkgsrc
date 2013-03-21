db67fd90ee9eb98110bddf6529942a04192585f7
Thomas Cort <tcort@minix3.org>
2013-03-21 00:43:29 +0000

    fonts/fontconfig: fix buildlink.mk

--- fonts/fontconfig/buildlink3.mk
+++ fonts/fontconfig/buildlink3.mk
@@ -1,25 +1,13 @@
 # $NetBSD: buildlink3.mk,v 1.28 2012/05/07 01:53:29 dholland Exp $
 
-.include "../../mk/bsd.fast.prefs.mk"
-
-.if ${X11_TYPE} != "modular" && !exists(${X11BASE}/lib/pkgconfig/fontconfig.pc)
-.include "../../mk/x11.buildlink3.mk"
-.else
 
 BUILDLINK_TREE+=	fontconfig
 
-.if !defined(FONTCONFIG_BUILDLINK3_MK)
 FONTCONFIG_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.fontconfig+=	fontconfig>=2.4.2
 BUILDLINK_ABI_DEPENDS.fontconfig+=	fontconfig>=2.8.0nb2
 BUILDLINK_PKGSRCDIR.fontconfig?=	../../fonts/fontconfig
 
-.include "../../devel/zlib/buildlink3.mk"
-.include "../../graphics/freetype2/buildlink3.mk"
-.include "../../textproc/expat/buildlink3.mk"
-.endif # FONTCONFIG_BUILDLINK3_MK
-
 BUILDLINK_TREE+=	-fontconfig
 
-.endif
